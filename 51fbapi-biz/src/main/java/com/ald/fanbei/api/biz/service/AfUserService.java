f38a4b611054aa82c26e6cd551ce4e91e1246588
unknown <Xiaotianjian@Alamap.com>
2017-01-19 13:52:57 +0800

    commit service package

---
+++ 51fbapi-biz/src/main/java/com/ald/fanbei/api/biz/service/AfUserService.java
@@ -0,0 +1,9 @@
+package com.ald.fanbei.api.biz.service;
+/**
+ *@类描述：
+ *@author Xiaotianjian 2017年1月19日下午1:51:46
+ *@注意：本内容仅限于杭州51反呗有限公司内部传阅，禁止外泄以及用于其他的商业目的 
+ */
+public interface AfUserService {
+
+}
---
+++ 51fbapi-biz/src/main/java/com/ald/fanbei/api/biz/service/impl/AfUserServiceImpl.java
@@ -0,0 +1,12 @@
+package com.ald.fanbei.api.biz.service.impl;
+
+import com.ald.fanbei.api.biz.service.AfUserService;
+
+/**
+ *@类描述：
+ *@author Xiaotianjian 2017年1月19日下午1:52:02
+ *@注意：本内容仅限于杭州51反呗有限公司内部传阅，禁止外泄以及用于其他的商业目的 
+ */
+public class AfUserServiceImpl implements AfUserService {
+
+}
