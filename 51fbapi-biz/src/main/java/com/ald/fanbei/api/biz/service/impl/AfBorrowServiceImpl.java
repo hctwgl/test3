42eb2f9001f35bd14f702c006ba76f0b257d79b5
hexin <hexin@edspay.com>
2017-02-09 19:00:03 +0800

    save

---
+++ 51fbapi-biz/src/main/java/com/ald/fanbei/api/biz/service/impl/AfBorrowServiceImpl.java
@@ -0,0 +1,103 @@
+package com.ald.fanbei.api.biz.service.impl;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallback;
+import org.springframework.transaction.support.TransactionTemplate;
+
+import com.ald.fanbei.api.biz.service.AfBorrowService;
+import com.ald.fanbei.api.common.Constants;
+import com.ald.fanbei.api.common.util.DateUtil;
+import com.ald.fanbei.api.common.util.NumberUtil;
+import com.ald.fanbei.api.common.util.StringUtil;
+import com.ald.fanbei.api.dal.dao.AfBorrowDao;
+import com.ald.fanbei.api.dal.dao.AfResourceDao;
+import com.ald.fanbei.api.dal.domain.AfBorrowBillDo;
+import com.ald.fanbei.api.dal.domain.AfBorrowDo;
+import com.ald.fanbei.api.dal.domain.AfResourceDo;
+
+/**
+ * 
+ * @类描述：
+ * @author hexin 2017年2月9日下午4:51:20
+ * @注意：本内容仅限于杭州阿拉丁信息科技股份有限公司内部传阅，禁止外泄以及用于其他的商业目的
+ */
+@Service("afBorrowService")
+public class AfBorrowServiceImpl implements AfBorrowService{
+
+	@Resource
+	AfBorrowDao afBorrowDao;
+	
+	@Resource
+	TransactionTemplate transactionTemplate;
+	
+	@Resource
+	AfResourceDao afResourceDao;
+	
+	@Override
+	public int dealWithTransferSuccess(final Long borrowId) {
+		
+		return transactionTemplate.execute(new TransactionCallback<Integer>() {
+
+			@Override
+			public Integer doInTransaction(TransactionStatus status) {
+				try {
+					AfBorrowDo borrow = afBorrowDao.getBorrowById(borrowId);
+					//
+					List<AfResourceDo> list = afResourceDao.getConfigByTypes(new StringBuffer("BORROW_").append(borrow.getType()).toString());
+					if(null == list || list.size()==0){
+						throw new Exception("取现利率配置不能为空");
+					}else{
+						AfResourceDo resource = list.get(0);
+						BigDecimal money = borrow.getAmount();//借款金额
+						BigDecimal dayRate = NumberUtil.objToBigDecimalDefault(resource.getValue(), BigDecimal.ZERO);//取现日利率
+						BigDecimal serviceCharge = NumberUtil.objToBigDecimalDefault(resource.getValue1(), BigDecimal.ZERO);//取现手续费率
+						BigDecimal rangeBegin = NumberUtil.objToBigDecimalDefault(Constants.DEFAULT_CHARGE_MIN, BigDecimal.ZERO);
+						BigDecimal rangeEnd = NumberUtil.objToBigDecimalDefault(Constants.DEFAULT_CHARGE_MAX, BigDecimal.ZERO);
+						String[] range = StringUtil.split(resource.getValue2(), ",");
+						if(null != range && range.length==2){
+							rangeBegin = NumberUtil.objToBigDecimalDefault(range[0], BigDecimal.ZERO);
+							rangeEnd = NumberUtil.objToBigDecimalDefault(range[1], BigDecimal.ZERO);
+						}
+						BigDecimal chargeAmount = money.multiply(serviceCharge);//计算手续费
+						if(rangeBegin.compareTo(chargeAmount)>0){
+							chargeAmount = rangeBegin;
+						}else if(rangeEnd.compareTo(chargeAmount)==-1){
+							chargeAmount = rangeEnd;
+						}
+						BigDecimal interestAmount = money.multiply(dayRate);//日利息
+						afBorrowDao.updateBorrow(borrowId, interestAmount, chargeAmount);
+						//afBorrowDao.addBorrowBill(borrowBill);
+					}
+					return 1;
+				} catch (Exception e) {
+					status.setRollbackOnly();
+					return 0;
+				}
+			}
+		});
+	}
+
+	private List<AfBorrowBillDo> buildBorrowBill(AfBorrowDo borrow){
+		List<AfBorrowBillDo> list = new ArrayList<AfBorrowBillDo>();
+		Date now = new Date();//当前时间
+		for (int i = 1; i <= borrow.getNper(); i++) {
+			AfBorrowBillDo bill = new AfBorrowBillDo();
+			bill.setUserId(borrow.getUserId());
+			bill.setBorrowId(borrow.getRid());
+			bill.setName(borrow.getName());
+			bill.setBillMonth(DateUtil.formatDate(now, DateUtil.MONTH_PATTERN));
+			bill.setBillNper(new StringBuffer(i).append("/").append(borrow.getNper()).toString());
+			list.add(bill);
+			DateUtil.addMonths(now, 1);
+		}
+		return list;
+	}
+}
---
+++ 51fbapi-web/src/main/java/com/ald/fanbei/api/web/api/fenbei/GetCashInfoApi.java
@@ -0,0 +1,46 @@
+package com.ald.fanbei.api.web.api.fenbei;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Component;
+
+import com.ald.fanbei.api.biz.service.AfUserAccountService;
+import com.ald.fanbei.api.common.FanbeiContext;
+import com.ald.fanbei.api.common.exception.FanbeiExceptionCode;
+import com.ald.fanbei.api.dal.domain.dto.AfUserAccountDto;
+import com.ald.fanbei.api.web.common.ApiHandle;
+import com.ald.fanbei.api.web.common.ApiHandleResponse;
+import com.ald.fanbei.api.web.common.RequestDataVo;
+
+/**
+ * 
+ *@类描述：GetCashInfoApi
+ *@author 何鑫 2017年2月07日  11:01:26
+ *@注意：本内容仅限于浙江阿拉丁电子商务股份有限公司内部传阅，禁止外泄以及用于其他的商业目的
+ */
+@Component("getCashInfoApi")
+public class GetCashInfoApi implements ApiHandle{
+
+	@Resource
+	private AfUserAccountService afUserAccountService;
+	
+	@Override
+	public ApiHandleResponse process(RequestDataVo requestDataVo,
+			FanbeiContext context, HttpServletRequest request) {
+		ApiHandleResponse resp = new ApiHandleResponse(requestDataVo.getId(),FanbeiExceptionCode.SUCCESS);
+		Long userId = context.getUserId();
+		//账户关联信息
+		AfUserAccountDto userDto = afUserAccountService.getUserAndAccountByUserId(userId);
+		Map<String, Object> data = new HashMap<String, Object>();
+		data.put("usableAmount", userDto.getCcAmount());
+		data.put("cardNo", userDto.getAlipayAccount());
+		data.put("realName", userDto.getRealName());
+		resp.setResponseData(data);
+		return resp;
+	}
+
+}
