4943096a436356844223d78f8c67aa70e003aceb
yoe <yoe0405@163.com>
2017-02-15 10:17:02 +0800

    删除多余的文件、添加专场商品列表、邀请有礼、领取优惠劵、奖励规则初步静态页面以及公用文件

---
+++ 51fbapi-web/src/main/webapp/js/app/sys/receiveCoupons.js
@@ -0,0 +1,15 @@
+/*
+* @Author: Yangyang
+* @Date:   2017-02-14 16:52:58
+* @Last Modified by:   Yangyang
+* @Last Modified time: 2017-02-14 17:02:00
+* @title:  领取优惠券
+*/
+
+
+// 领取优惠劵
+$(function(){
+	$(".cardCoupons_btn").click(function(event) {
+		$(this).addClass('cardCoupons_btn_gray');
+	});
+})
---
+++ 51fbapi-web/src/main/webapp/js/common/base.js
@@ -0,0 +1,16 @@
+/*
+* @Author: Yangyang
+* @Date:   2017-02-15 09:59:54
+* @Last Modified by:   Yangyang
+* @Last Modified time: 2017-02-15 10:00:48
+* @title:  公用的
+*/
+
+// 公用弹框内容
+function requestMsg (msg){
+	layer.open({
+        content: msg,
+        skin: 'msg',
+        time: 2
+    });
+}
