80478f7ead77db0f2efd30e6f8a05eedb9d3ac85
yoe <yoe0405@163.com>
2017-02-21 18:01:36 +0800

    update邀请奖励规则、领取优惠劵、注册

--- 51fbapi-web/src/main/webapp/js/app/sys/receiveCoupons.js
+++ 51fbapi-web/src/main/webapp/js/app/sys/receiveCoupons.js
@@ -2,7 +2,7 @@
 * @Author: Yangyang
 * @Date:   2017-02-14 16:52:58
 * @Last Modified by:   Yangyang
-* @Last Modified time: 2017-02-14 17:02:00
+* @Last Modified time: 2017-02-18 15:23:20
 * @title:  领取优惠券
 */
 
@@ -11,5 +11,6 @@
 $(function(){
 	$(".cardCoupons_btn").click(function(event) {
 		$(this).addClass('cardCoupons_btn_gray');
+		$(this).attr("disabled","true");
 	});
 })
