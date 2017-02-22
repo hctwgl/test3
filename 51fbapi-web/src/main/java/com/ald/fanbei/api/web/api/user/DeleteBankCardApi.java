aad3ba03c02bdc97e4744a9c4acab62c26f26d3a
苏伟丽 <1345156623@qq.com>
2017-02-22 21:11:22 +0800

    删除银行卡

---
+++ 51fbapi-web/src/main/java/com/ald/fanbei/api/web/api/user/DeleteBankCardApi.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package com.ald.fanbei.api.web.api.user;
+
+import java.util.Map;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang.ObjectUtils;
+import org.springframework.stereotype.Component;
+
+import com.ald.fanbei.api.biz.service.AfUserBankcardService;
+import com.ald.fanbei.api.common.FanbeiContext;
+import com.ald.fanbei.api.common.exception.FanbeiException;
+import com.ald.fanbei.api.common.exception.FanbeiExceptionCode;
+import com.ald.fanbei.api.common.util.NumberUtil;
+import com.ald.fanbei.api.web.common.ApiHandle;
+import com.ald.fanbei.api.web.common.ApiHandleResponse;
+import com.ald.fanbei.api.web.common.RequestDataVo;
+
+/**
+ * @类描述：
+ * @author suweili 2017年2月22日下午8:57:29
+ * @注意：本内容仅限于杭州阿拉丁信息科技股份有限公司内部传阅，禁止外泄以及用于其他的商业目的
+ */
+@Component("deleteBankCardApi")
+public class DeleteBankCardApi implements ApiHandle {
+
+
+	@Resource
+	AfUserBankcardService afUserBankcardService;
+	@Override
+	public ApiHandleResponse process(RequestDataVo requestDataVo, FanbeiContext context, HttpServletRequest request) {
+		ApiHandleResponse resp = new ApiHandleResponse(requestDataVo.getId(),FanbeiExceptionCode.SUCCESS);
+        Long userId = context.getUserId();
+        if (userId == null) {
+			throw new FanbeiException("user id is invalid", FanbeiExceptionCode.PARAM_ERROR);
+		}
+        Map<String, Object> params = requestDataVo.getParams();
+        Long bankId = NumberUtil.objToLongDefault(ObjectUtils.toString(params.get("bankId")), 0);
+        if(afUserBankcardService.deleteUserBankcardByIdAndUserId(userId, bankId)>0){
+       		return resp;
+          }
+        
+		throw new FanbeiException(FanbeiExceptionCode.FAILED);
+	}
+
+}
