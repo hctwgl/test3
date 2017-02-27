3bcc14adcd63381090b9e4c11357d55972803e75
yoe <yoe0405@qq.com>
2017-02-27 21:08:08 +0800

    update goodsListModel

--- 51fbapi-web/src/main/webapp/js/app/goods/goodsListModel.js
+++ 51fbapi-web/src/main/webapp/js/app/goods/goodsListModel.js
@@ -2,68 +2,31 @@
 * @Author: Yangyang
 * @Date:   2017-02-23 14:11:41
 * @Last Modified by:   Yangyang
-* @Last Modified time: 2017-02-24 16:14:28
+* @Last Modified time: 2017-02-27 20:04:39
 * @title:  商品列表页
 */
 
 
-// 导航固定在顶部
-$(function(){
+var pageTotal = $("#pageTotal").val();
+var modelIdNum = $("#modelId").val();
+var pageNoNum = 1;
+pageNoNum++;
+var typeCurrentNum = $("#typeCurrent").val();
+var categoryListType = $("#categoryListType").val();
 
-	$(window).scroll(function() {
-		var scrollTopH= $(document).scrollTop();
-		if ( scrollTopH >= 163) {
-			$(".header_wrap").addClass("header_wrap_fixed");
-		} else {
-			$(".header_wrap").removeClass("header_wrap_fixed");
-		}
-	});
-
-});
-
-
-// $(function(){
-
-//     var nav=$(".nav"); //得到导航对象
-
-//     var win=$(window); //得到窗口对象
-
-//     var sc=$(document);//得到document文档对象。
-
-//     win.scroll(function(){
-
-//         if(sc.scrollTop()>=60){
-
-//             nav.addClass("fixednav"); 
-
-//             $(".navTmp").fadeIn(); 
-
-//         }else{
-
-//             nav.removeClass("fixednav");
-
-//             $(".navTmp").fadeOut();
-
-//         }
-
-//     })
-// });
-
-
-
-
+$(".goodsListModel_main .goodsListModel_main_list:first-child").removeClass('goodsListModel_hide');
+$(".goodsListModel_main_list:first-child").siblings().html("");
 
 
 // 导航tab切换
 $(function(){
 
-    var liW = $(".goodsList_header li").outerWidth();
+    var liW = $(".goodsListModel_header li").outerWidth();
     var windowW = $(window).outerWidth();
     var centerLi = $(windowW-liW)/2;
-
     var liWArr = [], // 保存每一个li的索引(index),自身宽度(width),距离ul左侧的距离(offsetLeft)
     ulW = 0; // ul初始宽度  通过各个li宽度之和 计算出来
-    $(".goodsList_header li").each(function(index){
+    $(".goodsListModel_header li").each(function(index){
         var thisLiW = $(this).outerWidth();
         liWArr.push({
             index: index,
@@ -73,15 +36,16 @@ $(function(){
         ulW += thisLiW;
     });
 
-    $(".goodsList_header").css("width", ulW+"px");
-    $(".goodsList_header li").click(function(){
+    $(".goodsListModel_header").css("width", ulW+0.5+"px");
+
+    $(".goodsListModel_header li").click(function(){
 
         var i = $(this).index();
         $(this).find("span").addClass("current");
         $(this).siblings().find("span").removeClass("current");
-    	$(".goodsList_main_item").eq(i).removeClass("goodsList_main_hide").siblings().addClass("goodsList_main_hide");
+    	$(".goodsListModel_main_item").eq(i).removeClass("goodsListModel_main_hide").siblings().addClass("goodsList_main_hide");
 
-        ulOffsetLeft = $(".goodsList_header").offset().left;
+        ulOffsetLeft = $(".goodsListModel_header").offset().left;
         thisLiOffsetUl = liWArr[i].offsetLeft;
         thisLiOffsetDiv = thisLiOffsetUl + ulOffsetLeft; //距离 边框的距离
         var offsetLeft = ((windowW-liWArr[i].width)/2) - thisLiOffsetDiv;
@@ -91,11 +55,80 @@ $(function(){
         }else if(offset<windowW-ulW){
             offset = windowW-ulW;
         };
-		$(".goodsList_header").animate({
+		$(".goodsListModel_header").animate({
 			"left": offset + "px"
 		}, 200);
-    });
 
+        $.ajax({
+            url: "/app/goods/categoryGoodsList",
+            type: "POST",
+            dataType: "JSON",
+            data: {
+                modelId : modelIdNum,
+                pageNo: 1,
+                type: categoryListType
+            },
+            success: function(returnData){
+
+                if (returnData.success) {
+                    var html = '';
+                    var goodsList = returnData.data["goodsList"];
+                    for (var i = 0; i < goodsList.length; i++) {
+                        html+=  '<ul class="goodsListModel_mainContent clearfix">'
+                                    +'<li class="fl bdc_cb goodsListModel_item">'
+                                        +'<a href="'+goodsList[i].imageIcon+'">'
+                                            +'<img src="'+goodsList[i].imageIcon+'" class="goodsListModel_mainContent_img">'
+                                            +'<div class="goodsListModel_mainContent_main">'
+                                                +'<div class="goodsListModel_mainContent_wrap">'
+                                                    +'<p class="fs_28 fsc_1">'+goodsList[i].title+'</p>'
+                                                    +'<span class="fs_28 fsc_red">'+goodsList[i].priceAmount+'</span>'
+                                                +'</div>'
+                                                +'<div class="goodsListModel_mainContent_rebate clearfix">'
+                                                    +'<span class="fl fs_26 bgc_orange fsc_f tac">返</span><p class="fl fs_26 fsc_orange">'+goodsList[i].rebateAmount+'</p>'
+                                                +'</div>'
+                                            +'</div>'
+                                        +'</a>'
+                                    +'</li>'
+                                +'</ul>';
+                        
+                        // var j=0;
+                        var index=i+1;
+                        console.log(index);
+
+                        $('.goodsListModel_main_list').eq(index).append(html);
+                        $('.goodsListModel_main_list').eq(index).removeClass("goodsListModel_hide");
+                        $('.goodsListModel_main_list').eq(index).siblings().addClass("goodsListModel_hide");
+
+
+
+
+
+
+
+
+
+
+
+
+                        // var hasClass=$('.goodsListModel_main_list').hasClass('goodsListModel_hide');
+                        // var index=i++;
+                        // if (hasClass) {
+                        //     $('.goodsListModel_main_list').eq(index).append(html);
+                        // } else {
+                        //     $('.goodsListModel_main_list').eq(index).siblings().remove();
+                        // }
+                        
+                    }
+
+                } else {
+                    requestMsg(returnData.msg);
+                }
+            },
+            error: function(){
+                requestMsg("请求失败");
+            }
+        });
+    });
 });
 
 
@@ -104,16 +137,108 @@ $(function(){
 
 
 
+// 下拉加载
+$(function(){
 
+    var page = 1; // 默认页数从1开始
+    var finished = 0; 
+    var sover = 0;
+    
+    //加载完  
+    function loadover(){
+        if(sover==1){
+            var overtext="没有更多了...";
+            $(".loadmore").remove();
+            if($(".loadover").length>0){
+                $(".loadover span").eq(0).html(overtext);
+            }else{
+                var txt='<div class="loadover"><span>'+overtext+'</span></div>';
+                $("body").append(txt);
+            }
+        }
+    };
 
+    //加载更多  
+    function loadmore(obj){
+        if(finished==0 && sover==0){
+            var scrollTop = $(obj).scrollTop();
+            var scrollHeight = $(document).height();
+            var windowHeight = $(obj).height();
+            if($(".loadmore").length==0){
+                var txt='<div class="loadmore"><span class="loading"></span>加载中..</div>';
+                $("body").append(txt);
+            }
+            //if (scrollTop + windowHeight -scrollHeight<=500 ) { //此处是滚动条到底部时候触发的事件，在这里写要加载的数据，或者是拉动滚动条的操作
+                finished=1; //防止未加载完再次执行
 
+                $.ajax({
+                    url: "/app/goods/categoryGoodsList",
+                    type: "POST",
+                    dataType: "JSON",
+                    data: {
+                        modelId : modelIdNum,
+                        pageNo: pageNoNum  ,
+                        type: typeCurrentNum
+                    },
+                    success: function(returnData){
+                        if (returnData.success) {
+                            
+                            console.log(returnData);
 
+                            if(returnData==""){
+                                sover = 1;
+                                loadover();                  
+                                if (page == 1) {
+                                    $(".loadover").remove();
+                                }
+                            }else{
+                                var html = '';
+                                var goodsList = returnData.data["goodsList"];
+                                for(var i = 0; i < goodsList.length; i++){
+                                    html+= '<li class="fl bdc_cb goodsListModel_item">'
+                                                +'<a href="'+goodsList[i].imageIcon+'">'
+                                                    +'<img src="'+goodsList[i].imageIcon+'" class="goodsListModel_mainContent_img">'
+                                                    +'<div class="goodsListModel_mainContent_main">'
+                                                        +'<div class="goodsListModel_mainContent_wrap">'
+                                                            +'<p class="fs_28 fsc_1">'+goodsList[i].title+'</p>'
+                                                            +'<span class="fs_28 fsc_red">'+goodsList[i].priceAmount+'</span>'
+                                                        +'</div>'
+                                                        +'<div class="goodsListModel_mainContent_rebate clearfix">'
+                                                            +'<span class="fl fs_26 bgc_orange fsc_f tac">返</span><p class="fl fs_26 fsc_orange">'+goodsList[i].rebateAmount+'</p>'
+                                                        +'</div>'
+                                                    +'</div>'
+                                                +'</a>'
+                                            +'</li>';
+                                }
+                                $(".loadmore").remove();
+                                $('.goodsListModel_mainContent').append(html);
+                                page+=1;
+                                finished=0;
+                                if(page==pageTotal){ //最后一页
+                                    sover=1;
+                                    loadover();
+                                }
+                            }
+                        } else {
+                            requestMsg(returnData.msg);
+                        }
+                    },
+                    error: function(){
+                        requestMsg("请求失败");
+                    }
+                });
+            //}
+        }
+    }
 
-
-
-
-
-
+    $(window).scroll(function () {
+        var totalheight = parseFloat($(window).height()) + parseFloat($(window).scrollTop());
+        var documentheight = parseFloat($(document).height()); // 文本的高度
+        if (documentheight - totalheight <= 200) {
+            loadmore($(this));
+        }
+    });
+});
 
 
 
