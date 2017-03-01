5bd4520e36570b2c9d6205dcd7fa0cac181eb97c
yoe <yoe0405@qq.com>
2017-03-01 10:44:07 +0800

    update 邀请有礼、领取优惠劵 add 用户注册协议

--- 51fbapi-web/src/main/webapp/js/app/user/receiveCoupons.js
+++ 51fbapi-web/src/main/webapp/js/app/user/receiveCoupons.js
@@ -2,15 +2,15 @@
 * @Author: Yangyang
 * @Date:   2017-02-14 16:52:58
 * @Last Modified by:   Yangyang
-* @Last Modified time: 2017-02-23 17:51:58
+* @Last Modified time: 2017-03-01 10:41:43
 * @title:  领取优惠券
 */
 
 
 // 领取优惠劵
 $(function(){
-	$(".cardCoupons_btn").click(function(event) {
-		$(this).parent().addClass("cardCoupons_alreadyReceive");
+	$(".receiveCoupons_btn").click(function(event) {
+		$(this).parent().addClass("receiveCoupons_alreadyReceive");
 		$(this).attr("disabled","true");
 	});
-})
+});
