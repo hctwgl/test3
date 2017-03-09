bfc6c225b19bbdeea54528d45735e2fe1cdd801d
yoe <yoe0405@qq.com>
2017-03-09 11:14:44 +0800

    update 点击领取优惠劵

--- 51fbapi-web/src/main/webapp/js/app/user/receiveCoupons.js
+++ 51fbapi-web/src/main/webapp/js/app/user/receiveCoupons.js
@@ -2,28 +2,24 @@
 * @Author: Yangyang
 * @Date:   2017-03-08 17:42:52
 * @Last Modified by:   Yangyang
-* @Last Modified time: 2017-03-08 17:56:56
+* @Last Modified time: 2017-03-09 11:13:12
 * @title:  领取优惠劵
 */
 
 
-
-// 点击领取优惠劵
 var couponListString = $("#couponList").val();
 var couponList = eval('(' + couponListString + ')');
-
 var userName = $("#userName").val();
 
 // 领取优惠劵
 $(function(){
-	$(".receiveCoupons_btn").click(function(event) {
 
-        var kkk= $(this).index();
-        console.log(kkk);
-        var couponIdNum = couponList[kkk].rid;
-
-		
-		$.ajax({
+    $(".receiveCoupons_main li").bind("click",function(){
+        
+        var i= $(this).index();
+        var couponIdNum = couponList[i].rid;
+        
+        $.ajax({
             url: "/fanbei-web/pickCoupon",
             type: "POST",
             dataType: "JSON",
@@ -32,26 +28,25 @@ $(function(){
                 userName: userName
             },
             success: function(returnData){
-                alert(returnData.success);
-                console.log(returnData);
 
                 if (returnData.success) {
 
-                    $(this).parent().addClass("receiveCoupons_alreadyReceive");
-					$(this).attr("disabled","true");
+                    $(".receiveCoupons_main li").eq(i).addClass("receiveCoupons_alreadyReceive");
 
                 } else {
+                    
                     if (returnData.url.length>0) {
                         window.location.href = returnData.url;
-
+                    }else{
+                        requestMsg(returnData.msg);
                     }
-                    console.log(returnData);
-                    requestMsg(returnData.msg);
                 }
             },
             error: function(){
                 requestMsg("请求失败");
             }
         });
-	});
+
+        $(this).unbind("click");
+    });
 });
