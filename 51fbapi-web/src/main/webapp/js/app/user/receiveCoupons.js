b255f1a13176d80a4105dd29ee27b5d65595879f
yoe <yoe0405@qq.com>
2017-03-23 15:53:30 +0800

    update receiveCoupons

--- 51fbapi-web/src/main/webapp/js/app/user/receiveCoupons.js
+++ 51fbapi-web/src/main/webapp/js/app/user/receiveCoupons.js
@@ -2,7 +2,7 @@
 * @Author: Yangyang
 * @Date:   2017-03-08 17:42:52
 * @Last Modified by:   Yangyang
-* @Last Modified time: 2017-03-23 15:38:11
+* @Last Modified time: 2017-03-23 15:51:40
 * @title:  领取优惠劵
 */
 
@@ -12,46 +12,6 @@ var couponList = eval('(' + couponListString + ')');
 var userName = $("#userName").val();
 
 // 领取优惠劵
-// $(function(){
-
-//     $(".receiveCoupons_main li").bind("click",function(){
-        
-//         var i= $(this).index();
-//         var couponIdNum = couponList[i].rid;
-        
-//         $.ajax({
-//             url: "/fanbei-web/pickCoupon",
-//             type: "POST",
-//             dataType: "JSON",
-//             data: {
-//                 couponId: couponIdNum,
-//                 userName: userName
-//             },
-//             success: function(returnData){
-
-//                 if (returnData.success) {
-
-//                     $(".receiveCoupons_main li").eq(i).addClass("receiveCoupons_ylq");
-
-//                 } else {
-                    
-//                     if (returnData.url.length>0) {
-//                         window.location.href = returnData.url;
-//                     }else{
-//                         requestMsg(returnData.msg);
-//                         $(".receiveCoupons_main li").eq(i).addClass("receiveCoupons_ylw");
-//                     }
-//                 }
-//             },
-//             error: function(){
-//                 requestMsg("请求失败");
-//             }
-//         });
-//         $(this).unbind("click"); // 移除当前元素的点击时间(禁止重复点击)
-//     });
-// });
-
-
 $(function(){
 
     $(".receiveCoupons_main li").click(function(){
@@ -68,15 +28,14 @@ $(function(){
                 userName: userName
             },
             success: function(returnData){
-                console.log(returnData);
+
                 if (returnData.success) {
 
                     $(".receiveCoupons_main li").eq(i).addClass("receiveCoupons_ylq");
 
                 } else {
                     
-                    var couponListData = returnData.data["couponList"];
-                    var status = couponListData[i].status;
+                    var status = returnData.data["status"];
 
                     if (status == "USER_NOT_EXIST") { // 用户不存在
                         window.location.href = returnData.url;
