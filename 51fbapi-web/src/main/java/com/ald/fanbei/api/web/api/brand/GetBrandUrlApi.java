6579fe3a1ec69e4a73f6b27b0ba5d76b91997009
xiaotianjian <277541032@qq.com>
2017-03-30 14:18:09 +0800

    commit

--- 51fbapi-web/src/main/java/com/ald/fanbei/api/web/api/brand/GetBrandUrlApi.java
+++ 51fbapi-web/src/main/java/com/ald/fanbei/api/web/api/brand/GetBrandUrlApi.java
@@ -48,36 +48,36 @@ public class GetBrandUrlApi implements ApiHandle {
 	public ApiHandleResponse process(RequestDataVo requestDataVo, FanbeiContext context, HttpServletRequest request) {
 		ApiHandleResponse resp = new ApiHandleResponse(requestDataVo.getId(), FanbeiExceptionCode.SUCCESS);
 		
-		AfOrderDo orderInfo = afOrderDao.getOrderById(90179l);
-		BoluomePushPayResponseBo res =  boluomeUtil.pushPayStatus(orderInfo.getRid(), orderInfo.getOrderNo(), orderInfo.getThirdOrderNo(), PushStatus.PAY_SUC, orderInfo.getUserId(), orderInfo.getActualAmount());
+//		AfOrderDo orderInfo = afOrderDao.getOrderById(90179l);
+//		BoluomePushPayResponseBo res =  boluomeUtil.pushPayStatus(orderInfo.getRid(), orderInfo.getOrderNo(), orderInfo.getThirdOrderNo(), PushStatus.PAY_SUC, orderInfo.getUserId(), orderInfo.getActualAmount());
 		
 //		
-//		 Map<String, Object> params = requestDataVo.getParams();
-//		Map<String, String> buildParams = new HashMap<String, String>();
-//
-//		Long shopId = NumberUtil.objToLongDefault(params.get("shopId"), null);
-//		
-//		if (shopId == null) {
-//			logger.error("shopId is empty");
-//			return new ApiHandleResponse(requestDataVo.getId(), FanbeiExceptionCode.PARAM_ERROR);
-//		}
-//		
-//		AfShopDo shopInfo = afShopService.getShopById(shopId);
-//		if (shopInfo ==  null) {
-//			logger.error("shopId is invalid");
-//			return new ApiHandleResponse(requestDataVo.getId(), FanbeiExceptionCode.PARAM_ERROR);
-//		}
-//		String shopUrl = shopInfo.getShopUrl() + "/" + shopInfo.getType().toLowerCase() + "?";
-//		
-//		buildParams.put(BoluomeCore.CUSTOMER_USER_ID, context.getUserId()+StringUtils.EMPTY);
-//		buildParams.put(BoluomeCore.CUSTOMER_USER_PHONE, context.getMobile());
-//		buildParams.put(BoluomeCore.TIME_STAMP, requestDataVo.getSystem().get(Constants.REQ_SYS_NODE_TIME) + StringUtils.EMPTY);
-//		
-//		String sign =  BoluomeCore.buildSignStr(buildParams);
-//		buildParams.put(BoluomeCore.SIGN, sign);
-//		String paramsStr = BoluomeCore.createLinkString(buildParams);
-//		
-//		resp.addResponseData("shopUrl", shopUrl + paramsStr);
+		 Map<String, Object> params = requestDataVo.getParams();
+		Map<String, String> buildParams = new HashMap<String, String>();
+
+		Long shopId = NumberUtil.objToLongDefault(params.get("shopId"), null);
+		
+		if (shopId == null) {
+			logger.error("shopId is empty");
+			return new ApiHandleResponse(requestDataVo.getId(), FanbeiExceptionCode.PARAM_ERROR);
+		}
+		
+		AfShopDo shopInfo = afShopService.getShopById(shopId);
+		if (shopInfo ==  null) {
+			logger.error("shopId is invalid");
+			return new ApiHandleResponse(requestDataVo.getId(), FanbeiExceptionCode.PARAM_ERROR);
+		}
+		String shopUrl = shopInfo.getShopUrl() + "/" + shopInfo.getType().toLowerCase() + "?";
+		
+		buildParams.put(BoluomeCore.CUSTOMER_USER_ID, context.getUserId()+StringUtils.EMPTY);
+		buildParams.put(BoluomeCore.CUSTOMER_USER_PHONE, context.getMobile());
+		buildParams.put(BoluomeCore.TIME_STAMP, requestDataVo.getSystem().get(Constants.REQ_SYS_NODE_TIME) + StringUtils.EMPTY);
+		
+		String sign =  BoluomeCore.buildSignStr(buildParams);
+		buildParams.put(BoluomeCore.SIGN, sign);
+		String paramsStr = BoluomeCore.createLinkString(buildParams);
+		
+		resp.addResponseData("shopUrl", shopUrl + paramsStr);
 		return resp;
 	}
 
