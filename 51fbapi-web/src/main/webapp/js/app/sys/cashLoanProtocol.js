201e9b1814f2580af978c1efb26c88fb5b2d1da8
yoe <yoe0405@qq.com>
2017-04-07 19:54:36 +0800

    update 个人现金服务合同

--- 51fbapi-web/src/main/webapp/js/app/sys/cashLoanProtocol.js
+++ 51fbapi-web/src/main/webapp/js/app/sys/cashLoanProtocol.js
@@ -1,19 +1,61 @@
 /*
- * @Author: nizhiwei
- * @Date:   2017-04-06
- * @Last Modified by:   nizhiwei
- * @Last Modified time: 2017-04-06
- * @title:  借钱协议
- */
+* @Author: nizhiwei
+* @Date:   2017-04-06
+* @Last Modified by:   Yangyang
+* @Last Modified time: 2017-04-07 19:51:09
+* @title:  借钱协议
+*/
+
+
+// 利率截取最后两位小数
 new Vue({
-    el:'.cashLoanProtocol',
+    el:'.rate1',
     methods:{
         change: function(num){
             var rate=num;
-            rate=(rate*100).toFixed(4);       //截取小数点后4位
+            rate=(rate*100).toFixed(4);   //截取小数点后4位
             rate=parseFloat(rate);
             rate=rate+'%';
             return rate
+        }
+    }
+});
+
+new Vue({
+    el:'.rate2',
+    methods:{
+        change: function(num){
+            var rate=num;
+            rate=(rate*100).toFixed(4);   //截取小数点后4位
+            rate=parseFloat(rate);
+            rate=rate+'%';
+            return rate
+        }
+    }
+});
+
+new Vue({
+    el:'.rate3',
+    methods:{
+        change: function(num){
+            var rate=num;
+            rate=(rate*100).toFixed(4);  //截取小数点后4位
+            rate=parseFloat(rate);
+            rate=rate+'%';
+            return rate
+        }
+    }
+});
+
+// 本金金额保留二位小数
+new Vue({
+    el:'.principal',
+    methods:{
+        change: function(principalNum){
+
+            principalNum = parseFloat(principalNum);
+            principalNum = principalNum.toFixed(2);     //截取小数点后2位
+            return principalNum
         }
     }
 });
