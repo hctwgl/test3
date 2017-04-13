c484b6f5436bebbb88984824f4d5dc7f70fd3e8f
yoe <yoe0405@qq.com>
2017-04-13 14:05:05 +0800

    add 渠道注册、 update 修改51分呗分期付款协议

--- 51fbapi-web/src/main/webapp/js/app/sys/cashLoanProtocol.js
+++ 51fbapi-web/src/main/webapp/js/app/sys/cashLoanProtocol.js
@@ -2,56 +2,22 @@
 * @Author: nizhiwei
 * @Date:   2017-04-06
 * @Last Modified by:   Yangyang
-* @Last Modified time: 2017-04-07 19:51:09
+* @Last Modified time: 2017-04-13 13:32:43
 * @title:  借钱协议
 */
 
 
-// 利率截取最后两位小数
 new Vue({
-    el:'.rate1',
+    el:'.cashLoanProtocol',
     methods:{
-        change: function(num){
+        changeRate: function(num){ // 利率截取最后两位小数
             var rate=num;
-            rate=(rate*100).toFixed(4);   //截取小数点后4位
-            rate=parseFloat(rate);
-            rate=rate+'%';
+            rate = (rate*100).toFixed(4);   //截取小数点后4位
+            rate = parseFloat(rate);
+            rate = rate+'%';
             return rate
-        }
-    }
-});
-
-new Vue({
-    el:'.rate2',
-    methods:{
-        change: function(num){
-            var rate=num;
-            rate=(rate*100).toFixed(4);   //截取小数点后4位
-            rate=parseFloat(rate);
-            rate=rate+'%';
-            return rate
-        }
-    }
-});
-
-new Vue({
-    el:'.rate3',
-    methods:{
-        change: function(num){
-            var rate=num;
-            rate=(rate*100).toFixed(4);  //截取小数点后4位
-            rate=parseFloat(rate);
-            rate=rate+'%';
-            return rate
-        }
-    }
-});
-
-// 本金金额保留二位小数
-new Vue({
-    el:'.principal',
-    methods:{
-        change: function(principalNum){
+        },
+        changePrincipal: function(principalNum){ // 本金金额保留二位小数
 
             principalNum = parseFloat(principalNum);
             principalNum = principalNum.toFixed(2);     //截取小数点后2位
