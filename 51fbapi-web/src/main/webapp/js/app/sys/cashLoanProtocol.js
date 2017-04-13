afad46f968daf748b72a9e098ae0126f11e513b7
yoe <yoe0405@qq.com>
2017-04-13 16:00:47 +0800

    update 更改个人现金付款中的借款手续率、及文案

--- 51fbapi-web/src/main/webapp/js/app/sys/cashLoanProtocol.js
+++ 51fbapi-web/src/main/webapp/js/app/sys/cashLoanProtocol.js
@@ -2,7 +2,7 @@
 * @Author: nizhiwei
 * @Date:   2017-04-06
 * @Last Modified by:   Yangyang
-* @Last Modified time: 2017-04-13 13:32:43
+* @Last Modified time: 2017-04-13 15:47:40
 * @title:  借钱协议
 */
 
@@ -15,13 +15,21 @@ new Vue({
             rate = (rate*100).toFixed(4);   //截取小数点后4位
             rate = parseFloat(rate);
             rate = rate+'%';
-            return rate
+            return rate;
         },
         changePrincipal: function(principalNum){ // 本金金额保留二位小数
 
             principalNum = parseFloat(principalNum);
             principalNum = principalNum.toFixed(2);     //截取小数点后2位
-            return principalNum
+            return principalNum;
+        },
+        changeDailyRate: function(dailyRate){ // 日利率/365
+
+            dailyRate = dailyRate/360; // 日利率
+            dailyRate = (dailyRate*100).toFixed(4);   //截取小数点后4位
+            dailyRate = parseFloat(dailyRate);
+            dailyRate = dailyRate+'%';
+            return dailyRate;
         }
     }
 });
