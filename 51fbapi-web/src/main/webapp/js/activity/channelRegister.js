fc77df604095d4a761c674fa435587ed531b960e
yoe <yoe0405@qq.com>
2017-04-18 13:44:33 +0800

    update 渠道注册

--- 51fbapi-web/src/main/webapp/js/activity/channelRegister.js
+++ 51fbapi-web/src/main/webapp/js/activity/channelRegister.js
@@ -2,7 +2,7 @@
 * @Author: Yangyang
 * @Date:   2017-04-12 10:53:28
 * @Last Modified by:   Yangyang
-* @Last Modified time: 2017-04-17 18:02:48
+* @Last Modified time: 2017-04-18 12:49:37
 * @title:  渠道注册
 */
 
@@ -58,23 +58,23 @@ var sessionId = $("#sessionId").val();
 console.log(sessionId);
 
 // 同盾校验编号的sessionId
-(function() {
-    _fmOpt = {
-        partner: 'alading',
-        appName: 'eds_web',
-        token: json.msg                            
-    };
-    var cimg = new Image(1,1);
-    cimg.onload = function() {
-        _fmOpt.imgLoaded = true;
-    };
-    cimg.src = "https://fp.fraudmetrix.cn/fp/clear.png?partnerCode=alading&appName=eds_web&tokenId=" + _fmOpt.token;
-    var fm = document.createElement('script'); fm.type = 'text/javascript'; fm.async = true;
-    fm.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'static.fraudmetrix.cn/fm.js?ver=0.1&t=' + (new Date().getTime()/3600000).toFixed(0);
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(fm, s);
-    $("#tokenId").val(_fmOpt.token);
-	// alert(json.msg);
-})();
+// (function() {
+//     _fmOpt = {
+//         partner: 'alading',
+//         appName: 'eds_web',
+//         token: json.msg                            
+//     };
+//     var cimg = new Image(1,1);
+//     cimg.onload = function() {
+//         _fmOpt.imgLoaded = true;
+//     };
+//     cimg.src = "https://fp.fraudmetrix.cn/fp/clear.png?partnerCode=alading&appName=eds_web&tokenId=" + _fmOpt.token;
+//     var fm = document.createElement('script'); fm.type = 'text/javascript'; fm.async = true;
+//     fm.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'static.fraudmetrix.cn/fm.js?ver=0.1&t=' + (new Date().getTime()/3600000).toFixed(0);
+//     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(fm, s);
+//     $("#tokenId").val(_fmOpt.token);
+// 	// alert(json.msg);
+// })();
 
 
 
