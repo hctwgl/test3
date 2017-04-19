f88946995cdd58f08c138926b1e9b88c389c63ad
unknown <fumeiai@fumeiai.Alamap.com>
2017-04-19 19:21:32 +0800

    通讯录同步

---
+++ 51fbapi-dal/src/main/java/com/ald/fanbei/api/dal/domain/query/AfUserAuthQuery.java
@@ -0,0 +1,17 @@
+package com.ald.fanbei.api.dal.domain.query;
+
+import com.ald.fanbei.api.common.page.Page;
+import com.ald.fanbei.api.dal.domain.AfUserAuthDo;
+
+
+/**
+ * 
+ *@类描述：AfContactsOldQuery
+ *@author fumeiai 2017年4月19日  15:46:25
+ *@注意：本内容仅限于浙江阿拉丁电子商务股份有限公司内部传阅，禁止外泄以及用于其他的商业目的
+ */
+public class AfUserAuthQuery extends Page<AfUserAuthDo> {
+
+	private static final long serialVersionUID = -433203985401230132L;
+
+}
