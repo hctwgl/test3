962c1f3b4db4b61581169abb0310e1699fd98ae2
苏伟丽 <1345156623@qq.com>
2017-04-21 14:34:07 +0800

    商品代买

--- 51fbapi-biz/src/main/java/com/ald/fanbei/api/biz/third/util/TaobaoApiUtil.java
+++ 51fbapi-biz/src/main/java/com/ald/fanbei/api/biz/third/util/TaobaoApiUtil.java
@@ -16,11 +16,13 @@ import com.taobao.api.TaobaoClient;
 import com.taobao.api.request.TaeItemDetailGetRequest;
 import com.taobao.api.request.TaeItemsListRequest;
 import com.taobao.api.request.TbkItemGetRequest;
+import com.taobao.api.request.TbkItemInfoGetRequest;
 //import com.taobao.api.request.TbkItemInfoGetRequest;
 import com.taobao.api.request.TbkItemRecommendGetRequest;
 import com.taobao.api.response.TaeItemDetailGetResponse;
 import com.taobao.api.response.TaeItemsListResponse;
 import com.taobao.api.response.TbkItemGetResponse;
+import com.taobao.api.response.TbkItemInfoGetResponse;
 //import com.taobao.api.response.TbkItemInfoGetResponse;
 import com.taobao.api.response.TbkItemRecommendGetResponse;
 
@@ -45,6 +47,15 @@ public class TaobaoApiUtil extends AbstractThird {
 			client = new DefaultTaobaoClient(AesUtil.decrypt(ConfigProperties.get(Constants.CONFKEY_TAOBAO_BCDS_URL), ConfigProperties.get(Constants.CONFKEY_AES_KEY)),
 					AesUtil.decrypt(ConfigProperties.get(Constants.CONFKEY_TAOBAO_BCDS_APPID), ConfigProperties.get(Constants.CONFKEY_AES_KEY)),
 					AesUtil.decrypt(ConfigProperties.get(Constants.CONFKEY_TAOBAO_BCDS_SECRET), ConfigProperties.get(Constants.CONFKEY_AES_KEY)));
+			return client;
+		}
+		return client;
+	}
+	private static TaobaoClient getTaobaoLianMengClient() {
+		if (client == null) {
+			client = new DefaultTaobaoClient(AesUtil.decrypt(ConfigProperties.get(Constants.CONFKEY_TAOBAO_LIANMENG_URL), ConfigProperties.get(Constants.CONFKEY_AES_KEY)),
+					AesUtil.decrypt(ConfigProperties.get(Constants.CONFKEY_TAOBAO_LIANMENG_APPID), ConfigProperties.get(Constants.CONFKEY_AES_KEY)),
+					AesUtil.decrypt(ConfigProperties.get(Constants.CONFKEY_TAOBAO_LIANMENG_SECRET), ConfigProperties.get(Constants.CONFKEY_AES_KEY)));
 			return client;
 		}
 		return client;
@@ -150,19 +161,19 @@ public class TaobaoApiUtil extends AbstractThird {
 	 * @return
 	 * @throws ApiExceptionx
 	 */
-//	public TbkItemInfoGetResponse executeTakItemDetailSearch(String numIids) throws ApiException{
-//		logger.info("executeTaeItemDetailSearch start,openId={}",numIids);
-////		TaobaoClient client = getTaobaoClient();
-//		TaobaoClient client = new DefaultTaobaoClient("https://eco.taobao.com/router/rest", "23384158", "92e14df48e375e5f115b421d84305866");
-//
-//		
-//		TbkItemInfoGetRequest req = new TbkItemInfoGetRequest();
-//		req.setFields(ConfigProperties.get(Constants.CONFKEY_TAOBAO_TBK_ITEM_GET_FIELDS));
-//		req.setPlatform(1L);
-//		req.setNumIids(numIids);
-//		TbkItemInfoGetResponse rsp = client.execute(req);
-//		System.out.println(rsp.getBody());
-//		return client.execute(req);
-////		return null;
-//	}
+	public TbkItemInfoGetResponse executeTakItemDetailSearch(String numIids) throws ApiException{
+		logger.info("executeTaeItemDetailSearch start,openId={}",numIids);
+//		TaobaoClient client = getTaobaoClient();
+		TaobaoClient client = getTaobaoLianMengClient();
+
+		
+		TbkItemInfoGetRequest req = new TbkItemInfoGetRequest();
+		req.setFields(ConfigProperties.get(Constants.CONFKEY_TAOBAO_TBK_ITEM_GET_FIELDS));
+		req.setPlatform(1L);
+		req.setNumIids(numIids);
+		TbkItemInfoGetResponse rsp = client.execute(req);
+		System.out.println(rsp.getBody());
+		return client.execute(req);
+//		return null;
+	}
 }
