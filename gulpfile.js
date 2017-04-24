f0057b2bda42a2c4ff354f14ed289c509e080ead
倪稚炜 <821855466@qq.com>
2017-04-24 11:07:10 +0800

    新增sourcemap

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,7 @@ var gulp = require('gulp'),
     rename = require('gulp-rename'), // 重命名
     autoprefixer = require('gulp-autoprefixer'),// 添加 CSS 浏览器前缀
     plumber = require("gulp-plumber"),//出错打印日志不终止进程
+    sourcemaps = require('gulp-sourcemaps'),//source-map
     gulpsync = require('gulp-sync')(gulp);
 // clean 清空 dist 目录
 gulp.task('clean', function(cb) {
@@ -26,11 +27,13 @@ gulp.task('clean', function(cb) {
 // es6编译为es5
 gulp.task('es6', function() {
     return gulp.src(['51fbapi-web/build/js/**/*.js','!51fbapi-web/build/js/common/**/*'])
+        .pipe(sourcemaps.init())
         .pipe(plumber())
         .pipe(babel({presets: ['es2015']}))
         .pipe(cached('js'))
         .pipe(uglify())                  //压缩
         // .pipe(rename({suffix:".min"}))    //改名加前缀
+        .pipe(sourcemaps.write('_srcmap'))
         .pipe(gulp.dest('51fbapi-web/src/main/webapp/js'));
     // gulp.src('51fbapi-web/build/js/common/**/*')
     //     .pipe(gulp.dest('51fbapi-web/src/main/webapp/js/common'));
@@ -38,11 +41,13 @@ gulp.task('es6', function() {
 //less编译为css
 gulp.task('less', function() {
     return gulp.src(['51fbapi-web/build/less/**/*','!51fbapi-web/build/less/common/**/*'])
+        .pipe(sourcemaps.init())
         .pipe(plumber())
         .pipe(less())
         .pipe(cached('less'))
         .pipe(autoprefixer('last 6 version'))
         .pipe(minifycss())
+        .pipe(sourcemaps.write('_srcmap'))
         .pipe(gulp.dest('51fbapi-web/src/main/webapp/css'));
     // gulp.src('51fbapi-web/build/less/common/**/*')
     //     .pipe(gulp.dest('51fbapi-web/src/main/webapp/less/common'));
