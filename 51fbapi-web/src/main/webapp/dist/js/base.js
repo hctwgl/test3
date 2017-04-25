d8845b80b9386cf851e3c79bf8a09aa60dc27e20
yoe <yoe0405@qq.com>
2017-04-25 15:51:39 +0800

    优化活动页

--- 51fbapi-web/src/main/webapp/dist/js/base.js
+++ 51fbapi-web/src/main/webapp/dist/js/base.js
@@ -1,2 +1,75 @@
-"use strict";function requestMsg(t){layer.open({content:t,skin:"msg",time:2})}function getUrl(t){for(var n=location.search.substring(1).split("&"),r=0;r<n.length;r++)if(t==n[r].split("=")[0])return n[r].split("=")[1];return""}function toDecimal2(t){var n=parseFloat(t);if(isNaN(n))return!1;var r=Math.round(100*t)/100,e=r.toString(),i=e.indexOf(".");for(i<0&&(i=e.length,e+=".");e.length<=i+2;)e+="0";return e}function getBlatFrom(){var t=navigator.userAgent,n=t.indexOf("Android")>-1||t.indexOf("Adr")>-1,r=!!t.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/);return n?1:r?2:0}
-//# sourceMappingURL=../_srcmap/js/base.js.map
+/*
+* @Author: Yangyang
+* @Date:   2017-02-15 09:59:54
+* @Last Modified by:   Yangyang
+
+* @Last Modified time: 2017-04-21 21:16:36
+* @title:  公用的
+*/
+
+
+// 公用弹框内容
+function requestMsg (msg){
+	layer.open({
+        content: msg,
+        skin: 'msg',
+        time: 2
+    });
+}
+
+// 获取当前页面的URL 对其带的参数进行处理
+function getUrl(para){
+    var paraArr = location.search.substring(1).split("&");
+    for(var i = 0;i < paraArr.length;i++){
+        if(para == paraArr[i].split('=')[0]){
+            return paraArr[i].split('=')[1];
+        }
+    }
+    return '';
+}
+// 获取当前页面的URL 对其带的参数进行处理
+function getInfo(){
+    var url = decodeURIComponent(location.toString());
+    var paraArr = url.toString().split("_appInfo=");
+
+    if(paraArr.length>1){
+        return eval('(' + paraArr[1] + ')');
+    }else{
+        return ''
+    }
+    
+}
+// 处理默认的保留二位小数
+function toDecimal2(x) {
+    var f = parseFloat(x);
+    if (isNaN(f)) {
+        return false;
+    }
+    var fM = Math.round(x*100)/100;
+    var s = fM.toString();
+    var rs = s.indexOf('.');
+    if (rs < 0) {
+        rs = s.length;
+        s += '.';
+    }
+    while (s.length <= rs + 2) {
+        s += '0';
+    }
+    return s;
+}
+
+// 判断ios系统还是android系统
+function getBlatFrom(){
+    var u = navigator.userAgent;
+    var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Adr') > -1; //android终端
+    var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端
+    // 返回1是android系统
+    if (isAndroid){
+        return 1;
+    }
+    // 返回2是ios系统
+    if (isiOS)  {
+        return 2;
+    }
+    return 0;
+}
