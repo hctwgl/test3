2abb7e90750e3b2f6097a4239e65d57bff00549c
unknown <fumeiai@fumeiai.Alamap.com>
2017-04-25 14:23:57 +0800

    风控审批的时候，记录提交信息，以备审批不成功的时候，后台可以重新审批用

---
+++ 51fbapi-dal/src/main/java/com/ald/fanbei/api/dal/dao/AfCommitRecordDao.java
@@ -0,0 +1,20 @@
+package com.ald.fanbei.api.dal.dao;
+
+import com.ald.fanbei.api.dal.domain.AfCommitRecordDo;
+
+
+/**
+ * @类现描述：提交记录dao
+ * @author fumeiai 2017年4月24日 上午18:58:38
+ * @version
+ * @注意：本内容仅限于杭州阿拉丁信息科技股份有限公司内部传阅，禁止外泄以及用于其他的商业目的
+ */
+public interface AfCommitRecordDao {
+
+	/**
+	 * 增加记录
+	 * @param 
+	 * @return
+	 */
+	public int addRecord(AfCommitRecordDo afCommitRecordDo);
+}
