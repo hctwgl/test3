4588ad5c032770395420f6e52552e981e4c1f1d2
yoe <yoe0405@qq.com>
2017-04-27 11:56:58 +0800

    add  拆弹专家活动页

--- 51fbapi-web/src/main/java/com/ald/fanbei/api/web/h5/controller/H5Controller.java
+++ 51fbapi-web/src/main/java/com/ald/fanbei/api/web/h5/controller/H5Controller.java
@@ -39,7 +39,7 @@ public class H5Controller extends BaseController {
 	 * @return
 	 * @throws IOException
 	 */
-	@RequestMapping(value = { "app/*", "app/goods/*", "app/user/*", "app/sys/*", "activity/*" ,"fanbei-web/*"}, method = RequestMethod.GET)
+	@RequestMapping(value = { "app/*", "app/goods/*", "app/user/*", "app/sys/*", "activity/*", "activity/04/*" ,"fanbei-web/*"}, method = RequestMethod.GET)
 	public String newVmPage(Model model, HttpServletRequest request, HttpServletResponse response) throws IOException {
 		String returnUrl = request.getRequestURI();
 		return returnUrl;
