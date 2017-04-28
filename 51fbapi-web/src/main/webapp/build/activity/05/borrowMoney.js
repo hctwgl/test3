9a91a12586539788137d62b929fbb0d45ca4f497
zhuyunyun <yunyun.zhu@91ganlu.com>
2017-04-28 11:23:04 +0800

    fix borrowMoney js and vm

--- 51fbapi-web/src/main/webapp/build/activity/05/borrowMoney.js
+++ 51fbapi-web/src/main/webapp/build/activity/05/borrowMoney.js
@@ -1,14 +1,11 @@
-var userName = "";
-if(getInfo().userName){
-    userName=getInfo().userName
-}
+var userName=getInfo().userName;
 
 $(function(){
 	$(".borrow").click(function(){
 		if(userName!=''){
-			location.href="http://testapp.51fanbei.com/fanbei-web/opennative?name=BORROW_MONEY";
+			location.href="/fanbei-web/opennative?name=BORROW_MONEY";
 		}else{
-			location.href="http://testapp.51fanbei.com/fanbei-web/opennative?name=APP_LOGIN";
+			location.href="/fanbei-web/opennative?name=APP_LOGIN";
 		}
 	})
 })
--- 51fbapi-web/src/main/webapp/dist/activity/05/borrowMoney.js
+++ 51fbapi-web/src/main/webapp/dist/activity/05/borrowMoney.js
@@ -1,2 +1,2 @@
-"use strict";var userName="";getInfo().userName&&(userName=getInfo().userName),$(function(){$(".borrow").click(function(){location.href=""!=userName?"http://testapp.51fanbei.com/fanbei-web/opennative?name=BORROW_MONEY":"http://testapp.51fanbei.com/fanbei-web/opennative?name=APP_LOGIN"})});
+"use strict";var userName=getInfo().userName;$(function(){$(".borrow").click(function(){location.href=""!=userName?"/fanbei-web/opennative?name=BORROW_MONEY":"/fanbei-web/opennative?name=APP_LOGIN"})});
 //# sourceMappingURL=../../_srcmap/activity/05/borrowMoney.js.map
