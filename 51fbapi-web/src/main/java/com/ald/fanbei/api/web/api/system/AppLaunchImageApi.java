0c22d759a72961bf35486bdf63528952b92a2b93
jiantingdong <jiangtingdong@edspay.com>
2017-05-05 17:31:40 +0800

    1.APP启动也接口

---
+++ 51fbapi-web/src/main/java/com/ald/fanbei/api/web/api/system/AppLaunchImageApi.java
@@ -0,0 +1,40 @@
+package com.ald.fanbei.api.web.api.system;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Component;
+
+import com.ald.fanbei.api.biz.service.AfResourceService;
+import com.ald.fanbei.api.common.FanbeiContext;
+import com.ald.fanbei.api.common.exception.FanbeiExceptionCode;
+import com.ald.fanbei.api.dal.domain.AfResourceDo;
+import com.ald.fanbei.api.web.common.ApiHandle;
+import com.ald.fanbei.api.web.common.ApiHandleResponse;
+import com.ald.fanbei.api.web.common.RequestDataVo;
+import com.alibaba.fastjson.JSONObject;
+
+
+@Component("AppLaunchImageApi")
+public class AppLaunchImageApi implements ApiHandle{
+
+	private static final String RESOURCE_TYPE = "appLaunchImage";
+	
+	@Resource
+	AfResourceService afResourceService;
+	@Override
+	public ApiHandleResponse process(RequestDataVo requestDataVo,
+			FanbeiContext context, HttpServletRequest request) {
+		ApiHandleResponse response = new ApiHandleResponse(requestDataVo.getId(),FanbeiExceptionCode.SUCCESS);
+	
+		AfResourceDo resourceDo = afResourceService.getSingleResourceBytype(RESOURCE_TYPE);
+		JSONObject data = new JSONObject();
+		if (resourceDo != null){
+			data.put("imageUrl", resourceDo.getValue());
+			
+		}
+		response.setResponseData(data);
+		return response;
+	}
+
+}
