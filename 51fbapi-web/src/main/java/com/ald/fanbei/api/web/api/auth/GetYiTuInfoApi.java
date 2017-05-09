3453e4e0e77bb71cae04d521cb3e8f23ddb0af42
苏伟丽 <1345156623@qq.com>
2017-05-09 16:32:16 +0800

    依图接口信息获取

---
+++ 51fbapi-web/src/main/java/com/ald/fanbei/api/web/api/auth/GetYiTuInfoApi.java
@@ -0,0 +1,71 @@
+/**
+ * 
+ */
+package com.ald.fanbei.api.web.api.auth;
+
+import java.security.PublicKey;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Component;
+import com.ald.fanbei.api.biz.service.AfUserApiCallLimitService;
+import com.ald.fanbei.api.biz.third.util.yitu.EncryptionHelper;
+import com.ald.fanbei.api.biz.third.util.yitu.EncryptionHelper.RSAHelper.PublicKeyException;
+import com.ald.fanbei.api.common.Constants;
+import com.ald.fanbei.api.common.FanbeiContext;
+import com.ald.fanbei.api.common.enums.ApiCallType;
+import com.ald.fanbei.api.common.exception.FanbeiExceptionCode;
+import com.ald.fanbei.api.common.util.ConfigProperties;
+import com.ald.fanbei.api.dal.domain.AfUserApiCallLimitDo;
+import com.ald.fanbei.api.web.common.ApiHandle;
+import com.ald.fanbei.api.web.common.ApiHandleResponse;
+import com.ald.fanbei.api.web.common.RequestDataVo;
+
+/**
+ * @类描述：
+ * @author suweili 2017年5月9日下午3:36:23
+ * @注意：本内容仅限于杭州阿拉丁信息科技股份有限公司内部传阅，禁止外泄以及用于其他的商业目的
+ */
+@Component("getYiTuInfoApi")
+public class GetYiTuInfoApi implements ApiHandle {
+	@Resource
+	AfUserApiCallLimitService afUserApiCallLimitService;
+
+	@Override
+	public ApiHandleResponse process(RequestDataVo requestDataVo, FanbeiContext context, HttpServletRequest request) {
+		ApiHandleResponse resp = new ApiHandleResponse(requestDataVo.getId(),FanbeiExceptionCode.SUCCESS);
+		Long userId = context.getUserId();
+		Map<String, Object> data = new HashMap<String, Object>();
+
+		AfUserApiCallLimitDo faceDo = afUserApiCallLimitService.selectByUserIdAndType(userId, ApiCallType.YITU_FACE.getCode());
+		AfUserApiCallLimitDo cardDo = afUserApiCallLimitService.selectByUserIdAndType(userId, ApiCallType.YITU_CARD.getCode());
+
+		if(faceDo ==null){
+			data.put("faceCount", 0);
+		}else{
+			data.put("faceCount", faceDo.getCallNum());
+		}
+		if(cardDo==null){
+			data.put("cardCount", 0);
+		}else{
+			data.put("cardCount", cardDo.getCallNum());
+		}
+		try {
+			PublicKey publicKey = EncryptionHelper.RSAHelper.loadPublicKey(ConfigProperties.get(Constants.CONFKEY_YITU_PEM_PATH));
+			data.put("publicKey", publicKey);
+
+		} catch (PublicKeyException e) {
+			e.printStackTrace();
+			return new  ApiHandleResponse(requestDataVo.getId(),FanbeiExceptionCode.FAILED);
+		}
+
+		data.put("accessId", ConfigProperties.get(Constants.CONFKEY_YITU_ID));
+		data.put("accessKey", ConfigProperties.get(Constants.CONFKEY_YITU_KEY));
+		data.put("url", ConfigProperties.get(Constants.CONFKEY_YITU_URL));
+		resp.setResponseData(data);
+		return resp;
+	}
+
+}
--- 51fbapi-web/src/main/java/com/ald/fanbei/api/web/controller/FanbeiController.java
+++ 51fbapi-web/src/main/java/com/ald/fanbei/api/web/controller/FanbeiController.java
@@ -123,7 +123,7 @@ public class FanbeiController extends BaseController {
 	@RequestMapping(value = { "/auth/authRealname", "/auth/authContacts", "/auth/authCredit", "/auth/authZhima", "/auth/authBankcard", "/auth/checkBankcard", "/auth/getBankList",
 			"/auth/checkBankcardPay", "/auth/authFace", "/auth/authMobile", "/auth/authContactor", "/auth/authLocation", "/auth/authMobileBack", "/auth/getAllowConsume",
 			"/auth/getDailyRate", "/auth/saveIdNumber", "/auth/checkIdCardApi", "/auth/updateIdCardApi",
-			"/auth/checkFaceApi" }, method = RequestMethod.POST, produces = "application/json;charset=utf-8")
+			"/auth/checkFaceApi","/auth/getYiTuInfo" }, method = RequestMethod.POST, produces = "application/json;charset=utf-8")
 	@ResponseBody
 	public String authRequest(@RequestBody String body, HttpServletRequest request, HttpServletResponse response) throws IOException {
 		request.setCharacterEncoding(Constants.DEFAULT_ENCODE);
