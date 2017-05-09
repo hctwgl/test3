a17a2d59fb3ee445e60be6d4f3bff3e598ed436b
xiaotianjian <277541032@qq.com>
2017-05-09 15:54:12 +0800

    增加安卓审核

---
+++ 51fbapi-biz/src/main/java/com/ald/fanbei/api/biz/bo/CheckVersionBo.java
@@ -0,0 +1,90 @@
+package com.ald.fanbei.api.biz.bo;
+
+import com.ald.fanbei.api.common.AbstractSerial;
+
+/**
+ * 
+ * @类描述：
+ * @author xiaotianjian 2017年5月9日下午3:32:41
+ * @注意：本内容仅限于杭州阿拉丁信息科技股份有限公司内部传阅，禁止外泄以及用于其他的商业目的
+ */
+public class CheckVersionBo extends AbstractSerial{
+	
+	private static final long serialVersionUID = 3357820717471809358L;
+	
+	private String channelCode;   //渠道编号
+	private Integer version;      //版本号
+	
+	public CheckVersionBo() {
+		super();
+	}
+	public CheckVersionBo(String channelCode, Integer version) {
+		super();
+		this.channelCode = channelCode;
+		this.version = version;
+	}
+	
+	/**
+	 * @return the channelCode
+	 */
+	public String getChannelCode() {
+		return channelCode;
+	}
+	/**
+	 * @param channelCode the channelCode to set
+	 */
+	public void setChannelCode(String channelCode) {
+		this.channelCode = channelCode;
+	}
+	/**
+	 * @return the version
+	 */
+	public Integer getVersion() {
+		return version;
+	}
+	/**
+	 * @param version the version to set
+	 */
+	public void setVersion(Integer version) {
+		this.version = version;
+	}
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((channelCode == null) ? 0 : channelCode.hashCode());
+		result = prime * result + ((version == null) ? 0 : version.hashCode());
+		return result;
+	}
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CheckVersionBo other = (CheckVersionBo) obj;
+		if (channelCode == null) {
+			if (other.channelCode != null)
+				return false;
+		} else if (!channelCode.equals(other.channelCode))
+			return false;
+		if (version == null) {
+			if (other.version != null)
+				return false;
+		} else if (!version.equals(other.version))
+			return false;
+		return true;
+	}
+	
+	
+	
+}
