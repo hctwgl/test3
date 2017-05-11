9093d6877f6550e385f74522eb745b5a91a73c1a
zhuyunyun <yunyun.zhu@91ganlu.com>
2017-05-11 13:24:33 +0800

    add montherday

---
+++ 51fbapi-web/src/main/webapp/build/activity/05/montherDay.js
@@ -0,0 +1,35 @@
+$(function(){
+	//点击购买
+	 $('.buy').click(function(){
+        location.href="http://91ala.test.otosaas.com/shengxian";
+	 })
+    //点击抢优惠券
+	 var userName = "";
+     if(getInfo().userName){
+         userName=getInfo().userName
+     }
+	 $('.clickVoucher').click(function(){
+        $.ajax({
+            url: 'pickBoluomeCoupon',
+            data:{'sceneId':'387','userName':userName},
+            type: 'POST',
+            success:function (data) {
+                data=eval('(' + data + ')');
+                if(data.success){
+                    requestMsg("领劵成功")
+                }else{
+                    if(data.url){
+                        if (getBlatFrom() == 2) {
+                            location.href=data.url;
+                        }else{
+                            requestMsg("请退出当前活动页面,登录后再进行领劵");
+                        }
+                    }else{
+                        requestMsg(data.msg);
+                    }
+                }
+
+            }
+        });
+	 })
+})
