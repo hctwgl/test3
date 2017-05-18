085d0b81bb0be3604f626e313a55ddf91847cde0
zhuyunyun <yunyun.zhu@91ganlu.com>
2017-05-18 17:11:33 +0800

    add kouMovie

---
+++ 51fbapi-web/src/main/webapp/build/activity/05/kouMovie.js
@@ -0,0 +1,32 @@
+var userName = "";
+if(getInfo().userName){
+    userName=getInfo().userName;
+};
+
+$(function(){
+    //点击抢优惠券	 
+	$('.shareBtn').click(function(){
+        $.ajax({
+            url: '/fanbei-web/pickBoluomeCoupon',
+            data:{'sceneId':'390','userName':userName},
+            type: 'POST',
+            success:function (data) {
+                data=eval('(' + data + ')');
+                if(data.success){
+                    requestMsg("领劵成功")
+                }else{
+                    if(data.url){
+                        if (getBlatFrom() == 2) {
+                            location.href=data.url;
+                        }else{
+                            requestMsg("请退出当前活动页面,登录后再进行领劵");
+                        }
+                    }else{
+                        requestMsg(data.msg);
+                    }
+                }
+            }
+        });
+	});
+
+});
