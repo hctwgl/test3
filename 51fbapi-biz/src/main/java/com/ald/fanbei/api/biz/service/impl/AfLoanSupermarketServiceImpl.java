fd0b3f51cdeabdc32ed59aae18f0c65c120f6798
chengkang <chengkang@edspay.com>
2017-06-03 16:38:48 +0800

    借款超市接口添加

---
+++ 51fbapi-biz/src/main/java/com/ald/fanbei/api/biz/service/impl/AfLoanSupermarketServiceImpl.java
@@ -0,0 +1,40 @@
+package com.ald.fanbei.api.biz.service.impl;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import com.ald.fanbei.api.biz.service.AfLoanSupermarketService;
+import com.ald.fanbei.api.biz.service.BaseService;
+import com.ald.fanbei.api.dal.dao.AfLoanSupermarketDao;
+import com.ald.fanbei.api.dal.domain.AfLoanSupermarketDo;
+
+/**
+ * @类描述：
+ * 贷款超市Service实现
+ * @author chengkang 2017年6月3日下午2:25:56
+ * @注意：本内容仅限于杭州阿拉丁信息科技股份有限公司内部传阅，禁止外泄以及用于其他的商业目的
+ */
+@Service("afLoanSupermarketService")
+public class AfLoanSupermarketServiceImpl extends BaseService implements AfLoanSupermarketService {
+	
+	@Resource
+	AfLoanSupermarketDao afLoanSupermarketDao;
+	
+	@Override
+	public int addLoanSupermarket(AfLoanSupermarketDo afLoanSupermarketDo){
+		return afLoanSupermarketDao.addLoanSupermarket(afLoanSupermarketDo);
+	}
+   
+	@Override
+	public int updateLoanSupermarket(AfLoanSupermarketDo afLoanSupermarketDo){
+		return afLoanSupermarketDao.updateLoanSupermarket(afLoanSupermarketDo);
+	}
+
+	@Override
+	public List<AfLoanSupermarketDo> getLoanSupermarketListOrderNo(Integer start){
+		return afLoanSupermarketDao.getLoanSupermarketListOrderNo(start);
+	}
+}
