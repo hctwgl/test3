bb9bb382a994685106cf03086a7354e9a22166c2
zhuyunyun <yunyun.zhu@91ganlu.com>
2017-06-03 10:43:07 +0800

    fix game gunlun

--- 51fbapi-web/src/main/webapp/build/activity/06/receiveCoupons.js
+++ 51fbapi-web/src/main/webapp/build/activity/06/receiveCoupons.js
@@ -1,50 +1,66 @@
 //点击我要领券时判断用户是否登录 此时要先判断系统 iPhone和andriod 
 // getBlatFrom() == 2代表iOS
 //（参考motherDay点击抢优惠券function）
-let userName = "";
-if(getInfo().userName){
-    userName=getInfo().userName;
-};
+
+var couponListString = $("#couponList").val();
+var couponList = eval('(' + couponListString + ')');
+var userName = $("#userName").val();
+
 $(function(){
 
-	function createCoupon(data,imgUrl){
-		//if (num=0) {}
-		    let url="https://fs.51fanbei.com/h5/app/activity/06/"+imgUrl;
-			let cont=`<li class="couponLi">
-		    <p class="positionYuan positionYuan01"></p>
-            <p class="positionYuan positionYuan02"></p>
-			<div class="couponKind"><img src="${url}"></div>
-			<div class="couponTop">
-				<p class="cashCoupon">${data.name}</p>
-				<p class="timeOver">(即将过期)</p>
-				<p class="couponSize">￥<span>20</span></p>
-				<p class="expiryDate">有效期2017-08-08</p>
-				<p class="fullCut">满100元可用</p>
-			</div>
-            <p class="clickCoupon">点击领券</p>
-		</li>`;
-		$('.couponList').append(cont);
-	}
+   // 点击领取优惠劵
+    $(".couponLi").click(function(){        
+        var i= $(this).index();
+        var couponIdNum = couponList[i].rid;
+        $.ajax({
+            url: "/fanbei-web/pickCoupon",
+            type: "POST",
+            dataType: "JSON",
+            data: {
+                couponId: couponIdNum,
+                userName: userName
+            },
+            success: function(returnData){
+                if (returnData.success) {
+                    requestMsg("优惠劵领取成功");
+                    $(".couponLi").eq(i).find('.clickCoupon').html("去用券");
+                } else {                    
+                    var status = returnData.data["status"];
 
-	//ajax请求获取数据进行填充
-	let cont1={
-		name:'满减券'
+                    if (status == "USER_NOT_EXIST") { // 用户不存在
+                        window.location.href = returnData.url;
+                    }
 
-	};
-	let cont2={
-		name:"还款抵用券"
-	}
-    /*createCoupon(cont1,'getCoupon02.png')
-    createCoupon(cont2,'getCoupon02.png')*/
-    //请求优惠券列表数据
-    $.ajax({
-    	url:'/fanbei-web/receiveCoupons',
-    	data:{'userName':userName},
-    	type:'get',
-    	success:function(data){
-    		console.log(data)
-    		/*createCoupon(data,'getCoupon01.png')
-    		createCoupon(data,'getCoupon02.png')*/
-    	}
+                    if (status == "OVER") { // 优惠券个数超过最大领券个数
+                        requestMsg(returnData.msg);
+                        requestMsg("优惠券个数超过最大领券个数");                       
+                    }
+
+                    if (status == "MORE_THAN") { // 优惠券已领取完
+                        requestMsg(returnData.msg);
+                        $(".couponLi").eq(i).find('.clickCoupon').css('background','#999999');
+
+                    }
+
+                    if (status == "COUPON_NOT_EXIST") { // 优惠券不存在
+                        requestMsg(returnData.msg);
+                        $(".couponLi").eq(i).css('display', 'none');;
+                    }
+                }
+            },
+            error: function(){
+                requestMsg("请求失败");
+            }
+        });
+        $(this).unbind("click"); // 移除当前元素的点击时间(禁止重复点击)
+    });
+
+    //点击我的优惠券
+    $('.myCoupon').click(function(){
+    	location.href="/fanbei-web/opennative?name=MINE_COUPON_LIST";
     })
-})
+
+});
+
+
+
