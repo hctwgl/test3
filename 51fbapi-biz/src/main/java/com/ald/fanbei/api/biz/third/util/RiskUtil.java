908533894b1279f96676ef375d629e11904adc6c
xiaotianjian <277541032@qq.com>
2017-06-05 12:18:40 +0800

    修改菠萝觅支付  51返呗代付不管成功失败 都推送支付成功的问题

--- 51fbapi-biz/src/main/java/com/ald/fanbei/api/biz/third/util/RiskUtil.java
+++ 51fbapi-biz/src/main/java/com/ald/fanbei/api/biz/third/util/RiskUtil.java
@@ -41,6 +41,7 @@ import com.ald.fanbei.api.biz.service.AfUserAuthService;
 import com.ald.fanbei.api.biz.service.AfUserBankcardService;
 import com.ald.fanbei.api.biz.service.AfUserService;
 import com.ald.fanbei.api.biz.service.JpushService;
+import com.ald.fanbei.api.biz.service.boluome.BoluomeUtil;
 import com.ald.fanbei.api.biz.third.AbstractThird;
 import com.ald.fanbei.api.biz.util.CommitRecordUtil;
 import com.ald.fanbei.api.common.Constants;
@@ -48,7 +49,9 @@ import com.ald.fanbei.api.common.enums.AfBorrowCashReviewStatus;
 import com.ald.fanbei.api.common.enums.AfBorrowCashStatus;
 import com.ald.fanbei.api.common.enums.AfBorrowCashType;
 import com.ald.fanbei.api.common.enums.OrderStatus;
+import com.ald.fanbei.api.common.enums.OrderType;
 import com.ald.fanbei.api.common.enums.PayStatus;
+import com.ald.fanbei.api.common.enums.PushStatus;
 import com.ald.fanbei.api.common.enums.UserAccountLogType;
 import com.ald.fanbei.api.common.enums.YesNoStatus;
 import com.ald.fanbei.api.common.exception.FanbeiException;
@@ -131,6 +134,8 @@ public class RiskUtil extends AbstractThird {
 	TransactionTemplate transactionTemplate;
 	@Resource
 	AfAgentOrderService afAgentOrderService;
+	@Resource
+	BoluomeUtil boluomeUtil;
 
 	private static String getUrl() {
 		if (url == null) {
@@ -448,7 +453,10 @@ public class RiskUtil extends AbstractThird {
 
 						logger.info("updateOrder orderInfo = {}", orderInfo);
 						orderDao.updateOrder(orderInfo);
-
+						
+						if (StringUtils.equals(orderInfo.getOrderType(), OrderType.BOLUOME.getCode())) {
+							boluomeUtil.pushPayStatus(orderInfo.getRid(), orderInfo.getOrderNo(), orderInfo.getThirdOrderNo(), PushStatus.PAY_SUC, orderInfo.getUserId(), orderInfo.getSaleAmount());
+						}
 						// TODO:返回值
 						return 1L;
 					}
