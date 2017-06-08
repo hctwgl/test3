f986ce4b64a07a1a95ad630b1adf89e4a8b9d11b
handsome_cc <handsome_cc520@163.com>
2017-06-08 09:47:11 +0800

    update 分会场

--- 51fbapi-web/src/main/webapp/build/activity/06/activepart.js
+++ 51fbapi-web/src/main/webapp/build/activity/06/activepart.js
@@ -2,43 +2,60 @@
 $(function(){
       //导航栏点击滑动到对应部分
       $(".menu li").each(function(index,e){
-            e.index=index;
+            e.index = index;
              // console.log(index);
             //注册点击事件
             $(e).click(function(){
                   // console.log("1111");
                   var that=this.index;
                   var height=$("#new").outerHeight();
-                  var t=$("#new").offset().top;
-                  var H=height*that+t;
+                  var t= $("#new").offset().top;
+                  var H= height*that+t;
                   $('body').animate({scrollTop:H+'px'},500);
-
             });
       });
 
+      //当导航栏滑动到顶部的时候 导航栏固定
+      var theme_main = $("#theme_main");
+      var menu=$('#menu');//导航栏对象
+      var win=$(window);//窗口对象
+      var sc=$('body');//document文档对象
+
+      var new_top=$('#new').offsetTop;
+      var com_top=$('#com').offsetTop;
+      var play_top=$('#play').offsetTop;
+      var ele_top=$('#ele').offsetTop;
+      var big_top=$('#big').offsetTop;
+      var top ;
+      win.scroll(function(){
+          top = sc.scrollTop();
+           if(top>=theme_main[0].offsetTop){
+                menu.addClass('fixed');
+           }else{
+                menu.removeClass('fixed');
+           }
+
+          if(top>=new_top){
+              $('.new').addClass('cur').siblings().removeClass('cur');
+           }else if(top>=com_top){
+                 $('.com').addClass('cur').siblings().removeClass('cur');
+           }else if(top>=play_top){
+                 $('.play').addClass('cur').siblings().removeClass('cur');
+           }else if(top>=ele_top){
+                 $('.ele').addClass('cur').siblings().removeClass('cur');
+           }else if(top>=big_top){
+                $('.big').addClass('cur').siblings().removeClass('cur');
+           }
+      })
+      menu.find('li').click(function(){
+         $(this).addClass('cur').siblings().removeClass('cur');
+      })
+
 });
 
-//当导航栏滑动到顶部的时候 导航栏固定
-$(function(){
-        var t=$('#theme').height();//导航栏上部的高度
-        var menu=$('.menu');//导航栏对象
-        var win=$(window);//窗口对象
-        var sc=$(document);//document文档对象
-        win.scroll(function(){
-            console.log(t);
-             if(sc.scrollTop()>=t){
-                  menu.addClass('fixed');
-
-             }else{
-                  menu.removeClass('fixed');
-
-             }
-
-        })
-})
 
  //鼠标滑动对应导航栏高亮
- $(function(){
+/* $(function(){
         var new_top=$('#new').offset().top;
         var com_top=$('#com').offset().top;
         var play_top=$('#play').offset().top;
@@ -46,7 +63,7 @@ $(function(){
         var big_top=$('#big').offset().top;
 
         $(window).scroll(function(){
-             var scrolH=$(this).scrollTop();
+             var scrolH= $(this).scrollTop();
              if(scroH>=new_top){
                    $('.new').addClass('cur');
              }else if(scroH>=com_top){
@@ -60,7 +77,7 @@ $(function(){
              }
         })
 
- })
+ })*/
 
 
 
