d420235db4289129a982c9ca7940ae1565586cc3
hy <hy@192.168.96.127>
2017-06-10 14:32:31 +0800

    测试修改

--- 51fbapi-web/src/main/java/com/ald/fanbei/api/web/api/user/ResetPwdApi.java
+++ 51fbapi-web/src/main/java/com/ald/fanbei/api/web/api/user/ResetPwdApi.java
@@ -66,7 +66,7 @@ public class ResetPwdApi implements ApiHandle {
         	return new ApiHandleResponse(requestDataVo.getId(), FanbeiExceptionCode.USER_NOT_EXIST_ERROR);
         }
         
-        String salt = UserUtil.getSalt();
+        String salt = afUserDo.getSalt();
         String password = UserUtil.getPassword(passwordSrc, salt);
         
         AfUserDo userDo = new AfUserDo();
