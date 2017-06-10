11e9286d2644cb58b0b6f693beb5a277a7016105
nizhiwei <821855466@qq.com>
2017-06-10 23:29:15 +0800

    fix submit

--- 51fbapi-web/src/main/webapp/build/activity/06/game.js
+++ 51fbapi-web/src/main/webapp/build/activity/06/game.js
@@ -170,7 +170,7 @@ function dataInit() {
                                 $('#getPrize h5').html('奖项已发送至:我的-抵用券');
                             } else if (data.data.awardInfo.type == 'E') {
                                 $('#getPrize h4').html('恭喜您获得实物大奖！');
-                                if (data.data.isSubmitContacts == 'Y') {
+                                if (data.data.awardInfo.isSubmitContacts == 'Y') {
                                     $('#getPrize h5').html('奖项已发送至您的收货地址');
                                 } else {
                                     $('#getPrize h5').html('<a href="personInfo?url='+data.data.awardInfo.awardIcon+'">请提交资料</a>');
