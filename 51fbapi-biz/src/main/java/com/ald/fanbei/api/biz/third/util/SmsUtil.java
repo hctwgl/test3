76b80b61d3874d0e7820366bdee37cd1c22d0b74
chengkang <chengkang@edspay.com>
2017-06-10 15:01:13 +0800

    应产品要求，oppo预约短信通知文案更新

--- 51fbapi-biz/src/main/java/com/ald/fanbei/api/biz/third/util/SmsUtil.java
+++ 51fbapi-biz/src/main/java/com/ald/fanbei/api/biz/third/util/SmsUtil.java
@@ -56,7 +56,7 @@ public class SmsUtil extends AbstractThird {
 	private static String SETPAY_TEMPLATE = "验证码为:&param1;您正在设置51返呗支付密码，请在30分钟内完成";
 	private static String EMAIL_TEMPLATE = "验证码为:&param1;您正在设置51返呗更换绑定邮箱，请在30分钟内完成";
 	private static String BORROWCASH_TEMPLATE = "您的借款审核通过，请留意您尾号&param1的银行卡资金变动，还款请使用51返呗app【任何索要银行卡号、要求存入现金的行为都是诈骗】";
-	private static String GOODS_RESERVATION_SUCCESS = "恭喜你！预约成功！OPPOR11将于6月22日10点准时开售，你已成功获得300元抵用券，券将于22日发放至您的账户，请记得查收哦！";
+	private static String GOODS_RESERVATION_SUCCESS = "恭喜您！预约成功！OPPO R11将于6月22日10点准时开售，0元预约购机享12期免息再送价值300元原装配件，不要错过哦！";
 
 	private static String REPAY_BORROWCASH_SUCCESS_REMAINNOTREPAY = "成功还款&param1元，剩余待还金额&param2元。";
 	private static String REPAY_BORROWCASH_SUCCESS_FINISH = "成功还款&param1元，该笔借钱已还完。";
