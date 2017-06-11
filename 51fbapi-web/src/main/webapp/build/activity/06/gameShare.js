92472d0472401fdae9ca25d3c6a904e4e2678329
yoe <yoe0405@163.com>
2017-06-11 14:25:17 +0800

    update 分享注册 获得游戏机会

--- 51fbapi-web/src/main/webapp/build/activity/06/gameShare.js
+++ 51fbapi-web/src/main/webapp/build/activity/06/gameShare.js
@@ -4,11 +4,7 @@
 
 let chance=[],isLogin,isShow,clientRate,chanceCount;
 let recommendCode = getUrl('recommendCode');
-// $('.button a').attr('href','/app/user/register?recommendCode='+recommendCode)
-
-$('.button a').attr('href','http://testapp.51fanbei.com/app/user/channelRegister?channelCode=Xpyq1&pointCode=Xpyq2');
-
-
+$('.button a').attr('href','/app/user/register?recommendCode='+recommendCode)
 // app调用web的方法
 function alaShareData(){
     // 分享内容
