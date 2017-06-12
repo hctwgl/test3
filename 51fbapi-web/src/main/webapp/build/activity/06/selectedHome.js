fde9661ec167c001a087bc3bccf3fab1463fc86f
nizhiwei <821855466@qq.com>
2017-06-12 16:26:01 +0800

    finished

--- 51fbapi-web/src/main/webapp/build/activity/06/selectedHome.js
+++ 51fbapi-web/src/main/webapp/build/activity/06/selectedHome.js
@@ -1,4 +1,58 @@
 let modelId=getUrl('modelId');
+let windowW = $(window).outerWidth();
+//导航滑动
+class Swipe{
+    constructor(ele){
+        this.container = ele;
+        this.element = this.container.children[0];
+        this.distance=0;
+        this.length = this.element.children.length;
+        this.speed = 200;
+        //执行对象中的handleEvent函数
+        document.getElementById('navWrap').style.width=this.length*1.5+'rem';
+        this.element.addEventListener("touchstart", this);
+        this.element.addEventListener("touchmove", this);
+    }
+    handleEvent(a) {
+        switch (a.type) {
+            case "touchstart":
+                this.onTouchStart(a);
+                break;
+            case "touchmove":
+                this.onTouchMove(a);
+                break;
+        }
+    }
+    onTouchStart(a) {
+        this.isScrolling = false;
+        this.deltaX = 0;
+        this.start = {
+            pageX: a.touches[0].pageX,
+            pageY: a.touches[0].pageY
+        };
+        this.element.style.MozTransitionDuration = this.element.style.webkitTransitionDuration = this.speed + "ms";
+        this.startDistance=this.distance;
+        a.stopPropagation()
+    }
+    onTouchMove(a) {
+        if (a.touches.length > 1 || a.scale && a.scale !== 1) {
+            return
+        }
+        this.deltaX = a.touches[0].pageX - this.start.pageX;
+        (Math.abs(this.deltaX) > Math.abs(a.touches[0].pageY - this.start.pageY))?this.isScrolling = true:this.isScrolling = false;   //判断横滚还是竖滚
+        if (this.isScrolling) {
+            this.distance=this.startDistance-this.deltaX;
+            if(this.distance<0){
+                this.distance=0
+            }else if(this.distance>this.element.clientWidth-windowW){
+                this.distance=this.element.clientWidth-windowW
+            }
+            this.element.style.left = -this.distance + "px";
+            a.stopPropagation()
+        }
+    }
+}
+
 new Vue({
     el:'#vueCon',
     data:{
@@ -18,9 +72,9 @@ new Vue({
         handleScroll (){
             let win=jQuery(window).scrollTop();
             if(win>=this.divTop){
-                jQuery('#listAlert').addClass('fixTop');
+                jQuery('#navWrap').addClass('fixTop');
             }else{
-                jQuery('#listAlert').removeClass('fixTop');
+                jQuery('#navWrap').removeClass('fixTop');
             }
         },
         logData (){
@@ -31,7 +85,8 @@ new Vue({
                 self.content = eval('(' + res.data + ')');
                 console.log(self.content);
                 self.$nextTick(function () {                              //dom渲染完成后执行
-                    self.divTop=document.getElementById('listAlert').offsetTop
+                    self.divTop=document.getElementById('navWrap').offsetTop;
+                    new Swipe(document.getElementById('navWrap'));
                 })
             },function (response) {
                 console.log(response)
@@ -39,4 +94,6 @@ new Vue({
         }
     }
 });
+
+
 
