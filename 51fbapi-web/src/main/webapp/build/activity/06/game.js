095c2a58aca327edfa6dba19ddba666a4acbb765
yoe <yoe0405@163.com>
2017-06-12 14:47:22 +0800

    update gameShare url

--- 51fbapi-web/src/main/webapp/build/activity/06/game.js
+++ 51fbapi-web/src/main/webapp/build/activity/06/game.js
@@ -19,7 +19,7 @@ function alaShareData(){
         'shareAppTitle': '年中抓娃娃,让你一次玩个爽！',  // 分享的title
         'shareAppContent': '51返呗年中狂欢，全球好货折上折，iPhone 7+精美电器+上万礼券等你拿~',  // 分享的内容
         'shareAppImage': 'https://fs.51fanbei.com/h5/common/icon/midyearCorner.png',  // 分享右边小图
-        'shareAppUrl': 'https://app.51fanbei.com/fanbei-web/activity/gameShare?recommendCode='+recommendCode,  // 分享后的链接
+        'shareAppUrl': 'http://testapp.51fanbei.com/fanbei-web/activity/gameShare?recommendCode='+recommendCode,  // 分享后的链接
         'isSubmit': 'Y', // 是否需要向后台提交数据，Y需要，N不需要
         'sharePage': 'gameShare' // 分享的页面
     };
