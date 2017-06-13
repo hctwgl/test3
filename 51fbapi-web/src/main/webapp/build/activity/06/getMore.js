d0cc2a21c16bcc61194cdafbbbcd4008e10dd9ac
zhuyunyun <yunyun.zhu@91ganlu.com>
2017-06-13 11:43:58 +0800

    fix scroll data

--- 51fbapi-web/src/main/webapp/build/activity/06/getMore.js
+++ 51fbapi-web/src/main/webapp/build/activity/06/getMore.js
@@ -1,12 +1,13 @@
 
-var subjectId=getUrl('subjectId');
-var currentPage=1;
-      var vm = new Vue({
+      /*var vm = new Vue({
       el: '#productList',
       data: {
       	List:''
       },
       created:function(){
+        var self=this;
+        getData(self.List)
+        console.log(self.List)
       	var self=this;
         $.ajax({
          	  url:'/fanbei-web/subjectGoodsInfo',
@@ -22,5 +23,49 @@ var currentPage=1;
 		        }
         })
       }
-    })
+    })*/
+var subjectId=getUrl('subjectId');
+var currentPage=1;
+var List;
+$(function(){
+  getData();
+  //滚动条事件
+  $(window).scroll(function() {   
+
+     if($(document).scrollTop() >= $(document).height() - $(window).height()) {
+       //alert("滚动条已经到达底部为");   
+       $('.load').animate({'opacity':'1'},2000,function(){
+          getData();
+       }) 
+     } else if(List.length==0){
+                    $('.load').remove();
+                    $('.finish').css('opacity',1);
+                  }
+
+  });
+
+})
+
+
+function getData(){
+  $.ajax({
+            url:'/fanbei-web/subjectGoodsInfo',
+            data:{'subjectId':subjectId,'currentPage':currentPage},
+            dataType:'JSON',
+            type:'get',
+            success:function(data){
+                  List=data.data.subjectGoodsList;
+                  //console.log(List)
+                  for(var i=0;i<List.length;i++){
+                    var str='<li><div class="productImg"><img src="'+List[i].goodsIcon+'"></div><div class="productRight"><p class="productDes">'+List[i].goodName+'</p><p class="productPrice">￥'+List[i].saleAmount+'</p><p class="fan">返</p><p class="fanPrice">￥'+List[i].rebateAmount+'</p><a class="buyNow" href="'+data.data.notifyUrl+'&params={%22goodsId%22:%22'+List[i].goodsId+'%22}'
++'"}'+'"}'+'">马上抢</a></div></li>'
+                    $('#productList').append(str);
+                  }
+            },
+            error:function(){
+                  requestMsg("请求失败");
+            }
+        })
+  currentPage++; 
+}
 
