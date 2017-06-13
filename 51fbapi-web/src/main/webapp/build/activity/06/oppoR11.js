7f6f4c186a6961d755821496913111ec84d8caac
yoe <yoe0405@163.com>
2017-06-13 16:39:36 +0800

    update oppoR11 share 分享

--- 51fbapi-web/src/main/webapp/build/activity/06/oppoR11.js
+++ 51fbapi-web/src/main/webapp/build/activity/06/oppoR11.js
@@ -4,6 +4,29 @@ if(getInfo().userName){
     userName=getInfo().userName;
 };
 
+// app调用web的方法
+function alaShareData(){
+    // 分享内容
+    let dataObj = {
+        'appLogin': 'N', // 是否需要登录，Y需要，N不需要
+        'type': 'share', // 此页面的类型
+        'shareAppTitle': 'OPPO R11预约返利300元',  // 分享的title
+        'shareAppContent': 'OPPO R11全明星首发，疯陪到底！0元预约享12期分期免息，更有超级返利300元！有，且只在51返呗 GO>>>',  // 分享的内容
+        'shareAppImage': 'https://fs.51fanbei.com/h5/common/icon/midyearCorner.png',  // 分享右边小图
+        'shareAppUrl': 'http://192.168.96.42:8088/fanbei-web/activity/oppoR11?oppoR11Share=oppoR11Share',  // 分享后的链接
+        'isSubmit': 'N', // 是否需要向后台提交数据，Y需要，N不需要
+        'sharePage': 'oppoR11' // 分享的页面
+    };
+    let dataStr = JSON.stringify(dataObj);  // json数组转换成json对象
+    return dataStr;
+};
+
+var oppoR11Share = getUrl("oppoR11Share");
+if (oppoR11Share == "oppoR11Share") {
+  $(".goRegister").removeClass("hide");
+  $(".banner").addClass("hide");
+}
+
 // 初始化页面
 window.onload=function(){
   $.ajax({
