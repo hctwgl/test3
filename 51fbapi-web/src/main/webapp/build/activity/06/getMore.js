539a2e909a9b9cb351ecf06ff0b10c1eb39eddcc
zhuyunyun <yunyun.zhu@91ganlu.com>
2017-06-13 09:09:23 +0800

    fix  css

--- 51fbapi-web/src/main/webapp/build/activity/06/getMore.js
+++ 51fbapi-web/src/main/webapp/build/activity/06/getMore.js
@@ -1,26 +1,26 @@
 
 var subjectId=getUrl('subjectId');
 var currentPage=1;
-    var vm = new Vue({
+      var vm = new Vue({
       el: '#productList',
       data: {
       	List:''
       },
       created:function(){
       	var self=this;
-         $.ajax({
-         	url:'/fanbei-web/subjectGoodsInfo',
-		    data:{'subjectId':subjectId,'currentPage':currentPage},
-		    dataType:'JSON',
-		    type:'get',
-		    success:function(data){
-                self.List=data.data.subjectGoodsList;
-                console.log(self.List)
-		    },
-		    error:function(){
-                requestMsg("请求失败");
-		    }
-         })
+        $.ajax({
+         	  url:'/fanbei-web/subjectGoodsInfo',
+		        data:{'subjectId':subjectId,'currentPage':currentPage},
+		        dataType:'JSON',
+		        type:'get',
+		        success:function(data){
+                  self.List=data.data.subjectGoodsList;
+                  console.log(self.List)
+		        },
+		        error:function(){
+                  requestMsg("请求失败");
+		        }
+        })
       }
     })
 
