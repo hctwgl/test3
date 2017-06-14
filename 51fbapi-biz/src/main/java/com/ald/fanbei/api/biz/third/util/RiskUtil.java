89d722c9fa5090bd11ac6fbf3c64820db4cc3443
hy <hy@192.168.96.79>
2017-06-14 20:56:24 +0800

    合并冲处理修改

--- 51fbapi-biz/src/main/java/com/ald/fanbei/api/biz/third/util/RiskUtil.java
+++ 51fbapi-biz/src/main/java/com/ald/fanbei/api/biz/third/util/RiskUtil.java
@@ -131,6 +131,7 @@ public class RiskUtil extends AbstractThird {
 	AfAgentOrderService afAgentOrderService;
 	@Resource
 	BoluomeUtil boluomeUtil;
+	
 
 	private static String getUrl() {
 		if (url == null) {
@@ -332,9 +333,9 @@ public class RiskUtil extends AbstractThird {
 	 */
 	public RiskRespBo registerStrongRisk(String consumerNo, String event, AfUserDo afUserDo, AfUserAuthDo afUserAuthDo, 
 			String appName, String ipAddress, AfUserAccountDto accountDo, String blackBox, String cardNum, String riskOrderNo) {
-		
+		String directory = bizCacheUtil.getObject(Constants.CACHEKEY_USER_CONTACTS + consumerNo).toString();
 		RiskRegisterStrongReqBo reqBo = RiskAuthFactory.createRiskDo(consumerNo, event, riskOrderNo, afUserDo, afUserAuthDo, appName, 
-				ipAddress, accountDo, blackBox, cardNum, CHANNEL, PRIVATE_KEY, getNotifyHost());
+				ipAddress, accountDo, blackBox, cardNum, CHANNEL, PRIVATE_KEY, directory, getNotifyHost());
 		
 		reqBo.setSignInfo(SignUtil.sign(createLinkString(reqBo), PRIVATE_KEY));
 		
@@ -342,7 +343,7 @@ public class RiskUtil extends AbstractThird {
 		commitRecordUtil.addRecord("registerStrongRisk", consumerNo, content, url);
 		
 		String reqResult = HttpUtil.post(getUrl() + "/modules/api/user/registerAndRisk.htm", reqBo);
-//		String reqResult = HttpUtil.post("http://192.168.96.198:80/modules/api/user/registerAndRisk.htm", reqBo);
+		
 		logThird(reqResult, "registerAndRisk", reqBo);
 		if (StringUtil.isBlank(reqResult)) {
 			throw new FanbeiException(FanbeiExceptionCode.RISK_REGISTER_ERROR);
@@ -383,13 +384,13 @@ public class RiskUtil extends AbstractThird {
 		reqBo.setSignInfo(SignUtil.sign(createLinkString(reqBo), PRIVATE_KEY));
 
 		String url = getUrl() + "/modules/api/risk/weakRiskVerify.htm";
-//		String url = "http://192.168.96.198/modules/api/risk/weakRiskVerify.htm";
+		
 		String content = JSONObject.toJSONString(reqBo);
 		commitRecordUtil.addRecord("weakverify", borrowId, content, url);
 
 		String reqResult = HttpUtil.post(url, reqBo);
 
-		logThird(reqResult, "weakverify", reqBo);
+		logThird(reqResult, "weakRiskVerify", reqBo);
 		if (StringUtil.isBlank(reqResult)) {
 			throw new FanbeiException(FanbeiExceptionCode.RISK_VERIFY_ERROR);
 		}
@@ -556,7 +557,7 @@ public class RiskUtil extends AbstractThird {
 			logger.info("asyRegisterStrongRisk reqBo.getSignInfo()" + reqBo.getSignInfo());
 			JSONObject obj = JSON.parseObject(data);
 			String limitAmount = obj.getString("amount");
-			if (StringUtil.equals(limitAmount, ""))
+			if (StringUtil.equals(limitAmount, "") || limitAmount == null)
 				limitAmount = "0";
 			BigDecimal au_amount = new BigDecimal(limitAmount);
 			Long consumerNo = Long.parseLong(obj.getString("consumerNo"));
