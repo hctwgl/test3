94bf11b5311dfd3b7e3adec33ecf9392f3f2e11d
苏伟丽 <1345156623@qq.com>
2017-06-19 18:07:26 +0800

    下单

--- 51fbapi-web/src/main/java/com/ald/fanbei/api/web/controller/FanbeiController.java
+++ 51fbapi-web/src/main/java/com/ald/fanbei/api/web/controller/FanbeiController.java
@@ -75,7 +75,7 @@ public class FanbeiController extends BaseController {
     }
     
     @RequestMapping(value ={
-    	"/order/confirmReceipt","/order/mobileCharge","/order/getOrderDetailInfo","/order/getOrderList","/order/getOrderNoWithUser","/order/refundOrderApply","/order/deleteOrderInfo"
+    	"/order/confirmReceipt","/order/buySelfGoods","/order/mobileCharge","/order/getOrderDetailInfo","/order/getOrderList","/order/getOrderNoWithUser","/order/refundOrderApply","/order/deleteOrderInfo"
     },method = RequestMethod.POST,produces="application/json;charset=utf-8")
     @ResponseBody
     public String orderRequest(@RequestBody String body, HttpServletRequest request, HttpServletResponse response) throws IOException{
