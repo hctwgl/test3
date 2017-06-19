3e4ebc4d57112837d8308a405d437ce2520f24c0
苏伟丽 <1345156623@qq.com>
2017-06-19 15:00:10 +0800

    获取分期

--- 51fbapi-web/src/main/java/com/ald/fanbei/api/web/api/order/BuySelfGoodsApi.java
+++ 51fbapi-web/src/main/java/com/ald/fanbei/api/web/api/order/BuySelfGoodsApi.java
@@ -35,8 +35,7 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 
 /**
- * @类描述：自营商品下单（oppr11）
- * @author suweili 2017年6月16日下午3:44:12
+ * @类描述：自营商品下单（oppr11） @author suweili 2017年6月16日下午3:44:12
  * @注意：本内容仅限于杭州阿拉丁信息科技股份有限公司内部传阅，禁止外泄以及用于其他的商业目的
  */
 @Component("buySelfGoodsApi")
@@ -52,33 +51,38 @@ public class BuySelfGoodsApi implements ApiHandle {
 	AfUserAddressService afUserAddressService;
 	@Resource
 	private GeneratorClusterNo generatorClusterNo;
+
 	@Override
 	public ApiHandleResponse process(RequestDataVo requestDataVo, FanbeiContext context, HttpServletRequest request) {
 		ApiHandleResponse resp = new ApiHandleResponse(requestDataVo.getId(), FanbeiExceptionCode.SUCCESS);
 		Long userId = context.getUserId();
-		Long goodsId = NumberUtil.objToLongDefault(ObjectUtils.toString(requestDataVo.getParams().get("goodsId"),""), 0l);
-		Long addressId = NumberUtil.objToLongDefault(ObjectUtils.toString(requestDataVo.getParams().get("addressId"),""), 0l);
+		Long goodsId = NumberUtil.objToLongDefault(ObjectUtils.toString(requestDataVo.getParams().get("goodsId"), ""),
+				0l);
+		Long addressId = NumberUtil
+				.objToLongDefault(ObjectUtils.toString(requestDataVo.getParams().get("addressId"), ""), 0l);
 		String invoiceHeader = ObjectUtils.toString(requestDataVo.getParams().get("invoiceHeader"));
+		String payType = ObjectUtils.toString(requestDataVo.getParams().get("payType"));
 
-		
 		Integer count = NumberUtil.objToIntDefault(requestDataVo.getParams().get("count"), 1);
-        Integer nper = NumberUtil.objToIntDefault(requestDataVo.getParams().get("nper"), 0);
-        AfGoodsDo goodsDo =  afGoodsService.getGoodsById(goodsId);
-        if(goodsDo ==null){
+		Integer nper = NumberUtil.objToIntDefault(requestDataVo.getParams().get("nper"), 0);
+		AfGoodsDo goodsDo = afGoodsService.getGoodsById(goodsId);
+		if (goodsDo == null) {
 			throw new FanbeiException(FanbeiExceptionCode.GOODS_NOT_EXIST_ERROR);
-        }
-        AfUserAddressDo addressDo = afUserAddressService.selectUserAddressByrid(addressId);    
-        if(addressDo ==null){
-    		throw new FanbeiException(FanbeiExceptionCode.USER_ADDRESS_NOT_EXIST);
-        }
-        AfOrderDo afOrder = orderDoWithGoodsAndAddressDo(addressDo,goodsDo);
+		}
+		AfUserAddressDo addressDo = afUserAddressService.selectUserAddressByrid(addressId);
+
+		if (addressDo == null) {
+			throw new FanbeiException(FanbeiExceptionCode.USER_ADDRESS_NOT_EXIST);
+		}
+		AfOrderDo afOrder = orderDoWithGoodsAndAddressDo(addressDo, goodsDo);
 		afOrder.setUserId(userId);
 		afOrder.setActualAmount(goodsDo.getSaleAmount().multiply(new BigDecimal(count)));
 		afOrder.setCount(count);
 		afOrder.setNper(nper);
+		afOrder.setPayType(payType);
 		afOrder.setInvoiceHeader(invoiceHeader);
-		if(nper.intValue()>0){
-			//保存手续费信息
+		if (nper.intValue() > 0) {
+			// 保存手续费信息
 			BorrowRateBo borrowRate = afResourceService.borrowRateWithResource(nper);
 			afOrder.setBorrowRate(BorrowRateBoUtil.parseToDataTableStrFromBo(borrowRate));
 		}
@@ -86,31 +90,32 @@ public class BuySelfGoodsApi implements ApiHandle {
 		Map<String, Object> data = new HashMap<String, Object>();
 		data.put("orderId", afOrder.getRid());
 		resp.setResponseData(data);
-        return resp;
+		;
+		return resp;
 	}
-	
-	public AfOrderDo orderDoWithGoodsAndAddressDo(AfUserAddressDo addressDo,AfGoodsDo goodsDo){
-        AfOrderDo afOrder = new AfOrderDo();
-        afOrder.setConsignee(addressDo.getConsignee());
-        afOrder.setConsigneeMobile(addressDo.getMobile());
-        afOrder.setSaleAmount(goodsDo.getSaleAmount());
+
+	public AfOrderDo orderDoWithGoodsAndAddressDo(AfUserAddressDo addressDo, AfGoodsDo goodsDo) {
+		AfOrderDo afOrder = new AfOrderDo();
+		afOrder.setConsignee(addressDo.getConsignee());
+		afOrder.setConsigneeMobile(addressDo.getMobile());
+		afOrder.setSaleAmount(goodsDo.getSaleAmount());
 		afOrder.setPriceAmount(goodsDo.getPriceAmount());
 		afOrder.setGoodsIcon(goodsDo.getGoodsIcon());
 		afOrder.setGoodsName(goodsDo.getName());
-        afOrder.setAddress(addressDo.getAddress());
-        afOrder.setGoodsId(goodsDo.getRid());
-        afOrder.setOpenId(goodsDo.getOpenId());
-        afOrder.setNumId(goodsDo.getNumId());
-        afOrder.setShopName(goodsDo.getShopName());
-        afOrder.setRebateAmount(goodsDo.getRebateAmount());
+		afOrder.setAddress(addressDo.getAddress());
+		afOrder.setGoodsId(goodsDo.getRid());
+		afOrder.setOpenId(goodsDo.getOpenId());
+		afOrder.setNumId(goodsDo.getNumId());
+		afOrder.setShopName(goodsDo.getShopName());
+		afOrder.setRebateAmount(goodsDo.getRebateAmount());
 		afOrder.setMobile("");
 
 		afOrder.setBankId(0L);
 
-        afOrder.setOrderType(OrderType.SELFSUPPORT.getCode());
+		afOrder.setOrderType(OrderType.SELFSUPPORT.getCode());
 		final String orderNo = generatorClusterNo.getOrderNo(OrderType.SELFSUPPORT);
 		afOrder.setOrderNo(orderNo);
-        return afOrder;
+		return afOrder;
 	}
 
 }
