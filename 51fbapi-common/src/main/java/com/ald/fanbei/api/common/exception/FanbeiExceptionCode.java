ec498fb7dcb307227c077b236954e5194983e761
fumeiai <fumeiai@edspay.com>
2017-06-20 12:03:38 +0800

    可用额度不足提示添加

--- 51fbapi-common/src/main/java/com/ald/fanbei/api/common/exception/FanbeiExceptionCode.java
+++ 51fbapi-common/src/main/java/com/ald/fanbei/api/common/exception/FanbeiExceptionCode.java
@@ -155,7 +155,7 @@ SUCCESS("SUCCESS", 1000, "success", "成功"), FAILED("FAILED", 1001, "failed", 
     BORROW_DETAIL_NOT_EXIST_ERROR("BORROW_DETAIL_NOT_EXIST_ERROR",1705,"borrow detail not exist error","借款详情不存在"),
     REPAYMENT_DETAIL_NOT_EXIST_ERROR("REPAYMENT_DETAIL_NOT_EXIST_ERROR",1706,"repayment detail not exist error","还款详情不存在"),
     BORROW_CONSUME_GOODS_IS_EMPTY("BORROW_CONSUME_GOODS_IS_EMPTY",1707,"borrow consume goods is empty","商品不存在"),
-    
+    AVAILABLE_CREDIT_NOT_ENOUGH("AVAILABLE_CREDIT_NOT_ENOUGH",1708,"available credit not enough","可用额度不足"),
     
     //h5 1800-1900
     RESOURES_H5_ERROR("RESOURES_H5_ERROR",1800,"resoures h5 not exist error","信息不存在，请联系管理员"),
