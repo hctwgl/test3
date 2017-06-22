fd0f2050dd2680a82575634a1bf5d2ab764fa7c9
zhuyunyun <yunyun.zhu@91ganlu.com>
2017-06-22 18:13:11 +0800

    fix

--- 51fbapi-web/src/main/webapp/build/activity/06/superGoods.js
+++ 51fbapi-web/src/main/webapp/build/activity/06/superGoods.js
@@ -1,6 +1,7 @@
 /**
  * Created on 2017/6/20.
  */
+   var activityId = getUrl("activityId");
 let vm=new Vue({
     el:'#superGoods',
     data:{
@@ -16,7 +17,7 @@ let vm=new Vue({
             $.ajax({
                     type: 'post',
                     url: '/fanbei-web/encoreActivityInfo',
-                    data:{'activityId':4},
+                    data:{'activityId':activityId},
                     success:function(data) {                      
                       self.content = eval('(' + data + ')');                      
                       self.content = self.content.data;  
@@ -85,7 +86,20 @@ let vm=new Vue({
                     }
                 });
         },
-        alaShareData(){
+        buyNow(id){
+          let self=this
+            var superGoodsShare = getUrl("superGoodsShare");
+            if (superGoodsShare == "superGoodsShare") {
+               window.location.href="http://a.app.qq.com/o/simple.jsp?pkgname=com.alfl.www";
+            } else {
+               window.location.href=self.content.notifyUrl+'&params={"goodsId":"'+id+'"}'
+            }
+        }
+        
+    }
+});
+
+   function alaShareData(){
           // 分享内容
           var ipUrl=domainName();
           var dataObj = {
@@ -94,19 +108,10 @@ let vm=new Vue({
             'shareAppTitle': '特卖会',  // 分享的title
             'shareAppContent': '51返呗返场加购，精选好货抄低价！爆款精品仅在“特卖会”，拼的就是手速，赶紧来围观~',  // 分享的内容
             'shareAppImage': 'https://fs.51fanbei.com/h5/common/icon/midyearCorner.png',  // 分享右边小图
-            'shareAppUrl': ipUrl+'/fanbei-web/activity/superGoods?superGoodsShare=superGoodsShare',  // 分享后的链接
+            'shareAppUrl': ipUrl+'/fanbei-web/activity/superGoods?superGoodsShare=superGoodsShare&activityId='+activityId,  // 分享后的链接
             'isSubmit': 'N', // 是否需要向后台提交数据，Y需要，N不需要
             'sharePage': 'superGoods' // 分享的页面
           };
           var dataStr = JSON.stringify(dataObj);  // json数组转换成json对象
           return dataStr;
-        },
-        buyNow(){
-            var superGoodsShare = getUrl("superGoodsShare");
-            if (superGoodsShare != "superGoodsShare") {
-               window.location.href="http://a.app.qq.com/o/simple.jsp?pkgname=com.alfl.www";
-            }
-        }
-        
-    }
-})
+        };
