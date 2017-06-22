b265466c66b83866aacf0aa536c5acf488f5ed85
chenjinhu <chenjinhu@edspay.com>
2017-06-23 00:27:50 +0800

    fix bug

--- 51fbapi-biz/src/main/java/com/ald/fanbei/api/biz/third/util/RiskUtil.java
+++ 51fbapi-biz/src/main/java/com/ald/fanbei/api/biz/third/util/RiskUtil.java
@@ -566,11 +566,12 @@ public class RiskUtil extends AbstractThird {
 	 * @return 
 	 */
 	public long payOrder(final AfBorrowDo borrow, final String orderNo, final String result) {
-		return transactionTemplate.execute(new TransactionCallback<Long>() {
-			@Override
-			public Long doInTransaction(TransactionStatus status) {
-
-				try {
+		logger.info("payOrder:borrow=" + borrow + ",orderNo=" + orderNo + ",result=" + result);
+//		return transactionTemplate.execute(new TransactionCallback<Long>() {
+//			@Override
+//			public Long doInTransaction(TransactionStatus status) {
+//
+//				try {
 					// 增加事务
 //					RiskOperatorNotifyReqBo reqBo = new RiskOperatorNotifyReqBo();
 //					reqBo.setCode(code);
@@ -628,15 +629,16 @@ public class RiskUtil extends AbstractThird {
 					logger.info("updateOrder orderInfo = {}", orderInfo);
 					orderDao.updateOrder(orderInfo);
 					
-				} catch (Exception e) {
-					logger.info("asyPayOrder error:" + e);
-					status.setRollbackOnly();
-					throw e;
-				}
-				return 1l;
-			}
+//				} catch (Exception e) {
+//					logger.info("asyPayOrder error:" + e);
+//					status.setRollbackOnly();
+//					throw e;
+//				}
+//				return 1l;
+//			}
 
-		});
+//		});
+					return 1l;
 	}
 
 	/**
