f870b93634cd5be7f0313d70b2d8564d1a28f298
chengkang <chengkang@edspay.com>
2017-06-24 17:30:32 +0800

    运营商认证相关修改提交

--- 51fbapi-web/src/main/java/com/ald/fanbei/api/web/api/auth/AuthMobileApi.java
+++ 51fbapi-web/src/main/java/com/ald/fanbei/api/web/api/auth/AuthMobileApi.java
@@ -6,12 +6,17 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Component;
 
 import com.ald.fanbei.api.biz.bo.RiskOperatorRespBo;
+import com.ald.fanbei.api.biz.bo.risk.MoXieReqBo;
+import com.ald.fanbei.api.biz.service.AfUserAccountService;
 import com.ald.fanbei.api.biz.third.util.RiskUtil;
 import com.ald.fanbei.api.common.FanbeiContext;
 import com.ald.fanbei.api.common.exception.FanbeiExceptionCode;
+import com.ald.fanbei.api.common.util.StringUtil;
+import com.ald.fanbei.api.dal.domain.AfUserAccountDo;
 import com.ald.fanbei.api.web.common.ApiHandle;
 import com.ald.fanbei.api.web.common.ApiHandleResponse;
 import com.ald.fanbei.api.web.common.RequestDataVo;
+import com.alibaba.fastjson.JSON;
 
 /**
  *@类现描述：手机运营商认证
@@ -24,16 +29,27 @@ public class AuthMobileApi implements ApiHandle {
 
 	@Resource
 	RiskUtil riskUtil;
-	
+	@Resource
+	AfUserAccountService afUserAccountService;
 	
 	@Override
 	public ApiHandleResponse process(RequestDataVo requestDataVo,FanbeiContext context, HttpServletRequest request) {
 		ApiHandleResponse resp = new ApiHandleResponse(requestDataVo.getId(),FanbeiExceptionCode.SUCCESS);
+		Long userId = context.getUserId();
+		
+		//调风控
 		RiskOperatorRespBo respBo = riskUtil.operator(context.getUserId()+"", context.getUserName());
 		if(!respBo.isSuccess()){
 			return new ApiHandleResponse(requestDataVo.getId(), FanbeiExceptionCode.AUTH_MOBILE_ERROR);
 		}
-		resp.addResponseData("url",respBo.getUrl());
+		
+		//获取基本地址成功时，本服务拼接用户信息串（可选，主要为了减少用户信息填写步骤操作），请求三方H5
+		AfUserAccountDo currUserAccount = afUserAccountService.getUserAccountByUserId(userId);
+		MoXieReqBo moXieReqBo = new MoXieReqBo(StringUtil.null2Str(currUserAccount.getUserName()), StringUtil.null2Str(currUserAccount.getIdNumber()), StringUtil.null2Str(currUserAccount.getRealName()));
+		String reqExtraInfoJsonStr = JSON.toJSONString(moXieReqBo);
+		String reqUrl = respBo.getUrl()+"&loginParams="+reqExtraInfoJsonStr;
+		
+		resp.addResponseData("url",reqUrl);
 		return resp;
 	}
 
