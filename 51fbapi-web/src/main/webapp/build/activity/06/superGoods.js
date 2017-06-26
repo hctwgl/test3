9e5ee060e2a4682b6e23fd9f4c83b30f65dd9b82
yoe <yoe0405@163.com>
2017-06-26 15:30:24 +0800

    update superGoods

--- 51fbapi-web/src/main/webapp/build/activity/06/superGoods.js
+++ 51fbapi-web/src/main/webapp/build/activity/06/superGoods.js
@@ -25,9 +25,7 @@ let vm=new Vue({
                       var currentTime = self.content.currentTime;
                       var validStartTime = self.content.validStartTime;
                       var validEndTime = self.content.validEndTime;
-                      var diff=0; 
-                      var timer; 
-
+                      var diff=0;
                       //活动时间倒计时-----------
                         function showTime(word,time,className){
                             diff=parseInt((time-currentTime)/1000);
@@ -35,48 +33,61 @@ let vm=new Vue({
                                 hour=0,
                                 minute=0,
                                 second=0;                           
-                            if(diff > 0){
+                            if(diff > -1){
                               day = Math.floor(diff / (60 * 60 * 24));
                               hour = Math.floor(diff / (60 * 60)) - (day * 24);
                               minute = Math.floor(diff / 60) - (day * 24 * 60) - (hour * 60);
                               second = Math.floor(diff) - (day * 24 * 60 * 60) - (hour * 60 * 60) - (minute * 60);
+                                if (minute <= 9) {minute = '0' + minute;}
+                                if (second <= 9) {second = '0' + second;}
+                                $('.'+className).html(word+'：'+day+'天'+hour+'时'+minute+'分'+second+'秒');
                             }                        
-                            if (minute <= 9) {minute = '0' + minute;}
-                            if (second <= 9) {second = '0' + second;}
-                            $('.'+className).html(word+'：'+day+'天'+hour+'时'+minute+'分'+second+'秒'); 
+
                         }//------------倒计时
 
                         function interval(word01,word02,start,end,className){
-                           if(currentTime<start){                          
-                                showTime(word01,start,className);                            
-                                timer=setInterval(function(){                                
-                                    start-=1000;
-                                    showTime(word01,start,className);
-                                }, 1000);                            
-                            }else{       
+                            if(currentTime<start){
+                                showTime(word01,start,className);
+                            }else{
                                 showTime(word02,end,className);
-                                timer=setInterval(function(){
-                                    end-=1000;
-                                    showTime(word02,end,className);
-                                    if(diff==0){
-                                      //clearInterval(timer)
-                                      //$('.'+className).remove()
-                                      this.logData();
-                                    }
-                                }, 1000);                            
                             }
+                                let time1=setInterval(function(){
+                                    if(currentTime<start){
+                                        start-=1000;
+                                        showTime(word01,start,className);
+                                    }else{
+                                        end-=1000;
+                                        showTime(word02,end,className);
+                                        if(diff<=0){
+                                            $('.'+className).parent().hide()
+                                                     }
+                                    }
+                                }, 1000);
+
+                                // timer=setInterval(function(){
+                                //
+                                //     if(diff==0){
+                                //         window.clearInterval(timer);
+                                //
+                                //         self.logData();
+                                //     }else{
+                                //         end-=1000;
+                                //         showTime(word02,end,className);
+                                //     }
+                                // }, 1000);
+
                         }//定时器
                        interval('距活动开始','距活动结束',validStartTime,validEndTime,'countTime');
 
                       //商品列表---activityGoodsList
                       var activityGoodsList=self.content.activityGoodsList;                     
-                      for(var i=0;i<activityGoodsList.length;i++){                        
+                      for(var i=0;i<activityGoodsList.length;i++){
                           (function(j){
                             let dom='productCountTime'+j;
                             //console.log(dom)
                               setTimeout(function timer(){
                               interval('距开始仅剩','距结束仅剩',activityGoodsList[j].startTime,activityGoodsList[j].validEndTime,dom);
-                            },j*0)
+                            },0)
                           })(i)   
                       }//商品列表---activityGoodsList---end
 
