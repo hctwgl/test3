23cbc42953e95f6af6ce3148b5831fb9e25c1d7a
yoe <yoe0405@163.com>
2017-07-06 19:19:42 +0800

    update 预发ID

--- 51fbapi-web/src/main/webapp/build/activity/07/elmCouponsHome.js
+++ 51fbapi-web/src/main/webapp/build/activity/07/elmCouponsHome.js
@@ -9,12 +9,12 @@ if(getInfo().userName){
 // var startStamp = startDate.valueOf();
 
 // 结束时间的时间戳
-var endDate = new Date("July 10,2017 23:59:59");
-var endStamp = endDate.valueOf();
+// var endDate = new Date("July 10,2017 23:59:59");
+// var endStamp = endDate.valueOf();
 
 // 获取当前时间的时间戳
-var now = new Date();
-var nowTimeStamp = now.valueOf();
+// var now = new Date();
+// var nowTimeStamp = now.valueOf();
 
 
 new Vue({
@@ -42,31 +42,26 @@ new Vue({
             });
         },
         boluomiCoupons: function(sceneId){ // 点击领取优惠券
-            if ( nowTimeStamp<=endStamp ) {
-                $.ajax({
-                    url: '/fanbei-web/pickBoluomeCoupon',
-                    type: 'POST',
-                    dataType: "JSON",
-                    data:{
-                      'sceneId': sceneId,
-                      'userName': userName
-                    },
-                    success: function(returnData){
-                      if(returnData.success){
-                        requestMsg("领劵成功");
-                      }else{
-                        if(returnData.url){
-                          location.href=returnData.url;
-                        }else{
-                          requestMsg(returnData.msg);
-                        }
-                      }
+            $.ajax({
+                url: '/fanbei-web/pickBoluomeCoupon',
+                type: 'POST',
+                dataType: "JSON",
+                data:{
+                  'sceneId': sceneId,
+                  'userName': userName
+                },
+                success: function(returnData){
+                  if(returnData.success){
+                    requestMsg("领劵成功");
+                  }else{
+                    if(returnData.url){
+                      location.href=returnData.url;
+                    }else{
+                      requestMsg(returnData.msg);
                     }
-                });
-            } else {
-                requestMsg('活动时间已结束，请期待下一次的活动');
-            }
-
+                  }
+                }
+            });
         }
     }
 })
