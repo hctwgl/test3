717b6a070daee6dadc44cae863751f573451dc0c
peng <peng@sina.com>
2017-07-10 16:37:52 +0800

    fix

--- 51fbapi-web/src/main/webapp/build/activity/07/gamePoint.js
+++ 51fbapi-web/src/main/webapp/build/activity/07/gamePoint.js
@@ -37,31 +37,9 @@ let vm = new Vue({
             this.isActive = !this.isActive;
 
         },
-        buyNow(id) {
-            let self = this
-            var gamePointShare = getUrl("gamePointShare");
-            if (gamePointShare == "gamePointShare") {
-                window.location.href = "http://a.app.qq.com/o/simple.jsp?pkgname=com.alfl.www";
-            } else {
-                window.location.href = '/fanbei-web/opennative?params={"modelId":"' + id + '"}'
-            }
-        },
+        buyNow(id){
+            // window.location.href = '/fanbei-web/opennative?params={"goodsId":"' + id + '"}'
+            window.location.href = '/fanbei-web/opennative?name=GOODS_DETAIL_INFO&params={"goodsId":"' + id + '"}'
+        }
     }
 })
-
-function alaShareData(){
-          // 分享内容
-          var ipUrl=domainName();
-          var dataObj = {
-            'appLogin': 'N', // 是否需要登录，Y需要，N不需要
-            'type': 'share', // 此页面的类型
-            'shareAppTitle': '特卖会',  // 分享的title
-            'shareAppContent': '51返呗返场加购，精选好货抄低价！爆款精品仅在“特卖会”，拼的就是手速，赶紧来围观~',  // 分享的内容
-            'shareAppImage': 'https://fs.51fanbei.com/h5/common/icon/midyearCorner.png',  // 分享右边小图
-            'shareAppUrl': ipUrl+'/fanbei-web/activity/gamePoint?gamePointShare=gamePointShare&modelId='+modelId,  // 分享后的链接
-            'isSubmit': 'N', // 是否需要向后台提交数据，Y需要，N不需要
-            'sharePage': 'superGoods' // 分享的页面
-          };
-          var dataStr = JSON.stringify(dataObj);  // json数组转换成json对象
-          return dataStr;
-        };
