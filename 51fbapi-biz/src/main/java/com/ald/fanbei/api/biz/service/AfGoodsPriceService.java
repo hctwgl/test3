352e301c912ff5c157fbd1afd0d5fc7c4d2a2640
maqiaopan <maqiaopan@edspay.com>
2017-07-17 14:22:37 +0800

    增加接口销量和库存修改的方法，

--- 51fbapi-biz/src/main/java/com/ald/fanbei/api/biz/service/AfGoodsPriceService.java
+++ 51fbapi-biz/src/main/java/com/ald/fanbei/api/biz/service/AfGoodsPriceService.java
@@ -11,5 +11,5 @@ import com.ald.fanbei.api.dal.domain.AfGoodsPriceDo;
  * Copyright 本内容仅限于杭州阿拉丁信息科技股份有限公司内部传阅，禁止外泄以及用于其他的商业目的
  */
 public interface AfGoodsPriceService extends ParentService<AfGoodsPriceDo, Long>{
-
+	int updateStockAndSaleByPriceId(Long priceId,boolean isSold);
 }
