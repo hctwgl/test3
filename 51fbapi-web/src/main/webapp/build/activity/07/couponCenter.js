65e72d3f2c8f0cec9e2e2225a1376c7ad3219303
zhuyunyun <yunyun.zhu@91ganlu.com>
2017-07-17 20:07:04 +0800

    add js

--- 51fbapi-web/src/main/webapp/build/activity/07/couponCenter.js
+++ 51fbapi-web/src/main/webapp/build/activity/07/couponCenter.js
@@ -1,16 +1,14 @@
-var liLength=$('.navList').find('li').length;
+//var liLength=$('.navList').find('li').length;
 var tabWidth=0;
 var liWidth=0;
 var ulWidth=0;
-if(liLength<2){
+/*if(liLength<2){
      $('#tabNav').hide();
 }else if(liLength>=2&&liLength<=5){
+
      tabWidth=$('#tabNav').width();
-     $('.navList').find('li').eq(0).width();
-     console.log(liWidth)
-     //console.log(tabWidth)
+     console.log(tabWidth)
      liWidth=tabWidth/liLength;
-     //console.log(liWidth)
      $('.navList').find('li').width(liWidth);
      liClick();
 }else if(liLength>5){
@@ -18,27 +16,20 @@ if(liLength<2){
      liWidth=tabWidth/5;
      ulWidth=liWidth*liLength;
      $('.navList').find('li').width(liWidth);
+     //$('.navList li').eq(0).find('span').width(28*liWidth/75)
+     //console.log($('.navList li').eq(0).find('span').width())
      $('.navList').width(ulWidth);
      liClick();
-}
-//tab栏点击事件
-function liClick(){
-     $('.navList').find('li').click(function(){
-         var index=$(this).index();
-         //console.log(index);
-         $(this).addClass('border').siblings().removeClass('border');
-         $('.contList').find('li').eq(index).show().siblings().hide();         
-     })   
-}
+}*/
 //获取数据
 let vm = new Vue({
-    el: '#content',
+    el: '#couponCenter',
     data: {
-        content: {},
-        isActive: true
+        content: {}
     },
     created: function () {
         this.logData();
+        
     },
     methods: {
         logData() {
@@ -50,20 +41,62 @@ let vm = new Vue({
                 success: function (data) {
                     self.content = eval('(' + data + ')').data;
                     console.log(self.content);
+                    var liLength=self.content.couponCategoryList.length;
+                    self.$nextTick(function () {
+                        $('.navList li').eq(0).find('span').addClass('border');
+                        $('.contList li').eq(0).addClass('active');                               //dom渲染完成后执行
+                        
+                        if(liLength<2){
+                             $('#tabNav').hide();
+                        }else if(liLength>=2&&liLength<=5){                      
+                             tabWidth=$('#tabNav').width();
+                             liWidth=tabWidth/liLength;
+                             $('.navList').find('li').width(liWidth);                        
+                        }
+                    })
+                    //计算是否过期
+                    var diff=0;
+                    for(var i=0;i<liLength;i++){
+                        var couponCategory=self.content.couponCategoryList[i];
+                        //console.log(couponCategory.couponInfoList)  
+                        for(var j=0;j<couponCategory.couponInfoList.length;j++){
+                             var currentTime=couponCategory.couponInfoList[j].currentTime;
+                             var startTime=couponCategory.couponInfoList[j].gmtStart;
+                             var endTime=couponCategory.couponInfoList[j].gmtEnd;
+                             couponCategory.couponInfoList[j].start=format(startTime);
+                             couponCategory.couponInfoList[j].end=format(endTime);
+                             diff=(endTime-currentTime)/1000;
+                             var h=parseInt(diff/3600);
+                             var state;
+                             if(h<48){
+                                state='timeOver';                                
+                             }else{
+                                state='noTimeOver';
+                             }
+                             couponCategory.couponInfoList[j].state=state;
+                        }
+                    }
+                    function format(shijianchuo) { //shijianchuo是整数，否则要parseInt转换 
+                        var time = new Date(shijianchuo); 
+                        var y = time.getFullYear(); 
+                        var m = time.getMonth()+1; 
+                        var d = time.getDate();
+                        if(m<10){m='0'+m} 
+                        if(d<10){d='0'+d}
+                        return y+'.'+m+'.'+d; 
+                    } 
 
                 },
                 error: function () {
                     requestMsg("请求失败");
                 }
             });
-        }/*,
-        //点击tab栏切换
-        isshow: function () {
-            this.isActive = !this.isActive;
         },
-        buyNow(id){
-            // window.location.href = '/fanbei-web/opennative?params={"goodsId":"' + id + '"}'
-            window.location.href = '/fanbei-web/opennative?name=GOODS_DETAIL_INFO&params={"goodsId":"' + id + '"}'
-        }*/
+        liClick:function(index){
+            console.log(index);
+            $('.navList li').eq(index).find('span').addClass('border');
+            $('.navList li').eq(index).siblings().find('span').removeClass('border');
+            $('.contList').find('li').eq(index).show().siblings().hide();         
+        }
     }
 })
