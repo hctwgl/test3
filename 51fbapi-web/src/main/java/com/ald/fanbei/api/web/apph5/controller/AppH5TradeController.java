1976ce9cff065a8e3a163395c08d7f23e2dbeb9c
shencheng <aladin>
2017-07-18 16:58:04 +0800

    app端商圈接口

--- 51fbapi-web/src/main/java/com/ald/fanbei/api/web/apph5/controller/AppH5TradeController.java
+++ 51fbapi-web/src/main/java/com/ald/fanbei/api/web/apph5/controller/AppH5TradeController.java
@@ -31,7 +31,7 @@ import java.util.Map;
  * @注意:本内容仅限于浙江阿拉丁电子商务股份有限公司内部传阅，禁止外泄以及用于其他的商业目的
  */
 @Controller
-@RequestMapping("/fanbei-web/")
+@RequestMapping("/fanbei-web/app/")
 public class AppH5TradeController extends BaseController {
 
     @Resource
