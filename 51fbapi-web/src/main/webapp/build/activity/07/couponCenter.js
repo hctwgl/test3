8a51cf1cef26c5f1639070939903b7db6095fa23
zhuyunyun <yunyun.zhu@91ganlu.com>
2017-07-18 16:55:06 +0800

    fix js

--- 51fbapi-web/src/main/webapp/build/activity/07/couponCenter.js
+++ 51fbapi-web/src/main/webapp/build/activity/07/couponCenter.js
@@ -81,52 +81,56 @@ let vm = new Vue({
         couponClick:function(e){
             let self=this;
             var couponId=e.couponId;
-            var shopUrl=e.shopUrl; 
-            if(e.isDraw=='Y'){                 
+            var shopUrl=e.shopUrl;
+            var couponType=e.type;
+            //只有现金券、满减券、会场券时，券状态有去用券 可点击跳转 其他不可
+            if(couponType=='FULLVOUCHER' || couponType=='CASH' || couponType=='ACTIVITY'){
+                if(e.isDraw=='Y'){                 
                 //event.preventDefault();
                     //去用券
                     if(shopUrl){
                         window.location.href=shopUrl;
                     }else{
-                        console.log(0)
-                        window.location.href="https://www.baidu.com/";
+                        //alert(0)
+                        //window.location.href="https://www.baidu.com/";
+                        window.location.href='/fanbei-web/opennative?name=APP_HOME';
                     }
-            }else{
-                //点击领券
-                console.log(1)
-                    $.ajax({
-                        url: "/fanbei-web/pickCoupon",
-                        type: "POST",
-                        dataType: "JSON",
-                        data: {
-                            couponId:couponId
-                        },
-                        success: function(returnData){
-                            if(returnData.success){
-                                //alert(0)
-                               e.isDraw='Y';                           
-                            }else{
-                                var status = returnData.data["status"];
-                                if (status == "USER_NOT_EXIST") { // 用户不存在                                
-                                    window.location.href = returnData.url;
+                }else{
+                    //点击领券
+                    console.log(1)
+                        $.ajax({
+                            url: "/fanbei-web/pickCoupon",
+                            type: "POST",
+                            dataType: "JSON",
+                            data: {
+                                couponId:couponId
+                            },
+                            success: function(returnData){
+                                if(returnData.success){
+                                    //alert(0)
+                                   e.isDraw='Y';                           
+                                }else{
+                                    var status = returnData.data["status"];
+                                    if (status == "USER_NOT_EXIST") { // 用户不存在                                
+                                        window.location.href = returnData.url;
+                                    }
+                                    if (status == "OVER") { // 优惠券个数超过最大领券个数
+                                        requestMsg(returnData.msg);
+                                        requestMsg("优惠券个数超过最大领券个数");
+                                    }
+                                    if (status == "COUPON_NOT_EXIST") { // 优惠券不存在
+                                        requestMsg(returnData.msg);
+                                        $(".couponLi").eq(i).css('display', 'none');
+                                    } 
                                 }
-                                if (status == "OVER") { // 优惠券个数超过最大领券个数
-                                    requestMsg(returnData.msg);
-                                    requestMsg("优惠券个数超过最大领券个数");
-                                }
-                                if (status == "COUPON_NOT_EXIST") { // 优惠券不存在
-                                    requestMsg(returnData.msg);
-                                    $(".couponLi").eq(i).css('display', 'none');
-                                } 
+                            },
+                            error: function(){
+                                requestMsg("请求失败");
                             }
-                        },
-                        error: function(){
-                            requestMsg("请求失败");
-                        }
-                    });
-            }                   
-            
-        }
+                        });
+                } 
+            }     
+        }//couponClick--
 
     }
 })
