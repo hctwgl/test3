eaa013a33ce75466380d0eb1c6434e9ec3c8133f
shencheng <aladin>
2017-07-19 16:29:30 +0800

    app端商圈接口

--- 51fbapi-web/src/main/java/com/ald/fanbei/api/web/controller/FanbeiController.java
+++ 51fbapi-web/src/main/java/com/ald/fanbei/api/web/controller/FanbeiController.java
@@ -74,7 +74,7 @@ public class FanbeiController extends BaseController {
 
     @RequestMapping(value ={
     	"/order/confirmReceipt","/order/buySelfGoods","/order/mobileCharge","/order/getOrderDetailInfo","/order/getOrderList","/order/getOrderNoWithUser","/order/refundOrderApply","/order/deleteOrderInfo","/order/cancelAfterSaleApply","/order/afterSaleLogisticSupply","/order/getOrderAfterSaleInfo","/order/insufficientBalance"
-        ,"/order/tradeOrder"
+        ,"/order/tradeOrder","/order/getTradeOrderAfterSaleInfo"
     },method = RequestMethod.POST,produces="application/json;charset=utf-8")
     @ResponseBody
     public String orderRequest(@RequestBody String body, HttpServletRequest request, HttpServletResponse response) throws IOException{
