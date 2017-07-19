0d2ed64bdb04d5a0e244c1715c3521bcdee0cd6c
peng <peng@sina.com>
2017-07-19 11:11:34 +0800

    fix

--- 51fbapi-web/src/main/webapp/build/js/user/initTradeInfo.js
+++ 51fbapi-web/src/main/webapp/build/js/user/initTradeInfo.js
@@ -1,19 +1,17 @@
-   var bid = getUrl("bid");
-var isLogin=$('#isLogin').val();
-   let vm=new Vue({
-    el:'#businessCode',
-    data:{
-        content:{}
-    },
-    methods:{
-
-     buyNow(){
-         var amount=$('.paymoney').text();
-         window.location.href='/fanbei-web/opennative?name=APP_PAY'+'&params={"amount":"'+amount+'"}';
-         //window.location.href='http://www.baidu.com';
-        
-        }
-
-   }
-
-   })
+var isLogin=$('.isLogin').val();
+var canUseAmount=$('.canUseAmount').val();
+var name=$('.name').val();
+var id=$('.id').val();
+var isShowMention=$('.isShowMention').val();
+if(isShowMention=='yes'){
+    $('.pro-word').show();
+}else{
+    $('.pro-word').hide();
+}
+if(isLogin=='no'){
+    window.location.href='/fanbei-web/opennative?name=APP_LOGIN';
+}
+function buyNow(){
+      var amount=$('.paymoney').val();
+         window.location.href='/fanbei-web/opennative?name=APP_PAY&params={"amount":"'+amount+'"&id":"'+id+'"&name":"'+name+'"}';
+}
