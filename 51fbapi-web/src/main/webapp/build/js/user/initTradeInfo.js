2fac9a511f91b70dd18e1ebbea98b472182f3ff9
nip <821855466@qq.com>
2017-07-21 20:40:39 +0800

    js fix

--- 51fbapi-web/src/main/webapp/build/js/user/initTradeInfo.js
+++ 51fbapi-web/src/main/webapp/build/js/user/initTradeInfo.js
@@ -12,7 +12,7 @@ if(isLogin=='no'){
     window.location.href='/fanbei-web/opennative?name=APP_LOGIN';
 }
 $(document).ready(function(){
-    $(".paymoney").trigger('focus');
+    $(".paymoney").trigger('click').focus();
 })
 function buyNow(){
       var amount=$('.paymoney').val();
