d3804970116a681ca8294af16fe2f013296170b0
unknown <chenqiwei@Alamap.com>
2017-07-27 13:02:01 +0800

    逛逛和用户实名认证数埋点

--- 51fbapi-web/src/main/java/com/ald/fanbei/api/web/controller/FanbeiController.java
+++ 51fbapi-web/src/main/java/com/ald/fanbei/api/web/controller/FanbeiController.java
@@ -85,7 +85,7 @@ public class FanbeiController extends BaseController {
 
     @RequestMapping(value ={
     	"/system/appUpgrade","/system/commitFeedBack","/system/getSettingInfo","/system/checkVersion","/system/AppLaunchImage","/system/appPopImage",
-    	"/system/getTabbarInfor","/resource/getResourceConfig"
+    	"/system/getTabbarInfor","/resource/getResourceConfig","/system/maidian"
     },method = RequestMethod.POST,produces="application/json;charset=utf-8")
     @ResponseBody
     public String sysRequest(@RequestBody String body, HttpServletRequest request, HttpServletResponse response) throws IOException{
