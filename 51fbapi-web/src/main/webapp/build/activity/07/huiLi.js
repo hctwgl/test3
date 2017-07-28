10bcfbfabbf81d717b8649759f076b5a0ccd126c
zhuyunyun <yunyun.zhu@91ganlu.com>
2017-07-28 17:14:24 +0800

    fix  data

--- 51fbapi-web/src/main/webapp/build/activity/07/huiLi.js
+++ 51fbapi-web/src/main/webapp/build/activity/07/huiLi.js
@@ -4,37 +4,139 @@ console.log(modelId)
 let vm = new Vue({
     el: '#huiLi',
     data: {
-        content: {}
+        couponCont:{},
+        content: {},
+        list01:{},
+        list02:{},
+        list03:{},
+        firstId:{},
+        secondId:{}
     },
     created: function () {
-        this.logData();        
+        this.logCoupon();
+        this.logContent();
     },
     methods: {
-        logData() {
+        logCoupon() {
             //获取页面初始化信息
             let self = this;
             $.ajax({
                 type: 'post',
                 url: "/fanbei-web/activityCouponList",
                 success: function (data) {
-                    self.content = eval('(' + data + ')').data;
-                    console.log(self.content);
+                    self.couponCont = eval('(' + data + ')').data;
+                    console.log(self.couponCont);                   
                 },
                 error: function () {
                     requestMsg("请求失败");
                 }
             });
-            /*$.ajax({
+            
+        },
+        couponClick:function(e){
+            let self=this;
+            var couponId=e.couponId;
+            //点击领券
+            console.log(1)
+            $.ajax({
+                url: "/fanbei-web/pickCoupon",
+                type: "POST",
+                dataType: "JSON",
+                data: {
+                    couponId:couponId
+                },
+                success: function(returnData){
+                    if(returnData.success){
+                       requestMsg("优惠劵领取成功");                          
+                    }else{
+                        var status = returnData.data["status"];
+                        if (status == "USER_NOT_EXIST") { // 用户不存在                                
+                            window.location.href = returnData.url;
+                        }
+                        if (status == "OVER") { // 优惠券个数超过最大领券个数
+                            requestMsg(returnData.msg);
+                            requestMsg("优惠券个数超过最大领券个数");
+                        }
+                        if (status == "COUPON_NOT_EXIST") { // 优惠券不存在
+                            requestMsg(returnData.msg);
+                        } 
+                    }
+                },
+                error: function(){
+                    requestMsg("请求失败");
+                }
+            });
+            
+        },
+        logContent(){
+            let self = this;
+            $.ajax({
                 type: 'post',
                 url: "/fanbei-web/partActivityInfo?modelId="+modelId,
                 success: function (data) {
                     self.content = eval('(' + data + ')').data;
                     console.log(self.content);
+                    self.list01=self.content.activityList[0].activityGoodsList;
+                    self.list02=self.content.activityList[1].activityGoodsList;
+                    self.list03=self.content.activityList[2].activityGoodsList;
+                    self.firstId=self.list01[0].goodsId;
+                    self.secondId=self.list02[0].goodsId;
+                    self.list01=self.list01.slice(1);
+                    self.list02=self.list02.slice(1);             
                 },
                 error: function () {
                     requestMsg("请求失败");
                 }
-            });*/
+            });
+        },
+        productClick(id){
+            console.log(id)
+        },
+        txtFix01(i){
+            function get_length(s){
+                var char_length = 0;
+                for (var i = 0; i < s.length; i++){
+                    var son_char = s.charAt(i);
+                    encodeURI(son_char).length > 2 ? char_length += 1 : char_length += 0.5;
+                }
+                return char_length;
+            }
+            function cut_str(str, len){
+                var char_length = 0;
+                for (var i = 0; i < str.length; i++){
+                    var son_str = str.charAt(i);
+                    encodeURI(son_str).length > 2 ? char_length += 1 : char_length += 0.5;
+                    if (char_length >= len){
+                        var sub_len = char_length == len ? i+1 : i;
+                        return str.substr(0, sub_len);
+                        break;
+                    }
+                }
+            }
+            return cut_str(i, 20)
+        },
+        txtFix02(i){
+            function get_length(s){
+                var char_length = 0;
+                for (var i = 0; i < s.length; i++){
+                    var son_char = s.charAt(i);
+                    encodeURI(son_char).length > 2 ? char_length += 1 : char_length += 0.5;
+                }
+                return char_length;
+            }
+            function cut_str(str, len){
+                var char_length = 0;
+                for (var i = 0; i < str.length; i++){
+                    var son_str = str.charAt(i);
+                    encodeURI(son_str).length > 2 ? char_length += 1 : char_length += 0.5;
+                    if (char_length >= len){
+                        var sub_len = char_length == len ? i+1 : i;
+                        return str.substr(0, sub_len);
+                        break;
+                    }
+                }
+            }
+            return cut_str(i, 8)
         }
     }
 })
