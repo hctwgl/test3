6a0a13a4c931289baf90c2e725dc81367716258d
pxy <15316118261@sina.cn>
2017-07-31 20:02:32 +0800

    fix

---
+++ 51fbapi-web/src/main/webapp/dist/activity/07/rankingList.js
@@ -0,0 +1,2 @@
+"use strict";function scrollList(i){var t=$("#rankingList ul li:first").height();$uList.stop(200).animate({marginTop:-t},1100,function(){$uList.css({marginTop:0}).find("li:first").appendTo($uList)})}var $uList=$("#rankingList ul"),timer=null;$uList.hover(function(){clearInterval(timer)},function(){timer=setInterval(function(){scrollList($uList)},1500)}).trigger("mouseleave");
+//# sourceMappingURL=../../_srcmap/activity/07/rankingList.js.map
