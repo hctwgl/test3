9c5a8c6e2c88687a928214d05ea06cbb20c7b580
yoe <yoe0405@163.com>
2017-08-02 10:47:17 +0800

    update newFriend

--- 51fbapi-web/src/main/webapp/dist/activity/07/rankingList.js
+++
@@ -1,2 +0,0 @@
-"use strict";function play(){var e=ul.offsetTop;e>-50?(e+=-3,ul.style.top=e+"px"):ul.style.top=0}var roll=document.getElementById("roll"),ul=roll.children[0],lis=ul.children,timer=null;timer=setInterval(play,15),screen.onmouseover=function(){clearInterval(timer)},screen.onmouseout=function(){timer=setInterval(play,15)};
-//# sourceMappingURL=../../_srcmap/activity/07/rankingList.js.map
