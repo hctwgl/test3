d191ec89eb745287869188ece8532669d6276af2
zhuyunyun <yunyun.zhu@91ganlu.com>
2017-08-03 15:19:16 +0800

    add 索要卡片js

---
+++ 51fbapi-web/src/main/webapp/build/activity/08/alertCard.js
@@ -0,0 +1,103 @@
+var touch = true;           //touch=true为开启触摸滑动
+var slideNub;               //轮播图片数量
+$(function(){
+    slideNub = $(".imgList .img").size();             //获取轮播图片数量
+    for(var i=0;i<slideNub;i++){
+        $(".imgList .img:eq("+i+")").attr("data-slide-imgId",i);
+    }
+
+    //根据轮播图片数量设定图片位置对应的class
+    if(slideNub==1){
+        for(var i=0;i<slideNub;i++){
+            $(".imgList .img:eq("+i+")").addClass("img3");
+        }
+    }
+    if(slideNub==2){
+        for(var i=0;i<slideNub;i++){
+            $(".imgList .img:eq("+i+")").addClass("img"+(i+3));
+        }
+    }
+    if(slideNub==3){
+        for(var i=0;i<slideNub;i++){
+            $(".imgList .img:eq("+i+")").addClass("img"+(i+2));
+        }
+    }
+    if(slideNub>3&&slideNub<6){
+        for(var i=0;i<slideNub;i++){
+            $(".imgList .img:eq("+i+")").addClass("img"+(i+1));
+        }
+    }
+    if(slideNub>=6){
+        for(var i=0;i<slideNub;i++){
+            if(i<5){
+                $(".imgList .img:eq("+i+")").addClass("img"+(i+1));
+            }else{
+                $(".imgList .img:eq("+i+")").addClass("img5");
+            }
+        }
+    }
+    if(touch){
+        k_touch();
+    }
+    imgClickFy();
+})
+//右滑动
+function right(){
+    var fy = new Array();
+    for(var i=0;i<slideNub;i++){
+        fy[i]=$(".imgList .img[data-slide-imgId="+i+"]").attr("class");
+    }
+    for(var i=0;i<slideNub;i++){
+        if(i==0){
+            $(".imgList .img[data-slide-imgId="+i+"]").attr("class",fy[slideNub-1]);
+        }else{
+            $(".imgList .img[data-slide-imgId="+i+"]").attr("class",fy[i-1]);
+        }
+    }
+    imgClickFy();
+}
+//左滑动
+function left(){
+    var fy = new Array();
+    for(var i=0;i<slideNub;i++){
+        fy[i]=$(".imgList .img[data-slide-imgId="+i+"]").attr("class");
+    }
+    for(var i=0;i<slideNub;i++){
+        if(i==(slideNub-1)){
+            $(".imgList .img[data-slide-imgId="+i+"]").attr("class",fy[0]);
+        }else{
+            $(".imgList .img[data-slide-imgId="+i+"]").attr("class",fy[i+1]);
+        }
+    }
+    imgClickFy();
+}
+//轮播图片左右图片点击翻页
+function imgClickFy(){
+    $(".imgList .img").removeAttr("onclick");
+    $(".imgList .img2").attr("onclick","left()");
+    $(".imgList .img4").attr("onclick","right()");
+}
+//触摸滑动模块
+function k_touch() {
+    var _start = 0, _end = 0, _content = document.getElementById("slide");
+    _content.addEventListener("touchstart", touchStart, false);
+    _content.addEventListener("touchmove", touchMove, false);
+    _content.addEventListener("touchend", touchEnd, false);
+    function touchStart(event) {
+        var touch = event.targetTouches[0];
+        _start = touch.pageX;
+    }
+    function touchMove(event) {
+        var touch = event.targetTouches[0];
+        _end = (_start - touch.pageX);
+    }
+    function touchEnd(event) {
+        if (_end < -100) {
+            left();
+            _end=0;
+        }else if(_end > 100){
+            right();
+            _end=0;
+        }
+    }
+}
