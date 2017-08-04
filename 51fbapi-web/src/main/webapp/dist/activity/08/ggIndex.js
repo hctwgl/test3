0265ae40da7aa6497787df22045275b9570d1a19
zhuyunyun <yunyun.zhu@91ganlu.com>
2017-08-04 18:30:31 +0800

    fix ggIndex  js

--- 51fbapi-web/src/main/webapp/dist/activity/08/ggIndex.js
+++ 51fbapi-web/src/main/webapp/dist/activity/08/ggIndex.js
@@ -1,2 +1,2 @@
-"use strict";var vm=new Vue({el:"#ggIndex",data:{content:{}},created:function(){this.logData()},methods:{logData:function logData(){var self=this;$.ajax({type:"post",url:"/H5GGShare/initHomePage",data:{activityId:1},success:function success(data){self.content=eval("("+data+")").data,console.log(self.content)}})}}});
+"use strict";var vm=new Vue({el:"#ggIndex",data:{content:{}},created:function(){this.logData()},methods:{logData:function(){$.ajax({type:"get",url:"/H5GGShare/initHomePage",data:{activityId:1},success:function(t){console.log(t)}})}}});
 //# sourceMappingURL=../../_srcmap/activity/08/ggIndex.js.map
