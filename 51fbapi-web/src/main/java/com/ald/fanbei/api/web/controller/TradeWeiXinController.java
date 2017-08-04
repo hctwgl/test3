8fff658211de55fdb3d16332069c3596ee52bb76
shencheng <aladin>
2017-08-04 14:23:58 +0800

    商圈微信端接口

--- 51fbapi-web/src/main/java/com/ald/fanbei/api/web/controller/TradeWeiXinController.java
+++ 51fbapi-web/src/main/java/com/ald/fanbei/api/web/controller/TradeWeiXinController.java
@@ -31,7 +31,7 @@ import java.util.HashMap;
 @RequestMapping("/trade")
 public class TradeWeiXinController extends BaseController {
 
-    @RequestMapping(value = {"/login","/logout","/businessInfo","/twoBarCode","/getPayListDate", "/getPayList", "/getWithdrawListDate", "/getWithdrawList", "/getRefundListDate", "/getRefundList", "/withdraw", "/refund"},
+    @RequestMapping(value = {"/login","/logout","/getIndexData", "/businessInfo","/twoBarCode","/getPayListDate", "/getPayList", "/getWithdrawListDate", "/getWithdrawList", "/getRefundListDate", "/getRefundList", "/withdraw", "/refund"},
             method = RequestMethod.POST,
             produces = "application/json;charset=utf-8")
     @ResponseBody
