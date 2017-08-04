b49d5fe7ac7459fc0527423a6450f7b988ad876b
zhuyunyun <yunyun.zhu@91ganlu.com>
2017-08-04 14:30:46 +0800

    fix js

--- 51fbapi-web/src/main/webapp/build/activity/08/ggIndex.js
+++ 51fbapi-web/src/main/webapp/build/activity/08/ggIndex.js
@@ -1,3 +1,25 @@
-
-
-
+//获取数据
+let vm = new Vue({
+    el: '#ggIndex',
+    data: {
+        content: {}
+    },
+    created: function () {
+        this.logData();
+    },
+    methods: {
+        logData() {
+            //获取页面初始化信息
+            let self = this;
+            $.ajax({
+                type: 'post',
+                url: "/H5GGShare/initHomePage",
+                data:{activityId:1},
+                success: function (data) {
+                    self.content = eval('(' + data + ')').data;
+                    console.log(self.content);
+                }
+            })
+        }
+    }
+})
--- 51fbapi-web/src/main/webapp/dist/activity/08/ggIndex.js
+++ 51fbapi-web/src/main/webapp/dist/activity/08/ggIndex.js
@@ -1,2 +1,2 @@
-"use strict";
+"use strict";var vm=new Vue({el:"#ggIndex",data:{content:{}},created:function(){this.logData()},methods:{logData:function logData(){var self=this;$.ajax({type:"post",url:"/H5GGShare/initHomePage",data:{activityId:1},success:function success(data){self.content=eval("("+data+")").data,console.log(self.content)}})}}});
 //# sourceMappingURL=../../_srcmap/activity/08/ggIndex.js.map
