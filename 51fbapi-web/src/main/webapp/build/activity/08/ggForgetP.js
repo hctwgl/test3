a06510f4605e7b0962bf17da179bcbd035ada872
pengxiangyu <1344820123@qq.com>
2017-08-08 20:32:55 +0800

    fix gglogin interface completed

--- 51fbapi-web/src/main/webapp/build/activity/08/ggForgetP.js
+++ 51fbapi-web/src/main/webapp/build/activity/08/ggForgetP.js
@@ -1,27 +1,23 @@
     
 $(function(){
 
-        var user = JSON.parse(localStorage.getItem("user")); //获取到手机验证页的手机号
-        var paw = JSON.parse(localStorage.getItem("paw")); //获取到手机验证页的短信验证码
-
         //获取数据
         //点击完成跳转到手机验证
+        var password = $(".blank-in").val();//获取登录密码
         $(".btn").click(function (){
-                var password = $(".blank-in").val();//获取登录密码
+                
                 var password_md5 = String(CryptoJS.MD5(password));//md5加密
                 $.ajax({
                     url: "/H5GGShare/boluomeActivityResetPwd",
                     type: 'POST',
                     dataType: 'JSON',
                     data: {
-                        password: password_md5,
-                        mobile:user,
-                        verifyCode:paw
+                        password: password_md5
                     },
                     success: function (data) {
                         console.log(data)
                         if(data.success){
-                        window.location.href ="data.url";
+                        window.location.href ="http://localhost/fanbei-web/activity/gglogin";
                         }else{
                             requestMsg(data.msg);
                         }
--- 51fbapi-web/src/main/webapp/build/activity/08/gglogin.js
+++ 51fbapi-web/src/main/webapp/build/activity/08/gglogin.js
@@ -13,11 +13,13 @@
                     userName: userName,
                     password: password_md5,
                     activityId:	1,
+                    refUserName:17839218825
+
                 },
                 success: function (data) {
                     console.log(data)
                     if(data.success){
-                     window.location.href ="http://localhost/fanbei-web/activity/ggIndexShare";
+                     window.location.href ="ggIndexShare";
                     }else{
                         requestMsg(data.msg);
                     }
