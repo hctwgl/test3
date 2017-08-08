cf32b4b55b61788a1158fa7c38cc22ef1f69ee99
Administrator <yfjjob@163.com>
2017-08-08 17:36:05 +0800

    修改帮助中心

--- 51fbapi-web/src/main/webapp/build/js/app/help.js
+++ 51fbapi-web/src/main/webapp/build/js/app/help.js
@@ -10,11 +10,14 @@ $(function(){
     // 点击导航事件
     $(".nav li").on('click',function(e){
         e.preventDefault();
-        var i = $(this).index();
+        var k = $(this).index();
         $(this).addClass("current");
         $(this).siblings().removeClass("current");
         $(this).find('i').addClass("bg");
         $(this).siblings().find('i').removeClass("bg");
+
+        $(".main_wrap").find("ul").eq(k).removeClass("hide");
+        $(".main_wrap").find('ul').eq(k).siblings().addClass("hide");
     });
 
 });
