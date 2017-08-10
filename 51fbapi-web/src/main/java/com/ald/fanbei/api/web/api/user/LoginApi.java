f51f60fe0eca69602a51718705692b721a10e64a
caihuan <caihuan@edspay.com>
2017-08-10 18:19:57 +0800

    缓存 set key 为string类型

--- 51fbapi-web/src/main/java/com/ald/fanbei/api/web/api/user/LoginApi.java
+++ 51fbapi-web/src/main/java/com/ald/fanbei/api/web/api/user/LoginApi.java
@@ -164,7 +164,7 @@ public class LoginApi implements ApiHandle {
 		jo.put("allowConsume", afUserAuthService.getConsumeStatus(afUserDo.getRid(),context.getAppVersion()));
 		
 		//3.7.6 对于未结款的用户在登录后，结款按钮高亮显示
-		Boolean isBorrowed =  bizCacheUtil.isRedisSetValue(Constants.HAVE_BORROWED, afUserDo.getRid());
+		Boolean isBorrowed =  bizCacheUtil.isRedisSetValue(Constants.HAVE_BORROWED, String.valueOf(afUserDo.getRid()));
 		if(Boolean.TRUE.equals(isBorrowed)){
 			jo.put("borrowed", "Y");
 		}else{
