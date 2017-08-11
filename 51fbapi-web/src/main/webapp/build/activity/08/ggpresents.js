2de6a6bf6a23aec67790702efbb6129454afac9d
pengxiangyu <1344820123@qq.com>
2017-08-11 10:30:08 +0800

    fix

--- 51fbapi-web/src/main/webapp/build/activity/08/ggpresents.js
+++ 51fbapi-web/src/main/webapp/build/activity/08/ggpresents.js
@@ -30,6 +30,7 @@ $(function () {
                 var pic = ""; //banner图片
                 var friend = ""; //赠送者名字
                 var join = ""; //参与人数
+                var combo="";//e顿健康套餐
                 join += "<span class='join'>" + data.data.fakeJoin + "</span>";
                 $('.join').html(join);
                 friend += '<i class="friend">' + data.data.friend + '</i>';
@@ -38,6 +39,8 @@ $(function () {
                 $('.banner').html(pic);
                 light += '<span class="light">' + data.data.fakeFinal + '</span>';
                 $('.light').html(light);
+                combo+='<span class="combo">'+data.data.itemsDo.name+'</span>';
+                $('.combo').html(combo);
 
             } else {
                 requestMsg(data.msg);
@@ -45,6 +48,7 @@ $(function () {
 
         }
     })
+    
     ScrollImgLeft();
     //文字轮播
     function ScrollImgLeft() {
