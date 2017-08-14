b423433a0961a9e7a63c2bd823fa7b8e8f01d9d4
unknown <hongzhengpei@Alamap.com>
2017-08-14 22:02:20 +0800

    修改h5接口路由

--- 51fbapi-web/src/main/java/com/ald/fanbei/api/web/apph5/controller/AppH5RecommendUserController.java
+++ 51fbapi-web/src/main/java/com/ald/fanbei/api/web/apph5/controller/AppH5RecommendUserController.java
@@ -36,7 +36,7 @@ import java.util.List;
  * @注意：本内容仅限于杭州阿拉丁信息科技股份有限公司内部传阅，禁止外泄以及用于其他的商业目的
  */
 @Controller
-@RequestMapping("/app/recommenduser/")
+@RequestMapping("/fanbei-web/")
 public class AppH5RecommendUserController extends BaseController {
 
     @Resource
