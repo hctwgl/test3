68561e7d6aa724fd129ec64f420c5465e5c6e614
chengkang <chengkang@edspay.com>
2017-08-15 16:59:17 +0800

    逾期通知催收平台条件修改（时间判断，不用逾期状态判断）

--- 51fbapi-biz/src/main/java/com/ald/fanbei/api/biz/service/impl/AfRepaymentBorrowCashServiceImpl.java
+++ 51fbapi-biz/src/main/java/com/ald/fanbei/api/biz/service/impl/AfRepaymentBorrowCashServiceImpl.java
@@ -364,7 +364,7 @@ public class AfRepaymentBorrowCashServiceImpl extends BaseService implements AfR
 					//add by chengkang 待添加还款成功短信 end
 					
 					//会对逾期的借款还款，向催收平台同步还款信息
-					if(YesNoStatus.YES.getCode().equals(afBorrowCashDo.getOverdueStatus())){
+					if(DateUtil.compareDate(new Date(), afBorrowCashDo.getGmtPlanRepayment())){
 						try {
 							CollectionSystemReqRespBo respInfo = collectionSystemUtil.consumerRepayment(repayment.getRepayNo(), afBorrowCashDo.getBorrowNo(), repayment.getCardNumber(), repayment.getCardName(),DateUtil.formatDateTime(new Date()),tradeNo,repayment.getRepaymentAmount(), (afBorrowCashDo.getAmount().add(bcashDo.getRateAmount().add(bcashDo.getOverdueAmount().add(bcashDo.getSumRate().add(bcashDo.getSumOverdue())))).subtract(bcashDo.getRepayAmount()).setScale(2, RoundingMode.HALF_UP)), (afBorrowCashDo.getAmount().add(bcashDo.getRateAmount().add(bcashDo.getOverdueAmount().add(bcashDo.getSumRate().add(bcashDo.getSumOverdue())))).setScale(2, RoundingMode.HALF_UP)), bcashDo.getOverdueAmount(), bcashDo.getRepayAmount(),bcashDo.getRateAmount());
 							logger.info("collection consumerRepayment req success, respinfo={}",respInfo);
