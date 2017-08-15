cea1ce66b8c63d5e3240aa4d2367e694ac5be1c5
Administrator <yfjjob@163.com>
2017-08-15 16:53:32 +0800

    新增上月排行榜网络请求

--- 51fbapi-web/src/main/webapp/dist/js/app/inviteLastWinRank.js
+++ 51fbapi-web/src/main/webapp/dist/js/app/inviteLastWinRank.js
@@ -1,2 +1,2 @@
-"use strict";var finished=0,vm=new Vue({el:"#inviteLastWinRank",data:{returnData:[]},created:function(){this.initial()},methods:{initial:function(){var t=this;$.ajax({url:"/fanbei-web/getPrizeByLastMonth",dataType:"json",type:"post",success:function(e){console.log(e),t.returnData=e},error:function(){requestMsg("请求失败")}})}}});
+"use strict";var finished=0,vm=new Vue({el:"#inviteLastWinRank",data:{returnData:[]},created:function(){this.initial()},methods:{initial:function(){var t=this;$.ajax({url:"/fanbei-web/getPrizeByLastMonth",dataType:"json",type:"post",success:function(e){console.debug(e),t.returnData=e},error:function(){requestMsg("请求失败")}})}}});
 //# sourceMappingURL=../../_srcmap/js/app/inviteLastWinRank.js.map
