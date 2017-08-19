9bbd66e11ab919345865982345badcf9b3aa59eb
unknown <hongzhengpei@Alamap.com>
2017-08-19 14:58:14 +0800

    修改BANNER 只对借钱有用

--- 51fbapi-web/src/main/java/com/ald/fanbei/api/web/api/borrowCash/GetBowCashLogInInfoApi.java
+++ 51fbapi-web/src/main/java/com/ald/fanbei/api/web/api/borrowCash/GetBowCashLogInInfoApi.java
@@ -292,6 +292,18 @@ public class GetBowCashLogInInfoApi extends GetBorrowCashBase implements ApiHand
 			scrollbarVo = getAfScrollbarVo(resourceDo);
 		}else{
 			bannerResultList = bannerList;
+
+			List<AfResourceDo> recommend_imgs = afRecommendUserService.getActivieResourceByType("RECOMMEND_IMG");//获取活动图片
+			if(recommend_imgs != null && recommend_imgs.size()>0) {
+				for (AfResourceDo afResourceDo : recommend_imgs) {
+					Map<String, Object> map = new HashMap();
+					map.put("imageUrl",afResourceDo.getValue()+"?name=RECOMMEND_IMG" );
+					map.put("titleName",afResourceDo.getName());
+					map.put("type","RECOMMEND_IMG");
+					bannerResultList.add(0,map);
+				}
+			}
+
 		}
 
 		/*List<Object> _bannerResultList = new ArrayList<>();
