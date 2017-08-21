9af22068e2063521af6b60ab5663fee9ab55de26
ALAMAP\renchunlei <renchunlei@edspay.com>
2017-08-21 18:10:51 +0800

    物流功能开发

--- 51fbapi-biz/src/main/java/com/ald/fanbei/api/biz/service/impl/AfOrderLogisticsServiceImpl.java
+++ 51fbapi-biz/src/main/java/com/ald/fanbei/api/biz/service/impl/AfOrderLogisticsServiceImpl.java
@@ -2,6 +2,9 @@ package com.ald.fanbei.api.biz.service.impl;
 
 import javax.annotation.Resource;
 
+import com.ald.fanbei.api.biz.bo.AfOrderLogisticsBo;
+import com.ald.fanbei.api.common.kdniao.KdniaoReqDataDataTraces;
+import com.alibaba.fastjson.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -10,6 +13,8 @@ import com.ald.fanbei.api.dal.dao.AfOrderLogisticsDao;
 import com.ald.fanbei.api.dal.domain.AfOrderLogisticsDo;
 import com.ald.fanbei.api.biz.service.AfOrderLogisticsService;
 
+import java.util.Date;
+import java.util.List;
 
 
 /**
@@ -37,5 +42,64 @@ public class AfOrderLogisticsServiceImpl extends ParentServiceImpl<AfOrderLogist
     @Override
     public AfOrderLogisticsDo getByOrderId(Long orderId) {
         return afOrderLogisticsDao.getByOrderId(orderId);
+    }
+
+    @Override
+    public AfOrderLogisticsBo getOrderLogisticsBo(long orderId, long isOutTraces) {
+        AfOrderLogisticsDo afOrderLogisticsDo = getByOrderId(orderId);
+        AfOrderLogisticsBo afOrderLogisticsBo=new AfOrderLogisticsBo();
+        if (afOrderLogisticsDo != null) {
+            afOrderLogisticsBo.setStateDesc(convertState(afOrderLogisticsDo.getState()));
+            afOrderLogisticsBo.setShipperName(afOrderLogisticsDo.getShipperName());
+            afOrderLogisticsBo.setShipperCode(afOrderLogisticsDo.getLogisticCode());
+            List<KdniaoReqDataDataTraces> traces= JSONObject.parseArray( afOrderLogisticsDo.getTraces(), KdniaoReqDataDataTraces.class);
+            if(traces.size()>0){
+                afOrderLogisticsBo.setNewestInfo(traces.get(traces.size()-1));
+            }else{
+                //如果没有就虚拟一条空的轨迹
+                KdniaoReqDataDataTraces  empty=new KdniaoReqDataDataTraces();
+                empty.setAcceptTime(new Date());
+                empty.setAcceptStation("暂无物流轨迹");
+                afOrderLogisticsBo.setNewestInfo(empty);
+            }
+            if(isOutTraces>0){
+                afOrderLogisticsBo.setTracesInfo(traces);
+            }
+            return afOrderLogisticsBo;
+        }else{
+            return null;
+        }
+    }
+    /**
+     * 枚举转换
+     * @param state 状态值
+     * @return 描述
+     */
+    String convertState(int state) {
+        String stateDesc = "";
+        switch (state) {
+            case 0:
+                stateDesc = "无轨迹";
+                break;
+            case 1:
+                stateDesc = "已揽收";
+                break;
+            case 2:
+                stateDesc = "在途中";
+                break;
+            case 201:
+                stateDesc = "到达派件城市";
+                break;
+            case 3:
+                stateDesc = "签收";
+                break;
+            case 4:
+                stateDesc = "问题件";
+                break;
+            default:
+                stateDesc = "未识别的物流状态";
+                break;
+        }
+        return stateDesc;
     }
 }
--- 51fbapi-web/src/main/java/com/ald/fanbei/api/web/api/order/GetOrderLogisticsApi.java
+++ 51fbapi-web/src/main/java/com/ald/fanbei/api/web/api/order/GetOrderLogisticsApi.java
@@ -1,5 +1,6 @@
 package com.ald.fanbei.api.web.api.order;
 
+import com.ald.fanbei.api.biz.bo.AfOrderLogisticsBo;
 import com.ald.fanbei.api.biz.service.AfOrderLogisticsService;
 import com.ald.fanbei.api.common.FanbeiContext;
 import com.ald.fanbei.api.common.exception.FanbeiExceptionCode;
@@ -9,6 +10,7 @@ import com.ald.fanbei.api.dal.domain.AfOrderLogisticsDo;
 import com.ald.fanbei.api.web.common.ApiHandle;
 import com.ald.fanbei.api.web.common.ApiHandleResponse;
 import com.ald.fanbei.api.web.common.RequestDataVo;
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.taobao.api.response.OpenimUserserviceGetResponse;
 import org.apache.commons.lang.BooleanUtils;
@@ -39,63 +41,19 @@ public class GetOrderLogisticsApi implements ApiHandle {
             Map<String, Object> params = requestDataVo.getParams();
             long orderId = NumberUtil.strToLong(params.get("orderId").toString());
             long isOutTraces = NumberUtil.strToLong(params.get("traces")==null?String.valueOf(0) :params.get("traces").toString());
-            AfOrderLogisticsDo afOrderLogisticsDo = afOrderLogisticsService.getByOrderId(orderId);
-            if (afOrderLogisticsDo != null) {
-                resp.addResponseData("stateDesc", convertState(afOrderLogisticsDo.getState()));
-                resp.addResponseData("shipperName",afOrderLogisticsDo.getShipperName());
-                List<KdniaoReqDataDataTraces> traces= JSONObject.parseArray( afOrderLogisticsDo.getTraces(), KdniaoReqDataDataTraces.class);
-
-                if(traces.size()>0){
-                    resp.addResponseData("newestInfo",traces.get(traces.size()-1));
-                }else{
-                    //如果没有就虚拟一条空的轨迹
-                    KdniaoReqDataDataTraces  empty=new KdniaoReqDataDataTraces();
-                    empty.setAcceptTime(new Date());
-                    empty.setAcceptStation("暂无物流轨迹");
-                    resp.addResponseData("newestInfo",empty);
-                }
-                if(isOutTraces>0){
-                    resp.addResponseData("tracesInfo",traces);
-                }
+            AfOrderLogisticsBo afOrderLogisticsBo= afOrderLogisticsService.getOrderLogisticsBo(orderId,isOutTraces);
+            if(afOrderLogisticsBo==null){
+                resp = new ApiHandleResponse(requestDataVo.getId(), FanbeiExceptionCode.LOGISTICS_NOT_EXIST);
+            }else{
+                resp.addResponseData("logistics",afOrderLogisticsBo);
             }
         } catch (Exception e) {
               logger.error("getOrderLogisticsApi :",e.toString());
               resp = new ApiHandleResponse(requestDataVo.getId(), FanbeiExceptionCode.FAILED);
         }
-
+         System.out.println(JSON.toJSONString(resp));
         return resp;
     }
 
-    /**
-     * 枚举转换
-     * @param state 状态值
-     * @return 描述
-     */
-    String convertState(int state) {
-        String stateDesc = "";
-        switch (state) {
-            case 0:
-                stateDesc = "无轨迹";
-                break;
-            case 1:
-                stateDesc = "已揽收";
-                break;
-            case 2:
-                stateDesc = "在途中";
-                break;
-            case 201:
-                stateDesc = "到达派件城市";
-                break;
-            case 3:
-                stateDesc = "签收";
-                break;
-            case 4:
-                stateDesc = "问题件";
-                break;
-            default:
-                stateDesc = "未识别的物流状态";
-                break;
-        }
-        return stateDesc;
-    }
+
 }
