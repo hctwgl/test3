1ef5a75cd042b9ead1b134abf785cfae71c28023
fanmanfu <fanmanfu@fanmanfu.Alamap.com>
2017-08-25 19:42:43 +0800

    format

--- 51fbapi-biz/src/main/java/com/ald/fanbei/api/biz/third/util/CollectionSystemUtil.java
+++ 51fbapi-biz/src/main/java/com/ald/fanbei/api/biz/third/util/CollectionSystemUtil.java
@@ -38,12 +38,12 @@ import com.alibaba.fastjson.JSONObject;
  */
 @Component("collectionSystemUtil")
 public class CollectionSystemUtil extends AbstractThird {
-	
+
 	private static String url = null;
 
 	@Resource
 	AfRepaymentBorrowCashService afRepaymentBorrowCashService;
-	
+
 	@Resource
 	CommitRecordUtil commitRecordUtil;
 
@@ -54,7 +54,7 @@ public class CollectionSystemUtil extends AbstractThird {
 		}
 		return url;
 	}
-	
+
 	/**
 	 * 51返呗主动还款通知催收平台
 	 * 
@@ -65,106 +65,107 @@ public class CollectionSystemUtil extends AbstractThird {
 	 * @param cardNumber
 	 *            --卡号
 	 * @param cardName
-	 *  	      --银行卡名称（支付方式）
+	 *            --银行卡名称（支付方式）
 	 * @param amount
-	 *  	      --还款金额
+	 *            --还款金额
 	 * @param restAmount
-	 *  	      --剩余未还金额
+	 *            --剩余未还金额
 	 * @param repayAmount
-	 *  	      --理论应还款金额
+	 *            --理论应还款金额
 	 * @param overdueAmount
-	 *  	      --逾期手续费
+	 *            --逾期手续费
 	 * @param repayAmountSum
-	 *  	      --已还总额
+	 *            --已还总额
 	 * @param rateAmount
-	 *  	      --利息
+	 *            --利息
 	 * @return
 	 */
-	public CollectionSystemReqRespBo consumerRepayment(String repayNo,String borrowNo,String cardNumber,String cardName,String repayTime,String tradeNo,BigDecimal amount,
-			BigDecimal restAmount,BigDecimal repayAmount,BigDecimal overdueAmount,BigDecimal repayAmountSum,
-			BigDecimal rateAmount) {
-		CollectionDataBo data=new CollectionDataBo();
-		Map<String,String> reqBo=new HashMap<String,String>();
+	public CollectionSystemReqRespBo consumerRepayment(String repayNo, String borrowNo, String cardNumber, String cardName, String repayTime, String tradeNo, BigDecimal amount, BigDecimal restAmount, BigDecimal repayAmount, BigDecimal overdueAmount, BigDecimal repayAmountSum, BigDecimal rateAmount) {
+		CollectionDataBo data = new CollectionDataBo();
+		Map<String, String> reqBo = new HashMap<String, String>();
 		reqBo.put("repay_no", repayNo);
 		reqBo.put("borrow_no", borrowNo);
 		reqBo.put("card_number", cardNumber);
 		reqBo.put("card_name", cardName);
 		reqBo.put("repay_time", repayTime);
 		reqBo.put("trade_no", tradeNo);
-		reqBo.put("amount", amount.multiply(BigDecimalUtil.ONE_HUNDRED)+"");
-		reqBo.put("rest_amount", restAmount.multiply(BigDecimalUtil.ONE_HUNDRED)+"");
-		reqBo.put("repay_amount", repayAmount.multiply(BigDecimalUtil.ONE_HUNDRED)+"");
-		reqBo.put("overdue_amount", overdueAmount.multiply(BigDecimalUtil.ONE_HUNDRED)+"");
-		reqBo.put("repay_amount_sum", repayAmountSum.multiply(BigDecimalUtil.ONE_HUNDRED)+"");
-		reqBo.put("rate_amount", rateAmount.multiply(BigDecimalUtil.ONE_HUNDRED)+"");
-		
+		reqBo.put("amount", amount.multiply(BigDecimalUtil.ONE_HUNDRED) + "");
+		reqBo.put("rest_amount", restAmount.multiply(BigDecimalUtil.ONE_HUNDRED) + "");
+		reqBo.put("repay_amount", repayAmount.multiply(BigDecimalUtil.ONE_HUNDRED) + "");
+		reqBo.put("overdue_amount", overdueAmount.multiply(BigDecimalUtil.ONE_HUNDRED) + "");
+		reqBo.put("repay_amount_sum", repayAmountSum.multiply(BigDecimalUtil.ONE_HUNDRED) + "");
+		reqBo.put("rate_amount", rateAmount.multiply(BigDecimalUtil.ONE_HUNDRED) + "");
+
 		String json = JsonUtil.toJSONString(reqBo);
-		data.setData(json);//数据集合
+		data.setData(json);// 数据集合
 		data.setSign(DigestUtil.MD5(json));
 		String timestamp = DateUtil.getDateTimeFullAll(new Date());
 		data.setTimestamp(timestamp);
-		try{
+		try {
 			String reqResult = HttpUtil.post(getUrl() + "/api/getway/repayment/repaymentAchieve", data);
 			if (StringUtil.isBlank(reqResult)) {
 				throw new FanbeiException("consumerRepayment fail , reqResult is null");
-			}else{
-				logger.info("consumerRepayment req success,reqResult"+reqResult);
+			} else {
+				logger.info("consumerRepayment req success,reqResult" + reqResult);
 			}
 			CollectionSystemReqRespBo respInfo = JSONObject.parseObject(reqResult, CollectionSystemReqRespBo.class);
 			if (respInfo != null && StringUtil.equals("200", respInfo.getCode())) {
 				return respInfo;
 			} else {
-				throw new FanbeiException("renewalNotify fail , respInfo info is "+JSONObject.toJSONString(respInfo));
+				throw new FanbeiException("renewalNotify fail , respInfo info is " + JSONObject.toJSONString(respInfo));
 			}
-		}catch(Exception e){
-			commitRecordUtil.addRecord("repayment", borrowNo, json, getUrl()+"/api/getway/repayment/repaymentAchieve");
-			throw new FanbeiException("consumerRepayment fail Exception is "+e+",consumerRepayment send again");
+		} catch (Exception e) {
+			commitRecordUtil.addRecord("repaymentCollection", borrowNo, json, getUrl() + "/api/getway/repayment/repaymentAchieve");
+			throw new FanbeiException("consumerRepayment fail Exception is " + e + ",consumerRepayment send again");
 		}
 	}
-
 
 	/**
 	 * 51返呗续期通知接口
-	 * @param borrow_no  	借款单号
-	 * @param renewal_no  	续期编号
-	 * @param renewal_num	续借期数
-	 * @return 
+	 * 
+	 * @param borrow_no
+	 *            借款单号
+	 * @param renewal_no
+	 *            续期编号
+	 * @param renewal_num
+	 *            续借期数
+	 * @return
 	 * 
 	 * **/
-	public CollectionSystemReqRespBo renewalNotify(String borrowNo, String renewalNo, Integer renewalNum,String renewalAmount){
-		
-		CollectionDataBo data=new CollectionDataBo();
-		Map<String,String> reqBo=new HashMap<String,String>();
+	public CollectionSystemReqRespBo renewalNotify(String borrowNo, String renewalNo, Integer renewalNum, String renewalAmount) {
+
+		CollectionDataBo data = new CollectionDataBo();
+		Map<String, String> reqBo = new HashMap<String, String>();
 		reqBo.put("borrow_no", borrowNo);
 		reqBo.put("renewal_no", renewalNo);
-		reqBo.put("renewal_num", renewalNum+"");
+		reqBo.put("renewal_num", renewalNum + "");
 		reqBo.put("renewal_amount", renewalAmount);
-	
+
 		String json = JsonUtil.toJSONString(reqBo);
-		data.setData(json);//数据集合
+		data.setData(json);// 数据集合
 		data.setSign(DigestUtil.MD5(json));
 		String timestamp = DateUtil.getDateTimeFullAll(new Date());
 		data.setTimestamp(timestamp);
-		try{
-			String reqResult = HttpUtil.post(getUrl() + "/api/getway/repayment/renewalAchieve", data);	
+		try {
+			String reqResult = HttpUtil.post(getUrl() + "/api/getway/repayment/renewalAchieve", data);
 			if (StringUtil.isBlank(reqResult)) {
 				throw new FanbeiException("renewalNotify fail , reqResult is null");
-			}else{
-				logger.info("renewalNotify req success,reqResult"+reqResult);
+			} else {
+				logger.info("renewalNotify req success,reqResult" + reqResult);
 			}
 			CollectionSystemReqRespBo respInfo = JSONObject.parseObject(reqResult, CollectionSystemReqRespBo.class);
 			if (respInfo != null && StringUtil.equals("200", respInfo.getCode())) {
 				return respInfo;
 			} else {
-				throw new FanbeiException("renewalNotify fail , respInfo info is "+JSONObject.toJSONString(respInfo));
+				throw new FanbeiException("renewalNotify fail , respInfo info is " + JSONObject.toJSONString(respInfo));
 			}
-		}catch(Exception e){
-			commitRecordUtil.addRecord("renewal", borrowNo, json, getUrl()+"/api/getway/repayment/renewalAchieve");
-			throw new FanbeiException("renewalNotify fail Exception is "+e+",renewalNotify send again");
+		} catch (Exception e) {
+			commitRecordUtil.addRecord("renewalCollection", borrowNo, json, getUrl() + "/api/getway/repayment/renewalAchieve");
+			throw new FanbeiException("renewalNotify fail Exception is " + e + ",renewalNotify send again");
 		}
 
 	}
-	
+
 	public CollectionOperatorNotifyRespBo offlineRepaymentNotify(String timestamp, String data, String sign) {
 		// 响应数据,默认成功
 		CollectionOperatorNotifyRespBo notifyRespBo = new CollectionOperatorNotifyRespBo(FanbeiThirdRespCode.SUCCESS);
