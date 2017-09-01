07d3d2550ef720f55bcc61b72a0f29adec8453c0
pengxiangyu <1344820123@qq.com>
2017-09-01 17:45:00 +0800

    fix js

--- 51fbapi-web/src/main/webapp/build/activity/08/billion.js
+++ 51fbapi-web/src/main/webapp/build/activity/08/billion.js
@@ -1,3 +1,49 @@
-$(function(){
-    
-})
+var vm=new Vue({
+    el: '#billion',
+    data: {
+        returnData: []
+    },
+    created:function(){
+        let _this=this;
+        _this.initial();
+        // loading();
+    },
+    methods:{
+        initial(){
+            let _this=this;
+            $.ajax({
+                url: '/app/activity/borrowCashActivities',
+                dataType:'json',
+                type: 'post',
+                success:function (data) {
+                    console.log(data);
+                    // _this.returnData=data.data;
+                }
+            })
+            ScrollImgLeft();
+            //文字轮播
+            function ScrollImgLeft() {
+                var speed = 50;
+                var MyMar = null;
+                var scroll_begin = document.getElementById("scroll_begin");
+                var scroll_end = document.getElementById("scroll_end");
+                var scroll_div = document.getElementById("scroll_div");
+                scroll_end.innerHTML = scroll_begin.innerHTML;
+
+                function Marquee() {
+                    if (scroll_end.offsetWidth - scroll_div.scrollLeft <= 0)
+                        scroll_div.scrollLeft -= scroll_begin.offsetWidth;
+                    else
+                        scroll_div.scrollLeft++;
+                }
+                MyMar = setInterval(Marquee, speed);
+                scroll_div.onmouseover = function () {　　　　　　　
+                    clearInterval(MyMar);　　　　　
+                }
+                scroll_div.onmouseout = function () {　　　　　　　
+                    MyMar = setInterval(Marquee, speed);　　　　　　　　　
+                }
+            }
+        }, 
+    }
+});
