b11397d745018ea990e94cadf47e73dcaed4e9b8
pengxiangyu <1344820123@qq.com>
2017-09-03 18:09:49 +0800

    fix billion css js

--- 51fbapi-web/src/main/webapp/build/activity/08/billion.js
+++ 51fbapi-web/src/main/webapp/build/activity/08/billion.js
@@ -1,35 +1,35 @@
-var vm=new Vue({
+var vm = new Vue({
     el: '#billion',
     data: {
-        returnData: []
+        returnData: [],
+        num: ''
     },
-    created:function(){
-        let _this=this;
+    created: function () {
+        let _this = this;
         _this.initial();
         // loading();
     },
-    methods:{
-        initial(){
-            let _this=this;
+    methods: {
+        initial() {
+            let _this = this;
             $.ajax({
                 url: '/app/activity/borrowCashActivities',
-                dataType:'json',
+                dataType: 'json',
                 type: 'post',
-                success:function (data) {
+                success: function (data) {
                     console.log(data);
-                    var str=data.data;//获取返回的破十五亿金额
+                    var str = data.data; //获取返回的破十五亿金额
                     console.log(str);
-                    var s = str;//将字符串转换成数组
-                    var num = s.split("");// 在每个逗号(,)处进行分解。
-                    console.log(num);
-                    for(var i=0;i<num.length;i++){//对返回的狂送十亿金额进行遍历
-                        var index=num.index;
-                        num[i]=index;
-                        if(num[i]=='.'){
-                            num[i]=="";
-                        }
+                    str = str.replace(/"/g, ""); //替换掉返回字符串中的引号
+                    str = str.replace(/\./g, ""); //替换掉返回字符串中的点
+                    _this.num = str.split(""); //将字符串转化成数组
+                    console.log(_this.num);
+
+                    var num = document.getElementsByClassName("num"); //获取十五亿的总金额长度
+                    if (num.length >= 10) {
+                        element.src = "$commonUrl/billion_07.gif"; //当到达十亿金额的时候img替换成另一张图片
                     }
-                    
+
                     ScrollImgLeft();
                     //文字轮播
                     function ScrollImgLeft() {
@@ -57,7 +57,57 @@ var vm=new Vue({
 
                 }
             })
+        },
+        //点击立即借钱
+        goBorrowMoney() {
+            $.ajax({
+                url: '/fanbei-web/tearRiskPacketActivity',
+                dataType: 'json',
+                type: 'post',
+                success: function (data) {
+                    console.log(data);
+                    console.log(data.data.status);
+                    if (data.data.loginUrl) {
+                        location.href = data.data.loginUrl;
+                    } else {
+                        var appVersion = getInfo().appVersion.replace(/\./g, "");
+                        var status = data.data.status;
+                        var idNumber = data.data.idNumber;
+                        var realName = data.data.realName;
+ 
+                        if (appVersion <= "374") {
+                            window.location.href = '/fanbei-web/opennative?name=BORROW_MONEY'; // 老版本全部跳借钱
+                        } else {
+                            if (status == 'A1') {
+                                window.location.href = '/fanbei-web/opennative?name=DO_SCAN_ID'; // 去扫描身份证、人脸识别
+                            } else if (status == 'A2') {
+                                window.location.href = '/fanbei-web/opennative?name=DO_BIND_CARD&params={"idNumber":"' + idNumber + '","realName":"' + realName + '"}'; // 去绑定银行卡
+                            } else if (status == 'A3' || status == 'A4') {
+                                window.location.href = '/fanbei-web/opennative?name=DO_PROMOTE_BASIC'; // 去提升信用基础认证
+                            } else if (status == 'B' || status == 'D') {
+                                window.location.href = '/fanbei-web/opennative?name=BORROW_MONEY'; // 去借钱、进入借贷超市页面
+                            } else if (status == 'C') {
+                                window.location.href = '/fanbei-web/opennative?name=DO_PROMOTE_EXTRA'; // 去提升信用补充认证
+                            }
+                        } 
+                    }
+                },
+                error: function () {
+                    requestMsg("请求失败");
+                }
+            });
+        },
+        //点击查看更多弹出弹窗
+        list() {
+            $('.mask').show();
+            $(".alertRule").show();
+        },
+        //点击遮罩和叉叉隐藏
+        noShow() {
+            $('.mask').hide();
+            $(".alertRule").hide();
+        }
 
-        }, 
     }
+
 });
