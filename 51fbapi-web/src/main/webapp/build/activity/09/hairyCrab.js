af1cb54e88db5b818ed46395a13a122055327dfd
zhuyunyun <1090601532@qq.com>
2017-09-15 17:20:46 +0800

    fix 秋月蟹礼

---
+++ 51fbapi-web/src/main/webapp/build/activity/09/hairyCrab.js
@@ -0,0 +1,112 @@
+/*图片预加载*/
+$(".first").each(function() {
+    var img = $(this);
+    img.load(function () {
+        $(".loadingMask").fadeOut();
+    });
+    setTimeout(function () {
+        $(".loadingMask").fadeOut();
+    },1000)
+});
+let activityId=getUrl('activityId');//获取活动id
+let groupId=getUrl('groupId');//获取优惠券组id
+//获取数据
+let vm = new Vue({
+    el: '#hairyCrab',
+    data: {
+        content: {},
+        couponCont:{}
+    },
+    created: function () {
+        this.logData();
+        this.logCoupon();
+    },
+    methods: {
+        //获取页面初始化信息
+        logData() {
+            let self = this;
+            $.ajax({
+                type: 'post',
+                url: "/fanbei-web/newEncoreActivityInfo",
+                data:{'activityId':activityId},
+                success: function (data) {
+                    self.content = eval('(' + data + ')').data;
+                    console.log(self.content);
+                    self.$nextTick(function () {
+                        // lazy.init();
+                        $(".loadingMask").fadeOut();
+                        $("img.lazy").lazyload({
+                            placeholder : "http://f.51fanbei.com/h5/common/images/bitmap1.png",  //用图片提前占位
+                            effect : "fadeIn",  // 载入使用的效果
+                            threshold: 200 // 提前开始加载
+                        });
+                    })
+                },
+                error:function(){
+                    requestMsg('哎呀，出错了！')
+                }
+            })
+        },
+        //获取优惠券初始化信息
+        logCoupon() {
+            let self = this;
+            $.ajax({
+                type: 'post',
+                url: "/fanbei-web/activityCouponInfo",
+                data:{'groupId':groupId},
+                success: function (data) {
+                    self.couponCont = eval('(' + data + ')').data.couponInfoList[0];
+                    console.log(self.couponCont.couponId);
+                },
+                error: function () {
+                    requestMsg("哎呀，出错了！");
+                }
+            });
+        },
+        //点击领券
+        couponClick:function() {
+            let self = this;
+            let couponId = self.couponCont.couponId;
+            $.ajax({
+                url: "/fanbei-web/pickCoupon",
+                type: "POST",
+                dataType: "JSON",
+                data: {
+                    couponId: couponId
+                },
+                success: function (returnData) {
+                    if (returnData.success) {
+                        requestMsg("优惠劵领取成功");
+                    } else {
+                        var status = returnData.data["status"];
+                        if (status == "USER_NOT_EXIST") { // 用户不存在
+                            window.location.href = returnData.url;
+                        }
+                        if (status == "OVER") { // 优惠券个数超过最大领券个数
+                            requestMsg(returnData.msg);
+                            requestMsg("优惠券个数超过最大领券个数");
+                        }
+                        if (status == "COUPON_NOT_EXIST") { // 优惠券不存在
+                            requestMsg(returnData.msg);
+                        }
+                        if (status == "MORE_THAN") { // 优惠券已领取完
+                            requestMsg(returnData.msg);
+                        }
+                    }
+                },
+                error: function () {
+                    requestMsg("哎呀，出错了！");
+                }
+            })
+        },
+        //点击商品
+        goodClick:function(item){
+            if ( item.source=="SELFSUPPORT" ) {
+                window.location.href='/fanbei-web/opennative?name=GOODS_DETAIL_INFO&params={"privateGoodsId":"'+item.goodsId+'"}'
+            } else {
+                window.location.href='/fanbei-web/opennative?name=GOODS_DETAIL_INFO&params={"goodsId":"'+item.goodsId+'"}'
+            }
+        }
+    }
+});
+
