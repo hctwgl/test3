b7f82988a5c090160b8898cf54494a265051c86c
pengxiangyu <1344820123@qq.com>
2017-09-20 12:11:52 +0800

    creat godness js and vm

--- 51fbapi-web/src/main/webapp/build/activity/09/godness.js
+++ 51fbapi-web/src/main/webapp/build/activity/09/godness.js
@@ -3,39 +3,27 @@
  */
 let activityId = getUrl("activityId");
 let vue=new Vue({
-    el:'#vueCon',
+    el:'#godness',
     data:{
         content:[],
-        tableContent:[]
+        tableContent:[],
+        message:{},
     },
     created:function () {
-        this.logData();
+         this.init();
     },
     methods: {
-        getCoupon:function (data,type) {
+        init(){
             let self=this;
-            let url='pickBoluomeCoupon';
-            let postData={"sceneId":data,"userName":self.content.userName};
-            if(type=='1'){
-                url='pickCoupon';
-                postData={couponId:data};
-            }
             $.ajax({
-                url:"/fanbei-web/"+url,
+                url:"/fanbei-web/newEncoreActivityInfo",
                 type:'post',
-                data:postData,
-                success:function (res) {
-                    res=eval('(' + res+ ')');
-
-                    if(res.success==false){
-                        if(res.url&&res.url!==""){
-                            window.location.href=res.url
-                        }else{
-                            requestMsg(res.msg)
-                        }
-                    }else{
-                        requestMsg(res.msg)
-                    }
+                data:{'activityId':activityId},
+                success:function (data) {
+                   var del=eval('(' + data+ ')');
+                   self.message=del.data;
+                    console.log(self.message);
+                   
                 }
             });
         },
@@ -46,67 +34,6 @@ let vue=new Vue({
             }else{
                 window.location.href=self.tableContent.notifyUrl+'&params={"goodsId":"'+item.goodsId+'"}'
             }
-        },
-        txtFix(i){
-            function cut_str(str,len){
-                var char_length = 0;
-                if(str.length<=len){
-                    return str
-                }else{
-                    for (var i = 0; i < str.length; i++){
-                        var son_str = str.charAt(i);
-                        encodeURI(son_str).length > 2 ? char_length += 1 : char_length += 0.5;
-                        if (char_length >= len){
-                            var sub_len = char_length == len ? i+1 : i;
-                            return str.substr(0, sub_len);
-                            break;
-                        }
-                    }
-                }
-            }
-            return cut_str(i, 20)
-        },
-        logData:function(){
-            let self=this;
-            $.ajax({
-                url:'/fanbei-web/encoreActivityInfo',
-                type:'post',
-                data:{'activityId':activityId},
-                success:function (data) {
-                    self.tableContent = eval('(' + data + ')');
-                    self.tableContent = self.tableContent.data;
-                }
-            });
-            $.ajax({
-                url:'/fanbei-web/newUserGift',
-                type:'post',
-                success:function (data) {
-                    self.content = eval('(' + data + ')');
-                    self.content = self.content.data;
-                    console.log(self.content);
-                }
-            });
-        },
-        oneKeyReceive(){
-            $.ajax({
-                url:'/fanbei-web/pickSysAndBoluomeCoupon',
-                type:'post',
-                success:function (returnData) {
-                    let data=eval('(' + returnData + ')');
-                    if(data.success){
-                        requestMsg("领劵成功");
-                    }else{
-                        if(data.url){
-                            location.href=data.url;
-                        }else{
-                            requestMsg(data.msg);
-                        }
-                    }
-                },
-                error: function(){
-                    requestMsg("请求失败");
-                }
-            });
         }
     }
 });
