ef83648120261bc3a7c2a2abce6ef06407f16107
chengkang <chengkang@edspay.com>
2017-09-22 15:22:04 +0800

    打日志导致的空指针修复，避免后续业务添加时导致问题

--- 51fbapi-biz/src/main/java/com/ald/fanbei/api/biz/service/impl/AfOrderServiceImpl.java
+++ 51fbapi-biz/src/main/java/com/ald/fanbei/api/biz/service/impl/AfOrderServiceImpl.java
@@ -1343,7 +1343,7 @@ public class AfOrderServiceImpl extends BaseService implements AfOrderService{
 			boluomeUtil.pushPayStatus(orderInfo.getRid(), orderInfo.getOrderNo(), orderInfo.getThirdOrderNo(), PushStatus.PAY_SUC, orderInfo.getUserId(), orderInfo.getActualAmount());
 			//iPhone预约
 			AfGoodsDo goods = afGoodsService.getGoodsById(orderInfo.getGoodsId());
-			logger.info("iPhone8 reservationActivity" +goods.getRid());
+			logger.info("iPhone8 reservationActivity" +(goods!=null?goods.getRid():0L));
 			if(goods != null){
 				if(goods.getTags().equals("subscribe")){
 					AfUserDo afUserDo = afUserService.getUserById(orderInfo.getUserId());
