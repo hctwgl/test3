3439bddcf398643b6d64881e3e936b16d221f552
朱江丰 <zhujiangfeng@edspay.com>
2017-09-25 11:22:34 +0800

    更改手机号-短信发送通用模块

---
+++ 51fbapi-web/src/main/java/com/ald/fanbei/api/web/api/user/ChangeMobileSyncConactsApi.java
@@ -0,0 +1,67 @@
+/**
+ * 
+ */
+package com.ald.fanbei.api.web.api.user;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang.ObjectUtils;
+import org.springframework.stereotype.Component;
+
+import com.ald.fanbei.api.biz.service.AfSmsRecordService;
+import com.ald.fanbei.api.biz.service.AfUserService;
+import com.ald.fanbei.api.biz.third.util.SmsUtil;
+import com.ald.fanbei.api.common.FanbeiContext;
+import com.ald.fanbei.api.common.enums.SmsType;
+import com.ald.fanbei.api.common.exception.FanbeiException;
+import com.ald.fanbei.api.common.exception.FanbeiExceptionCode;
+import com.ald.fanbei.api.common.util.StringUtil;
+import com.ald.fanbei.api.dal.domain.AfUserDo;
+import com.ald.fanbei.api.web.common.ApiHandle;
+import com.ald.fanbei.api.web.common.ApiHandleResponse;
+import com.ald.fanbei.api.web.common.RequestDataVo;
+
+/**
+ * @类描述：更改手机——新手机号入库&同步新手机号和通讯录到风控，原子操作
+ * @author zjf 2017年9月22日下午16:19:00
+ * @注意：本内容仅限于杭州阿拉丁信息科技股份有限公司内部传阅，禁止外泄以及用于其他的商业目的
+ */
+@Component("changeMobileSyncConactsApi")
+public class ChangeMobileSyncConactsApi implements ApiHandle {
+
+	@Resource
+	AfUserService afUserService;
+	@Resource
+	AfSmsRecordService afSmsRecordService;
+	
+	@Resource
+	SmsUtil smsUtil;
+	@Override
+	public ApiHandleResponse process(RequestDataVo requestDataVo, FanbeiContext context, HttpServletRequest request) {
+		  ApiHandleResponse resp = new ApiHandleResponse(requestDataVo.getId(),FanbeiExceptionCode.SUCCESS);
+			Long userId = context.getUserId();
+
+	        String verifyCode = ObjectUtils.toString(requestDataVo.getParams().get("verifyCode"));
+	        String mobile = ObjectUtils.toString(requestDataVo.getParams().get("mobile"));
+	        String userName = ObjectUtils.toString(requestDataVo.getSystem().get("userName"));
+	        if(StringUtil.isBlank(verifyCode) || StringUtil.isBlank(mobile)){
+	        	logger.error("changeMobile verifyCode or mobile is empty verifyCode = " + verifyCode + " mobile = " + mobile);
+	        	return new ApiHandleResponse(requestDataVo.getId(), FanbeiExceptionCode.PARAM_ERROR); 
+	        }
+	        AfUserDo userDo = afUserService.getUserByUserName(userName);
+	        //验证原手机验证码
+	        smsUtil.checkSmsByMobileAndType(userDo.getMobile(),verifyCode, SmsType.MOBILE_BIND);
+	        AfUserDo afUserDo = new AfUserDo();
+			afUserDo.setRid(userId);
+			afUserDo.setMobile(mobile);
+			afUserDo.setUserName(context.getUserName());
+			if (afUserService.updateUser(afUserDo) > 0) {
+				return resp;
+			}
+			
+			throw new FanbeiException("change Mobile  info failed", FanbeiExceptionCode.SYSTEM_ERROR);
+
+	}
+
+}
---
+++ 51fbapi-web/src/main/java/com/ald/fanbei/api/web/api/user/ChangeMobileVerifyApi.java
@@ -0,0 +1,13 @@
+package com.ald.fanbei.api.web.api.user;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @类描述：更改手机——验证支付密码/验证原手机/验证原手机和身份证
+ * @author zjf 2017年9月22日下午16:19:00
+ * @注意：本内容仅限于杭州阿拉丁信息科技股份有限公司内部传阅，禁止外泄以及用于其他的商业目的
+ */
+@Component("changeMobileVerifyApi")
+public class ChangeMobileVerifyApi {
+	
+}
