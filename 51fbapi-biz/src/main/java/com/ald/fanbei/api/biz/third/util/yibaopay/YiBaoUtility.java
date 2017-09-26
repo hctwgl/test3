329e768dabdf49dd85b9d7267b9fca48544450e0
hongzhengpei <805328895@qq.com>
2017-09-26 16:05:55 +0800

    修改测试环境支付宝支付为0.01

--- 51fbapi-biz/src/main/java/com/ald/fanbei/api/biz/third/util/yibaopay/YiBaoUtility.java
+++ 51fbapi-biz/src/main/java/com/ald/fanbei/api/biz/third/util/yibaopay/YiBaoUtility.java
@@ -83,12 +83,12 @@ public class YiBaoUtility {
         ret.put("merchantNo",merchantNo);
         ret.put("orderId",orderId);
 
-        if(baseUrl.contains("http://testapp.51fanbei.com")){
+//        if(baseUrl.contains("http://testapp.51fanbei.com")){
             ret.put("orderAmount","0.01");
-        }
-        else{
-            ret.put("orderAmount",String.valueOf( orderAmount));
-        }
+//        }
+//        else{
+//            ret.put("orderAmount",String.valueOf( orderAmount));
+//        }
         ret.put("redirectUrl",redirectUrl);
         ret.put("notifyUrl",notifyUrl);
         ret.put("goodsParamExt",goodsParamExt);
