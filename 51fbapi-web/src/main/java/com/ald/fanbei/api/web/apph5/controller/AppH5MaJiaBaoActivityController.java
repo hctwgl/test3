e5636a71e813b510baa57f064b2827a629072fe8
yuyue <yuyue@edspay.com>
2017-09-29 09:54:03 +0800

    a

---
+++ 51fbapi-web/src/main/java/com/ald/fanbei/api/web/apph5/controller/AppH5MaJiaBaoActivityController.java
@@ -0,0 +1,76 @@
+package com.ald.fanbei.api.web.apph5.controller;
+
+import com.ald.fanbei.api.biz.bo.BorrowRateBo;
+import com.ald.fanbei.api.biz.service.AfRecommendUserService;
+import com.ald.fanbei.api.biz.service.AfResourceService;
+import com.ald.fanbei.api.biz.util.BorrowRateBoUtil;
+import com.ald.fanbei.api.common.FanbeiContext;
+import com.ald.fanbei.api.common.FanbeiWebContext;
+import com.ald.fanbei.api.common.exception.FanbeiException;
+import com.ald.fanbei.api.common.exception.FanbeiExceptionCode;
+import com.ald.fanbei.api.dal.dao.*;
+import com.ald.fanbei.api.dal.domain.*;
+import com.ald.fanbei.api.web.common.BaseController;
+import com.ald.fanbei.api.web.common.BaseResponse;
+import com.ald.fanbei.api.web.common.H5CommonResponse;
+import com.ald.fanbei.api.web.common.RequestDataVo;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * @类描述：
+ * 推荐活动
+ * @author 洪正沛 2017年8月12日下午1:41:05
+ * @注意：本内容仅限于杭州阿拉丁信息科技股份有限公司内部传阅，禁止外泄以及用于其他的商业目的
+ */
+@Controller
+@RequestMapping("/fanbei-web/")
+public class AppH5MaJiaBaoActivityController extends BaseController {
+
+	@Resource
+	AfResourceService afResourceService;
+	
+    @RequestMapping(value = "getDiagram", method = RequestMethod.GET)
+    @ResponseBody
+	public String getDiagram() {
+		List<AfResourceDo> list = afResourceService.getResourceListByType("MJB_REGIST_IMAGE");
+		if (list == null || list.size() < 1) {
+			return null;
+		}
+		return list.get(0).getValue();
+	}
+	
+    @Override
+    public BaseResponse doProcess(RequestDataVo requestDataVo, FanbeiContext context, HttpServletRequest httpServletRequest) {
+        return null;
+    }
+
+    @Override
+    public RequestDataVo parseRequestData(String requestData, HttpServletRequest request) {
+    	return null;
+    }
+
+    @Override
+    public String checkCommonParam(String reqData, HttpServletRequest request, boolean isForQQ) {
+        return null;
+    }
+
+}
