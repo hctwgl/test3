0f0c41bb9090ba6e8ad1f2ee01608a04a2c2caff
朱江丰 <zhujiangfeng@edspay.com>
2017-10-09 10:52:07 +0800

    接口模板

--- 51fbapi-biz/src/main/java/com/ald/fanbei/api/biz/service/redpacket/RedpacketPool.java
+++ 51fbapi-biz/src/main/java/com/ald/fanbei/api/biz/service/redpacket/RedpacketPool.java
@@ -1,50 +1,41 @@
 package com.ald.fanbei.api.biz.service.redpacket;
 
-import java.util.concurrent.ArrayBlockingQueue;
-
-import javax.annotation.PostConstruct;
+import java.util.concurrent.BlockingQueue;
 
 import org.springframework.stereotype.Component;
 
 @Component
-class RedpacketPool {
+interface RedpacketPool {
 	
 	// TODO 池，可选方案A：BlockQueue，B：Redis List
-	private ArrayBlockingQueue<Redpacket> cashCoupons;		//现金券
-	private ArrayBlockingQueue<Redpacket> moneyOffCoupons;	//满减券
+	// private ArrayBlockingQueue<Redpacket> cashCoupons;		//现金券
+	// private ArrayBlockingQueue<Redpacket> moneyOffCoupons;	//满减券
 	// ...
 	
 	
-	public int tmpSum; //临时的数据总数
-	
-	@PostConstruct
-	public void init() {
-		// TODO 启动定时扫描仪
-	}
+	// TODO 红包池状态信息字段
+	// ...
 	
 	/**
 	 * 注入红包
+	 * @param 
 	 */
-	public void inject() {
-		// TODO 扫描到合法场次则数据入池，并且添加定时器到点清理红包池
-		
-		// TODO 
-	}
+	public void inject(BlockingQueue<Redpacket> packets);
 	
 	/**
 	 * 申请一个红包
 	 */
-	public Redpacket apply() {
-		
-		return null;
-	}
+	public Redpacket apply();
 	
 	/**
 	 * 清空红包池
 	 */
-	private void clear() {
-		
-	}
+	// TODO 
+	
+	/**
+	 * 红包池状态信息
+	 */
+	// TODO 
 	
 	static class Redpacket{
 		
