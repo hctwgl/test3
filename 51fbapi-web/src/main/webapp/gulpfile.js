aa8280738bb9413784591b291d84203d4939f622
= <unknown9@shangyuan.cn>
2017-10-17 12:50:40 +0800

    修改gulpfile.js配置文件

--- 51fbapi-web/src/main/webapp/gulpfile.js
+++ 51fbapi-web/src/main/webapp/gulpfile.js
@@ -17,6 +17,9 @@ var gulp = require('gulp'),
     sequence = require('gulp-sequence'),//顺序执行任务
     gulpsync = require('gulp-sync')(gulp),
     browserSync = require('browser-sync'),//浏览器自动刷新
+    changed = require('gulp-changed'), //检查改变状态
+    watch = require('gulp-watch'), //监听更改的文件
+    livereload = require('gulp-livereload'),
     reload = browserSync.reload;
 
 // clean 清空 dist 目录
@@ -66,12 +69,61 @@ gulp.task('build',function (cb) {
 });
 // 浏览器重载
 gulp.task('js-watch', ['es6'], browserSync.reload);
+
+
+gulp.task('watch', function() {
+    livereload.listen()
+    gulp.watch('build/**/*.*', function(event) {
+        livereload.changed(event.path)
+    })
+    gulp.watch('WEB-INF/**/*.*', function(event) {
+        livereload.changed(event.path)
+    })
+})
+
+gulp.task('dev-es', function() {
+    return gulp.src(['build/**/*.js'])
+        .pipe(watch(['build/**/*.js'])) 
+        .pipe(sourcemaps.init())
+        .pipe(plumber())
+        .pipe(babel({presets: ['es2015']}))
+        .pipe(sourcemaps.write('_srcmap'))
+        .pipe(gulp.dest('dist'))
+        .pipe(reload({stream: true}))
+})
+
+gulp.task('dev-less', function() {
+    return gulp.src(['build/**/*.less','build/**/*.css'])
+        .pipe(watch(['build/**/*.less','build/**/*.css']))  
+        .pipe(less())
+        .pipe(gulp.dest('dist'))
+        .pipe(reload({stream: true}))
+})
+
+gulp.task('serve', ['clean'], function() {
+    gulp.start('dev-es', 'dev-less')
+    browserSync.init({
+        port: 3003,
+        proxy: 'localhost:8001'
+    })
+
+    gulp.watch('build/**/*.js', ['dev-es']);
+    gulp.watch(['build/**/*.less','build/**/*.css'], ['dev-less']);
+
+})
+
+gulp.task('default', ['serve', 'watch'])
+
+
 // 监控 build 目录的改动自动编译
-gulp.task('default',['build'],function () {
-    browserSync({
-        open:false,
-        proxy:'localhost:80'
-    });
-     gulp.watch('build/**/*.js',gulpsync.sync(['js-watch']));
-     gulp.watch(['build/**/*.less','build/**/*.css'],gulpsync.sync(['less']));
-});
+// gulp.task('default',['build'],function () {
+//     browserSync({
+//         open:false,
+//         proxy:'localhost:80'
+//     });
+//      gulp.watch('build/**/*.js',gulpsync.sync(['js-watch']));
+//      gulp.watch(['build/**/*.less','build/**/*.css'],gulpsync.sync(['less']));
+// });
+
+
+
