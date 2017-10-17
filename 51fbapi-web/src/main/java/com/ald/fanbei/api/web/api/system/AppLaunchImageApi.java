cf3018352fb8993669b1ef4ab83afcc2548af3d1
DESKTOP-MA60KKU\sun <huangqiujun@edspay.com>
2017-10-17 19:47:15 +0800

    今日头条增加激活接口

--- 51fbapi-web/src/main/java/com/ald/fanbei/api/web/api/system/AppLaunchImageApi.java
+++ 51fbapi-web/src/main/java/com/ald/fanbei/api/web/api/system/AppLaunchImageApi.java
@@ -3,6 +3,11 @@ package com.ald.fanbei.api.web.api.system;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
+import com.ald.fanbei.api.biz.service.AfUserService;
+import com.ald.fanbei.api.biz.service.AfUserToutiaoService;
+import com.ald.fanbei.api.common.util.HttpUtil;
+import com.ald.fanbei.api.dal.domain.AfUserDo;
+import com.ald.fanbei.api.dal.domain.AfUserToutiaoDo;
 import org.apache.commons.lang.ObjectUtils;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Component;
@@ -18,6 +23,9 @@ import com.ald.fanbei.api.web.common.ApiHandleResponse;
 import com.ald.fanbei.api.web.common.RequestDataVo;
 import com.alibaba.fastjson.JSONObject;
 
+import java.security.MessageDigest;
+import java.util.Date;
+
 
 @Component("AppLaunchImageApi")
 public class AppLaunchImageApi implements ApiHandle{
@@ -28,7 +36,10 @@ public class AppLaunchImageApi implements ApiHandle{
 	AfResourceService afResourceService;
 	@Resource
 	TongdunUtil tongdunUtil;
-	
+	@Resource
+	AfUserToutiaoService afUserToutiaoService;
+	@Resource
+	AfUserService afUserService;
 	@Override
 	public ApiHandleResponse process(RequestDataVo requestDataVo,
 			FanbeiContext context, HttpServletRequest request) {
@@ -54,8 +65,62 @@ public class AppLaunchImageApi implements ApiHandle{
 		} catch (Exception e) {
 			logger.error("appLaunchImageApi activeOperate error",e);
 		}
+		//头条激活处理
+		ToutiaoAdOpen(requestDataVo,context);
 		
 		return response;
+	}
+
+	private void ToutiaoAdOpen(RequestDataVo requestDataVo, FanbeiContext context) {
+		try {
+			String userName = context.getUserName();
+			AfUserDo afUserDo = afUserService.getUserByUserName(userName);
+			if(afUserDo!=null){
+				return;
+			}
+			String imei = ObjectUtils.toString(requestDataVo.getParams().get("IMEI"), null);
+			String androidId = ObjectUtils.toString(requestDataVo.getParams().get("AndroidID"), null);
+			String idfa = ObjectUtils.toString(requestDataVo.getParams().get("IDFA"), null);
+			String imeiMd5="";
+			logger.error("toutiaoopen para:"+imei+","+androidId+","+idfa);
+			if(StringUtils.isNotBlank(imei)){
+				imeiMd5=getMd5(imei);
+			}
+			if(StringUtils.isNotBlank(imei)||StringUtils.isNotBlank(androidId)||StringUtils.isNotBlank(idfa)){
+				AfUserToutiaoDo tdo = afUserToutiaoService.getUserOpen(imeiMd5,androidId,idfa);
+				if(tdo!=null){
+					Long rid = tdo.getRid();
+					Long userIdToutiao = context.getUserId()==null?-1l:context.getUserId();
+					String userNameToutiao = context.getUserName()==null?"":context.getUserName();
+					afUserToutiaoService.uptUserOpen(rid,userIdToutiao,userNameToutiao);
+					String callbackUrl = tdo.getCallbackUrl();
+					String result= HttpUtil.doGet(callbackUrl,20);
+					logger.error("toutiaoopen:update success,isopen=1,callbacr_url="+callbackUrl+",result="+result);
+				}
+			}
+		}catch (Exception e){
+			logger.error("toutiaoopen:catch error",e);
+		}
+	}
+
+	/**
+	 * 用于获取一个String的md5值
+	 * @param string
+	 * @return
+	 */
+	public static String getMd5(String str) throws Exception {
+		MessageDigest md5 = MessageDigest.getInstance("MD5");
+
+		byte[] bs = md5.digest(str.getBytes());
+		StringBuilder sb = new StringBuilder(40);
+		for(byte x:bs) {
+			if((x & 0xff)>>4 == 0) {
+				sb.append("0").append(Integer.toHexString(x & 0xff));
+			} else {
+				sb.append(Integer.toHexString(x & 0xff));
+			}
+		}
+		return sb.toString();
 	}
 
 }
