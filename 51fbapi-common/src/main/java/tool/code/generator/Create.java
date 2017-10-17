775838d1086b91988f6efa1415778c14f4c50872
gaojibin <gaojibin@alamap.com>
2017-10-17 11:44:07 +0800

    添加数据库表相关对象

--- 51fbapi-common/src/main/java/tool/code/generator/Create.java
+++ 51fbapi-common/src/main/java/tool/code/generator/Create.java
@@ -1,5 +1,7 @@
 package tool.code.generator;
 
+import org.apache.commons.lang.StringUtils;
+
 
 public class Create {
 	public static void main(String[] args) {
@@ -9,39 +11,40 @@ public class Create {
 	
 	public void test(){
 
-		// 数据库连接信息
-		String url = "jdbc:mysql://192.168.106.76:3306/51fanbei_app?useUnicode=true&characterEncoding=utf8";
+	 // 数据库连接信息
+		String url = "jdbc:mysql://51fanbeitest.db.com:3306/51fanbei_app?useUnicode=true&characterEncoding=utf8";
 		String MysqlUser = "51fanbei";
 		String mysqlPassword = "Hello1234";
-		
+
 		// 数据库及数据表名称
 		String database = "51fanbei_app";
-		//String table = "tt_test";
-		String table = "af_fund_side_account";
 
-		
-		
-		
+		String tables = "af_de_goods,af_de_goods_coupon,af_de_random_property,af_de_user_cutInfo,af_de_user_goods";//"af_maoyan_order,af_maoyan_filmlist,af_maoyan_filmcrawl,af_maoyan_showlist,af_maoyan_hotcomment,af_maoyan_cinemafeature,af_maoyan_cinema,af_maoyan_city,af_maoyan_district,af_maoyan_film,af_maoyan_filmmember,af_maoyan_filmsp,af_maoyan_filmtrailer,af_maoyan_hall,af_maoyan_hallseat";
+
 		// 配置作者及Domain说明
-		String classAuthor = "chegnkang";
-		//TODO:String classAuthor = " jinhu.chenjh";
-		String functionName = " 资金方账户信息表";
-		
+		String classAuthor = "gaojibin_temple";
+		String functionName = "双十一砍价";
+
 		// 公共包路径 (例如 BaseDao、 BaseService、 BaseServiceImpl)
-		String commonName ="";
-		
+		String commonName = "";
+
 		String packageName ="com.ald.fanbei.api";
 		String moduleName = "";
 
-		//Mapper文件存储地址  默认在resources中
+		// Mapper文件存储地址 默认在resources中
 		String batisName = "config/mapping";
-		String db="mysql";
-		
+		String db = "mysql";
+
 		try {
-			String classNamePrefix = getClassName(table);
-			MybatisGenerate.generateCode(db,url, MysqlUser, mysqlPassword, database, table,commonName,packageName,batisName,moduleName,classAuthor,functionName,classNamePrefix);
+		    String[] table = tables.split(",");
+		    for (String tableName : table) {
+			if (StringUtils.isNotBlank(tableName)) {
+			    String classNamePrefix = getClassName(tableName);
+			    MybatisGenerate.generateCode(db, url, MysqlUser, mysqlPassword, database, tableName, commonName, packageName, batisName, moduleName, classAuthor, functionName, classNamePrefix);
+			}
+		    }
 		} catch (Exception e) {
-			e.printStackTrace();
+		    e.printStackTrace();
 		}
 	}
 	
---
+++ 51fbapi-dal/src/main/java/com/ald/fanbei/api/dal/dao/AfDeGoodsDao.java
@@ -0,0 +1,17 @@
+package com.ald.fanbei.api.dal.dao;
+
+import com.ald.fanbei.api.dal.domain.AfDeGoodsDo;
+
+/**
+ * 双十一砍价Dao
+ * 
+ * @author gaojibin_temple
+ * @version 1.0.0 初始化
+ * @date 2017-10-17 11:40:19
+ * Copyright 本内容仅限于杭州阿拉丁信息科技股份有限公司内部传阅，禁止外泄以及用于其他的商业目的
+ */
+public interface AfDeGoodsDao extends BaseDao<AfDeGoodsDo, Long> {
+
+    
+
+}
