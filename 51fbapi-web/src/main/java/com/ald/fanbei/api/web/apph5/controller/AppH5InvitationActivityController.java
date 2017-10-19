41fde716690d5c844aae0a70634749c478a1b2ac
caowu <2456087356@qq.com>
2017-10-19 18:58:45 +0800

    加上登录者信息

--- 51fbapi-web/src/main/java/com/ald/fanbei/api/web/apph5/controller/AppH5InvitationActivityController.java
+++ 51fbapi-web/src/main/java/com/ald/fanbei/api/web/apph5/controller/AppH5InvitationActivityController.java
@@ -4,6 +4,7 @@ import com.ald.fanbei.api.biz.service.AfRecommendUserService;
 import com.ald.fanbei.api.biz.service.AfUserService;
 import com.ald.fanbei.api.common.FanbeiContext;
 import com.ald.fanbei.api.common.FanbeiWebContext;
+import com.ald.fanbei.api.common.exception.FanbeiException;
 import com.ald.fanbei.api.common.exception.FanbeiExceptionCode;
 import com.ald.fanbei.api.dal.domain.AfRecommendUserDo;
 import com.ald.fanbei.api.dal.domain.AfUserDo;
@@ -150,11 +151,22 @@ public class AppH5InvitationActivityController extends BaseController {
 
     @Override
     public RequestDataVo parseRequestData(String requestData, HttpServletRequest request) {
-        return null;
+        try {
+            RequestDataVo reqVo = new RequestDataVo();
+
+            JSONObject jsonObj = JSON.parseObject(requestData);
+            reqVo.setId(jsonObj.getString("id"));
+            reqVo.setMethod(request.getRequestURI());
+            reqVo.setSystem(jsonObj);
+            return reqVo;
+        } catch (Exception e) {
+            throw new FanbeiException("参数格式错误"+e.getMessage(), FanbeiExceptionCode.REQUEST_PARAM_ERROR);
+        }
     }
 
     @Override
     public BaseResponse doProcess(RequestDataVo requestDataVo, FanbeiContext context, HttpServletRequest httpServletRequest) {
+        // TODO Auto-generated method stub
         return null;
     }
 }
