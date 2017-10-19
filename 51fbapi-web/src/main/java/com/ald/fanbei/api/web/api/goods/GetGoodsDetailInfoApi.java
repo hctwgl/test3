b071f387bb2d35123f6f87647a42ccda9239b189
RONGBO\rongbo <jiangrongbo@edspay.com>
2017-10-19 21:25:09 +0800

    商品详情增加最低月供

--- 51fbapi-web/src/main/java/com/ald/fanbei/api/web/api/goods/GetGoodsDetailInfoApi.java
+++ 51fbapi-web/src/main/java/com/ald/fanbei/api/web/api/goods/GetGoodsDetailInfoApi.java
@@ -1,16 +1,22 @@
 package com.ald.fanbei.api.web.api.goods;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.lang.ObjectUtils;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Component;
 
 import com.ald.fanbei.api.biz.service.AfGoodsService;
+import com.ald.fanbei.api.biz.service.AfInterestFreeRulesService;
 import com.ald.fanbei.api.biz.service.AfResourceService;
+import com.ald.fanbei.api.biz.service.AfSchemeGoodsService;
 import com.ald.fanbei.api.biz.third.util.TaobaoApiUtil;
 import com.ald.fanbei.api.common.Constants;
 import com.ald.fanbei.api.common.FanbeiContext;
@@ -20,11 +26,18 @@ import com.ald.fanbei.api.common.exception.FanbeiExceptionCode;
 import com.ald.fanbei.api.common.util.NumberUtil;
 import com.ald.fanbei.api.common.util.StringUtil;
 import com.ald.fanbei.api.dal.domain.AfGoodsDo;
+import com.ald.fanbei.api.dal.domain.AfInterestFreeRulesDo;
+import com.ald.fanbei.api.dal.domain.AfResourceDo;
+import com.ald.fanbei.api.dal.domain.AfSchemeGoodsDo;
 import com.ald.fanbei.api.web.common.ApiHandle;
 import com.ald.fanbei.api.web.common.ApiHandleResponse;
+import com.ald.fanbei.api.web.common.InterestFreeUitl;
 import com.ald.fanbei.api.web.common.RequestDataVo;
 import com.ald.fanbei.api.web.vo.AfGoodsDetailInfoVo;
 import com.ald.fanbei.api.web.vo.GoodsDetailPicInfoVo;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 
 /**
  * 
@@ -43,6 +56,12 @@ public class GetGoodsDetailInfoApi implements ApiHandle{
 	
 	@Resource
 	private AfResourceService afResourceService;
+	
+	@Resource
+	private AfSchemeGoodsService afSchemeGoodsService;
+	
+	@Resource
+	private AfInterestFreeRulesService afInterestFreeRulesService;
 
 	@Override
 	public ApiHandleResponse process(RequestDataVo requestDataVo,
@@ -53,10 +72,54 @@ public class GetGoodsDetailInfoApi implements ApiHandle{
     	if(null == goods){
 			throw new FanbeiException("goods not exist error", FanbeiExceptionCode.GOODS_NOT_EXIST_ERROR);
     	}
-    	AfGoodsDetailInfoVo vo = getGoodsVo(goods);
+    	//获取借款分期配置信息
+        AfResourceDo resource = afResourceService.getConfigByTypesAndSecType(Constants.RES_BORROW_RATE, Constants.RES_BORROW_CONSUME);
+        JSONArray array = JSON.parseArray(resource.getValue());
+        //删除2分期
+        if (array == null) {
+            throw new FanbeiException(FanbeiExceptionCode.BORROW_CONSUME_NOT_EXIST_ERROR);
+        }
+        removeSecondNper(array);
+        
+    	BigDecimal saleAmount = goods.getSaleAmount();
+    	AfSchemeGoodsDo schemeGoodsDo = null;
+		try {
+			schemeGoodsDo = afSchemeGoodsService.getSchemeGoodsByGoodsId(goodsId);
+		} catch(Exception e){
+			logger.error(e.toString());
+		}
+		JSONArray interestFreeArray = null;
+		if(schemeGoodsDo != null){
+			AfInterestFreeRulesDo  interestFreeRulesDo = afInterestFreeRulesService.getById(schemeGoodsDo.getInterestFreeId());
+			String interestFreeJson = interestFreeRulesDo.getRuleJson();
+			if (StringUtils.isNotBlank(interestFreeJson) && !"0".equals(interestFreeJson)) {
+				interestFreeArray = JSON.parseArray(interestFreeJson);
+			}
+		}
+		List<Map<String, Object>> nperList = InterestFreeUitl.getConsumeList(array, interestFreeArray, BigDecimal.ONE.intValue(),
+				saleAmount, resource.getValue1(), resource.getValue2());
+		AfGoodsDetailInfoVo vo = getGoodsVo(goods);
+		if(nperList!= null){
+			Map nperMap = nperList.get(nperList.size() - 1);
+			vo.setNperMap(nperMap);
+		}
 		resp.setResponseData(vo);
 		return resp;
 	}
+	
+    private void removeSecondNper(JSONArray array) {
+        if (array == null) {
+            return;
+        }
+        Iterator<Object> it = array.iterator();
+        while (it.hasNext()) {
+            JSONObject json = (JSONObject) it.next();
+            if (json.getString(Constants.DEFAULT_NPER).equals("2")) {
+                it.remove();
+                break;
+            }
+        }
+    }
 	
 	private AfGoodsDetailInfoVo getGoodsVo(AfGoodsDo goods){
 		AfGoodsDetailInfoVo vo = new AfGoodsDetailInfoVo();
