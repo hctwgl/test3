bd9dedbf3cb12415b59d66ece75a57813653d7c9
chefeipeng <chefeipeng@chefeipeng.Alamap.com>
2017-10-19 14:19:03 +0800

    1

--- 51fbapi-web/src/main/java/com/ald/fanbei/api/web/api/clickAmount/clickAmountNumApi.java
+++ 51fbapi-web/src/main/java/com/ald/fanbei/api/web/api/clickAmount/clickAmountNumApi.java
@@ -31,7 +31,7 @@ import javax.servlet.http.HttpServletRequest;
  * @author chefeipeng 2017年10月28日下午3:40:31
  * @注意：本内容仅限于杭州阿拉丁信息科技股份有限公司内部传阅，禁止外泄以及用于其他的商业目的
  */
-@Component("clickAmountNum")
+@Component("clickAmountNumApi")
 public class ClickAmountNumApi implements ApiHandle{
 
 
