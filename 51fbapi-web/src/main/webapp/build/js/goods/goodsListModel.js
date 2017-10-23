ce1abf2d4aa3478bc16b72598b2c0c1a48dbb9ea
zhuyunyun <1090601532@qq.com>
2017-10-23 12:27:35 +0800

    fix  js

--- 51fbapi-web/src/main/webapp/build/js/goods/goodsListModel.js
+++ 51fbapi-web/src/main/webapp/build/js/goods/goodsListModel.js
@@ -251,6 +251,41 @@ $(function(){
             }
         }
     });
-
 });
-
+//点击优惠券
+function couponClick(rid,a){
+    let couponId=rid;
+    let index=a.index();
+    $.ajax({
+        url: "/fanbei-web/pickCoupon",
+        type: "POST",
+        dataType: "JSON",
+        data: {
+            couponId: couponId
+        },
+        success: function (returnData) {
+            if (returnData.success) {
+                requestMsg("优惠劵领取成功");
+                $('.coupon').eq(index).addClass('couponclose');
+            } else {
+                var status = returnData.data["status"];
+                if (status == "USER_NOT_EXIST") { // 用户不存在
+                    window.location.href = returnData.url;
+                }
+                if (status == "OVER") { // 优惠券个数超过最大领券个数
+                    //requestMsg(returnData.msg);
+                    requestMsg("您已经领取，快去使用吧");
+                }
+                if (status == "COUPON_NOT_EXIST") { // 优惠券不存在
+                    requestMsg(returnData.msg);
+                }
+                if (status == "MORE_THAN") { // 优惠券已领取完
+                    requestMsg(returnData.msg);
+                }
+            }
+        },
+        error: function () {
+            requestMsg("哎呀，出错了！");
+        }
+    })
+}
