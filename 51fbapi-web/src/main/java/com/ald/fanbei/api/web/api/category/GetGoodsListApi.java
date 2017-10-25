3935bbc9afe0ccb687196b7bbf420000af3b949b
chefeipeng <chefeipeng@chefeipeng.Alamap.com>
2017-10-25 11:46:14 +0800

    商品分类

--- 51fbapi-web/src/main/java/com/ald/fanbei/api/web/api/category/GetGoodsListApi.java
+++ 51fbapi-web/src/main/java/com/ald/fanbei/api/web/api/category/GetGoodsListApi.java
@@ -27,6 +27,7 @@ import org.springframework.stereotype.Component;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import java.math.BigDecimal;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -59,6 +60,7 @@ public class GetGoodsListApi implements ApiHandle {
         AfResourceDo resource = afResourceService.getConfigByTypesAndSecType(Constants.RES_BORROW_RATE, Constants.RES_BORROW_CONSUME);
         JSONArray array = JSON.parseArray(resource.getValue());
         for(AfGoodsCategoryDto goodsDo : list) {
+            double volume = new Long(goodsDo.getVolume()).intValue();
             Map<String, Object> goodsInfo = new HashMap<String, Object>();
             goodsInfo.put("goodName",goodsDo.getName());
             goodsInfo.put("rebateAmount",goodsDo.getRebateAmount());
@@ -69,7 +71,14 @@ public class GetGoodsListApi implements ApiHandle {
             goodsInfo.put("goodsUrl",goodsDo.getGoodsUrl());
             goodsInfo.put("source",goodsDo.getSource());
             goodsInfo.put("numId",goodsDo.getNumId());
-            goodsInfo.put("volume",goodsDo.getVolume());
+            if(volume>10000){
+                DecimalFormat df = new DecimalFormat("0.00");
+                BigDecimal bigDecimal = new BigDecimal(df.format(volume/10000));
+                bigDecimal.setScale(3,bigDecimal.ROUND_HALF_UP);
+                goodsInfo.put("volume",bigDecimal.toString()+"万");
+            }else{
+                goodsInfo.put("volume",goodsDo.getVolume());
+            }
             goodsInfo.put("saleCount",goodsDo.getSaleCount());
             goodsInfo.put("goodsType", "0");
             // 如果是分期免息商品，则计算分期
