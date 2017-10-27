62e78d7b08bd788d79bd76da7c83fbe896a2c3e1
jinzhouping <jinzhouping@edspay.com>
2017-10-27 22:10:21 +0800

    test

--- 51fbapi-web/src/main/webapp/build/activity/11/barginProduct.js
+++ 51fbapi-web/src/main/webapp/build/activity/11/barginProduct.js
@@ -68,7 +68,7 @@ let vm = new Vue({
                 success: function(data){
                     if (!data.success) {
                         if (self.isWX) {
-                           self.toLogin(); 
+                           // self.toLogin(); 
                         }else {
                             location.href = data.data.loginUrl;
                         }
@@ -99,7 +99,6 @@ let vm = new Vue({
                     requestMsg("哎呀，出错了！")
                 }
             });
-
         },
         listFn: function() {
             let self = this;
@@ -119,7 +118,7 @@ let vm = new Vue({
                         }
                     } else {
                         if (isWX) {
-                           self.toLogin(); 
+                           // self.toLogin(); 
                         }else {
                             location.href = data.data.loginUrl;
                         }
