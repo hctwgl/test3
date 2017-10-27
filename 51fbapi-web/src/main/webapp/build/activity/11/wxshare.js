b5bb780a2236bad35fb6f901341d79f2eae55153
jinzhouping <jinzhouping@edspay.com>
2017-10-27 20:00:51 +0800

    share.js

--- 51fbapi-web/src/main/webapp/build/activity/11/wxshare.js
+++ 51fbapi-web/src/main/webapp/build/activity/11/wxshare.js
@@ -29,6 +29,8 @@ $(function(){
         dataType: 'json',
         data: {url: shareInfo.link},
         success: function (res) {
+            alert("share")
+            alert(JSON.stringify(res))
              // 用户确认分享后执行的回调函数
             wx.config({
                 debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
