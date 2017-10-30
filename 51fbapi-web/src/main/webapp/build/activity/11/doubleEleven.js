74cca94765bc0ef7168d106660b88116d0c0ed01
pengxiangyu <1344820123@qq.com>
2017-10-30 17:11:48 +0800

    fix bug

--- 51fbapi-web/src/main/webapp/build/activity/11/doubleEleven.js
+++ 51fbapi-web/src/main/webapp/build/activity/11/doubleEleven.js
@@ -18,7 +18,7 @@ if (currentStarmp > oneTime && currentStarmp < twoTime) { //11.1-11.8
 if (currentStarmp >= twoTime && currentStarmp <= sevenTime) { //11.9-11.10
     addStyle(1);
 }
-if (currentStarmp >= fiveTime && currentStarmp < threeTime) { //11.11
+if (currentStarmp >= fiveTime && currentStarmp < sixTime) { //11.11-11.12
     addStyle(2);
 }
 if (currentStarmp > sixTime) { //11.13
