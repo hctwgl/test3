d57cc942808d49608511c1735fe987bc214649f5
pengxiangyu <1344820123@qq.com>
2017-10-31 23:19:00 +0800

    fix bug

--- 51fbapi-web/src/main/webapp/build/activity/11/doubleEleven.js
+++ 51fbapi-web/src/main/webapp/build/activity/11/doubleEleven.js
@@ -690,7 +690,7 @@ let vm = new Vue({
         //点击回到顶部
         toTop(){
             $(".backTop").click(function() {
-                $("html,body").animate({scrollTop:1500}, 500);
+                $("html,body").animate({scrollTop:0}, 500);
             }); 
         }
     }
