df57bf6f3b5298318991ea342ec3291869f7cacf
倪稚炜 <821855466@qq.com>
2017-11-06 16:35:06 +0800

    fix url

--- 51fbapi-web/src/main/webapp/build/js/user/channelRegister.js
+++ 51fbapi-web/src/main/webapp/build/js/user/channelRegister.js
@@ -22,7 +22,7 @@ if( style==21 ){
     document.title="借钱平台注册";
     $("#BrwPlatform").click(function(){  // 更换已登陆链接
         if ( os==1 ){
-            window.location.href="http://a.app.qq.com/o/simple.jsp?pkgname=com.ala.borrowMoney";
+            window.location.href="http://a.app.qq.com/o/simple.jsp?pkgname=com.alfl.www";
         }else if( os==2 ){
             window.location.href="https://itunes.apple.com/cn/app/%E5%80%9F%E9%92%B1%E5%B9%B3%E5%8F%B0-%E5%B0%8F%E9%A2%9D%E6%9E%81%E9%80%9F%E7%8E%B0%E9%87%91%E5%80%9F%E8%B4%B7/id1259127316?mt=8";
         }
@@ -261,12 +261,12 @@ $(function(){
                                                     window.location.href = 'https://itunes.apple.com/us/app/51%E8%BF%94%E5%91%97/id1136587444?mt=8';
                                                 }
                                             });
-                                        } else if( style==21 ){
-                                            if ( os==1 ){
-                                                window.location.href="https://f.51fanbei.com/online/jiekuancaoren_v3.7.1.apk";
-                                            }else if( os==2 ){
-                                                window.location.href="https://itunes.apple.com/cn/app/%E5%80%9F%E6%AC%BE%E8%B6%85%E4%BA%BA-%E5%85%8D%E6%81%AF%E5%B0%8F%E9%A2%9D%E5%80%9F%E8%B4%B7%E6%89%8B%E6%9C%BA%E8%BD%AF%E4%BB%B6/id1263792729?mt=8";
-                                            }
+                                        // } else if( style==21 ){
+                                        //     if ( os==1 ){
+                                        //         window.location.href="https://f.51fanbei.com/online/jiekuancaoren_v3.7.1.apk";
+                                        //     }else if( os==2 ){
+                                        //         window.location.href="https://itunes.apple.com/cn/app/%E5%80%9F%E6%AC%BE%E8%B6%85%E4%BA%BA-%E5%85%8D%E6%81%AF%E5%B0%8F%E9%A2%9D%E5%80%9F%E8%B4%B7%E6%89%8B%E6%9C%BA%E8%BD%AF%E4%BB%B6/id1263792729?mt=8";
+                                        //     }
                                         } else{
                                             // js判断微信和QQ
                                             let ua = navigator.userAgent.toLowerCase();
