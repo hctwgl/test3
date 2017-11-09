894de60f639185ad3ed5e9d2089fdcac1c70f6fd
renchunlei <renchunlei@edspay.com>
2017-11-09 16:39:27 +0800

    测试逾期情况代码

--- 51fbapi-web/src/main/java/com/ald/fanbei/api/web/api/checkoutcounter/StartCashierApi.java
+++ 51fbapi-web/src/main/java/com/ald/fanbei/api/web/api/checkoutcounter/StartCashierApi.java
@@ -348,10 +348,10 @@ public class StartCashierApi implements ApiHandle {
         // 风控逾期订单处理
         RiskQueryOverdueOrderRespBo resp = riskUtil.queryOverdueOrder(orderInfo.getUserId() + StringUtil.EMPTY);
         String rejectCode =  resp.getRejectCode();
-        if(userDto.getUserName()=="13460011555"){
+        if(userDto.getUserName().equals("13460011555")){
             rejectCode=RiskErrorCode.OVERDUE_BORROW.getCode();
         }
-        if(userDto.getUserName()=="17710378476"){
+        if(userDto.getUserName().equals("17710378476")){
             rejectCode=RiskErrorCode.OVERDUE_BORROW_CASH.getCode();
         }
         if (StringUtil.isNotBlank(rejectCode)) {
@@ -360,6 +360,9 @@ public class StartCashierApi implements ApiHandle {
             switch (erorrCode) {
                 case OVERDUE_BORROW:
                     String borrowNo = resp.getBorrowNo();
+                    if(userDto.getUserName().equals("13460011555")){
+                        borrowNo="jk2017101518502700303";
+                    }
                     AfBorrowDo borrowInfo = afBorrowService.getBorrowInfoByBorrowNo(borrowNo);
                     Long billId = afBorrowBillService.getOverduedAndNotRepayBillId(borrowInfo.getRid());
                     cashierTypeVo.setBillId(billId);
