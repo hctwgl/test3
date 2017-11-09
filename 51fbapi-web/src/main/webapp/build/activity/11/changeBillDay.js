1f0e753cf6d1ac933c9d9aaa4061f7033628ab9f
pengxiangyu <1344820123@qq.com>
2017-11-09 14:58:29 +0800

    fix bug

--- 51fbapi-web/src/main/webapp/build/activity/11/changeBillDay.js
+++ 51fbapi-web/src/main/webapp/build/activity/11/changeBillDay.js
@@ -1,80 +1,103 @@
-let startTime='',endTime='';
-//增加开始日期
-function addStartDate(){
-    // dtpicker组件适用于弹出日期选择器
-    var dtpicker = new mui.DtPicker({
-        type: "date", //设置日历初始视图模式
-        labels: ['年', '月', '日'] //设置默认标签区域提示语
-    });
+// let startTime='',endTime='';
+// //增加开始日期
+// function addStartDate(){
+//     // dtpicker组件适用于弹出日期选择器
+//     var dtpicker = new mui.DtPicker({
+//         type: "date", //设置日历初始视图模式
+//         labels: ['年', '月', '日'] //设置默认标签区域提示语
+//     });
 
-    dtpicker.show(function(items) {
-        startTime=items.y.value + "-"+ items.m.value + "-"+ items.d.value;
-        var dateStr = items.y.value + "-" + items.m.value + "-" + items.d.value + " ";
-        $('.startTime').text(dateStr);
-        dtpicker.dispose();
-    })
-}
+//     dtpicker.show(function(items) {
+//         startTime=items.y.value + "-"+ items.m.value + "-"+ items.d.value;
+//         var dateStr = items.y.value + "-" + items.m.value + "-" + items.d.value + " ";
+//         $('.startTime').text(dateStr);
+//         dtpicker.dispose();
+//     })
+// }
 
-//增加结束日期
-/* function addEndDate(){
-    // dtpicker组件适用于弹出日期选择器
-    var dtpicker = new mui.DtPicker({
-        type: "date", //设置日历初始视图模式
-        labels: ['年', '月', '日'] //设置默认标签区域提示语
-    });
+// //增加结束日期
+// /* function addEndDate(){
+//     // dtpicker组件适用于弹出日期选择器
+//     var dtpicker = new mui.DtPicker({
+//         type: "date", //设置日历初始视图模式
+//         labels: ['年', '月', '日'] //设置默认标签区域提示语
+//     });
 
-    dtpicker.show(function(items) {
-        endTime = items.y.value + "-" + items.m.value + "-" + items.d.value;
-        var dateStr = items.y.value + "-" + items.m.value + "-" + items.d.value + " ";
-        $('.endTime').text(dateStr);
-        dtpicker.dispose();
-    })
-} */
+//     dtpicker.show(function(items) {
+//         endTime = items.y.value + "-" + items.m.value + "-" + items.d.value;
+//         var dateStr = items.y.value + "-" + items.m.value + "-" + items.d.value + " ";
+//         $('.endTime').text(dateStr);
+//         dtpicker.dispose();
+//     })
+// } */
 
-function addEndDate(){
-    // dtpicker组件适用于弹出日期选择器
-    var dtpicker = new mui.DtPicker({
-        type: "Int", //设置日历初始视图模式
-        picker:2
-    });
+// function addEndDate(){
+//     // dtpicker组件适用于弹出日期选择器
+//     var dtpicker = new mui.DtPicker({
+//         type: "Int", //设置日历初始视图模式
+//         picker:2
+//     });
 
-    dtpicker.show(function(items) {
-        endTime = items.y.value + "-" + items.m.value + "-" + items.d.value;
-        var dateStr = items.y.value + "-" + items.m.value + "-" + items.d.value + " ";
-        $('.endTime').text(dateStr);
-        dtpicker.dispose();
-    })
-}
-
-
-//选择开始结束时间 
-$(".dateOne").click(function(){
-     addStartDate(); 
-});
-
- $(".dateTwo").click(function(){
-    addEndDate();
-}); 
-
-//返回
-$('.return').click(function(){
-    window.location.href=paraArr+".html";
-});
-//完成
-$('.finish').click(function(){
-    if(Number(endTime)>0&&Number(endTime)<Number(startTime)){
-        requestMsg('结束时间不能小于开始时间')
-    }else{
-        window.location.href=paraArr+".html?startTime="+startTime+'&endTime='+endTime+'&stateStatus='+stateStatus;
-    }
-});
+//     dtpicker.show(function(items) {
+//         endTime = items.y.value + "-" + items.m.value + "-" + items.d.value;
+//         var dateStr = items.y.value + "-" + items.m.value + "-" + items.d.value + " ";
+//         $('.endTime').text(dateStr);
+//         dtpicker.dispose();
+//     })
+// }
 
 
-/* let vm = new Vue({
+// //选择开始结束时间 
+// $(".dateOne").click(function(){
+//      addStartDate(); 
+// });
+
+//  $(".dateTwo").click(function(){
+//     addEndDate();
+// }); 
+
+// //返回
+// $('.return').click(function(){
+//     window.location.href=paraArr+".html";
+// });
+// //完成
+// $('.finish').click(function(){
+//     if(Number(endTime)>0&&Number(endTime)<Number(startTime)){
+//         requestMsg('结束时间不能小于开始时间')
+//     }else{
+//         window.location.href=paraArr+".html?startTime="+startTime+'&endTime='+endTime+'&stateStatus='+stateStatus;
+//     }
+// });
+
+
+ let vm = new Vue({
     el:'#changbillDay',
-    data:{},
-    created:{},
+    data:{
+        content:''
+    },
+    created:function(){
+        this.logData();
+    },
     methods:{
+        logData(){
+            let self=this;
+            $.ajax({
+                type:'post',
+                url:'/fanbei-web/changeOutDay/updateOutDay',
+                success:function(data){
+                    self.content = eval('(' + data + ')');
+                    console.log(self.content);
 
+                    if(self.content.success==false){
+                        window.location.href='http://testapp.51fanbei.com/fanbei-web/opennative?name=APP_LOGIN';//未登录跳登录
+                    }
+                     
+                    
+                }
+                    
+                
+            })
+            
+        },
     }
-}) */
+}) 
--- 51fbapi-web/src/main/webapp/build/activity/11/dynamicBill.js
+++ 51fbapi-web/src/main/webapp/build/activity/11/dynamicBill.js
@@ -2,6 +2,7 @@ let vm = new Vue({
     el:'#dynamicBill',
     data:{
         content:'',
+        contentOne:''
     },
     created:function(){
         this.logData();
@@ -16,12 +17,27 @@ let vm = new Vue({
                     self.content = eval('(' + data + ')');
                     console.log(self.content);
 
-
                     if(self.content.success==false){
-                        // window.location.href='http://testapp.51fanbei.com/fanbei-web/opennative?name=APP_LOGIN';
+                        window.location.href='http://testapp.51fanbei.com/fanbei-web/opennative?name=APP_LOGIN';
                     }
                      
                     
+                }
+                    
+                
+            })
+        },
+        changeBillDay(){
+            $.ajax({
+                type:'post',
+                url:'/fanbei-web/changeOutDay/getOutDayList',
+                success:function(data){
+                    self.contentOne = eval('(' + data + ')');
+                    console.log(self.contentOne);
+                     //window.location.href='changebillDay';//没有逾期账单跳修改账单日
+                   /* if(self.contentOne.success==false){
+                       window.location.href='cunpaidBill';//有逾期账单跳账单未还清
+                   } */
                 }
                     
                 
