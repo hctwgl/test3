b83c59a78fdb8324a4fbcfd6655f4e53f83dc481
pengxiangyu <1344820123@qq.com>
2017-11-10 15:59:35 +0800

    fix bug

--- 51fbapi-web/src/main/webapp/build/activity/11/changeBillDay.js
+++ 51fbapi-web/src/main/webapp/build/activity/11/changeBillDay.js
@@ -140,7 +140,7 @@
                     console.log(self.contentTwo,'self.contentTwo');
                     window.location.href='changeSuccess?testUser=17839218825';//点击提交跳转修改成功页面
                      if(self.contentTwo.success==false){
-                        window.location.href='http://testapp.51fanbei.com/fanbei-web/opennative?name=APP_LOGIN';//未登录跳登录
+                        //window.location.href='http://testapp.51fanbei.com/fanbei-web/opennative?name=APP_LOGIN';//未登录跳登录
                     } 
                      
                     
