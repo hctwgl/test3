ff4f81bcc259aaf360fb9850e8f6bc75dce30034
chenqiwei <chenqiwei@edspay.com>
2017-11-14 14:11:52 +0800

    吃玩住行活动controler

---
+++ 51fbapi-web/src/main/java/com/ald/fanbei/api/web/apph5/controller/APPH5GgActivityController.java
@@ -0,0 +1,19 @@
+package com.ald.fanbei.api.web.apph5.controller;
+
+
+/**
+ * 
+ * @ClassName: APPH5GgActivityController
+ * @Description: 吃玩住行活动
+ * @author chenqiwei
+ * @Copyright (c) 浙江阿拉丁电子商务股份有限公司 All Rights Reserved.
+ * @date 2017年11月14日 下午2:07:40
+ *
+ */
+@RestController
+@RequestMapping(value = "/h5GgActivity", produces = "application/json;charset=UTF-8")
+public class APPH5GgActivityController extends BaseController {
+
+	
+
+}
