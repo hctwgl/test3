35776dd8b0cc4351d7fb50807a31a2380fc2b835
maqiaopan <maqiaopan@edspay.com>
2017-11-14 14:59:48 +0800

    init controller

--- 51fbapi-web/src/main/java/com/ald/fanbei/api/web/apph5/controller/APPH5GgActivityController.java
+++ 51fbapi-web/src/main/java/com/ald/fanbei/api/web/apph5/controller/APPH5GgActivityController.java
@@ -1,5 +1,18 @@
 package com.ald.fanbei.api.web.apph5.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ald.fanbei.api.common.FanbeiContext;
+import com.ald.fanbei.api.common.exception.FanbeiException;
+import com.ald.fanbei.api.common.exception.FanbeiExceptionCode;
+import com.ald.fanbei.api.web.common.BaseController;
+import com.ald.fanbei.api.web.common.BaseResponse;
+import com.ald.fanbei.api.web.common.RequestDataVo;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 
 /**
  * 
@@ -13,6 +26,35 @@ package com.ald.fanbei.api.web.apph5.controller;
 @RestController
 @RequestMapping(value = "/h5GgActivity", produces = "application/json;charset=UTF-8")
 public class APPH5GgActivityController extends BaseController {
+
+	@Override
+	public String checkCommonParam(String reqData, HttpServletRequest request, boolean isForQQ) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public RequestDataVo parseRequestData(String requestData, HttpServletRequest request) {
+		try {
+			RequestDataVo reqVo = new RequestDataVo();
+
+			JSONObject jsonObj = JSON.parseObject(requestData);
+			reqVo.setId(jsonObj.getString("id"));
+			reqVo.setMethod(request.getRequestURI());
+			reqVo.setSystem(jsonObj);
+
+			return reqVo;
+		} catch (Exception e) {
+			throw new FanbeiException("参数格式错误" + e.getMessage(), FanbeiExceptionCode.REQUEST_PARAM_ERROR);
+		}
+	}
+
+	@Override
+	public BaseResponse doProcess(RequestDataVo requestDataVo, FanbeiContext context,
+			HttpServletRequest httpServletRequest) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	
 
