d237055a234a8a545d837a5f9a193f902761c7a7
chenqiwei <chenqiwei@edspay.com>
2017-11-15 13:23:02 +0800

    return coupon add register time

--- 51fbapi-common/src/main/java/com/ald/fanbei/api/common/enums/H5GgActivity.java
+++ 51fbapi-common/src/main/java/com/ald/fanbei/api/common/enums/H5GgActivity.java
@@ -2,7 +2,10 @@ package com.ald.fanbei.api.common.enums;
 
 public enum H5GgActivity {
     REGISTER("REGISTER", "注册获得的优惠券"), PICK("PICK", "点击连接获取优惠券"), RECOMMEND("RECOMMEND", "推荐人获得优惠券"),
-    WAIMAI("WAIMAI", "外卖");
+    WAIMAI("WAIMAI", "外卖"),NOFINISH("NOFINISH", "未完成"),ALREADYCONSUME("ALREADYCONSUMED","已消费"),
+    NOCONSUME("NOCONSUME","未消费");
+    
+    
     private String code;
 
     private String description;
