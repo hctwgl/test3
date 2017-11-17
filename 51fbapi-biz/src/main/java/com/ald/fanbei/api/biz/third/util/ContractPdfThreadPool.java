b774a40e77cc12b62b1fc334dc12dfa50b7ba02f
unknown <guoshuaiqiang@Alamap.com>
2017-11-17 22:03:04 +0800

    修改线程池核心线程数和最大线程数

--- 51fbapi-biz/src/main/java/com/ald/fanbei/api/biz/third/util/ContractPdfThreadPool.java
+++ 51fbapi-biz/src/main/java/com/ald/fanbei/api/biz/third/util/ContractPdfThreadPool.java
@@ -3,7 +3,6 @@ package com.ald.fanbei.api.biz.third.util;
 import com.ald.fanbei.api.biz.service.AfContractPdfCreateService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.InitializingBean;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
@@ -20,13 +19,13 @@ import java.util.concurrent.TimeUnit;
  * @注意:本内容仅限于杭州阿拉丁信息科技股份有限公司内部传阅，禁止外泄以及用于其他的商业目的
  */
 @Component("contractPdfThreadPool")
-public class ContractPdfThreadPool implements InitializingBean {
+public class ContractPdfThreadPool{
 
     protected static final Logger logger = LoggerFactory.getLogger(ContractPdfThreadPool.class);
     @Resource
     private AfContractPdfCreateService afContractPdfCreateService;
-    private int nThreads = Runtime.getRuntime().availableProcessors() * 2;
-    private int maxThreads = Runtime.getRuntime().availableProcessors() * 3;
+    private int nThreads = Runtime.getRuntime().availableProcessors() ;
+    private int maxThreads = Runtime.getRuntime().availableProcessors() * 2;
     private ExecutorService service;// 线程池
     private int blockLength = 50; // 队列长度 。每个线程处理50条记录，保证ArrayBlockingQueue正常够用
     public ContractPdfThreadPool() {
@@ -95,45 +94,6 @@ public class ContractPdfThreadPool implements InitializingBean {
         public void run() {
             afContractPdfCreateService.protocolRenewal(userId, borrowId, renewalId, renewalDay, renewalAmount);
         }
-    }
-    /*class ContractTask implements Runnable {
-        private Map<String, String> map;
-
-        public ContractTask(Map<String, String> type) {
-            map = type;
-        }
-
-        @Override
-        public void run() {
-            contractCreate(map);
-        }
-    }
-
-    private void contractCreate(Map<String, String> map) {
-        String contractType = map.get("contractType");
-        Long borrowId = NumberUtil.objToLongDefault(map.get("borrowId"), 0l);
-        Long userId = NumberUtil.objToLongDefault(map.get("userId"), 0l);
-        try {
-            if ("1".equals(contractType)) {//借款协议
-                BigDecimal borrowAmount = NumberUtil.objToBigDecimalDefault(map.get("borrowAmount"), BigDecimal.ZERO);
-                afContractPdfCreateService.protocolCashLoan(borrowId, borrowAmount, userId);
-            } else if ("2".equals(contractType)) {//分期服务协议
-                Integer nper = NumberUtil.objToIntDefault(map.get("nper"), 0);
-                BigDecimal amount = NumberUtil.objToBigDecimalDefault(map.get("amount"), BigDecimal.ZERO);
-                afContractPdfCreateService.protocolInstalment(userId, nper, amount, borrowId);
-            } else if ("3".equals(contractType)) {//续借协议
-                Long renewalId = NumberUtil.objToLongDefault(map.get("renewalId"), 0l);
-                Integer renewalDay = NumberUtil.objToIntDefault(map.get("renewalDay"), 0);
-                BigDecimal renewalAmount = NumberUtil.objToBigDecimalDefault(map.get("amount"), BigDecimal.ZERO);
-                afContractPdfCreateService.protocolRenewal(userId, borrowId, renewalId, renewalDay, renewalAmount);
-            }
-        } catch (Exception e) {
-            logger.error("contractPdfThreadPool nThreads error => {}", e.getMessage());
-        }
-    }*/
-
-    @Override
-    public void afterPropertiesSet() throws Exception {
     }
 
 }
