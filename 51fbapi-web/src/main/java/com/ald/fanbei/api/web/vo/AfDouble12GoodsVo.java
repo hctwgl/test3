27e73e899a740509c6813ad286703f7850b4a135
yanghailong <yanghailong@ALDPC00.Alamap.com>
2017-11-21 11:25:08 +0800

    双十二goodsVo

---
+++ 51fbapi-web/src/main/java/com/ald/fanbei/api/web/vo/AfDouble12GoodsVo.java
@@ -0,0 +1,15 @@
+package com.ald.fanbei.api.web.vo;
+
+public class AfDouble12GoodsVo extends AfGoodsVo {
+
+	private String stockCount;
+
+	public String getStockCount() {
+		return stockCount;
+	}
+
+	public void setStockCount(String stockCount) {
+		this.stockCount = stockCount;
+	}
+	
+}
