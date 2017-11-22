b2e40a71e5a7fa0c1066ab7f774b5e045263b512
gaojibin <gaojibin@edspay.com>
2017-11-22 17:39:52 +0800

    add  getHotGoodsList interface and implemention

--- 51fbapi-dal/src/main/java/com/ald/fanbei/api/dal/dao/AfSupGameDao.java
+++ 51fbapi-dal/src/main/java/com/ald/fanbei/api/dal/dao/AfSupGameDao.java
@@ -1,17 +1,23 @@
 package com.ald.fanbei.api.dal.dao;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
 import com.ald.fanbei.api.dal.domain.AfSupGameDo;
+import com.ald.fanbei.api.dal.domain.dto.GameGoods;
+import com.ald.fanbei.api.dal.domain.dto.GameGoodsGroup;
 
 /**
  * 新人专享Dao
  * 
  * @author gaojibin_temple
  * @version 1.0.0 初始化
- * @date 2017-11-22 13:57:28
- * Copyright 本内容仅限于杭州阿拉丁信息科技股份有限公司内部传阅，禁止外泄以及用于其他的商业目的
+ * @date 2017-11-22 13:57:28 Copyright 本内容仅限于杭州阿拉丁信息科技股份有限公司内部传阅，禁止外泄以及用于其他的商业目的
  */
 public interface AfSupGameDao extends BaseDao<AfSupGameDo, Long> {
 
-    
+    List<GameGoodsGroup> getGoodsList(@Param("type") String type);
 
+    List<GameGoods> getHotGoodsList(@Param("type") String type);
 }
