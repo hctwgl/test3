63c9302b079244928f78db288ffa9a6ed6663d49
zhuyunyun <1090601532@qq.com>
2017-11-23 13:37:24 +0800

    fix 游戏充值

--- 51fbapi-web/src/main/webapp/dist/activity/11/gamePayIndex.js
+++ 51fbapi-web/src/main/webapp/dist/activity/11/gamePayIndex.js
@@ -1,2 +1,2 @@
-"use strict";
+"use strict";var vm=new Vue({el:"#gamePayIndex",data:{content:{},ruleShow:!1},created:function(){this.logData()},methods:{logData:function(){$.ajax({type:"post",url:"/game/pay/goods",data:{type:"GAME"},success:function(e){console.log(e)},error:function(){requestMsg("哎呀，出错了！")}})}}});
 //# sourceMappingURL=../../_srcmap/activity/11/gamePayIndex.js.map
