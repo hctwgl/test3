c1fc5960daf090a6206b264a0349f3010c9fba8b
zhuyunyun <1090601532@qq.com>
2017-11-23 19:46:48 +0800

    add 充值服务页面

---
+++ 51fbapi-web/src/main/webapp/dist/activity/11/recreationPay.js
@@ -0,0 +1,2 @@
+"use strict";var vm=new Vue({el:"#recreationPay",data:{content:{},ruleShow:!1},created:function(){this.logData()},methods:{logData:function(){}}});
+//# sourceMappingURL=../../_srcmap/activity/11/recreationPay.js.map
