7d354270f16b4343078e0210fec96a1808459a6d
gaojibin <gaojibin@edspay.com>
2017-11-27 17:36:12 +0800

    记录游戏充值订单信息（菠萝觅类型）

--- 51fbapi-biz/src/main/java/com/ald/fanbei/api/biz/service/impl/AfSupOrderServiceImpl.java
+++ 51fbapi-biz/src/main/java/com/ald/fanbei/api/biz/service/impl/AfSupOrderServiceImpl.java
@@ -7,16 +7,12 @@ import java.util.Map;
 
 import javax.annotation.Resource;
 
-import org.apache.commons.lang.ObjectUtils;
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.TransactionStatus;
-import org.springframework.transaction.support.TransactionCallback;
 
-import com.ald.fanbei.api.biz.bo.BorrowRateBo;
 import com.ald.fanbei.api.biz.service.AfOrderService;
 import com.ald.fanbei.api.biz.service.AfResourceService;
 import com.ald.fanbei.api.biz.service.AfSupCallbackService;
@@ -25,33 +21,21 @@ import com.ald.fanbei.api.biz.service.AfSupOrderService;
 import com.ald.fanbei.api.biz.service.AfUserAccountService;
 import com.ald.fanbei.api.biz.service.AfUserCouponService;
 import com.ald.fanbei.api.biz.third.util.yitu.EncryptionHelper.MD5Helper;
-import com.ald.fanbei.api.biz.util.BorrowRateBoUtil;
+import com.ald.fanbei.api.biz.util.GeneratorClusterNo;
 import com.ald.fanbei.api.common.Constants;
-import com.ald.fanbei.api.common.enums.AfGoodsStatus;
 import com.ald.fanbei.api.common.enums.CouponStatus;
-import com.ald.fanbei.api.common.enums.PayType;
+import com.ald.fanbei.api.common.enums.OrderType;
 import com.ald.fanbei.api.common.exception.FanbeiException;
 import com.ald.fanbei.api.common.exception.FanbeiExceptionCode;
 import com.ald.fanbei.api.common.util.DateUtil;
-import com.ald.fanbei.api.common.util.NumberUtil;
 import com.ald.fanbei.api.dal.dao.AfSupOrderDao;
 import com.ald.fanbei.api.dal.dao.BaseDao;
-import com.ald.fanbei.api.dal.domain.AfDeUserGoodsDo;
-import com.ald.fanbei.api.dal.domain.AfGoodsDo;
-import com.ald.fanbei.api.dal.domain.AfGoodsPriceDo;
-import com.ald.fanbei.api.dal.domain.AfInterestFreeRulesDo;
 import com.ald.fanbei.api.dal.domain.AfOrderDo;
-import com.ald.fanbei.api.dal.domain.AfResourceDo;
-import com.ald.fanbei.api.dal.domain.AfSchemeGoodsDo;
-import com.ald.fanbei.api.dal.domain.AfShareGoodsDo;
-import com.ald.fanbei.api.dal.domain.AfShareUserGoodsDo;
 import com.ald.fanbei.api.dal.domain.AfSupCallbackDo;
 import com.ald.fanbei.api.dal.domain.AfSupGameDo;
 import com.ald.fanbei.api.dal.domain.AfSupOrderDo;
 import com.ald.fanbei.api.dal.domain.AfUserAccountDo;
-import com.ald.fanbei.api.dal.domain.AfUserAddressDo;
 import com.ald.fanbei.api.dal.domain.dto.AfUserCouponDto;
-import com.itextpdf.text.pdf.PdfStructTreeController.returnType;
 
 /**
  * 新人专享ServiceImpl
@@ -124,46 +108,68 @@ public class AfSupOrderServiceImpl extends ParentServiceImpl<AfSupOrderDo, Long>
     }
 
     @Override
-    public Map<String, Object> addSupOrder(Long userId, Long goodsId, BigDecimal actualAmount, Long couponId, String acctType, String gameName, String userName, BigDecimal goodsNum, String gameType, String gameAcct, String gameArea, String gameSrv, String userIp) {
-	final int nper = 3;
-	Date currTime = new Date();
-	Date gmtPayEnd = DateUtil.addHoures(currTime, Constants.ORDER_PAY_TIME_LIMIT);
-	if (actualAmount.compareTo(BigDecimal.ZERO) == 0) {
+    public Map<String, Object> addSupOrder(Long userId, Long goodsId, BigDecimal actualAmount, Long couponId, String acctType, String gameName, String userName, Integer goodsNum, String gameType, String gameAcct, String gameArea, String gameSrv, String userIp) {
+	// 验证参数须大于零
+	if (actualAmount.compareTo(BigDecimal.ZERO) <= 0 || goodsNum <= 0) {
 	    throw new FanbeiException(FanbeiExceptionCode.PARAM_ERROR);
 	}
 
+	// 获取游戏产品信息
 	AfSupGameDo supGameDo = afSupGameService.getById(goodsId);
 	if (supGameDo == null) {
 	    logger.error("sup game is not exist :" + goodsId);
 	    throw new FanbeiException(FanbeiExceptionCode.GAME_IS_NOT_EXIST);
 	}
 
-	BigDecimal checkActualAmount = supGameDo.getBusinessDiscount().multiply(goodsNum);
-	BigDecimal rebateAmountScale = supGameDo.getOfficalDiscount().subtract(supGameDo.getBusinessDiscount());
-
-	AfOrderDo afOrder = new AfOrderDo();
-	afOrder.setUserId(userId);
-	afOrder.setGoodsPriceId(goodsId);
-
+	// 获取优惠卷信息
+	BigDecimal couponAmount = new BigDecimal(0);
 	if (couponId > 0) {
 	    AfUserCouponDto couponDo = afUserCouponService.getUserCouponById(couponId);
 	    if (couponDo.getGmtEnd().before(new Date()) || StringUtils.equals(couponDo.getStatus(), CouponStatus.EXPIRE.getCode())) {
 		logger.error("coupon end less now :" + couponId);
 		throw new FanbeiException(FanbeiExceptionCode.USER_COUPON_ERROR);
 	    }
+	    couponAmount = couponDo.getAmount();
+	}
+	// 验证优惠卷
+	BigDecimal checkActualAmount = supGameDo.getBusinessDiscount().multiply(new BigDecimal(goodsNum));
+	BigDecimal rebateAmountScale = supGameDo.getOfficalDiscount().subtract(supGameDo.getBusinessDiscount());
+	checkActualAmount = checkActualAmount.subtract(couponAmount);
+	if (checkActualAmount.compareTo(BigDecimal.ZERO) <= 0) {
+	    logger.error("checkActualAmount less than zero:couponId " + couponId + " ,goodsId:" + goodsId + " ,checkActualAmount:" + checkActualAmount);
+	    throw new FanbeiException(FanbeiExceptionCode.USER_COUPON_ERROR);
+	}
+	// 验证客户端传递金额参数
+	if (actualAmount.compareTo(checkActualAmount) != 0) {
+	    logger.error("checkActualAmount actualAmount " + actualAmount + " ,checkActualAmount:" + checkActualAmount);
+	    throw new FanbeiException(FanbeiExceptionCode.PARAM_ERROR);
+	}
+
+	if (couponAmount.compareTo(BigDecimal.ZERO) > 0) {
 	    afUserCouponService.updateUserCouponSatusUsedById(couponId);
 	}
 
+	// 添加订单信息
+	AfOrderDo afOrder = new AfOrderDo();
+	afOrder.setUserId(userId);
+	afOrder.setGoodsPriceId(goodsId);
 	afOrder.setActualAmount(actualAmount);
-	//afOrder.setSaleAmount();
-	//afOrder.setRebateAmount(rebateAmount);
-	afOrder.setGmtCreate(currTime);
-	afOrder.setGmtPayEnd(gmtPayEnd);
-
+	afOrder.setSaleAmount(actualAmount);
+	afOrder.setRebateAmount(rebateAmountScale);
+	afOrder.setGmtCreate(new Date());
+	afOrder.setGmtPayEnd(DateUtil.addHoures(new Date(), Constants.ORDER_PAY_TIME_LIMIT));
+	afOrder.setPriceAmount(actualAmount);
+	afOrder.setGoodsIcon(supGameDo.getImage());
+	afOrder.setGoodsName(supGameDo.getName());
+	afOrder.setGoodsId(goodsId);
+	afOrder.setOrderType(OrderType.BOLUOME.getCode());
+	afOrder.setOrderNo(generatorClusterNo.getOrderNo(OrderType.SELFSUPPORT));
+	afOrder.setCount(goodsNum);
 	afOrder.setUserCouponId(couponId);
 	AfUserAccountDo userAccountInfo = afUserAccountService.getUserAccountByUserId(userId);
 	afOrder.setAuAmount(userAccountInfo.getAuAmount());
 	afOrder.setUsedAmount(userAccountInfo.getUsedAmount());
+	afOrder.setThirdDetailUrl("");
 	afOrderService.createOrder(afOrder);
 
 	Map<String, Object> data = new HashMap<String, Object>();
@@ -189,4 +195,6 @@ public class AfSupOrderServiceImpl extends ParentServiceImpl<AfSupOrderDo, Long>
     private AfResourceService afResourceService;
     @Autowired
     private AfSupGameService afSupGameService;
+    @Autowired
+    private GeneratorClusterNo generatorClusterNo;
 }
