0880b33d2243986dd2ccc811296eb053396a21a5
jiangrongbo <jiangrongbo@edspay.com>
2017-11-28 15:54:05 +0800

    Ab测试优化

--- 51fbapi-web/src/main/java/com/ald/fanbei/api/abtest/controller/LoginAbTestController.java
+++ 51fbapi-web/src/main/java/com/ald/fanbei/api/abtest/controller/LoginAbTestController.java
@@ -26,6 +26,7 @@ import com.ald.fanbei.api.web.controller.AbTestController;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import com.google.common.collect.Maps;
 
 /**
  *
@@ -51,7 +52,7 @@ public class LoginAbTestController extends AbTestController {
 	@Override
 	protected void abTest(RequestDataVo requestDataVo, FanbeiContext context, HttpServletRequest httpServletRequest) {
 		AfTestManageDo testManage = afTestManageService.getTestInfoByTag("LOGIN_TYPE");
-		Map<String,Object> params = new HashMap();
+		Map<String,Object> params = Maps.newHashMap();
 		params.put("loginType","old");
 		String deviceId = (String) requestDataVo.getParams().get("deviceId");
 		try {
