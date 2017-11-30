e01645220be34602bd3394eb876de22876f7e498
chefeipeng <chefeipeng@chefeipeng.Alamap.com>
2017-11-30 15:48:51 +0800

    限时抢购

--- 51fbapi-web/src/main/java/com/ald/fanbei/api/web/apph5/controller/AppH5FlashSaleController.java
+++ 51fbapi-web/src/main/java/com/ald/fanbei/api/web/apph5/controller/AppH5FlashSaleController.java
@@ -292,7 +292,7 @@ public class AppH5FlashSaleController extends BaseController {
 		Long goodsId = NumberUtil.objToLongDefault(request.getParameter("goodsId"),0l);
 		Integer sumCount = afGoodsPriceService.selectSumStock(goodsId);
 		if(null == sumCount || sumCount == 0){
-			throw new FanbeiException(FanbeiExceptionCode.SOLD_OUT);
+			return  H5CommonResponse.getNewInstance(false, "商品库存不足，请重新购买~", "", goodsId).toString();
 		}
 		return  H5CommonResponse.getNewInstance(true, "成功", "", goodsId).toString();
 	}
