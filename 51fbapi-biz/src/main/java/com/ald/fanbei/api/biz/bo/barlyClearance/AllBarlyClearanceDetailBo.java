8da78ab327b9e693c2faa52544270c96cc16a0cd
hongzhengpei <805328895@qq.com>
2017-11-30 15:11:05 +0800

    提前结清

--- 51fbapi-biz/src/main/java/com/ald/fanbei/api/biz/bo/barlyClearance/AllBarlyClearanceDetailBo.java
+++ 51fbapi-biz/src/main/java/com/ald/fanbei/api/biz/bo/barlyClearance/AllBarlyClearanceDetailBo.java
@@ -8,6 +8,7 @@ import java.math.BigDecimal;
  * @注意：本内容仅限于杭州阿拉丁信息科技股份有限公司内部传阅，禁止外泄以及用于其他的商业目的
  */
 public class AllBarlyClearanceDetailBo {
+    private Long billId;
     private int nper;  //期数
     private BigDecimal amount; //金额
     private BigDecimal poundAmount; //手续费
@@ -70,5 +71,13 @@ public class AllBarlyClearanceDetailBo {
 
     public void setOverdue(int overdue) {
         this.overdue = overdue;
+    }
+
+    public Long getBillId() {
+        return billId;
+    }
+
+    public void setBillId(Long billId) {
+        this.billId = billId;
     }
 }
