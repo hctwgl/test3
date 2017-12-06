da80b6219cc33cd3df2c89e33f30d04b2807e95a
zhuyunyun <1090601532@qq.com>
2017-12-06 11:26:24 +0800

    fix css

--- 51fbapi-web/src/main/webapp/build/activity/11/recreationPayIndex.js
+++ 51fbapi-web/src/main/webapp/build/activity/11/recreationPayIndex.js
@@ -22,11 +22,13 @@ let vm = new Vue({
                     console.log(self.content);
                     self.$nextTick(function(){
                         let windowWidth=$(window).width();
-                        let hotContWidth=windowWidth*0.95;
+                        //let hotContWidth=windowWidth*0.95;
                         let lastLiMarRight=windowWidth*0.056; //最后一个li的margin-right
                         let liLen=$('.hotGameList li').length; //hotCont宽度
-                        let liWidth=parseInt(0.266*hotContWidth); //li宽度
-                        let liMarRight=parseInt(0.072*hotContWidth);//每一个li的margin-right
+                        //let liWidth=parseInt(0.266*hotContWidth); //li宽度
+                        //let liMarRight=parseInt(0.072*hotContWidth);//每一个li的margin-right
+                        let liWidth=parseInt(4*windowWidth/15); //li宽度
+                        let liMarRight=parseInt(0.072*windowWidth);//每一个li的margin-right
                         let ulWidth=(liWidth*liLen)+(liLen-1)*liMarRight+lastLiMarRight;
                         $('.hotGameList li').width(liWidth);
                         $('.hotGameList li').css('margin-right',liMarRight);
