9c1d821f95b05e3ade6db7343017b0429657271b
chengkang <chengkang@edspay.com>
2017-12-07 14:43:59 +0800

    催收线下还款校验错误修复

--- 51fbapi-biz/src/main/java/com/ald/fanbei/api/biz/third/util/CollectionSystemUtil.java
+++ 51fbapi-biz/src/main/java/com/ald/fanbei/api/biz/third/util/CollectionSystemUtil.java
@@ -228,7 +228,7 @@ public class CollectionSystemUtil extends AbstractThird {
 					//还款金额校验
 					AfBorrowCashDo afBorrowCashDo = afBorrowCashService.getBorrowCashInfoByBorrowNo(borrowNo);
 			        BigDecimal amount = BigDecimalUtil.add(afBorrowCashDo.getAmount(),afBorrowCashDo.getOverdueAmount(),afBorrowCashDo.getRateAmount() ,afBorrowCashDo.getSumOverdue(), afBorrowCashDo.getSumRate()).subtract(afBorrowCashDo.getRepayAmount());
-			        if(NumberUtil.objToBigDecimalDefault(repayAmount, BigDecimal.ZERO).compareTo(amount) > 0) {
+			        if(NumberUtil.objToBigDecimalDivideOnehundredDefault(repayAmount, BigDecimal.ZERO).compareTo(amount) > 0) {
 			            logger.info("offlineRepaymentNotify is fail,borrowCashId= "+afBorrowCashDo.getRid()+",reqInfo="+StringUtil.appendStrs(repayNo, borrowNo, repayType, repayTime, repayAmount, restAmount, tradeNo, isBalance));
 			            notifyRespBo.resetMsgInfo(FanbeiThirdRespCode.FAILED);
 			            return notifyRespBo;
