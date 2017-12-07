044f1636c6cb35d07d9d19c35012547a8a4792dc
maqiaopan <maqiaopan@edspay.com>
2017-12-07 13:37:29 +0800

    add Controller

---
+++ 51fbapi-web/src/main/java/com/ald/fanbei/api/web/apph5/controller/AppH5DoubleEggsController.java
@@ -0,0 +1,57 @@
+package com.ald.fanbei.api.web.apph5.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import com.ald.fanbei.api.common.FanbeiContext;
+import com.ald.fanbei.api.common.exception.FanbeiException;
+import com.ald.fanbei.api.common.exception.FanbeiExceptionCode;
+import com.ald.fanbei.api.web.common.BaseController;
+import com.ald.fanbei.api.web.common.BaseResponse;
+import com.ald.fanbei.api.web.common.RequestDataVo;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+
+/**
+ * @Title: AppH5DoubleEggsController.java
+ * @Package com.ald.fanbei.api.web.apph5.controller
+ * @Description: TODO(用一句话描述该文件做什么)
+ * @Copyright (c) 浙江阿拉丁电子商务股份有限公司 All Rights Reserved.
+ * @author qiao
+ * @date 2017年12月7日 下午1:26:54
+ * @version V1.0
+ */
+
+public class AppH5DoubleEggsController extends BaseController {
+	
+
+	@Override
+	public String checkCommonParam(String reqData, HttpServletRequest request, boolean isForQQ) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public RequestDataVo parseRequestData(String requestData, HttpServletRequest request) {
+		try {
+			RequestDataVo reqVo = new RequestDataVo();
+
+			JSONObject jsonObj = JSON.parseObject(requestData);
+			reqVo.setId(jsonObj.getString("id"));
+			reqVo.setMethod(request.getRequestURI());
+			reqVo.setSystem(jsonObj);
+
+			return reqVo;
+		} catch (Exception e) {
+			throw new FanbeiException("参数格式错误" + e.getMessage(), FanbeiExceptionCode.REQUEST_PARAM_ERROR);
+		}
+
+	}
+
+	@Override
+	public BaseResponse doProcess(RequestDataVo requestDataVo, FanbeiContext context,
+			HttpServletRequest httpServletRequest) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
