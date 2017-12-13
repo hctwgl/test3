68f1d8abcd9958c78b60d9ad57407de4e70fa90a
unknown <guoshuaiqiang@Alamap.com>
2017-12-13 16:28:11 +0800

    订单记录查询

--- 51fbapi-web/src/main/java/com/ald/fanbei/api/web/api/legalborrow/GetBorrowLegalChargeInfoApi.java
+++ 51fbapi-web/src/main/java/com/ald/fanbei/api/web/api/legalborrow/GetBorrowLegalChargeInfoApi.java
@@ -1,17 +1,13 @@
 package com.ald.fanbei.api.web.api.legalborrow;
 
-import com.ald.fanbei.api.biz.service.AfBorrowLegalOrderCashService;
-import com.ald.fanbei.api.biz.service.AfBorrowLegalOrderService;
+import com.ald.fanbei.api.biz.service.AfResourceService;
 import com.ald.fanbei.api.common.FanbeiContext;
 import com.ald.fanbei.api.common.exception.FanbeiExceptionCode;
-import com.ald.fanbei.api.common.util.NumberUtil;
-import com.ald.fanbei.api.dal.domain.AfBorrowLegalOrderCashDo;
-import com.ald.fanbei.api.dal.domain.AfBorrowLegalOrderDo;
+import com.ald.fanbei.api.dal.domain.AfResourceDo;
 import com.ald.fanbei.api.web.common.ApiHandle;
 import com.ald.fanbei.api.web.common.ApiHandleResponse;
 import com.ald.fanbei.api.web.common.RequestDataVo;
 import com.google.common.collect.Maps;
-import org.apache.commons.lang.ObjectUtils;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
@@ -23,13 +19,14 @@ import java.util.Map;
  * @类描述：查询用户订单记录
  * @注意：本内容仅限于杭州阿拉丁信息科技股份有限公司内部传阅，禁止外泄以及用于其他的商业目的
  */
-@Component("getBorrowLegalOrderDetailApi")
-public class GetBorrowLegalOrderDetailApi implements ApiHandle {
+@Component("getBorrowLegalChargeInfoApi")
+public class GetBorrowLegalChargeInfoApi implements ApiHandle {
 
     @Resource
-    AfBorrowLegalOrderService afBorrowLegalOrderService;
-    @Resource
-    AfBorrowLegalOrderCashService afBorrowLegalOrderCashService;
+    private AfResourceService afResourceService;
+
+    private static final String BORROW_RATE = "BORROW_RATE";
+    private static final String BORROW_CASH_INFO_LEGAL = "BORROW_CASH_INFO_LEGAL";
 
     @Override
     public ApiHandleResponse process(RequestDataVo requestDataVo, FanbeiContext context, HttpServletRequest request) {
@@ -37,17 +34,18 @@ public class GetBorrowLegalOrderDetailApi implements ApiHandle {
         ApiHandleResponse resp = new ApiHandleResponse(requestDataVo.getId(), FanbeiExceptionCode.SUCCESS);
         Map<String, Object> data = Maps.newHashMap();
         resp.setResponseData(data);
-        Long orderId = NumberUtil.objToLong(requestDataVo.getParams().get("orderId"));
+
         // 判断用户是否登录
         Long userId = context.getUserId();
         if (userId == null) {
             return new ApiHandleResponse(requestDataVo.getId(), FanbeiExceptionCode.REQUEST_PARAM_TOKEN_ERROR);
         }
-        AfBorrowLegalOrderDo afBorrowLegalOrderDo = afBorrowLegalOrderService.getLastBorrowLegalOrderById(orderId);
-        AfBorrowLegalOrderCashDo afBorrowLegalOrderCashDo = afBorrowLegalOrderCashService.getBorrowLegalOrderCashByBorrowLegalOrderId(orderId);
-        data.put("afBorrowLegalOrderCashDo", afBorrowLegalOrderCashDo);
-        data.put("afBorrowLegalOrderDo", afBorrowLegalOrderDo);
-        resp.setResponseData(data);
+        AfResourceDo afResourceDo = afResourceService.getConfigByTypesAndSecType(BORROW_RATE, BORROW_CASH_INFO_LEGAL);
+        if (afResourceDo != null) {
+            data.put("description", afResourceDo.getDescription());
+        } else {
+            data.put("description", "");
+        }
         return resp;
     }
 
--- 51fbapi-web/src/main/java/com/ald/fanbei/api/web/api/legalborrow/GetBorrowLegalOrderInfoApi.java
+++ 51fbapi-web/src/main/java/com/ald/fanbei/api/web/api/legalborrow/GetBorrowLegalOrderInfoApi.java
@@ -1,13 +1,5 @@
 package com.ald.fanbei.api.web.api.legalborrow;
 
-import java.util.List;
-import java.util.Map;
-
-import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
-
-import org.springframework.stereotype.Component;
-
 import com.ald.fanbei.api.biz.service.AfBorrowLegalOrderService;
 import com.ald.fanbei.api.common.FanbeiContext;
 import com.ald.fanbei.api.common.exception.FanbeiExceptionCode;
@@ -18,41 +10,47 @@ import com.ald.fanbei.api.web.common.ApiHandle;
 import com.ald.fanbei.api.web.common.ApiHandleResponse;
 import com.ald.fanbei.api.web.common.RequestDataVo;
 import com.google.common.collect.Maps;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.Map;
 
 /**
- * @类描述：查询用户订单记录
- * 
  * @author Jiang Rongbo 2017年3月25日下午1:06:18
+ * @类描述：查询用户订单记录
  * @注意：本内容仅限于杭州阿拉丁信息科技股份有限公司内部传阅，禁止外泄以及用于其他的商业目的
  */
 @Component("getBorrowLegalOrderInfoApi")
 public class GetBorrowLegalOrderInfoApi implements ApiHandle {
 
-	@Resource
-	AfBorrowLegalOrderService afBorrowLegalOrderService;
-	
-	@Override
-	public ApiHandleResponse process(RequestDataVo requestDataVo, FanbeiContext context, HttpServletRequest request) {
+    @Resource
+    AfBorrowLegalOrderService afBorrowLegalOrderService;
 
-		ApiHandleResponse resp = new ApiHandleResponse(requestDataVo.getId(), FanbeiExceptionCode.SUCCESS);
-		Map<String, Object> data = Maps.newHashMap();
-		resp.setResponseData(data);
-		
-		// 判断用户是否登录
-		Long userId = context.getUserId();
-		if (userId == null) {
-			return new ApiHandleResponse(requestDataVo.getId(), FanbeiExceptionCode.REQUEST_PARAM_TOKEN_ERROR);
-		}
-		// 获取查询页码
-		int pageNo = NumberUtil.objToIntDefault(requestDataVo.getParams().get("pageNo"), 0);
+    @Override
+    public ApiHandleResponse process(RequestDataVo requestDataVo, FanbeiContext context, HttpServletRequest request) {
 
-		AfBorrowLegalOrderQuery query = new AfBorrowLegalOrderQuery();
-		query.setUserId(userId);
-		query.setPageNo(pageNo);
-		// 查询用户订单记录
-		List<AfBorrowLegalOrderDo> borrowLegalOrdersList = afBorrowLegalOrderService.getUserBorrowLegalOrderList(query);
-		data.put("orderList", borrowLegalOrdersList);
-		return resp;
-	}
+        ApiHandleResponse resp = new ApiHandleResponse(requestDataVo.getId(), FanbeiExceptionCode.SUCCESS);
+        Map<String, Object> data = Maps.newHashMap();
+        resp.setResponseData(data);
+
+        // 判断用户是否登录
+        Long userId = context.getUserId();
+        if (userId == null) {
+            return new ApiHandleResponse(requestDataVo.getId(), FanbeiExceptionCode.REQUEST_PARAM_TOKEN_ERROR);
+        }
+        // 获取查询页码
+        int pageNo = NumberUtil.objToIntDefault(requestDataVo.getParams().get("pageNo"), 0);
+
+        AfBorrowLegalOrderQuery query = new AfBorrowLegalOrderQuery();
+        query.setUserId(userId);
+        query.setPageNo(pageNo);
+        // 查询用户订单记录
+        List<AfBorrowLegalOrderDo> borrowLegalOrdersList = afBorrowLegalOrderService.getUserBorrowLegalOrderList(query);
+        data.put("orderList", borrowLegalOrdersList);
+        resp.setResponseData(data);
+        return resp;
+    }
 
 }
