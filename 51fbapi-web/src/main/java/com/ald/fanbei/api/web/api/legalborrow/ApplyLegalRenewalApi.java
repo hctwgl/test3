081f894341cc0dc00a034cc24550a1bc50f52014
yanghailong <yanghailong@ALDPC00.Alamap.com>
2017-12-15 13:53:09 +0800

    续期-修改手续费&利息计算

--- 51fbapi-web/src/main/java/com/ald/fanbei/api/web/api/legalborrow/ApplyLegalRenewalApi.java
+++ 51fbapi-web/src/main/java/com/ald/fanbei/api/web/api/legalborrow/ApplyLegalRenewalApi.java
@@ -198,9 +198,9 @@ public class ApplyLegalRenewalApi implements ApiHandle {
 			//续借过
 			AfRenewalDetailDo renewalDetail = afRenewalLegalDetailService.getLastRenewalDetailByBorrowId(afBorrowCashDo.getRid());
 			// 续期手续费 = 上期续借金额 * 上期续借天数 * 借款手续费率  / 360
-			borrowPoundage = renewalDetail.getRenewalAmount().multiply(allowRenewalDay).multiply(newServiceRate).divide(oneYeayDays).setScale(2, RoundingMode.HALF_UP);
+			borrowPoundage = renewalDetail.getRenewalAmount().multiply(allowRenewalDay).multiply(newServiceRate).divide(oneYeayDays, 2, RoundingMode.HALF_UP);
 			// 续期利息 = 上期续借金额 * 上期续借天数  * 借款利率 / 360
-			borrowRateAmount = renewalDetail.getRenewalAmount().multiply(allowRenewalDay).multiply(newRate).divide(oneYeayDays).setScale(2, RoundingMode.HALF_UP);
+			borrowRateAmount = renewalDetail.getRenewalAmount().multiply(allowRenewalDay).multiply(newRate).divide(oneYeayDays, 2, RoundingMode.HALF_UP);
 		}else {
 			//未续借过
 			borrowPoundage = afBorrowCashDo.getPoundage();
