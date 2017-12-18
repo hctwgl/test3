898c3a37f7a725eff783d1389ea9860d7c8cd937
朱江丰 <zhujiangfeng@edspay.com>
2017-12-18 22:21:33 +0800

    4.0.2-线下还款

--- 51fbapi-biz/src/main/java/com/ald/fanbei/api/biz/service/impl/AfBorrowLegalRepaymentServiceImpl.java
+++ 51fbapi-biz/src/main/java/com/ald/fanbei/api/biz/service/impl/AfBorrowLegalRepaymentServiceImpl.java
@@ -189,6 +189,7 @@ public class AfBorrowLegalRepaymentServiceImpl extends ParentServiceImpl<AfBorro
 		bo.userId = orderCashDo.getUserId();
 		bo.userDo = afUserAccountDao.getUserAccountInfoByUserId(bo.userId);
 		
+		bo.cardId = (long) -4;
 		bo.repaymentAmount = NumberUtil.objToBigDecimalDivideOnehundredDefault(repayAmount, BigDecimal.ZERO);
 		bo.actualAmount =  bo.repaymentAmount;
 		bo.borrowId = orderCashDo.getBorrowId();
@@ -868,6 +869,8 @@ public class AfBorrowLegalRepaymentServiceImpl extends ParentServiceImpl<AfBorro
 			repayment.setCardNo("");
 			repayment.setCardName(Constants.DEFAULT_ZFB_PAY_NAME);
 		} else if (bo.cardId == -4) {
+			repayment.setCardNo("");
+			repayment.setCardName(Constants.DEFAULT_OFFLINE_PAY_NAME);
 		} else {
 			AfBankUserBankDto bank = afUserBankcardDao.getUserBankcardByBankId(bo.cardId);
 			repayment.setCardNo(bank.getCardNumber());
