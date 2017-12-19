2dad569021ea562752d7f897cf86a1d0641fe53b
maqiaopan <maqiaopan@edspay.com>
2017-12-19 11:20:17 +0800

    the start for right now

---
+++ 51fbapi-web/src/main/java/com/ald/fanbei/api/web/apph5/controller/AppH5AllSearchController.java
@@ -0,0 +1,61 @@
+package com.ald.fanbei.api.web.apph5.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ald.fanbei.api.common.FanbeiContext;
+import com.ald.fanbei.api.common.exception.FanbeiException;
+import com.ald.fanbei.api.common.exception.FanbeiExceptionCode;
+import com.ald.fanbei.api.web.common.BaseController;
+import com.ald.fanbei.api.web.common.BaseResponse;
+import com.ald.fanbei.api.web.common.RequestDataVo;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+
+/**  
+ * @Title: AppH5AllSearchController.java
+ * @Package com.ald.fanbei.api.web.apph5.controller
+ * @Description: TODO(用一句话描述该文件做什么)
+ * @Copyright (c) 浙江阿拉丁电子商务股份有限公司 All Rights Reserved.
+ * @author qiao
+ * @date 2017年12月18日 下午6:10:04
+ * @version V1.0  
+ */
+@RestController
+@RequestMapping(value = "/appH5DoubleEggs", produces = "application/json;charset=UTF-8")
+public class AppH5AllSearchController extends BaseController{
+
+	@Override
+	public String checkCommonParam(String reqData, HttpServletRequest request, boolean isForQQ) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public RequestDataVo parseRequestData(String requestData, HttpServletRequest request) {
+		try {
+			RequestDataVo reqVo = new RequestDataVo();
+
+			JSONObject jsonObj = JSON.parseObject(requestData);
+			reqVo.setId(jsonObj.getString("id"));
+			reqVo.setMethod(request.getRequestURI());
+			reqVo.setSystem(jsonObj);
+
+			return reqVo;
+		} catch (Exception e) {
+			throw new FanbeiException("参数格式错误" + e.getMessage(), FanbeiExceptionCode.REQUEST_PARAM_ERROR);
+		}
+
+	}
+
+	@Override
+	public BaseResponse doProcess(RequestDataVo requestDataVo, FanbeiContext context,
+			HttpServletRequest httpServletRequest) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+
+}
