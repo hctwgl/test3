b653920df7ef15c00e41351cf54b0ad7126a8e6b
chefeipeng <chefeipeng@chefeipeng.Alamap.com>
2017-12-27 16:14:44 +0800

    短信文案

--- 51fbapi-biz/src/main/java/com/ald/fanbei/api/biz/third/util/JpushUtil.java
+++ 51fbapi-biz/src/main/java/com/ald/fanbei/api/biz/third/util/JpushUtil.java
@@ -48,8 +48,8 @@ public class JpushUtil extends AbstractThird{
 		if	(jpushClient == null){
 //			String appKey = AesUtil.decrypt(ConfigProperties.get(Constants.CONFKEY_JPUSH_APPKEY), ConfigProperties.get(Constants.CONFKEY_AES_KEY));
 //			String appSecret = AesUtil.decrypt(ConfigProperties.get(Constants.CONFKEY_JPUSH_SECRET), ConfigProperties.get(Constants.CONFKEY_AES_KEY));
-			String appSecret = "713d3b209185a3dbd069dcce";
-			String appKey = "212354c60466f1c6732059f2";
+			String appKey = "713d3b209185a3dbd069dcce";
+			String appSecret = "212354c60466f1c6732059f2";
 			jpushClient = new JPushClient(appSecret,appKey);
 			return jpushClient;
 		}
