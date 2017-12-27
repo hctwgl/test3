d304caa5f142d64249a51999fa735ee1be8d0a6b
gaojibin <gaojibin@edspay.com>
2017-12-27 16:31:57 +0800

    添加自动生成代码

---
+++ 51fbapi-biz/src/main/java/com/ald/fanbei/api/biz/service/AfUserDrawService.java
@@ -0,0 +1,15 @@
+package com.ald.fanbei.api.biz.service;
+
+import com.ald.fanbei.api.dal.domain.AfUserDrawDo;
+
+/**
+ * 年会抽奖Service
+ * 
+ * @author gaojibin
+ * @version 1.0.0 初始化
+ * @date 2017-12-27 16:31:00
+ * Copyright 本内容仅限于杭州阿拉丁信息科技股份有限公司内部传阅，禁止外泄以及用于其他的商业目的
+ */
+public interface AfUserDrawService extends ParentService<AfUserDrawDo, Long>{
+
+}
---
+++ 51fbapi-dal/src/main/java/com/ald/fanbei/api/dal/dao/AfUserDrawDao.java
@@ -0,0 +1,17 @@
+package com.ald.fanbei.api.dal.dao;
+
+import com.ald.fanbei.api.dal.domain.AfUserDrawDo;
+
+/**
+ * 年会抽奖Dao
+ * 
+ * @author gaojibin
+ * @version 1.0.0 初始化
+ * @date 2017-12-27 16:31:00
+ * Copyright 本内容仅限于杭州阿拉丁信息科技股份有限公司内部传阅，禁止外泄以及用于其他的商业目的
+ */
+public interface AfUserDrawDao extends BaseDao<AfUserDrawDo, Long> {
+
+    
+
+}
