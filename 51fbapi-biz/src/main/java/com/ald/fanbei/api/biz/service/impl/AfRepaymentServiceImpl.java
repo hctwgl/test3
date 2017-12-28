efcbd658a894da650695e93baa0b949c045140d9
hongzhengpei <805328895@qq.com>
2017-12-28 21:04:40 +0800

    还款逻辑BUG修复

--- 51fbapi-biz/src/main/java/com/ald/fanbei/api/biz/service/impl/AfRepaymentServiceImpl.java
+++ 51fbapi-biz/src/main/java/com/ald/fanbei/api/biz/service/impl/AfRepaymentServiceImpl.java
@@ -300,6 +300,7 @@ public class AfRepaymentServiceImpl extends BaseService implements AfRepaymentSe
             afRepaymentDao.addRepayment(repayment);
             //addRepaymentyDetail(totalAmount,repaymentAmount,repayment.getRid());
             repayment.setStatus(RepaymentStatus.PROCESS.getCode());
+            afUserAmountService.addUseAmountDetail(repayment);
             afUserAmountService.updateUserAmount(AfUserAmountProcessStatus.PROCESS, repayment);
             try {
                 if (StringUtil.equals("sysJob", clientIp)) {
