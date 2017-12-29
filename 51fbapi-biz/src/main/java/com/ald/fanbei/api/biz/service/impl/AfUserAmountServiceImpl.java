9776e7216abdc1e77c868fa826c64ed1e8a694f8
hongzhengpei <805328895@qq.com>
2017-12-29 14:12:14 +0800

    修改自行支付

--- 51fbapi-biz/src/main/java/com/ald/fanbei/api/biz/service/impl/AfUserAmountServiceImpl.java
+++ 51fbapi-biz/src/main/java/com/ald/fanbei/api/biz/service/impl/AfUserAmountServiceImpl.java
@@ -249,7 +249,8 @@ public class AfUserAmountServiceImpl implements AfUserAmountService {
 
         afUserAmountDetailDao.addUserAmountDetail(buildAmountDetail(afUserAmountDo.getId(), BigDecimal.ZERO.subtract(youhuijuan), 1, AfUserAmountDetailType.YOUHUIJUANGDIKOU));
 
-        afUserAmountDetailDao.addUserAmountDetail(buildAmountDetail(afUserAmountDo.getId(), bankPay, 1, AfUserAmountDetailType.ZHIJIEZHIFU));
+//        afUserAmountDetailDao.addUserAmountDetail(buildAmountDetail(afUserAmountDo.getId(), bankPay, 1, AfUserAmountDetailType.ZHIJIEZHIFU));
+        afUserAmountDetailDao.addUserAmountDetail(buildAmountDetail(afUserAmountDo.getId(), afUserAmountDo.getAmount(), 1, AfUserAmountDetailType.ZHIJIEZHIFU));
 
         return 1;
     }
