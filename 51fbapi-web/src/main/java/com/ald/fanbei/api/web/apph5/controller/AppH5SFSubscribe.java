013456e9ecd5f0058066d0c604c291f719e254ae
maqiaopan <maqiaopan@edspay.com>
2018-01-02 13:52:12 +0800

    add tag

---
+++ 51fbapi-web/src/main/java/com/ald/fanbei/api/web/apph5/controller/AppH5SFSubscribe.java
@@ -0,0 +1,14 @@
+package com.ald.fanbei.api.web.apph5.controller;
+
+public class AppH5SFSubscribe {
+
+	/**  
+	 * @Title: AppH5SFSubscribe.java
+	 * @Package com.ald.fanbei.api.web.apph5.controller
+	 * @Description: TODO(用一句话描述该文件做什么)
+	 * @Copyright (c) 浙江阿拉丁电子商务股份有限公司 All Rights Reserved.
+	 * @author qiao
+	 * @date 2018年1月2日 上午11:17:14
+	 * @version V1.0  
+	 */
+}
