4fbf61c7aedd611f418a3621287dc60d141c87ab
unknown <fanmanfu@Alamap.com>
2018-01-03 12:51:19 +0800

    添加一个新接口，往催收传递实际应还金额

--- 51fbapi-web/src/main/java/com/ald/fanbei/api/web/third/controller/CollectionController.java
+++ 51fbapi-web/src/main/java/com/ald/fanbei/api/web/third/controller/CollectionController.java
@@ -1,16 +1,13 @@
 package com.ald.fanbei.api.web.third.controller;
 
+import java.text.ParseException;
 import java.util.Date;
+
 import org.apache.commons.lang.StringUtils;
+
 import com.ald.fanbei.api.biz.bo.CollectionOperatorNotifyRespBo;
 import com.ald.fanbei.api.biz.bo.CollectionUpdateResqBo;
 import com.ald.fanbei.api.biz.service.*;
-import com.ald.fanbei.api.biz.service.AfBorrowBillService;
-import com.ald.fanbei.api.biz.service.AfBorrowCashOverdueService;
-import com.ald.fanbei.api.biz.service.AfBorrowCashService;
-import com.ald.fanbei.api.biz.service.AfBorrowLegalOrderCashService;
-import com.ald.fanbei.api.biz.service.AfIdNumberService;
-import com.ald.fanbei.api.biz.service.AfRepaymentBorrowCashService;
 import com.ald.fanbei.api.biz.third.util.CollectionSystemUtil;
 import com.ald.fanbei.api.common.Constants;
 import com.ald.fanbei.api.common.enums.AfBorrowCashStatus;
@@ -21,6 +18,7 @@ import com.ald.fanbei.api.dal.domain.*;
 import com.ald.fanbei.api.dal.domain.dto.AfOverdueOrderDto;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+
 import org.apache.commons.lang.ObjectUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -32,6 +30,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.HashMap;
@@ -103,12 +102,10 @@ public class CollectionController {
 	 * @param response
 	 * @return
 	 */
-	@RequestMapping(value = { "/findBorrowCashByBorrowNo" }, method = RequestMethod.POST)
+	@RequestMapping(value = { "/findBorrowCashByBorrowNoV1" }, method = RequestMethod.POST)
 	@ResponseBody
-	public CollectionUpdateResqBo findBorrowCashByBorrowNo(HttpServletRequest request, HttpServletResponse response){
+	public CollectionUpdateResqBo findBorrowCashByBorrowNoV1(HttpServletRequest request, HttpServletResponse response){
 		String data = ObjectUtils.toString(request.getParameter("data"));
-	public CollectionUpdateResqBo findBorrowCashByBorrowNo(HttpServletRequest request, HttpServletResponse response) {
-		String borrowNo = ObjectUtils.toString(request.getParameter("data"));
 		String timestamp = ObjectUtils.toString(request.getParameter("timestamp"));
 		String sign1 = ObjectUtils.toString(request.getParameter("sign"));
 
@@ -128,11 +125,8 @@ public class CollectionController {
 
 		Map<String,String> map=new HashMap<String,String>();
 		CollectionUpdateResqBo updteBo=new CollectionUpdateResqBo();
-		try{
-		logger.info("findBorrowCashByBorrowNo data=" + borrowNo + ",timestamp=" + timestamp + ",sign1=" + sign1 + "");
-
-		Map<String, String> map = new HashMap<String, String>();
-		CollectionUpdateResqBo updteBo = new CollectionUpdateResqBo();
+	
+	
 		try {
 			AfBorrowCashDo afBorrowCashDo = borrowCashService.getBorrowCashInfoByBorrowNo(borrowNo);
 			if (afBorrowCashDo == null) {
@@ -217,8 +211,8 @@ public class CollectionController {
 					BigDecimal actual_amount= BigDecimalUtil.add(afBorrowCashDo.getAmount(),afBorrowCashDo.getRateAmount(),afBorrowCashDo.getSumRate(),afBorrowCashDo.getOverdueAmount(),afBorrowCashDo.getSumOverdue()).subtract(afBorrowCashDo.getRepayAmount());
 					map.put("actual_amount", actual_amount.multiply(BigDecimalUtil.ONE_HUNDRED)+"");
 				} else {   //逾期时计算应还金额
-					BigDecimal amount = afBorrowCashOverdueService.getAfBorrowCashOverdueDoByBorrowId(afBorrowCashDo.getRid(), numberOfDatesBetween);
-					BigDecimal actual_amount= BigDecimalUtil.add(afBorrowCashDo.getAmount(),afBorrowCashDo.getRateAmount(),afBorrowCashDo.getSumRate(),amount).subtract(afBorrowCashDo.getRepayAmount());
+					BigDecimal amounts = afBorrowCashOverdueService.getAfBorrowCashOverdueDoByBorrowId(afBorrowCashDo.getRid(), numberOfDatesBetween);
+					BigDecimal actual_amount= BigDecimalUtil.add(afBorrowCashDo.getAmount(),afBorrowCashDo.getRateAmount(),afBorrowCashDo.getSumRate(),amounts).subtract(afBorrowCashDo.getRepayAmount());
 					map.put("actual_amount", actual_amount.multiply(BigDecimalUtil.ONE_HUNDRED)+"");
 				}
 				//
@@ -246,6 +240,125 @@ public class CollectionController {
 			return updteBo;
 		}
 	}
+		
+		/**
+		 * 催收平台获取借款记录信息用于数据同步刷新
+		 * 
+		 * @param request
+		 * @param response
+		 * @return
+		 */
+		@RequestMapping(value = { "/findBorrowCashByBorrowNo" }, method = RequestMethod.POST)
+		@ResponseBody
+		public CollectionUpdateResqBo findBorrowCashByBorrowNo(HttpServletRequest request, HttpServletResponse response) {
+			String borrowNo = ObjectUtils.toString(request.getParameter("data"));
+			String timestamp = ObjectUtils.toString(request.getParameter("timestamp"));
+			String sign1 = ObjectUtils.toString(request.getParameter("sign"));
+			logger.info("findBorrowCashByBorrowNo data=" + borrowNo + ",timestamp=" + timestamp + ",sign1=" + sign1 + "");
+
+			Map<String, String> map = new HashMap<String, String>();
+			CollectionUpdateResqBo updteBo = new CollectionUpdateResqBo();
+			try {
+				AfBorrowCashDo afBorrowCashDo = borrowCashService.getBorrowCashInfoByBorrowNo(borrowNo);
+				if (afBorrowCashDo == null) {
+					logger.error("findBorrowCashByBorrowNo afBorrowCashDo is null,borrowNo=" + borrowNo);
+					updteBo.setCode(FanbeiThirdRespCode.FAILED.getCode());
+					updteBo.setMsg(FanbeiThirdRespCode.FAILED.getMsg());
+					return updteBo;
+				}
+
+				
+				String sign2 = DigestUtil.MD5(afBorrowCashDo.getBorrowNo());
+				if (StringUtil.equals(sign1, sign2)) {// 验签成功
+					BigDecimal amount = afBorrowCashDo.getAmount();
+					BigDecimal rateAmount = afBorrowCashDo.getRateAmount();
+					BigDecimal overdueAmount = afBorrowCashDo.getOverdueAmount();
+					BigDecimal repayAmount = afBorrowCashDo.getAmount().add(afBorrowCashDo.getRateAmount()).add(afBorrowCashDo.getOverdueAmount()).add(afBorrowCashDo.getSumRate()).add(afBorrowCashDo.getSumOverdue()).setScale(2, RoundingMode.HALF_UP);
+					BigDecimal restAmount = afBorrowCashDo.getAmount().add(afBorrowCashDo.getRateAmount()).add(afBorrowCashDo.getOverdueAmount()).add(afBorrowCashDo.getSumRate()).add(afBorrowCashDo.getSumOverdue()).subtract(afBorrowCashDo.getRepayAmount()).setScale(2, RoundingMode.HALF_UP);
+
+					BigDecimal repayAmountSum = afBorrowCashDo.getRepayAmount();
+
+					// 查询订单借款
+					AfBorrowLegalOrderCashDo afBorrowLegalOrderCashDo = afBorrowLegalOrderCashService.getBorrowLegalOrderCashByBorrowId(afBorrowCashDo.getRid());
+					if (afBorrowLegalOrderCashDo != null) {
+						overdueAmount = overdueAmount.add(afBorrowLegalOrderCashDo.getOverdueAmount());
+						amount = amount.add(afBorrowLegalOrderCashDo.getAmount());
+						rateAmount = rateAmount.add(afBorrowLegalOrderCashDo.getInterestAmount());
+						restAmount = restAmount.add(afBorrowLegalOrderCashDo.getAmount()).add(afBorrowLegalOrderCashDo.getInterestAmount()).add(afBorrowLegalOrderCashDo.getOverdueAmount()).add(afBorrowLegalOrderCashDo.getPoundageAmount()).add(afBorrowLegalOrderCashDo.getSumRepaidInterest())
+								.add(afBorrowLegalOrderCashDo.getSumRepaidOverdue()).add(afBorrowLegalOrderCashDo.getSumRepaidPoundage()).subtract(afBorrowLegalOrderCashDo.getRepaidAmount());
+
+						repayAmount = repayAmount.add(afBorrowLegalOrderCashDo.getAmount()).add(afBorrowLegalOrderCashDo.getInterestAmount()).add(afBorrowLegalOrderCashDo.getOverdueAmount()).add(afBorrowLegalOrderCashDo.getPoundageAmount()).add(afBorrowLegalOrderCashDo.getSumRepaidInterest())
+								.add(afBorrowLegalOrderCashDo.getSumRepaidOverdue()).add(afBorrowLegalOrderCashDo.getSumRepaidPoundage());
+
+						repayAmountSum = repayAmountSum.add(afBorrowLegalOrderCashDo.getRepaidAmount());
+					}
+
+					if (afBorrowLegalOrderCashDo != null) {
+						if (AfBorrowCashStatus.finsh.getCode().equals(afBorrowCashDo.getStatus()) && AfBorrowLegalOrderCashStatus.FINISHED.getCode().equals(afBorrowLegalOrderCashDo.getStatus())) {
+							overdueAmount = BigDecimal.ZERO;
+							repayAmount = repayAmountSum;
+							restAmount = BigDecimal.ZERO;
+						}
+					} else {
+						if (AfBorrowCashStatus.finsh.getCode().equals(afBorrowCashDo.getStatus())) {
+							overdueAmount = BigDecimal.ZERO;
+							repayAmount = repayAmountSum;
+							restAmount = BigDecimal.ZERO;
+						}
+					}
+
+					// 如果预计还款日在今天之前，且为待还款状态，则已逾期，逾期天数=现在减去预计还款日
+					// 区分商品借款逾期 和 借款逾期
+					long overdueDay = 0;
+					Date nowDate = new Date();
+					if (StringUtils.equals(afBorrowCashDo.getStatus(), "TRANSED") && afBorrowCashDo.getGmtPlanRepayment().before(nowDate)) {
+						overdueDay = DateUtil.getNumberOfDatesBetween(afBorrowCashDo.getGmtPlanRepayment(), nowDate);
+					} else if (afBorrowLegalOrderCashDo != null && StringUtils.equals("AWAIT_REPAY", afBorrowLegalOrderCashDo.getStatus()) && afBorrowLegalOrderCashDo.getGmtPlanRepay().before(nowDate)) {
+						// 如果借款没有逾期，判断商品借款是否逾期
+						overdueDay = DateUtil.getNumberOfDatesBetween(afBorrowLegalOrderCashDo.getGmtPlanRepay(), nowDate);
+					}
+
+					map.put("consumer_no", afBorrowCashDo.getUserId() + "");
+					map.put("borrow_id", afBorrowCashDo.getRid() + "");
+					map.put("borrow_no", afBorrowCashDo.getBorrowNo());
+					map.put("card_name", afBorrowCashDo.getCardName());
+					map.put("card_number", afBorrowCashDo.getCardNumber());
+					map.put("gmt_arrival", DateUtil.formatDateTime(afBorrowCashDo.getGmtArrival()));
+					map.put("type", afBorrowCashDo.getType());
+					map.put("amount", amount.multiply(BigDecimalUtil.ONE_HUNDRED) + "");
+					map.put("rate_amount", rateAmount.multiply(BigDecimalUtil.ONE_HUNDRED) + "");
+					map.put("overdue_amount", overdueAmount.multiply(BigDecimalUtil.ONE_HUNDRED) + "");
+					map.put("repay_amount", repayAmount.multiply(BigDecimalUtil.ONE_HUNDRED) + "");
+					map.put("rest_amount", restAmount.multiply(BigDecimalUtil.ONE_HUNDRED) + "");
+					map.put("overdue_day", overdueDay + "");
+					map.put("renewal_num", afBorrowCashDo.getRenewalNum() + "");
+					map.put("repay_amount_sum", repayAmountSum + "");
+					map.put("status", afBorrowCashDo.getStatus());
+					map.put("gmt_plan_repayment", DateUtil.formatDateTime(afBorrowCashDo.getGmtPlanRepayment()));
+					map.put("majiabao_name", StringUtil.null2Str(afBorrowCashDo.getMajiabaoName()));
+					if (StringUtil.equals(afBorrowCashDo.getStatus(), AfBorrowCashStatus.finsh.getCode())) {
+						map.put("gmt_repayment", DateUtil.formatDateTime(afBorrowCashDo.getGmtModified()) + "");
+					} else {
+						map.put("gmt_repayment", "");
+					}
+					String jsonString = JsonUtil.toJSONString(map);
+					updteBo.setCode(FanbeiThirdRespCode.SUCCESS.getCode());
+					updteBo.setMsg(FanbeiThirdRespCode.SUCCESS.getMsg());
+					updteBo.setData(jsonString);
+					return updteBo;
+				} else {
+					updteBo.setCode(FanbeiThirdRespCode.REQUEST_INVALID_SIGN_ERROR.getCode());
+					updteBo.setMsg(FanbeiThirdRespCode.REQUEST_INVALID_SIGN_ERROR.getMsg());
+					return updteBo;
+				}
+			} catch (Exception e) {
+				logger.error("error message " + e);
+				updteBo.setCode(FanbeiThirdRespCode.SYSTEM_ERROR.getCode());
+				updteBo.setMsg(FanbeiThirdRespCode.SYSTEM_ERROR.getMsg());
+				return updteBo;
+			}
+		}
+
 
 	/**
 	 * 催收平台获取用户身份证图片和人脸识别信息
