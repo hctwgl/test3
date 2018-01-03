7ccd747782b82919f2b80259b957b1dccf7a8e69
gaojibin <gaojibin@edspay.com>
2018-01-03 16:27:07 +0800

    添加抽奖接口微信授权code校验逻辑

--- 51fbapi-web/src/main/java/com/ald/fanbei/api/web/h5/controller/H5DrawController.java
+++ 51fbapi-web/src/main/java/com/ald/fanbei/api/web/h5/controller/H5DrawController.java
@@ -1,20 +1,29 @@
 package com.ald.fanbei.api.web.h5.controller;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ald.fanbei.api.biz.service.AfResourceService;
 import com.ald.fanbei.api.biz.service.AfUserDrawService;
+import com.ald.fanbei.api.biz.util.BizCacheUtil;
 import com.ald.fanbei.api.biz.util.WxUtil;
+import com.ald.fanbei.api.common.Constants;
 import com.ald.fanbei.api.common.enums.UserDrawStatus;
+import com.ald.fanbei.api.common.util.ConfigProperties;
+import com.ald.fanbei.api.dal.dao.AfResourceDao;
+import com.ald.fanbei.api.dal.domain.AfResourceDo;
 import com.ald.fanbei.api.dal.domain.AfUserDrawDo;
 import com.ald.fanbei.api.dal.domain.dto.UserDrawInfo;
 import com.ald.fanbei.api.web.common.H5CommonResponse;
@@ -39,9 +48,15 @@ public class H5DrawController extends H5Controller {
     @Autowired
     AfUserDrawService afUserDrawService;
 
+    @Autowired
+    AfResourceDao afResourceDao;
+
+    @Resource
+    BizCacheUtil bizCacheUtil;
+
     @RequestMapping(value = "/signin", method = RequestMethod.POST)
     public String share(HttpServletRequest request, HttpServletResponse response) {
-
+	// 验证请求参数
 	String phone = request.getParameter("phone");
 	if (StringUtils.isNotBlank(phone) && phone.length() != 11) {
 	    return H5CommonResponse.getNewInstance(false, "手机号码输入错误").toString();
@@ -54,24 +69,30 @@ public class H5DrawController extends H5Controller {
 	AfUserDrawDo userDrawDo = afUserDrawService.getByPhone(phone);
 	if (userDrawDo != null) {
 	    if (userDrawDo.getStatus().equals(UserDrawStatus.NORMAL.getCode())) {
+		// 获取配置信息
+		AfResourceDo afResourceDo = getWechatConfig();
+		String appid = afResourceDo.getValue();
+		String secret = afResourceDo.getValue1();
 		// 获取微信信息openId、头像、昵称
-		String appid = afResourceService.getConfigByTypesAndSecType("ACCESSTOKEN", "WX").getValue();
-		String secret = afResourceService.getConfigByTypesAndSecType("ACCESSTOKEN", "WX").getValue1();
 		JSONObject wxUserInfo = WxUtil.getUserInfo(appid, secret, code);
-
 		String openId = wxUserInfo.getString("openid");
 		String nickName = wxUserInfo.getString("nickname");
 		String headImagUrl = wxUserInfo.getString("headimgurl");
 
-		AfUserDrawDo afUserDrawDo = new AfUserDrawDo();
-		afUserDrawDo.setRid(userDrawDo.getRid());
-		afUserDrawDo.setHeaderImg(headImagUrl);
-		afUserDrawDo.setNickName(nickName);
-		afUserDrawDo.setOpenId(openId);
-		afUserDrawDo.setStatus(UserDrawStatus.SIGNIN.getCode());
-		afUserDrawService.updateById(afUserDrawDo);
+		if (StringUtils.isNotBlank(openId) && StringUtils.isNotBlank(nickName) && StringUtils.isNotBlank(headImagUrl)) {
+		    // 获取微信信息成功
+		    AfUserDrawDo afUserDrawDo = new AfUserDrawDo();
+		    afUserDrawDo.setRid(userDrawDo.getRid());
+		    afUserDrawDo.setHeaderImg(headImagUrl);
+		    afUserDrawDo.setNickName(nickName);
+		    afUserDrawDo.setOpenId(openId);
+		    afUserDrawDo.setStatus(UserDrawStatus.SIGNIN.getCode());
+		    afUserDrawService.updateById(afUserDrawDo);
 
-		return H5CommonResponse.getNewInstance(true, "签到成功").toString();
+		    return H5CommonResponse.getNewInstance(true, "签到成功").toString();
+		} else {
+		    return H5CommonResponse.getNewInstance(false, "获取微信信息失败").toString();
+		}
 	    } else {
 		return H5CommonResponse.getNewInstance(false, "该手机号码已签到").toString();
 	    }
@@ -90,9 +111,13 @@ public class H5DrawController extends H5Controller {
 
     @RequestMapping(value = "/draw/result", method = RequestMethod.POST)
     public String goodsInfo(HttpServletRequest request, HttpServletResponse response) {
-
+	// 验证请求参数
 	String count = request.getParameter("count");
 	if (StringUtils.isNotBlank(count)) {
+	    return H5CommonResponse.getNewInstance(false, "请求参数错误").toString();
+	}
+	Integer winCount = Integer.parseInt(count);
+	if (winCount <= 0) {
 	    return H5CommonResponse.getNewInstance(false, "请求参数错误").toString();
 	}
 	String code = request.getParameter("code");
@@ -100,24 +125,64 @@ public class H5DrawController extends H5Controller {
 	    return H5CommonResponse.getNewInstance(false, "请求参数错误").toString();
 	}
 
-	// 获取微信信息openId、头像、昵称
-	String appid = afResourceService.getConfigByTypesAndSecType("ACCESSTOKEN", "WX").getValue();
-	String secret = afResourceService.getConfigByTypesAndSecType("ACCESSTOKEN", "WX").getValue1();
-	JSONObject wxUserInfo = WxUtil.getUserInfo(appid, secret, code);
+	// 获取配置信息
+	AfResourceDo afResourceDo = getWechatConfig();
+	String appid = afResourceDo.getValue();
+	String secret = afResourceDo.getValue1();
 
-	String openId = wxUserInfo.getString("openid");
-	// 判断是否与数据库保持一致
-	if (openId.equals("")) {
-	    // 记录code值
-
-	    // 计算中间用户
-	    Integer winCount = Integer.parseInt(count);
-	    List<UserDrawInfo> users = afUserDrawService.getByStatus(UserDrawStatus.SIGNIN.getCode());
-
-	    return H5CommonResponse.getNewInstance(true, "获取抽奖用户成功", "", users).toString();
-	} else {
-
-	    return H5CommonResponse.getNewInstance(false, "非法请求").toString();
+	String openId = "";
+	String configOpenId = afResourceDo.getValue2();
+	String codeKey = "DRAW:CODE:" + code;
+	if (bizCacheUtil.getObject(codeKey) == null) {
+	    // 获取微信信息openId
+	    JSONObject wxUserInfo = WxUtil.getUserInfo(appid, secret, code);
+	    openId = wxUserInfo.getString("openid");
+	    if (StringUtils.isNotBlank(openId)) {
+		return H5CommonResponse.getNewInstance(false, "非法请求code").toString();
+	    }
 	}
+
+	// 判断是否与数据库保持一致
+	if ("DEFAULT".equals(configOpenId) || openId.equals(configOpenId)) {
+	    // 记录code信息
+	    bizCacheUtil.saveObject(codeKey, code, Constants.SECOND_OF_ONE_DAY);
+
+	    // 获取参与抽奖用户数据
+	    List<UserDrawInfo> users = afUserDrawService.getByStatus(UserDrawStatus.SIGNIN.getCode());
+	    winCount = winCount < users.size() ? winCount : users.size();
+	    if (winCount > 0) {
+		// 计算中奖用户
+		List<Integer> winIndex = new ArrayList<Integer>(winCount);
+		Random random = new Random();
+		do {
+		    int index = random.nextInt(users.size());
+		    if (!winIndex.contains(index))
+			winIndex.add(index);
+		} while (winIndex.size() < winCount);
+
+		List<UserDrawInfo> winUsers = new ArrayList<UserDrawInfo>(winCount);
+		for (int i = 0; i < winCount; i++) {
+		    winUsers.add(users.get(winIndex.get(i)));
+		}
+
+		return H5CommonResponse.getNewInstance(true, "获取抽奖用户成功", "", winUsers).toString();
+	    } else {
+		return H5CommonResponse.getNewInstance(true, "所有用户均中奖").toString();
+	    }
+	} else {
+	    return H5CommonResponse.getNewInstance(false, "非法请求openId").toString();
+	}
+    }
+
+    private AfResourceDo getWechatConfig() {
+	// 获取配置信息
+	String resourceType = "ACCESSTOKEN";
+	String resourceSecType = "WX";
+	String type = ConfigProperties.get(Constants.CONFKEY_INVELOMENT_TYPE);
+	if (Constants.INVELOMENT_TYPE_PRE_ENV.equals(type)) {
+	    resourceType = "ACCESSTOKEN_PRE";
+	    resourceSecType = "WX_PRE";
+	}
+	return afResourceService.getConfigByTypesAndSecType(resourceType, resourceSecType);
     }
 }
