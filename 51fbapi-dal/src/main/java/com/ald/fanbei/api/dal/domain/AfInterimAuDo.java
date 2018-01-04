4838e600182abac3600468401a73282053f66653
caowu <2456087356@qq.com>
2018-01-04 13:56:49 +0800

    临时额度申请失败后,按自然日算

--- 51fbapi-dal/src/main/java/com/ald/fanbei/api/dal/domain/AfInterimAuDo.java
+++ 51fbapi-dal/src/main/java/com/ald/fanbei/api/dal/domain/AfInterimAuDo.java
@@ -8,7 +8,7 @@ import com.ald.fanbei.api.common.AbstractSerial;
 
 
 /**
- * @author zhourui on 2017年11月17日 15:15
+ * @author zhourui on \ 15:15
  * @类描述：用户临时额度
  * @注意：本内容仅限于杭州阿拉丁信息科技股份有限公司内部传阅，禁止外泄以及用于其他的商业目的
  */
