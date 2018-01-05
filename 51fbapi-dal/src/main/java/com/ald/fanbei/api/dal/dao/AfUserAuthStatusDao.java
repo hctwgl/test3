5d7e301dae0d0158a2e2a8c1859ee9ae9ec69a0c
gaojibin <gaojibin@edspay.com>
2018-01-05 18:29:43 +0800

    添加接口默认实现AfUserAuthStatusServiceImpl

--- 51fbapi-dal/src/main/java/com/ald/fanbei/api/dal/dao/AfUserAuthStatusDao.java
+++ 51fbapi-dal/src/main/java/com/ald/fanbei/api/dal/dao/AfUserAuthStatusDao.java
@@ -11,7 +11,7 @@ import org.apache.ibatis.annotations.Param;
  * @date 2018-01-05 14:58:48
  * Copyright 本内容仅限于杭州阿拉丁信息科技股份有限公司内部传阅，禁止外泄以及用于其他的商业目的
  */
-public interface AfUserAuthStatusDao {
+public interface AfUserAuthStatusDao extends BaseDao<AfUserAuthStatusDo, Long>{
     /**
      * 新增一个用户认证信息场景
      * @param afUserAuthStatusDo
