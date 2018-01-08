2797cdb6664d174822664e9c6f7a5bf8ca229f82
gaojibin <gaojibin@edspay.com>
2018-01-08 16:07:09 +0800

    取消平率限制

--- 51fbapi-web/src/main/java/com/ald/fanbei/api/web/h5/controller/H5DrawController.java
+++ 51fbapi-web/src/main/java/com/ald/fanbei/api/web/h5/controller/H5DrawController.java
@@ -122,14 +122,14 @@ public class H5DrawController extends H5Controller {
 	    return H5CommonResponse.getNewInstance(false, "请求参数错误").toString();
 	}
 
-	// 控制抽奖接口调用频率
-	String drawFrequencyKey = "Draw:Result:Runing";
-	if (bizCacheUtil.getObject(drawFrequencyKey) == null) {
-	    // 添加控制锁
-	    bizCacheUtil.saveObject(drawFrequencyKey, 1, Constants.SECOND_OF_TEN);
-	} else {
-	    return H5CommonResponse.getNewInstance(false, "请稍后发起请求result").toString();
-	}
+//	// 控制抽奖接口调用频率
+//	String drawFrequencyKey = "Draw:Result:Runing";
+//	if (bizCacheUtil.getObject(drawFrequencyKey) == null) {
+//	    // 添加控制锁
+//	    bizCacheUtil.saveObject(drawFrequencyKey, 1, Constants.SECOND_OF_TEN);
+//	} else {
+//	    return H5CommonResponse.getNewInstance(false, "请稍后发起请求result").toString();
+//	}
 
 	// 获取配置信息
 	AfResourceDo afResourceDo = afResourceService.getWechatConfig();
