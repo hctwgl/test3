a80288cbbf880e9025c6c9295d89d78434ce71be
chefeipeng <chefeipeng@Alamap.com>
2018-01-15 17:15:36 +0800

    代买协议

--- 51fbapi-web/src/main/java/com/ald/fanbei/api/web/api/legalborrowV2/AgentBuyHintsV2Api.java
+++ 51fbapi-web/src/main/java/com/ald/fanbei/api/web/api/legalborrowV2/AgentBuyHintsV2Api.java
@@ -26,7 +26,7 @@ import java.util.Map;
  * @注意：本内容仅限于浙江阿拉丁电子商务股份有限公司内部传阅，禁止外泄以及用于其他的商业目的
  */
 @Component("agentBuyHintsV2Api")
-public class AgentBuyHintsV2Api extends GetBorrowCashBase implements ApiHandle {
+public class AgentBuyHintsV2Api  implements ApiHandle {
 
     private static final String RESOURCE_TYPE = "AGENTBUY_HINTS";
 
