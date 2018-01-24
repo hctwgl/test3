1dbc5909acff4b922f208b51a3a85c3b9b62fc60
jiangrongbo <jiangrongbo@edspay.com>
2018-01-24 16:13:13 +0800

    新增开通白领贷接口

---
+++ 51fbapi-web/src/main/java/com/ald/fanbei/api/web/h5/api/loan/whitecollar/ApplyWhiteCollarLoanApi.java
@@ -0,0 +1,37 @@
+package com.ald.fanbei.api.web.h5.api.loan.whitecollar;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Component;
+
+import com.ald.fanbei.api.common.exception.FanbeiException;
+import com.ald.fanbei.api.common.exception.FanbeiExceptionCode;
+import com.ald.fanbei.api.context.Context;
+import com.ald.fanbei.api.web.common.H5Handle;
+import com.ald.fanbei.api.web.common.H5HandleResponse;
+import com.ald.fanbei.api.web.validator.Validator;
+import com.ald.fanbei.api.web.validator.bean.ApplyWhiteCollarLoanParam;
+import com.google.common.collect.Maps;
+
+@Component("applyWhiteCollarLoanApi")
+@Validator("applyWhiteCollarLoanParam")
+public class ApplyWhiteCollarLoanApi implements H5Handle {
+
+	@Override
+	public H5HandleResponse process(Context context) {
+		H5HandleResponse resp = new H5HandleResponse(context.getId(), FanbeiExceptionCode.SUCCESS);
+		Map<String,Object> data = Maps.newHashMap();
+		Long userId = context.getUserId();
+		// 用户未登录
+		if(userId == null) {
+			throw new FanbeiException(FanbeiExceptionCode.REQUEST_PARAM_TOKEN_ERROR);
+		}
+		ApplyWhiteCollarLoanParam param = (ApplyWhiteCollarLoanParam) context.getParamEntity();
+		
+		
+		
+		resp.setResponseData(data);
+		return resp;
+	}
+
+}
