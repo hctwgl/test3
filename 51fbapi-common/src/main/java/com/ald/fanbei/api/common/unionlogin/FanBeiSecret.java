135ca4d4bb40a98a8e7f3722b61f3c2973ac7718
wujun <wujun@ALDPC00731.Alamap.com>
2018-01-25 14:28:00 +0800

    公积金代码完善

--- 51fbapi-common/src/main/java/com/ald/fanbei/api/common/unionlogin/FanBeiSecret.java
+++ 51fbapi-common/src/main/java/com/ald/fanbei/api/common/unionlogin/FanBeiSecret.java
@@ -5,7 +5,6 @@ import com.alibaba.fastjson.JSON;
 
 import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.codec.digest.Md5Crypt;
-import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -233,7 +232,6 @@ public class FanBeiSecret {
 
     //endregion
 
-    @Test
     public void test01(){
         //region 获取token
         TreeMap<String, String> paramSortedMap = new TreeMap<>();
@@ -252,7 +250,6 @@ public class FanBeiSecret {
         String data=HttpUtil.doHttpPostJsonParam("https://t.51gjj.com/gjj/getToken", JSON.toJSONString(resultSortedMap));
     }
     
-    @Test
     public void test02(){
 	    String token = "7bb48b11a30f433587ac4e31f97c7884";
         TreeMap<String, String> paramSortedMap = new TreeMap<>();
@@ -269,7 +266,6 @@ public class FanBeiSecret {
         String data = HttpUtil.doHttpPostJsonParam("https://t.51gjj.com/gjj/getOrderSn", JSON.toJSONString(resultSortedMap));
     }
     
-    @Test
     public void test03(){
        String orderNo="84fc84d5-cc6e-42e0-89d1-8ca48c5d47db";
        String token = "7bb48b11a30f433587ac4e31f97c7884";
@@ -287,7 +283,6 @@ public class FanBeiSecret {
        String data = HttpUtil.doHttpPostJsonParam("https://t.51gjj.com/gjj/getCityConfig", JSON.toJSONString(resultSortedMap));
     }
     
-    @Test
     public void test04(){
       String orderSn = "84fc84d5-cc6e-42e0-89d1-8ca48c5d47db";
       String token = "7bb48b11a30f433587ac4e31f97c7884";
@@ -384,8 +379,6 @@ public class FanBeiSecret {
 //        String urlFull = "https://t.51gjj.com/gjj?"+urlParams;
 //        //endregion
 //        System.out.println(urlFull);
-
-
 
 
     }
