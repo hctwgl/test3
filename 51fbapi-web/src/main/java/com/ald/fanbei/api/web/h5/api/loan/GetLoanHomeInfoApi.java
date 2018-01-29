519851d040e6a9d427606ba633ff4170ce617f8b
朱江丰 <zhujiangfeng@edspay.com>
2018-01-29 17:53:19 +0800

    白领贷-首页

--- 51fbapi-web/src/main/java/com/ald/fanbei/api/web/h5/api/loan/GetLoanHomeInfoApi.java
+++ 51fbapi-web/src/main/java/com/ald/fanbei/api/web/h5/api/loan/GetLoanHomeInfoApi.java
@@ -1,10 +1,5 @@
 package com.ald.fanbei.api.web.h5.api.loan;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Component;
@@ -13,12 +8,8 @@ import com.ald.fanbei.api.biz.service.AfBorrowLegalService;
 import com.ald.fanbei.api.biz.service.AfLoanService;
 import com.ald.fanbei.api.biz.service.AfRecommendUserService;
 import com.ald.fanbei.api.biz.service.AfResourceService;
-import com.ald.fanbei.api.common.Constants;
-import com.ald.fanbei.api.common.enums.AfResourceType;
 import com.ald.fanbei.api.common.exception.FanbeiExceptionCode;
-import com.ald.fanbei.api.common.util.ConfigProperties;
 import com.ald.fanbei.api.context.Context;
-import com.ald.fanbei.api.dal.domain.AfResourceDo;
 import com.ald.fanbei.api.web.common.H5Handle;
 import com.ald.fanbei.api.web.common.H5HandleResponse;
 
@@ -44,40 +35,11 @@ public class GetLoanHomeInfoApi implements H5Handle {
 		H5HandleResponse resp = new H5HandleResponse(context.getId(),FanbeiExceptionCode.SUCCESS);
 		Long userId = context.getUserId();
 		
-		resp.addResponseData("bannerList", getBannerList(context));
+		resp.addResponseData("bannerList", afResourceService.getLoanHomeListByType());
 		resp.addResponseData("loanInfos", afLoanService.getHomeInfo(userId));
 		resp.addResponseData("xdInfo", afBorrowLegalService.getHomeInfo(userId));
 		
 		return resp;
 	}
-	
-	private List<Object> getBannerList(Context context) {
-		List<Object> bannerList = new ArrayList<Object>();
-
-		String type = ConfigProperties.get(Constants.CONFKEY_INVELOMENT_TYPE);
-		if (Constants.INVELOMENT_TYPE_ONLINE.equals(type) || Constants.INVELOMENT_TYPE_TEST.equals(type)) {//生产和测试环境
-			bannerList = getBannerObjectWithResourceDolist(afResourceService.getResourceHomeListByTypeOrderBy(AfResourceType.BorrowTopBanner.getCode()));
-		} else if (Constants.INVELOMENT_TYPE_PRE_ENV.equals(type)) { 	//预发环境
-			bannerList = getBannerObjectWithResourceDolist(afResourceService.getResourceHomeListByTypeOrderByOnPreEnv(AfResourceType.BorrowTopBanner.getCode()));
-		}
-		
-		return bannerList;
-	}
-	
-	private List<Object> getBannerObjectWithResourceDolist(List<AfResourceDo> bannerResclist) {
-		List<Object> bannerList = new ArrayList<Object>();
-		for (AfResourceDo afResourceDo : bannerResclist) {
-			Map<String, Object> data = new HashMap<String, Object>();
-			data.put("imageUrl", afResourceDo.getValue());
-			data.put("titleName", afResourceDo.getName());
-			data.put("type", afResourceDo.getValue1());
-			data.put("content", afResourceDo.getValue2());
-			data.put("sort", afResourceDo.getSort());
-
-			bannerList.add(data);
-		}
-		return bannerList;
-	}
-
 	
 }
