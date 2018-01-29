170701e27d3a21d18aa71b2c7ccd9d127f4c8ffc
gaojibin <gaojibin@edspay.com>
2018-01-29 11:08:48 +0800

    认证提示信息

--- 51fbapi-biz/src/main/java/com/ald/fanbei/api/biz/service/impl/AfUserAuthServiceImpl.java
+++ 51fbapi-biz/src/main/java/com/ald/fanbei/api/biz/service/impl/AfUserAuthServiceImpl.java
@@ -380,11 +380,9 @@ public class AfUserAuthServiceImpl implements AfUserAuthService {
 	}
 
 	data.put("showExtraTab", "1");
-	/**
-	 * 是否经过强风控
-	 */
-	// 购物额度 未通过强风控
-	if (!"CASH".equals(scene)) {
+
+	afResourceDo = afResourceService.getSingleResourceBytype("AUTH_STATUS_DESCRIPTION");
+	if (!SceneType.CASH.getName().equals(scene)) {
 	    data.put("showExtraTab", afResourceDoAuth.getValue());
 	    AfUserAuthStatusDo afUserAuthStatus = afUserAuthStatusService.getAfUserAuthStatusByUserIdAndScene(userId, scene);
 	    if (afUserAuthStatus == null || UserAuthSceneStatus.NO.getCode().equals(afUserAuthStatus.getStatus()) || UserAuthSceneStatus.PASSING.getCode().equals(afUserAuthStatus.getStatus())) {// 从未认证
@@ -403,13 +401,22 @@ public class AfUserAuthServiceImpl implements AfUserAuthService {
 		data.put("title1", "暂无信用额度");
 		if (between > 1) {
 		    data.put("title2", "请" + between + "天后尝试重新提交");
-		    data.put("riskRetrialRemind", "审核不通过，请" + between + "天后可重新提交审核");
+		    if (SceneType.ONLINE.getName().equals(scene))
+			data.put("riskRetrialRemind", afResource.getValue3() + "，请" + between + "天后可重新提交审核");
+		    else {
+			data.put("riskRetrialRemind", afResource.getValue4() + "，请" + between + "天后可重新提交审核");
+		    }
 		} else if (between == 1) {
-		    data.put("riskRetrialRemind", "审核不通过，明天可以重新提交审核");
-		    data.put("title2", "审核不通过，明天可以重新提交审核");
+		    if (SceneType.ONLINE.getName().equals(scene)) {
+			data.put("riskRetrialRemind", afResource.getValue3() + "，明天可以重新提交审核");
+			data.put("title2", afResource.getValue3() + "，明天可以重新提交审核");
+		    } else {
+			data.put("riskRetrialRemind", afResource.getValue4() + "，明天可以重新提交审核");
+			data.put("title2", afResource.getValue4() + "，明天可以重新提交审核");
+		    }
 		} else {
-		    data.put("title2", "可以尝试重新提交啦，完成补充认证可提高成功率");
-		    data.put("riskRetrialRemind", "可以尝试重新提交啦，完成补充认证可以提高成功率");
+		    data.put("title2", afResource.getValue());
+		    data.put("riskRetrialRemind", afResource.getValue());
 		    data.put("riskStatus", "A");
 		    data.put("basicStatus", "A");
 		}
@@ -422,15 +429,14 @@ public class AfUserAuthServiceImpl implements AfUserAuthService {
 		data.put("highestAmount", afResource.getValue());// 可获取最高额度
 		data.put("currentAmount", userDto.getAuAmount());// 当前认证额度
 		data.put("useableAmount", userDto.getAuAmount().subtract(userDto.getUsedAmount()));// 剩余可使用额度
-		data.put("title2", "每完成一项补充认证都会提高相应额度");
+		data.put("title2", afResource.getValue1());
 		data.put("sceneStatus", "4");// 认证成功
-	    }
-	    else {
+	    } else {
 		data.put("basicStatus", "P");
 		data.put("riskStatus", "P");
 		data.put("flag", "Y");
 		data.put("title1", "基础信息认证中");
-		data.put("title2", "完善补充认证能够增加审核通过率");
+		data.put("title2", afResource.getValue2());
 		data.put("sceneStatus", "6");// 认证中
 	    }
 
