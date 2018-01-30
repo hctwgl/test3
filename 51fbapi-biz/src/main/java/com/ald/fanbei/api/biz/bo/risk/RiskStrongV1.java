295f3e0fed360536cb02317a668d7da4bee1e5cc
gaojibin <gaojibin@edspay.com>
2018-01-30 11:47:21 +0800

    恢复base64

--- 51fbapi-biz/src/main/java/com/ald/fanbei/api/biz/bo/risk/RiskStrongV1.java
+++ 51fbapi-biz/src/main/java/com/ald/fanbei/api/biz/bo/risk/RiskStrongV1.java
@@ -82,7 +82,7 @@ public class RiskStrongV1 extends RiskRegisterStrongReqBo {
 		eventInfo.put("blackBox", blackBox);
 		eventInfo.put("bqsBlackBox", bqsBlackBox);
 		eventInfo.put("ipAddress", ipAddress);
-		setEventInfo(JSON.toJSONString(eventInfo));
+		setEventInfo(Base64.encodeString(JSON.toJSONString(eventInfo)));
 
 	}
 
