afb60f2667b5a8fb5cd6a4e547d891171c275409
xieqiang <xieqiang@Alamap.com>
2018-01-31 13:35:55 +0800

    风控数据对接加入uuid

--- 51fbapi-biz/src/main/java/com/ald/fanbei/api/biz/bo/risk/RiskStrongV1.java
+++ 51fbapi-biz/src/main/java/com/ald/fanbei/api/biz/bo/risk/RiskStrongV1.java
@@ -82,14 +82,14 @@ public class RiskStrongV1 extends RiskRegisterStrongReqBo {
 		eventInfo.put("blackBox", blackBox);
 		eventInfo.put("bqsBlackBox", bqsBlackBox);
 		eventInfo.put("ipAddress", ipAddress);
-		/*ServletRequestAttributes requestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+		ServletRequestAttributes requestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
 		String uuid = "";
 		if (requestAttributes != null){
 			String id = requestAttributes.getRequest().getHeader(Constants.REQ_SYS_NODE_ID);
 			String array[] = id == null?null:id.split("_");
 			uuid = array ==null || array.length<2?"":array[1];
 		}
-		eventInfo.put("uuid",uuid);*/
+		eventInfo.put("uuid",uuid);
 		setEventInfo(Base64.encodeString(JSON.toJSONString(eventInfo)));
 
 	}
