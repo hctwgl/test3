bdd8966dcfc2ef2074816afd891e2aa720eaf12b
xieqiang <xieqiang@Alamap.com>
2018-01-31 14:28:22 +0800

    风控数据对接加入uuid

--- 51fbapi-biz/src/main/java/com/ald/fanbei/api/biz/bo/risk/RiskStrongV1.java
+++ 51fbapi-biz/src/main/java/com/ald/fanbei/api/biz/bo/risk/RiskStrongV1.java
@@ -11,6 +11,8 @@ import com.ald.fanbei.api.dal.domain.dto.AfUserAccountDto;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import org.dbunit.util.Base64;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 /**
  * 
