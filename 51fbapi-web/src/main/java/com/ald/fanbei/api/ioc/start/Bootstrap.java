842129dde9d5f641674ab7dcf8276d122a5bb4f9
jiangrongbo <jiangrongbo@edspay.com>
2018-02-01 10:42:59 +0800

    jetty支持

---
+++ 51fbapi-web/src/main/java/com/ald/fanbei/api/ioc/start/Bootstrap.java
@@ -0,0 +1,12 @@
+package com.ald.fanbei.api.ioc.start;
+
+public class Bootstrap {
+	public static void main(String[] args) {
+	
+		String webapp = "src/main/webapp";
+		new JettyServerStart(webapp, 9090, "/").start();
+		
+		
+		
+	}
+}
