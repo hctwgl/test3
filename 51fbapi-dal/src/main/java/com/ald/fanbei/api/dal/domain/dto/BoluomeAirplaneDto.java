84de7c23dc96caa3442f62dbd5236c472607da53
gaojibin <gaojibin@edspay.com>
2018-02-01 16:34:09 +0800

    菠萝觅机票订单详情添加dto

---
+++ 51fbapi-dal/src/main/java/com/ald/fanbei/api/dal/domain/dto/BoluomeAirplaneDto.java
@@ -0,0 +1,159 @@
+package com.ald.fanbei.api.dal.domain.dto;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class BoluomeAirplaneDto {
+    private Integer canCancel;
+    private String channel;
+    private BoluomeAirplaneContactDto contactor;
+    private Integer count;
+    private String displayStatus;
+    private List<BoluomeAirplaneFlightDto> flights;
+    private String id;
+    private String name;
+    private BigDecimal orderPrice;
+    private String orderType;
+
+    private List<BoluomeAirplanePassengerDto> passengers;
+
+    private List<String> paymentSerialList;
+    private BigDecimal price;
+    private Integer status;
+    private String userId;
+    private String userPhone;
+
+    public Integer getCanCancel() {
+	return canCancel;
+    }
+
+    public void setCanCancel(Integer canCancel) {
+	this.canCancel = canCancel;
+    }
+
+    public String getChannel() {
+	return channel;
+    }
+
+    public void setChannel(String channel) {
+	this.channel = channel;
+    }
+
+    public BoluomeAirplaneContactDto getContactor() {
+	return contactor;
+    }
+
+    public void setContactor(BoluomeAirplaneContactDto contactor) {
+	this.contactor = contactor;
+    }
+
+    public Integer getCount() {
+	return count;
+    }
+
+    public void setCount(Integer count) {
+	this.count = count;
+    }
+
+    public String getDisplayStatus() {
+	return displayStatus;
+    }
+
+    public void setDisplayStatus(String displayStatus) {
+	this.displayStatus = displayStatus;
+    }
+
+    public List<BoluomeAirplaneFlightDto> getFlights() {
+	return flights;
+    }
+
+    public void setFlights(List<BoluomeAirplaneFlightDto> flights) {
+	this.flights = flights;
+    }
+
+    public String getId() {
+	return id;
+    }
+
+    public void setId(String id) {
+	this.id = id;
+    }
+
+    public String getName() {
+	return name;
+    }
+
+    public void setName(String name) {
+	this.name = name;
+    }
+
+    public BigDecimal getOrderPrice() {
+	return orderPrice;
+    }
+
+    public void setOrderPrice(BigDecimal orderPrice) {
+	this.orderPrice = orderPrice;
+    }
+
+    public String getOrderType() {
+	return orderType;
+    }
+
+    public void setOrderType(String orderType) {
+	this.orderType = orderType;
+    }
+
+    public List<BoluomeAirplanePassengerDto> getPassengers() {
+	return passengers;
+    }
+
+    public void setPassengers(List<BoluomeAirplanePassengerDto> passengers) {
+	this.passengers = passengers;
+    }
+
+    public List<String> getPaymentSerialList() {
+	return paymentSerialList;
+    }
+
+    public void setPaymentSerialList(List<String> paymentSerialList) {
+	this.paymentSerialList = paymentSerialList;
+    }
+
+    public BigDecimal getPrice() {
+	return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+	this.price = price;
+    }
+
+    public Integer getStatus() {
+	return status;
+    }
+
+    public void setStatus(Integer status) {
+	this.status = status;
+    }
+
+    public String getUserId() {
+	return userId;
+    }
+
+    public void setUserId(String userId) {
+	this.userId = userId;
+    }
+
+    public String getUserPhone() {
+	return userPhone;
+    }
+
+    public void setUserPhone(String userPhone) {
+	this.userPhone = userPhone;
+    }
+
+    @Override
+    public String toString() {
+	return "BoluomeAirplaneDto [canCancel=" + canCancel + ", channel=" + channel + ", contactor=" + contactor + ", count=" + count + ", displayStatus=" + displayStatus + ", flights=" + flights + ", id=" + id + ", name=" + name + ", orderPrice=" + orderPrice + ", orderType=" + orderType + ", passengers=" + passengers + ", paymentSerialList=" + paymentSerialList + ", price=" + price + ", status=" + status + ", userId=" + userId + ", userPhone=" + userPhone + "]";
+    }
+
+}
