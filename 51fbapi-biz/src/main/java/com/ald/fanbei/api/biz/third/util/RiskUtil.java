36551de72f223e83c46bc4976f85041cd445ec99
jiangrongbo <jiangrongbo@edspay.com>
2018-02-03 19:40:15 +0800

    风控获取利率接口去除设备指纹

--- 51fbapi-biz/src/main/java/com/ald/fanbei/api/biz/third/util/RiskUtil.java
+++ 51fbapi-biz/src/main/java/com/ald/fanbei/api/biz/third/util/RiskUtil.java
@@ -2443,9 +2443,9 @@ public class RiskUtil extends AbstractThird {
 			JSONObject eventObj = new JSONObject();
 			eventObj.put("appName", params.get("appName") + "");
 			eventObj.put("cardNo", card == null ? "" : card.getCardNumber());
-			eventObj.put("blackBox", params.get("blackBox") == null ? "" : params.get("blackBox"));
+			//eventObj.put("blackBox", params.get("blackBox") == null ? "" : params.get("blackBox"));
 			eventObj.put("ipAddress", params.get("ipAddress") == null ? "" : params.get("ipAddress"));
-			eventObj.put("bqsBlackBox", params.get("bqsBlackBox") == null ? "" : params.get("bqsBlackBox"));
+			//eventObj.put("bqsBlackBox", params.get("bqsBlackBox") == null ? "" : params.get("bqsBlackBox"));
 			reqBo.setEventInfo(JSONObject.toJSONString(eventObj));
 		}
 		HashMap summaryData = afBorrowDao.getUserSummary(Long.parseLong(consumerNo));
