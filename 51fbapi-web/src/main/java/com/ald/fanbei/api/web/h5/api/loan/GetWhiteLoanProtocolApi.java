11e5cf8164c8fa5de98bb74960b5b7209d608cbf
chefeipeng <chefeipeng@Alamap.com>
2018-02-09 13:55:53 +0800

    白领贷 借钱前相关协议

--- 51fbapi-web/src/main/java/com/ald/fanbei/api/web/h5/api/loan/GetWhiteLoanProtocolApi.java
+++ 51fbapi-web/src/main/java/com/ald/fanbei/api/web/h5/api/loan/GetWhiteLoanProtocolApi.java
@@ -27,7 +27,7 @@ import java.util.Map;
  * @类描述：展示商品代买提示语
  * @注意：本内容仅限于浙江阿拉丁电子商务股份有限公司内部传阅，禁止外泄以及用于其他的商业目的
  */
-@Component("getWhiteLoanProtocolV2Api")
+@Component("getWhiteLoanProtocolApi")
 @Validator("getWhiteLoanProtocolParam")
 public class GetWhiteLoanProtocolApi extends GetBorrowCashBase implements ApiHandle {
 
