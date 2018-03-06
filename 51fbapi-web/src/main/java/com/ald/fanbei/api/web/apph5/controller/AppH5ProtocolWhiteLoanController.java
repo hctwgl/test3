51f88173f45ba8f2e1e34c5e2a8540200c5d20f4
chefeipeng <chefeipeng@Alamap.com>
2018-03-06 17:47:23 +0800

    白领贷借钱协议 解决显示问题

--- 51fbapi-web/src/main/java/com/ald/fanbei/api/web/apph5/controller/AppH5ProtocolWhiteLoanController.java
+++ 51fbapi-web/src/main/java/com/ald/fanbei/api/web/apph5/controller/AppH5ProtocolWhiteLoanController.java
@@ -214,8 +214,9 @@ public class AppH5ProtocolWhiteLoanController extends BaseController {
                     String periodsTime = periodsYear + "年" + periodsMonth + "月" + periodsDay + "日";
                     model.put("gmtEnd", periodsTime);
                     model.put("days", periodsDay);
-                    model.put("month",periodsMonth);
-                    model.put("year",periodsYear);
+                    map.put("days", day);
+                    map.put("month",month);
+                    map.put("year",year);
                 }
                 map.put("gmtPlanRepay", afLoanPeriodsDo.getGmtPlanRepay());
                 map.put("loanAmount", afLoanPeriodsDo.getAmount());
@@ -258,8 +259,9 @@ public class AppH5ProtocolWhiteLoanController extends BaseController {
                     String time = year + "年" + month + "月" + day + "日";
                     model.put("gmtEnd", time);
                     model.put("days", day);
-                    model.put("month",month);
-                    model.put("year",year);
+                    map.put("days", day);
+                    map.put("month",month);
+                    map.put("year",year);
                 }
                 map.put("gmtPlanRepay", afLoanPeriodsDo.getGmtPlanRepay());
                 map.put("loanAmount", afLoanPeriodsDo.getAmount());
