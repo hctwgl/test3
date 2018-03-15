d64161b27b2049e5299273b92c8ad897441854c3
DESKTOP-MA60KKU\sun <huangqiujun@edspay.com>
2018-03-15 15:50:06 +0800

    秒杀活动

--- 51fbapi-web/src/main/java/com/ald/fanbei/api/web/api/order/BuySelfGoodsApi.java
+++ 51fbapi-web/src/main/java/com/ald/fanbei/api/web/api/order/BuySelfGoodsApi.java
@@ -354,7 +354,7 @@ public class BuySelfGoodsApi implements ApiHandle {
 
 			//秒杀活动增加逻辑
 			int activityType = NumberUtil.objToIntDefault(ObjectUtils.toString(requestDataVo.getParams().get("activityType"), ""),
-					0);
+					2);
 			if(activityType==2){
 				AfSeckillActivityGoodsDto afSeckillActivityGoodsDto = afSeckillActivityService.getActivityPriceByPriceId(goodsPriceId);
 				Long activityId = afSeckillActivityGoodsDto.getActivityId();
