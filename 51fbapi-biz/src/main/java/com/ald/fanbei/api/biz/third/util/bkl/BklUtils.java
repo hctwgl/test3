23477fca986e2b8a1f1510534c9add9b7457e585
郭帅强 <guoshuaiqiang@Alamap.com>
2018-03-29 20:28:50 +0800

    电核修改

--- 51fbapi-biz/src/main/java/com/ald/fanbei/api/biz/third/util/bkl/BklUtils.java
+++ 51fbapi-biz/src/main/java/com/ald/fanbei/api/biz/third/util/bkl/BklUtils.java
@@ -11,6 +11,7 @@ import com.ald.fanbei.api.biz.iagent.utils.AOSJson;
 import com.ald.fanbei.api.biz.iagent.utils.HttpRequestVO;
 import com.ald.fanbei.api.biz.iagent.utils.HttpResponseVO;
 import com.ald.fanbei.api.biz.service.AfUserService;
+import com.ald.fanbei.api.dal.domain.AfBklDo;
 import com.google.common.collect.Maps;
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Component;
@@ -24,23 +25,25 @@ import java.util.Map;
 public class BklUtils {
 
     private static Logger logger = Logger.getLogger(BklUtils.class);
-    @Resource
-    AfUserService afUserService;
 
-    public  void requestJob(String csv_phone_num,String csv_arn,String csv_name,String csv_sex,String csv_digit_4,String csv_birth_date,String csv_staging,String csv_amt,String csv_pay_way,String csv_product_category) {
+
+    public  void submitJob(AfBklDo bklDo) {
         Map map=new HashMap();
         map.put("work_id", "work_0008");
         map.put("corp_code", "51返呗");
         map.put("access_token", "xCGQF6wNeA1z6Cmz2UMW8F1as");
         map.put("job_code", "51FB-SP01");
         map.put("order_id", "trade0008");
-        String work_data = "{'csv_phone_num':'15990182307','csv_arn':'tr008','csv_name':'任春雷','csv_sex':'男','csv_digit_4':8755,'csv_birth_date':'19890610','csv_staging':9,'csv_amt':1500,'csv_pay_way':'分期付款','csv_product_category':'手机通讯' }";
+        String work_data = "{'csv_phone_num':'"+bklDo.getCsvPhoneNum()+"','csv_arn':'"+bklDo.getCsvArn()+"','csv_name':'"+bklDo.getCsvName()+"','csv_sex':'"+bklDo.getCsvSex()+
+                "','csv_digit_4':"+bklDo.getCsvDigit4()+",'csv_birth_date':'"+bklDo.getCsvBirthDate()+"','csv_staging':"+bklDo.getCsvStaging()+
+                ",'csv_amt':"+bklDo.getCsvAmt()+",'csv_pay_way':'"+bklDo.getCsvPayWay()+"','csv_product_category':'"+bklDo.getCsvProductCategory()+"' }";
         Map dtt=null;
         try {
             dtt = AOSJson.fromJson(work_data, HashMap.class);
-
+            logger.info("bklUtils submitJob success json dtt = "+dtt);
         } catch (Exception e) {
             e.printStackTrace();
+            logger.error("bklUtils submitJob json error = >{}",e);
         }
 
         Map<String, String> mdata = Maps.newHashMap();
@@ -55,9 +58,10 @@ public class BklUtils {
             httpResponseVO.getOut();
             httpResponseVO.getStatus();
             System.out.println(httpResponseVO.getOut());
-
+            logger.info("bklUtils submitJob httpResponseVO success out =" + httpResponseVO.getOut());
         } catch (Exception e) {
             e.printStackTrace();
+            logger.info("bklUtils submitJob httpResponseVO error =",e);
         }
     }
 
