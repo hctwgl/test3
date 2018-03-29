82a93d1c45bed19e55148160c5ec61d115283fc9
chefeipeng <chefeipeng@Alamap.com>
2018-03-29 20:31:41 +0800

    H5 配置 活动 优化

--- 51fbapi-dal/src/main/java/com/ald/fanbei/api/dal/domain/dto/AfResourceH5ItemDto.java
+++ 51fbapi-dal/src/main/java/com/ald/fanbei/api/dal/domain/dto/AfResourceH5ItemDto.java
@@ -1,84 +1,25 @@
+/*
+ *@Copyright (c) 2016, 浙江阿拉丁电子商务股份有限公司 All Rights Reserved.
+ */
 package com.ald.fanbei.api.dal.domain.dto;
 
-import java.math.BigDecimal;
-import java.util.List;
-import java.util.Map;
-
+import com.ald.fanbei.api.dal.domain.AfResourceH5Do;
 import com.ald.fanbei.api.dal.domain.AfResourceH5ItemDo;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.util.List;
 
 /**
- * @author Jingru
- * @version 创建时间：2018年3月21日 下午5:41:09
- * @Description 类描述
+ *
+ * @类描述：
+ * @author 江荣波 2017年6月21日下午2:23:12
+ * @注意：本内容仅限于杭州阿拉丁信息科技股份有限公司内部传阅，禁止外泄以及用于其他的商业目的
  */
+@Getter
+@Setter
 public class AfResourceH5ItemDto extends AfResourceH5ItemDo {
-	private static final long serialVersionUID = 4612251113399624959L;
 
-	private Long goodsCount;
-	private String categoryName;
-	private BigDecimal saleAmount;
-	private String goodsName;
+	private List goodsList;
 
-	/**
-	 * @return the goodsCount
-	 */
-	public Long getGoodsCount() {
-		return goodsCount;
-	}
-
-	/**
-	 * @param goodsCount the goodsCount to set
-	 */
-	public void setGoodsCount(Long goodsCount) {
-		this.goodsCount = goodsCount;
-	}
-
-
-	/**
-	 * @return the saleAmount
-	 */
-	public BigDecimal getSaleAmount() {
-		return saleAmount;
-	}
-
-	/**
-	 * @param saleAmount the saleAmount to set
-	 */
-	public void setSaleAmount(BigDecimal saleAmount) {
-		this.saleAmount = saleAmount;
-	}
-
-	/**
-	 * @return the categoryName
-	 */
-	public String getCategoryName() {
-		return categoryName;
-	}
-
-	/**
-	 * @param categoryName the categoryName to set
-	 */
-	public void setCategoryName(String categoryName) {
-		this.categoryName = categoryName;
-	}
-
-	/**
-	 * @return the goodsName
-	 */
-	public String getGoodsName() {
-		return goodsName;
-	}
-
-	/**
-	 * @param goodsName the goodsName to set
-	 */
-	public void setGoodsName(String goodsName) {
-		this.goodsName = goodsName;
-	}
-
-	public void setGoodsList(List<Map<String, Object>> goodsList) {
-		// TODO Auto-generated method stub
-		
-	}
 }
