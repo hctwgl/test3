902f633b0c56059140ee9d149f60d51c147b2332
maqiaopan <maqiaopan@edspay.com>
2018-03-29 16:44:42 +0800

    add method for nper list

--- 51fbapi-dal/src/main/java/com/ald/fanbei/api/dal/dao/AfInterestReduceGoodsDao.java
+++ 51fbapi-dal/src/main/java/com/ald/fanbei/api/dal/dao/AfInterestReduceGoodsDao.java
@@ -1,6 +1,9 @@
 package com.ald.fanbei.api.dal.dao;
 
+import org.apache.ibatis.annotations.Param;
+
 import com.ald.fanbei.api.dal.domain.AfInterestReduceGoodsDo;
+import com.ald.fanbei.api.dal.domain.AfInterestReduceRulesDo;
 
 /**
  * 降息Dao
@@ -11,6 +14,8 @@ import com.ald.fanbei.api.dal.domain.AfInterestReduceGoodsDo;
  * Copyright 本内容仅限于杭州阿拉丁信息科技股份有限公司内部传阅，禁止外泄以及用于其他的商业目的
  */
 public interface AfInterestReduceGoodsDao extends BaseDao<AfInterestReduceGoodsDo, Long> {
+
+	AfInterestReduceRulesDo checkIfReduce(@Param("ruleType")int i, @Param("goodsId")Long goodsId);
 
     
 
