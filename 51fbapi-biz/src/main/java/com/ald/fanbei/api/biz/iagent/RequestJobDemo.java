a6b629914fab55abb14ab6d7418a7249e9046a27
xieqiang <xieqiang@Alamap.com>
2018-03-30 19:06:05 +0800

    智能电核

--- 51fbapi-biz/src/main/java/com/ald/fanbei/api/biz/iagent/RequestJobDemo.java
+++ 51fbapi-biz/src/main/java/com/ald/fanbei/api/biz/iagent/RequestJobDemo.java
@@ -23,12 +23,12 @@ public class RequestJobDemo {
 	 */
 	public static void requestJob() {
 		Map map=new HashMap();
-		map.put("work_id", "work_0008");
+		map.put("work_id", "11111111");
 		map.put("corp_code", "51返呗");
 		map.put("access_token", "xCGQF6wNeA1z6Cmz2UMW8F1as");
 		map.put("job_code", "51FB-SP01");
-		map.put("order_id", "trade0008");
-		String work_data = "{'csv_phone_num':'15990182307','csv_arn':'tr008','csv_name':'任春雷','csv_sex':'男','csv_digit_4':8755,'csv_birth_date':'19890610','csv_staging':9,'csv_amt':1500,'csv_pay_way':'分期付款','csv_product_category':'手机通讯' }";
+		map.put("order_id", "11111111");
+		String work_data = "{'csv_phone_num':'13613317541','csv_arn':'SG18033003238','csv_name':'谢强','csv_sex':'男','csv_digit_4':8755,'csv_birth_date':'19890610','csv_staging':9,'csv_amt':1500,'csv_pay_way':'分期付款','csv_product_category':'手机通讯' }";
 		Map dtt=null;
 		try {
 			dtt = AOSJson.fromJson(work_data, HashMap.class);
