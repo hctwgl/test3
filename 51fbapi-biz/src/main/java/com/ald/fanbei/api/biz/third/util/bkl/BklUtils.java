1043b92a0c0344661feec0cf64725b518e450707
xieqiang <xieqiang@Alamap.com>
2018-04-03 10:20:52 +0800

    智能电核

--- 51fbapi-biz/src/main/java/com/ald/fanbei/api/biz/third/util/bkl/BklUtils.java
+++ 51fbapi-biz/src/main/java/com/ald/fanbei/api/biz/third/util/bkl/BklUtils.java
@@ -47,7 +47,7 @@ public class BklUtils {
         map.put("access_token", "xCGQF6wNeA1z6Cmz2UMW8F1as");
         map.put("job_code", "51FB-SP01");
         map.put("order_id", bklDo.getCsvArn() + new Date().getTime());
-        String work_data = "{'csv_phone_num':'"+"13613317541"+"','csv_arn':'"+bklDo.getCsvArn()+"','csv_name':'"+bklDo.getCsvName()+"','csv_sex':'"+bklDo.getCsvSex()+
+        String work_data = "{'csv_phone_num':'"+bklDo.getCsvPhoneNum()+"','csv_arn':'"+bklDo.getCsvArn()+"','csv_name':'"+bklDo.getCsvName()+"','csv_sex':'"+bklDo.getCsvSex()+
                 "','csv_digit_4':"+bklDo.getCsvDigit4()+",'csv_birth_date':'"+bklDo.getCsvBirthDate()+"','csv_staging':"+bklDo.getCsvStaging()+
                 ",'csv_amt':"+bklDo.getCsvAmt()+",'csv_pay_way':'"+bklDo.getCsvPayWay()+"','csv_product_category':'"+bklDo.getCsvProductCategory()+"' }";
         Map dtt=null;
