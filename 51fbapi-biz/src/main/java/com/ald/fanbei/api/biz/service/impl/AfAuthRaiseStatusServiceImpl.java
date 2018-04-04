443252a45a8b469e060f6c5e21a37105af8ec265
gaojibin <gaojibin@edspay.com>
2018-04-04 20:11:59 +0800

    补充认证提额初始化添加日志

--- 51fbapi-biz/src/main/java/com/ald/fanbei/api/biz/service/impl/AfAuthRaiseStatusServiceImpl.java
+++ 51fbapi-biz/src/main/java/com/ald/fanbei/api/biz/service/impl/AfAuthRaiseStatusServiceImpl.java
@@ -26,96 +26,89 @@ import com.ald.fanbei.api.dal.domain.AfAuthRaiseStatusDo;
  */
 
 @Service("afAuthRaiseStatusService")
-public class AfAuthRaiseStatusServiceImpl extends ParentServiceImpl<AfAuthRaiseStatusDo, Long>
-		implements AfAuthRaiseStatusService {
+public class AfAuthRaiseStatusServiceImpl extends ParentServiceImpl<AfAuthRaiseStatusDo, Long> implements AfAuthRaiseStatusService {
 
-	private static final Logger logger = LoggerFactory.getLogger(AfAuthRaiseStatusServiceImpl.class);
+    private static final Logger logger = LoggerFactory.getLogger(AfAuthRaiseStatusServiceImpl.class);
 
-	@Resource
-	private AfAuthRaiseStatusDao afAuthRaiseStatusDao;
+    @Resource
+    private AfAuthRaiseStatusDao afAuthRaiseStatusDao;
 
-	@Override
-	public BaseDao<AfAuthRaiseStatusDo, Long> getDao() {
-		return afAuthRaiseStatusDao;
+    @Override
+    public BaseDao<AfAuthRaiseStatusDo, Long> getDao() {
+	return afAuthRaiseStatusDao;
+    }
+
+    @Override
+    public AfAuthRaiseStatusDo buildAuthRaiseStatusDo(Long userId, String authType, String prdType, String raiseStatus, BigDecimal amount, Date gmtFinish) {
+	AfAuthRaiseStatusDo raiseStatusDo = new AfAuthRaiseStatusDo();
+	raiseStatusDo.setAuthType(authType);
+	raiseStatusDo.setPrdType(prdType);
+	raiseStatusDo.setUserId(userId);
+	raiseStatusDo.setRaiseStatus(raiseStatus);
+	raiseStatusDo.setAmount(amount);
+	raiseStatusDo.setGmtFinish(gmtFinish);
+	return raiseStatusDo;
+    }
+
+    @Override
+    public void saveOrUpdateRaiseStatus(AfAuthRaiseStatusDo raiseStatus) {
+	saveOrUpdateRaiseStatus(raiseStatus, true);
+    }
+
+    private void saveOrUpdateRaiseStatus(AfAuthRaiseStatusDo raiseStatus, boolean isUpdate) {
+	AfAuthRaiseStatusDo delegateRaiseStatus = new AfAuthRaiseStatusDo();
+	delegateRaiseStatus.setAuthType(raiseStatus.getAuthType());
+	delegateRaiseStatus.setPrdType(raiseStatus.getPrdType());
+	delegateRaiseStatus.setUserId(raiseStatus.getUserId());
+	AfAuthRaiseStatusDo existRaiseStatusDo = afAuthRaiseStatusDao.getByPrdTypeAndUserId(raiseStatus.getPrdType(), raiseStatus.getUserId());
+	delegateRaiseStatus.setRaiseStatus(raiseStatus.getRaiseStatus());
+	delegateRaiseStatus.setGmtFinish(raiseStatus.getGmtFinish());
+	if (existRaiseStatusDo != null) {
+	    logger.info("saveOrUpdateRaiseStatus exist:" + existRaiseStatusDo.toString() + ",isUpdate:" + isUpdate);
+	    if (isUpdate) {
+		delegateRaiseStatus.setRid(existRaiseStatusDo.getRid());
+		afAuthRaiseStatusDao.updateById(delegateRaiseStatus);
+	    }
+	} else {
+	    logger.info("saveOrUpdateRaiseStatus save:" + delegateRaiseStatus.toString());
+	    afAuthRaiseStatusDao.saveRecord(delegateRaiseStatus);
 	}
+    }
 
-	@Override
-	public AfAuthRaiseStatusDo buildAuthRaiseStatusDo(Long userId, String authType, String prdType, String raiseStatus,
-			BigDecimal amount, Date gmtFinish) {
-		AfAuthRaiseStatusDo raiseStatusDo = new AfAuthRaiseStatusDo();
-		raiseStatusDo.setAuthType(authType);
-		raiseStatusDo.setPrdType(prdType);
-		raiseStatusDo.setUserId(userId);
-		raiseStatusDo.setRaiseStatus(raiseStatus);
-		raiseStatusDo.setAmount(amount);
-		raiseStatusDo.setGmtFinish(gmtFinish);
-		return raiseStatusDo;
-	}
+    @Override
+    public void initRaiseStatus(Long userId, String authType) {
+	AfAuthRaiseStatusDo bldRaiseStatus = this.buildAuthRaiseStatusDo(userId, authType, LoanType.BLD_LOAN.getCode(), RaiseStatus.N.getCode(), BigDecimal.ZERO, new Date());
+	this.saveOrUpdateRaiseStatus(bldRaiseStatus, false);
+	AfAuthRaiseStatusDo xjdRaiseStatus = this.buildAuthRaiseStatusDo(userId, authType, LoanType.CASH.getCode(), RaiseStatus.N.getCode(), BigDecimal.ZERO, new Date());
+	this.saveOrUpdateRaiseStatus(xjdRaiseStatus, false);
 
-	@Override
-	public void saveOrUpdateRaiseStatus(AfAuthRaiseStatusDo raiseStatus) {
-		saveOrUpdateRaiseStatus(raiseStatus, true);
-	}
+    }
 
-	private void saveOrUpdateRaiseStatus(AfAuthRaiseStatusDo raiseStatus,boolean isUpdate) {
-		AfAuthRaiseStatusDo delegateRaiseStatus = new AfAuthRaiseStatusDo();
-		delegateRaiseStatus.setAuthType(raiseStatus.getAuthType());
-		delegateRaiseStatus.setPrdType(raiseStatus.getPrdType());
-		delegateRaiseStatus.setUserId(raiseStatus.getUserId());
-		AfAuthRaiseStatusDo existRaiseStatusDo = afAuthRaiseStatusDao.getByPrdTypeAndUserId(raiseStatus.getPrdType(),raiseStatus.getUserId());
-		delegateRaiseStatus.setRaiseStatus(raiseStatus.getRaiseStatus());
-		delegateRaiseStatus.setGmtFinish(raiseStatus.getGmtFinish());
-		if (existRaiseStatusDo != null) {
-		    if(isUpdate)
-		    {
-			delegateRaiseStatus.setRid(existRaiseStatusDo.getRid());
-			afAuthRaiseStatusDao.updateById(delegateRaiseStatus);
-		    }
-		} else {
-			afAuthRaiseStatusDao.saveRecord(delegateRaiseStatus);
-		}
-	}
-	
-	@Override
-	public void initRaiseStatus(Long userId, String authType) {
-		AfAuthRaiseStatusDo bldRaiseStatus = this.buildAuthRaiseStatusDo(userId, authType, LoanType.BLD_LOAN.getCode(),
-				RaiseStatus.N.getCode(), BigDecimal.ZERO, new Date());
-		this.saveOrUpdateRaiseStatus(bldRaiseStatus,false);
-		AfAuthRaiseStatusDo xjdRaiseStatus = this.buildAuthRaiseStatusDo(userId, authType, LoanType.CASH.getCode(),
-				RaiseStatus.N.getCode(), BigDecimal.ZERO, new Date());
-		this.saveOrUpdateRaiseStatus(xjdRaiseStatus,false);
+    @Override
+    public void initCreditRaiseStatus(Long userId, String authType) {
+	AfAuthRaiseStatusDo bldRaiseStatus = this.buildAuthRaiseStatusDo(userId, authType, LoanType.BLD_LOAN.getCode(), RaiseStatus.Y.getCode(), BigDecimal.ZERO, new Date());
+	this.saveOrUpdateRaiseStatus(bldRaiseStatus, false);
+	AfAuthRaiseStatusDo xjdRaiseStatus = this.buildAuthRaiseStatusDo(userId, authType, LoanType.CASH.getCode(), RaiseStatus.N.getCode(), BigDecimal.ZERO, new Date());
+	this.saveOrUpdateRaiseStatus(xjdRaiseStatus, false);
+    }
 
-	}
+    @Override
+    public void initZhengxinRaiseStatus(Long userId, String authType) {
+	this.initCreditRaiseStatus(userId, authType);
+    }
 
-	@Override
-	public void initCreditRaiseStatus(Long userId, String authType) {
-		AfAuthRaiseStatusDo bldRaiseStatus = this.buildAuthRaiseStatusDo(userId, authType, LoanType.BLD_LOAN.getCode(),
-				RaiseStatus.Y.getCode(), BigDecimal.ZERO, new Date());
-		this.saveOrUpdateRaiseStatus(bldRaiseStatus,false);
-		AfAuthRaiseStatusDo xjdRaiseStatus = this.buildAuthRaiseStatusDo(userId, authType, LoanType.CASH.getCode(),
-				RaiseStatus.N.getCode(), BigDecimal.ZERO, new Date());
-		this.saveOrUpdateRaiseStatus(xjdRaiseStatus,false);
-	}
+    @Override
+    public void initOnlinebankRaiseStatus(Long userId, String authType) {
+	AfAuthRaiseStatusDo bldRaiseStatus = this.buildAuthRaiseStatusDo(userId, authType, LoanType.BLD_LOAN.getCode(), RaiseStatus.N.getCode(), BigDecimal.ZERO, new Date());
+	this.saveOrUpdateRaiseStatus(bldRaiseStatus, false);
+	AfAuthRaiseStatusDo xjdRaiseStatus = this.buildAuthRaiseStatusDo(userId, authType, LoanType.CASH.getCode(), RaiseStatus.Y.getCode(), BigDecimal.ZERO, new Date());
+	this.saveOrUpdateRaiseStatus(xjdRaiseStatus, false);
 
-	@Override
-	public void initZhengxinRaiseStatus(Long userId, String authType) {
-		this.initCreditRaiseStatus(userId, authType);
-	}
+    }
 
-	@Override
-	public void initOnlinebankRaiseStatus(Long userId, String authType) {
-		AfAuthRaiseStatusDo bldRaiseStatus = this.buildAuthRaiseStatusDo(userId, authType, LoanType.BLD_LOAN.getCode(),
-				RaiseStatus.N.getCode(), BigDecimal.ZERO, new Date());
-		this.saveOrUpdateRaiseStatus(bldRaiseStatus,false);
-		AfAuthRaiseStatusDo xjdRaiseStatus = this.buildAuthRaiseStatusDo(userId, authType, LoanType.CASH.getCode(),
-				RaiseStatus.Y.getCode(), BigDecimal.ZERO, new Date());
-		this.saveOrUpdateRaiseStatus(xjdRaiseStatus,false);
+    @Override
+    public AfAuthRaiseStatusDo getByPrdTypeAndAuthType(String prdType, String authType, Long userId) {
 
-	}
-
-	@Override
-	public AfAuthRaiseStatusDo getByPrdTypeAndAuthType(String prdType, String authType, Long userId) {
-        
-        	return afAuthRaiseStatusDao.getByPrdTypeAndAuthType(prdType, authType, userId);
-	}
+	return afAuthRaiseStatusDao.getByPrdTypeAndAuthType(prdType, authType, userId);
+    }
 }
