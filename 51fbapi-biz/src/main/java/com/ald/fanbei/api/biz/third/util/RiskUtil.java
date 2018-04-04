5980de330a09b6a834a5f958e434159412051139
renchunlei@edspay.com <renchunlei@edspay.com>
2018-04-04 16:23:04 +0800

    消息同步优化

--- 51fbapi-biz/src/main/java/com/ald/fanbei/api/biz/third/util/RiskUtil.java
+++ 51fbapi-biz/src/main/java/com/ald/fanbei/api/biz/third/util/RiskUtil.java
@@ -868,8 +868,8 @@ public class RiskUtil extends AbstractThird {
 		String reqResult = requestProxy.post(url, reqBo);
 		try{
 			logger.info("sync kafka data consumerNo:"+consumerNo);
-			kafkaSync.syncEvent(consumerNo, KafkaConstants.SYNC_CONSUMPTION_PERIOD,true);
-			kafkaSync.syncEvent(consumerNo, KafkaConstants.SYNC_CASH_LOAN,true);
+			kafkaSync.syncEvent(Long.parseLong(consumerNo), KafkaConstants.SYNC_CONSUMPTION_PERIOD,true);
+			kafkaSync.syncEvent(Long.parseLong(consumerNo), KafkaConstants.SYNC_CASH_LOAN,true);
 		}catch (Exception e){
 
 		}
