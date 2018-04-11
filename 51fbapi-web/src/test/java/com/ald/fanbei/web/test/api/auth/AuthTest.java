7ecc778bd61a1422cb10b504ddcd29228277c30c
jilong <jilong@edspay.com>
2018-04-11 18:21:30 +0800

    用户基础接口增加是否绑定银行卡字段

--- 51fbapi-web/src/test/java/com/ald/fanbei/web/test/api/auth/AuthTest.java
+++ 51fbapi-web/src/test/java/com/ald/fanbei/web/test/api/auth/AuthTest.java
@@ -13,8 +13,8 @@ public class AuthTest  extends BaseTest{
 	/**
 	 * 自测根据自己的业务修改下列属性 TODO
 	 */
-//	String urlBase = "https://atestapp.51fanbei.com";
-	String urlBase = "http://localhost:8080";
+	String urlBase = "https://testapp.51fanbei.com";
+//	String urlBase = "http://localhost:8080";
 //	String userName = "13638668564";	//田建成 cardId:3111464419 支付密码123456
 //	String userName = "15669066271";	//田建成 cardId:3111464125 支付密码123456
 //	String userName = "13958004662";	//胡朝永 支付密码123456
