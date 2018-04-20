24bf564645084f817311cdfefd81967a685c2f2e
gaojibin <gaojibin@edspay.com>
2018-04-20 10:05:34 +0800

    月供分期金额应按活动最低价计算;在活动页面中商品的库存应为所有sku相加的总库存

--- 51fbapi-web/src/main/java/com/ald/fanbei/api/web/apph5/controller/AppH5FlashSaleController.java
+++ 51fbapi-web/src/main/java/com/ald/fanbei/api/web/apph5/controller/AppH5FlashSaleController.java
@@ -178,7 +178,7 @@ public class AppH5FlashSaleController extends BaseController {
 	    }
 	    
 	    List<Map<String, Object>> nperList = InterestFreeUitl.getConsumeList(array, interestFreeArray, BigDecimal.ONE.intValue(), 
-		    homePageSecKillGoods.getSaleAmount(), resource.getValue1(), resource.getValue2(), goodsId, "0");
+		    homePageSecKillGoods.getActivityAmount(), resource.getValue1(), resource.getValue2(), goodsId, "0");
 	    if (nperList != null) {
 		goodsInfo.put("goodsType", "1");
 		Map<String, Object> nperMap = nperList.get(nperList.size() - 1);
