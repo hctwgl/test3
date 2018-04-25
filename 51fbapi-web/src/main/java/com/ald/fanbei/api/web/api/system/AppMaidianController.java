5c6f31d8dab75b1abfd18b3bae06d46e33c1c6e7
gaojibin <gaojibin@edspay.com>
2018-04-25 10:09:44 +0800

    app 埋点

--- 51fbapi-web/src/main/java/com/ald/fanbei/api/web/api/system/AppMaidianController.java
+++ 51fbapi-web/src/main/java/com/ald/fanbei/api/web/api/system/AppMaidianController.java
@@ -1,24 +1,21 @@
 package com.ald.fanbei.api.web.api.system;
-        import java.io.IOException;
 
-        import javax.servlet.http.HttpServletRequest;
+import com.ald.fanbei.api.biz.service.impl.MaidianRunnable;
+import com.ald.fanbei.api.common.exception.FanbeiExceptionCode;
+import com.ald.fanbei.api.dal.domain.dto.AppMaidianDto;
+import com.ald.fanbei.api.web.common.ApiHandleResponse;
+import org.apache.commons.lang.ObjectUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-        import com.ald.fanbei.api.biz.service.impl.MaidianRunnable;
-        import com.ald.fanbei.api.web.common.*;
-        import org.apache.commons.lang.ObjectUtils;
-        import org.springframework.beans.factory.annotation.Autowired;
-        import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-        import org.springframework.stereotype.Controller;
-        import org.springframework.ui.ModelMap;
-        import org.springframework.web.bind.annotation.RequestMapping;
-        import org.springframework.web.bind.annotation.RequestMethod;
-        import org.springframework.web.bind.annotation.ResponseBody;
-
-        import com.ald.fanbei.api.common.FanbeiContext;
-        import com.ald.fanbei.api.common.exception.FanbeiException;
-        import com.ald.fanbei.api.common.exception.FanbeiExceptionCode;
-        import com.alibaba.fastjson.JSON;
-        import com.alibaba.fastjson.JSONObject;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @类描述：
@@ -28,27 +25,28 @@ package com.ald.fanbei.api.web.api.system;
  */
 @Controller
 @RequestMapping("/fanbei-app")
-public class AppMaidianController implements ApiHandle {
+public class AppMaidianController{
+
+    Logger logger = LoggerFactory.getLogger(AppMaidianController.class);
 
     @Autowired
     ThreadPoolTaskExecutor threadPoolMaidianTaskExecutor;
 
     @ResponseBody
-    @Override
     @RequestMapping(value = "postMaidianInfo", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
-    public ApiHandleResponse process(RequestDataVo requestDataVo, FanbeiContext context, HttpServletRequest request) {
+    public ApiHandleResponse process(@RequestBody AppMaidianDto appMaidianDto, HttpServletRequest request) {
         try {
-            String maidianInfo = ObjectUtils.toString(request.getParameter("maidianInfo"), "").toString();
+            String maidianInfo = ObjectUtils.toString(appMaidianDto.getMaidianInfo(), "").toString();
             String maidianInfo1 = ObjectUtils.toString(request.getHeader("appVersion"), "").toString();
-            String maidianInfo2 = ObjectUtils.toString(request.getParameter("maidianInfo2"), "").toString();
-            String maidianInfo3 = ObjectUtils.toString(request.getParameter("maidianInfo3"), "").toString();
+            String maidianInfo2 = ObjectUtils.toString(appMaidianDto.getMaidianInfo2(), "").toString();
+            String maidianInfo3 = ObjectUtils.toString(appMaidianDto.getMaidianInfo3(), "").toString();
 
             MaidianRunnable maidianRunnable = new MaidianRunnable(request, "",true, maidianInfo, maidianInfo1, maidianInfo2, maidianInfo3);
             threadPoolMaidianTaskExecutor.execute(maidianRunnable);
-            return new ApiHandleResponse(requestDataVo.getId(), FanbeiExceptionCode.SUCCESS);
+            return new ApiHandleResponse(request.getHeader("id"), FanbeiExceptionCode.SUCCESS);
         } catch (Exception e) {
             logger.error("fanbei-app postMaidianInfo error:", e);
-            return new ApiHandleResponse(requestDataVo.getId(), FanbeiExceptionCode.SYSTEM_ERROR);
+            return new ApiHandleResponse(request.getHeader("id"), FanbeiExceptionCode.SYSTEM_ERROR);
         }
     }
 
