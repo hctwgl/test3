cce748fa5527194a38925584baa0718e1b85a2ed
gaojibin <gaojibin@edspay.com>
2018-04-26 20:05:21 +0800

    订单支付重复时间控制由30秒增加到45秒

--- 51fbapi-web/src/main/java/com/ald/fanbei/api/web/api/brand/PayOrderV1Api.java
+++ 51fbapi-web/src/main/java/com/ald/fanbei/api/web/api/brand/PayOrderV1Api.java
@@ -217,7 +217,7 @@ public class PayOrderV1Api implements ApiHandle {
 	if (!BankPayChannel.KUAIJIE.getCode().equals(bankChannel)) {
 	    String lockKey = "payOrder:" + userId + ":" + payId + ":" + orderId;
 	    if (bizCacheUtil.getObject(lockKey) == null) {
-		bizCacheUtil.saveObject(lockKey, lockKey, 30);
+		bizCacheUtil.saveObject(lockKey, lockKey, 45);
 	    } else {
 		return new ApiHandleResponse(requestDataVo.getId(), FanbeiExceptionCode.ORDER_PAY_DEALING);
 	    }
