fb4cc45d10be99d9b8f9aca22533781dd8f83446
郭帅强 <guoshuaiqiang@Alamap.com>
2018-04-28 14:03:17 +0800

    回收商品表

---
+++ 51fbapi-biz/src/main/java/com/ald/fanbei/api/biz/service/AfRecycleGoodsService.java
@@ -0,0 +1,15 @@
+package com.ald.fanbei.api.biz.service;
+
+import com.ald.fanbei.api.dal.domain.AfRecycleGoodsDo;
+
+/**
+ * 回收商品表Service
+ * 
+ * @author guoshuaiqiang
+ * @version 1.0.0 初始化
+ * @date 2018-04-28 14:00:36
+ * Copyright 本内容仅限于杭州阿拉丁信息科技股份有限公司内部传阅，禁止外泄以及用于其他的商业目的
+ */
+public interface AfRecycleGoodsService extends ParentService<AfRecycleGoodsDo, Long>{
+
+}
