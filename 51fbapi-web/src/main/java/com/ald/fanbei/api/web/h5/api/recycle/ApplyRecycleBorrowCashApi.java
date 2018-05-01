391ed661c883af55ff7ab99ad0b5d3bee67a6292
郭帅强 <guoshuaiqiang@Alamap.com>
2018-05-01 11:02:06 +0800

    回收商品借款接口修改

--- 51fbapi-web/src/main/java/com/ald/fanbei/api/web/h5/api/recycle/ApplyRecycleBorrowCashApi.java
+++ 51fbapi-web/src/main/java/com/ald/fanbei/api/web/h5/api/recycle/ApplyRecycleBorrowCashApi.java
@@ -41,7 +41,7 @@ import java.util.List;
  * @类描述：申请借钱V2
  * @注意：本内容仅限于杭州阿拉丁信息科技股份有限公司内部传阅，禁止外泄以及用于其他的商业目的
  */
-@Component("applyRecycleBorrowCashApi")
+@Component("applyBorrowRecycleCashApi")
 @Validator("applyRecycleBorrowCashParam")
 public class ApplyRecycleBorrowCashApi implements H5Handle {
 
