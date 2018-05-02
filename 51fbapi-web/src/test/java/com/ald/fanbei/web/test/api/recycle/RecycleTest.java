0c1696140c0715026d79c6ef1ef1f87777950e97
朱江丰 <zhujiangfeng@edspay.com>
2018-05-02 11:25:30 +0800

    测试代码

--- 51fbapi-web/src/test/java/com/ald/fanbei/web/test/api/recycle/RecycleTest.java
+++ 51fbapi-web/src/test/java/com/ald/fanbei/web/test/api/recycle/RecycleTest.java
@@ -1,33 +1,24 @@
 package com.ald.fanbei.web.test.api.recycle;
 
-import com.ald.fanbei.api.common.enums.PayOrderSource;
-import com.ald.fanbei.api.common.enums.UserAccountLogType;
-import com.ald.fanbei.api.context.Context;
-import com.ald.fanbei.api.context.ContextImpl;
-import com.ald.fanbei.web.test.common.BaseTest;
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.apache.commons.codec.digest.DigestUtils;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.io.UnsupportedEncodingException;
-import java.util.HashMap;
-import java.util.Map;
+import com.ald.fanbei.api.common.enums.PayOrderSource;
+import com.ald.fanbei.api.common.enums.UserAccountLogType;
+import com.ald.fanbei.web.test.common.AccountOfTester;
+import com.ald.fanbei.web.test.common.BaseTest;
 
 public class RecycleTest extends BaseTest{
 	/**
 	 * 自测根据自己的业务修改下列属性 TODO
 	 */
-	String urlBase = "http://localhost:8080";
-//	String urlBase = "http://localhost:8080";
-//	String userName = "13638668564";	//田建成 cardId:3111464419 支付密码123456
-	String userName = "15669066271";	//田建成 cardId:3111464125 支付密码123456
-//	String userName = "13958004662";	//胡朝永 支付密码123456
-//	String userName = "13460011555";	//张飞凯 支付密码123456
-//	String userName = "15293971826";	//秦继强 支付密码888888
-//	String userName = "13370127054";	//王卿 	支付密码123456
-//	String userName = "13656648524";	//朱玲玲 支付密码123456
-//	String userName = "13510301615";	//王绪武 支付密码123456
-//	String userName = "17756648524";	//新账号 支付密码123456
+	String urlBase = "https://btestapp.51fanbei.com";
+	String userName = AccountOfTester.田建成.mobile;
 	
 	/**
 	 * 自动注入登陆令牌，当needLogin为true时，不得注释此方法
