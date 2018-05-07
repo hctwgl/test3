d2f789c99a7cb0a32161591fb07d59ecb29bd98e
chefeipeng <chefeipeng@Alamap.com>
2018-05-07 14:03:28 +0800

    边逛边赚

---
+++ 51fbapi-biz/src/main/java/com/ald/fanbei/api/biz/service/impl/AfSignRewardExtServiceImpl.java
@@ -0,0 +1,36 @@
+package com.ald.fanbei.api.biz.service.impl;
+
+import javax.annotation.Resource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import com.ald.fanbei.api.dal.dao.BaseDao;
+import com.ald.fanbei.api.dal.dao.AfSignRewardExtDao;
+import com.ald.fanbei.api.dal.domain.AfSignRewardExtDo;
+import com.ald.fanbei.api.biz.service.AfSignRewardExtService;
+
+
+
+/**
+ * 分类运营位配置ServiceImpl
+ * 
+ * @author chefeipeng
+ * @version 1.0.0 初始化
+ * @date 2018-05-07 14:01:11
+ * Copyright 本内容仅限于杭州阿拉丁信息科技股份有限公司内部传阅，禁止外泄以及用于其他的商业目的
+ */
+ 
+@Service("afSignRewardExtService")
+public class AfSignRewardExtServiceImpl  implements AfSignRewardExtService {
+	
+    private static final Logger logger = LoggerFactory.getLogger(AfSignRewardExtServiceImpl.class);
+   
+    @Resource
+    private AfSignRewardExtDao afSignRewardExtDao;
+
+		@Override
+	public BaseDao<AfSignRewardExtDo, Long> getDao() {
+		return afSignRewardExtDao;
+	}
+}
---
+++ 51fbapi-biz/src/main/java/com/ald/fanbei/api/biz/service/impl/AfSignRewardServiceImpl.java
@@ -0,0 +1,33 @@
+package com.ald.fanbei.api.biz.service.impl;
+
+import javax.annotation.Resource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import com.ald.fanbei.api.dal.dao.BaseDao;
+import com.ald.fanbei.api.dal.dao.AfSignRewardDao;
+import com.ald.fanbei.api.dal.domain.AfSignRewardDo;
+import com.ald.fanbei.api.biz.service.AfSignRewardService;
+
+
+
+/**
+ * 分类运营位配置ServiceImpl
+ * 
+ * @author liutengyuan
+ * @version 1.0.0 初始化
+ * @date 2018-05-07 13:51:06
+ * Copyright 本内容仅限于杭州阿拉丁信息科技股份有限公司内部传阅，禁止外泄以及用于其他的商业目的
+ */
+ 
+@Service("afSignRewardService")
+public class AfSignRewardServiceImpl  implements AfSignRewardService {
+	
+    private static final Logger logger = LoggerFactory.getLogger(AfSignRewardServiceImpl.class);
+   
+    @Resource
+    private AfSignRewardDao afSignRewardDao;
+
+
+}
