3ca85c24a6db365d0acd99859356572b1d00fa83
郭帅强 <guoshuaiqiang@Alamap.com>
2018-05-07 14:16:08 +0800

    回收商品认证信息查询增加强风控未认证判断

--- 51fbapi-biz/src/main/java/com/ald/fanbei/api/biz/service/impl/AfBorrowRecycleServiceImpl.java
+++ 51fbapi-biz/src/main/java/com/ald/fanbei/api/biz/service/impl/AfBorrowRecycleServiceImpl.java
@@ -178,6 +178,9 @@ public class AfBorrowRecycleServiceImpl extends ParentServiceImpl<AfBorrowCashDo
             bo.action="DO_BIND_CARD";
         }else if (userAuth.getRiskStatus().equals("N")){
             bo.rejectCode=AfBorrowCashRejectType.NO_PASS_STRO_RISK.name();
+        }else if (userAuth.getRiskStatus().equals("A")||userAuth.getRiskStatus().equals("P")){
+            bo.rejectCode=AfBorrowCashRejectType.NO_AUTHZ.name();
+            bo.action="DO_BIND_CARD";
         }else if (afUserAuthStatusDo != null && afUserAuthStatusDo.getStatus().equals("Y")){
             bo.rejectCode=AfBorrowCashRejectType.PASS.name();
             //检查额度
