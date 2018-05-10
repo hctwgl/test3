e7d9d801c7fa67094243617309babf13ac4e2d40
DESKTOP-MA60KKU\sun <huangqiujun@edspay.com>
2018-05-10 14:19:00 +0800

    领取优惠券api接口

--- 51fbapi-web/src/main/java/com/ald/fanbei/api/web/controller/FanbeiController.java
+++ 51fbapi-web/src/main/java/com/ald/fanbei/api/web/controller/FanbeiController.java
@@ -59,7 +59,7 @@ public class FanbeiController extends BaseController {
 
     @RequestMapping(value ={
     	"/address/addressList","/address/addAddress","/address/updateInfo","/address/deleteAddress","/mine/commitCode",
-    	"/coupon/couponList","/mine/getInviteInfo","/mine/getOrderListCount","/mine/getSettingInfo","/order/payResultOfAlipay","/order/getOrderLogistics","/coupon/getDetailCouponList"
+    	"/coupon/couponList","/mine/getInviteInfo","/mine/getOrderListCount","/mine/getSettingInfo","/order/payResultOfAlipay","/order/getOrderLogistics","/coupon/getDetailCouponList","/coupon/getPickCoupon"
     },method = RequestMethod.POST,produces="application/json;charset=utf-8")
     @ResponseBody
     public String mineRequest(@RequestBody String body, HttpServletRequest request, HttpServletResponse response) throws IOException{
