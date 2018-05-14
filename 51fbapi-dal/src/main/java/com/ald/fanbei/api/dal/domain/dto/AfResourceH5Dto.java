67ae8d8f15b28769da4ab3871dedb2312d753a63
chefeipeng <chefeipeng@Alamap.com>
2018-05-14 17:58:09 +0800

    fix

--- 51fbapi-dal/src/main/java/com/ald/fanbei/api/dal/domain/dto/AfResourceH5Dto.java
+++ 51fbapi-dal/src/main/java/com/ald/fanbei/api/dal/domain/dto/AfResourceH5Dto.java
@@ -4,16 +4,21 @@ import java.util.Date;
 import java.util.List;
 
 import com.ald.fanbei.api.dal.domain.AfResourceH5Do;
-
+import lombok.Getter;
+import lombok.Setter;
 
 
 /**
  * @author Jingru
  * @version 创建时间：2018年3月21日 下午5:39:58
  */
+@Setter
+@Getter
 public class AfResourceH5Dto extends AfResourceH5Do{
 
 private static final long serialVersionUID = -9058221367468975525L;
+
+	private List itemList;
 
 private Long id;
 
@@ -31,120 +36,6 @@ private String url;
 
 private Long status;
 private Long sort;
-
-/**
- * @return the id
- */
-public Long getId() {
-	return id;
-}
-
-/**
- * @param id the id to set
- */
-public void setId(Long id) {
-	this.id = id;
-}
-
-/**
- * @return the gmtCreate
- */
-public Date getGmtCreate() {
-	return gmtCreate;
-}
-
-/**
- * @param gmtCreate the gmtCreate to set
- */
-public void setGmtCreate(Date gmtCreate) {
-	this.gmtCreate = gmtCreate;
-}
-
-/**
- * @return the gmtModified
- */
-public Date getGmtModified() {
-	return gmtModified;
-}
-
-/**
- * @param gmtModified the gmtModified to set
- */
-public void setGmtModified(Date gmtModified) {
-	this.gmtModified = gmtModified;
-}
-
-/**
- * @return the creator
- */
-public String getCreator() {
-	return creator;
-}
-
-/**
- * @param creator the creator to set
- */
-public void setCreator(String creator) {
-	this.creator = creator;
-}
-
-/**
- * @return the modifier
- */
-public String getModifier() {
-	return modifier;
-}
-
-/**
- * @param modifier the modifier to set
- */
-public void setModifier(String modifier) {
-	this.modifier = modifier;
-}
-
-/**
- * @return the name
- */
-public String getName() {
-	return name;
-}
-
-/**
- * @param name the name to set
- */
-public void setName(String name) {
-	this.name = name;
-}
-
-
-/**
- * @return the url
- */
-public String getUrl() {
-	return url;
-}
-
-/**
- * @param url the url to set
- */
-public void setUrl(String url) {
-	this.url = url;
-}
-
-
-/**
- * @return the status
- */
-public Long getStatus() {
-	return status;
-}
-
-/**
- * @param status the status to set
- */
-public void setStatus(Long status) {
-	this.status = status;
-}
 
 /**
  * 
