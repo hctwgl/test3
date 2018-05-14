8ad4e5650755453c4ec4a082b6b9da2174bf5543
gaojibin <gaojibin@edspay.com>
2018-05-14 18:23:53 +0800

    弱风控接口增加用户第一个大额订单日期参数

--- 51fbapi-biz/src/main/java/com/ald/fanbei/api/biz/service/AfOrderService.java
+++ 51fbapi-biz/src/main/java/com/ald/fanbei/api/biz/service/AfOrderService.java
@@ -1,23 +1,16 @@
 package com.ald.fanbei.api.biz.service;
 
+import com.ald.fanbei.api.common.enums.BorrowType;
+import com.ald.fanbei.api.dal.domain.*;
+import com.ald.fanbei.api.dal.domain.dto.*;
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.data.annotation.Id;
+
 import java.math.BigDecimal;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
-import com.ald.fanbei.api.common.enums.BorrowType;
-import com.ald.fanbei.api.common.enums.PayType;
-import com.ald.fanbei.api.dal.domain.AfBorrowDo;
-import com.ald.fanbei.api.dal.domain.AfInterimAuDo;
-import com.ald.fanbei.api.dal.domain.AfOrderDo;
-import com.ald.fanbei.api.dal.domain.AfOrderLeaseDo;
-import com.ald.fanbei.api.dal.domain.AfOrderSceneAmountDo;
-import com.ald.fanbei.api.dal.domain.AfUserAccountDo;
-import com.ald.fanbei.api.dal.domain.AfUserAccountSenceDo;
-import com.ald.fanbei.api.dal.domain.AfUserBankcardDo;
-import com.ald.fanbei.api.dal.domain.dto.*;
-import com.alibaba.fastjson.JSONObject;
 
 
 /**
@@ -474,7 +467,8 @@ public interface AfOrderService {
 	 */
 	AfOrderCountDto countStatusNum(Long userId);
 
-
 	int getSelfsupportPaySuccessOrderByUserId(Long userId);
+
+	String getUserFirstBigOrderDate(Long userId, Integer amount);
 
 }
