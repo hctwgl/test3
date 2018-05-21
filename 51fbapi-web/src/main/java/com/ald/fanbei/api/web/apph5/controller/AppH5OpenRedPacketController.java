efe2abd44f162a85aaed26a354015c0e7f39cd1f
wangli1 <wangli@edspay.com>
2018-05-21 11:45:38 +0800

    增加日志调试信息

--- 51fbapi-web/src/main/java/com/ald/fanbei/api/web/apph5/controller/AppH5OpenRedPacketController.java
+++ 51fbapi-web/src/main/java/com/ald/fanbei/api/web/apph5/controller/AppH5OpenRedPacketController.java
@@ -30,10 +30,10 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import java.math.BigDecimal;
@@ -49,7 +49,7 @@ import java.util.Map;
  * @author wangli
  * @date 2018/4/11 14:24
  */
-@Controller
+@RestController
 @RequestMapping("/fanbei-web/redPacket")
 public class AppH5OpenRedPacketController extends BaseController {
 
@@ -272,7 +272,6 @@ public class AppH5OpenRedPacketController extends BaseController {
     }
 
     @RequestMapping(value = "/sendVerifyCode", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
-    @ResponseBody
     public String sendVerifyCode(String phone) {
         try {
             logger.info("/redPacket/sendVerifyCode：" + phone);
