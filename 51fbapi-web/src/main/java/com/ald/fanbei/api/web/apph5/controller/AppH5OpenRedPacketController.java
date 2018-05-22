f582659a54444df8f3401611c23d5420afe1be54
gaojibin <gaojibin@edspay.com>
2018-05-22 16:09:28 +0800

    ceshi

--- 51fbapi-web/src/main/java/com/ald/fanbei/api/web/apph5/controller/AppH5OpenRedPacketController.java
+++ 51fbapi-web/src/main/java/com/ald/fanbei/api/web/apph5/controller/AppH5OpenRedPacketController.java
@@ -30,7 +30,10 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
@@ -291,7 +294,6 @@ public class AppH5OpenRedPacketController extends BaseController {
             logger.info("/redPacket/sendVerifyCode：request.getParameter(mobile)=" + mobile2);
             logger.info("/redPacket/sendVerifyCode：param.getMobile=" + mobile);
             return H5CommonResponse.getNewInstance(true, "发送成功").toString();
-
 
             /*if (StringUtils.isBlank(param.getMobile())) {
                 return H5CommonResponse.getNewInstance(false, "手机号不能为空").toString();
