c2f545fe38515702ccfbf610bcbadbd518b3cc88
朱江丰 <zhujiangfeng@edspay.com>
2018-05-31 15:56:03 +0800

    spring unit测试

--- 51fbapi-web/src/test/java/com/ald/fanbei/JunitBaseTest.java
+++ 51fbapi-web/src/test/java/com/ald/fanbei/JunitBaseTest.java
@@ -4,7 +4,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = { "classpath:/spring/import-beans.xml","classpath:/spring/web/web-main.xml"})
+@ContextConfiguration(locations = { "classpath:test/spring-test.xml","classpath:spring/web/web-main.xml"})
 public class JunitBaseTest {
 
 }
--- 51fbapi-web/src/test/java/com/ald/fanbei/NewTest.java
+++ 51fbapi-web/src/test/java/com/ald/fanbei/NewTest.java
@@ -1,19 +1,33 @@
 package com.ald.fanbei;
-import com.ald.fanbei.api.biz.service.AfUserService;
-import com.ald.fanbei.api.dal.dao.AfUserDao;
-import com.ald.fanbei.api.dal.domain.AfUserDo;
+import java.math.BigDecimal;
+
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import com.ald.fanbei.api.dal.dao.AfUserAccountSenceDao;
+import com.ald.fanbei.api.dal.dao.AfUserDao;
+import com.ald.fanbei.api.dal.domain.AfUserAccountSenceDo;
+import com.ald.fanbei.api.dal.domain.AfUserDo;
 
 public class NewTest extends JunitBaseTest {
 
     @Autowired
     private AfUserDao userDao;
-
+    @Autowired
+    private AfUserAccountSenceDao afUserAccountSenceDao;
 
     @Test
     public void getUserInfo(){
         AfUserDo userDo = userDao.getUserByMobile("13018933980");
         System.out.println(userDo.getRid());
+    }
+    
+    @Test
+    public void updateUserAccountScene(){
+    	AfUserAccountSenceDo accSceneDo = new AfUserAccountSenceDo();
+    	accSceneDo.setRid(36535776L);
+    	accSceneDo.setScene("LOAN_TOTAL");
+    	accSceneDo.setAuAmount(new BigDecimal(5000));
+	    afUserAccountSenceDao.updateById(accSceneDo);
     }
 }
