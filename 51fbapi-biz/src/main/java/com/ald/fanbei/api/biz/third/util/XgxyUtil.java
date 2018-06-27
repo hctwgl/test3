c5c67025ba3f616f6debfe51572ff5494532f5e8
jilong <jl>
2018-06-27 13:47:05 +0800

    绑定银行卡

--- 51fbapi-biz/src/main/java/com/ald/fanbei/api/biz/third/util/XgxyUtil.java
+++ 51fbapi-biz/src/main/java/com/ald/fanbei/api/biz/third/util/XgxyUtil.java
@@ -82,13 +82,23 @@ public class XgxyUtil {
         try {
             Map<String,String> params=new HashMap<>();
             params.put("appId",repayBo.get(0).getTradeNo());
-            Map<String,List> rePayList=new HashMap<>();
+            List<Map<String,Object>> list=new ArrayList();
             for(XgxyRepayBo rePay:repayBo){
-                Map<String,String> rePayBo=new HashMap<>();
+                Map<String,Object> rePayBo=new HashMap<>();
                 rePayBo.put("borrowNo",rePay.getBorrowNo());
                 rePayBo.put("status",rePay.getStatus());
+                rePayBo.put("amount",rePay.getAmount());
+                Map<String,String> borrowBillDetails=new HashMap<>();
+                borrowBillDetails.put("curPeriod",rePay.getCurPeriod());
+                borrowBillDetails.put("isFinish",rePay.getIsFinish());
+                borrowBillDetails.put("unrepayAmount",rePay.getUnrepayAmount());
+                borrowBillDetails.put("unrepayInterestFee",rePay.getUnrepayInterestFee());
+                borrowBillDetails.put("unrepayOverdueFee",rePay.getUnrepayOverdueFee());
+                borrowBillDetails.put("unrepayServiceFee",rePay.getUnrepayServiceFee());
+                rePayBo.put("borrowBillDetails",borrowBillDetails);
+                list.add(rePayBo);
             }
-            params.put("data",JSON.toJSONString(rePayList));
+            params.put("data",JSON.toJSONString(list));
             params.put("sign",DsedSignUtil.paramsEncrypt(JSONObject.parseObject(JSON.toJSONString(params)), PRIVATE_KEY));
             String reqResult = HttpUtil.post(getXgxyUrl(), params);
             if(StringUtil.isBlank(reqResult)){
