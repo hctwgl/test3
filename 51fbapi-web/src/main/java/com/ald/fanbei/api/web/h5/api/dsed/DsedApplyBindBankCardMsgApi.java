3500e000431832246db23f640505b26c87805c79
jilong <jl>
2018-06-27 15:35:05 +0800

    绑定银行卡

---
+++ 51fbapi-web/src/main/java/com/ald/fanbei/api/web/h5/api/dsed/DsedApplyBindBankCardMsgApi.java
@@ -0,0 +1,108 @@
+package com.ald.fanbei.api.web.h5.api.dsed;
+
+
+import com.ald.fanbei.api.biz.bo.UpsAuthSignRespBo;
+import com.ald.fanbei.api.biz.service.DsedBankService;
+import com.ald.fanbei.api.biz.service.DsedUserBankcardService;
+import com.ald.fanbei.api.biz.service.DsedUserService;
+import com.ald.fanbei.api.biz.third.util.UpsUtil;
+import com.ald.fanbei.api.common.FanbeiContext;
+import com.ald.fanbei.api.common.enums.BankcardStatus;
+import com.ald.fanbei.api.common.enums.YesNoStatus;
+import com.ald.fanbei.api.common.exception.FanbeiException;
+import com.ald.fanbei.api.common.exception.FanbeiExceptionCode;
+import com.ald.fanbei.api.context.Context;
+import com.ald.fanbei.api.dal.domain.AfUserBankcardDo;
+import com.ald.fanbei.api.dal.domain.DsedBankDo;
+import com.ald.fanbei.api.dal.domain.DsedUserBankcardDo;
+import com.ald.fanbei.api.dal.domain.DsedUserDo;
+import com.ald.fanbei.api.web.common.*;
+import com.ald.fanbei.api.web.validator.Validator;
+import org.apache.commons.lang.ObjectUtils;
+import org.springframework.stereotype.Component;
+import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *@类现描述：申请绑卡银行短信
+ *@author jilong
+ *@version
+ *@注意：本内容仅限于杭州阿拉丁信息科技股份有限公司内部传阅，禁止外泄以及用于其他的商业目的
+ */
+@Component("dsedApplyBindBankCardMsgApi")
+@Validator("bankCardBindParam")
+public class DsedApplyBindBankCardMsgApi implements DsedH5Handle {
+
+    @Resource
+    private DsedUserService dsedUserService;
+
+    @Resource
+    private DsedUserBankcardService dsedUserBankcardService;
+
+    @Resource
+    private DsedBankService dsedBankService;
+
+    @Resource
+    UpsUtil upsUtil;
+
+    @Override
+    public DsedH5HandleResponse process(Context context)  {
+        DsedH5HandleResponse resp = new DsedH5HandleResponse(200, "");
+        Long userId = context.getUserId();
+        String bankNo = ObjectUtils.toString(context.getData("bankNo"), null);
+        String bankName = ObjectUtils.toString(context.getData("bankName"), null);
+        String bankMobile = ObjectUtils.toString(context.getData("bankMobile"), null);
+        String validDate = ObjectUtils.toString(context.getData("validDate"), null);
+        String safeCode = ObjectUtils.toString(context.getData("safeCode"), null);
+//        Long userId= Long.parseLong(String.valueOf(context.getData("userId"))) ;
+       //判断是否已经被绑定
+        if(dsedUserBankcardService.getUserBankByCardNo(bankNo)>0){
+            throw new FanbeiException("user bankcard exist error", FanbeiExceptionCode.USER_BANKCARD_EXIST_ERROR);
+        }
+
+
+     //默认赋值为借记卡
+     String cardType = "00";
+     //获取用户身份信息
+     DsedUserDo userDo=dsedUserService.getById(userId);
+
+     DsedBankDo bank=dsedBankService.getBankByName(bankName);
+     //调用ups
+     UpsAuthSignRespBo upsResult = upsUtil.authSign(userId.toString(), userDo.getRealName(), bankMobile, userDo.getIdNumber(), bankNo, "02",
+             bank.getBankCode(),cardType,validDate,safeCode);
+
+     if(!upsResult.isSuccess()){
+         return new DsedH5HandleResponse(200, FanbeiExceptionCode.AUTH_BINDCARD_ERROR.getErrorMsg());
+      }else if(!"10".equals(upsResult.getNeedCode())){
+          return new DsedH5HandleResponse(200, FanbeiExceptionCode.AUTH_BINDCARD_SMS_ERROR.getErrorMsg());
+      }
+     //是否是设主卡
+      String isMain = YesNoStatus.NO.getCode();
+      if(dsedUserBankcardService.getUserBankCardInfoByUserId(userId).size()==0){
+          isMain=YesNoStatus.YES.getCode();
+      }
+      //创建用户银行卡新加状态
+      DsedUserBankcardDo userBankcard= buildUserCard( bank.getBankCode(),bankName,bankNo,bankMobile,userId,isMain,validDate,safeCode);
+      dsedUserBankcardService.addUserBankcard(userBankcard);
+      Map<String,Object> map = new HashMap<String,Object>();
+      map.put("userBankcardId",userBankcard.getRid());
+      resp.setData(map);
+      return resp;
+    }
+
+    private DsedUserBankcardDo buildUserCard(String bankCode, String bankName, String cardNumber, String mobile, Long userId, String isMain,String validDate,String safeCode){
+        DsedUserBankcardDo bank = new DsedUserBankcardDo();
+        bank.setBankCode(bankCode);
+        bank.setBankName(bankName);
+        bank.setCardNumber(cardNumber);
+        bank.setIsMain(isMain);
+        bank.setMobile(mobile);
+        bank.setStatus(BankcardStatus.NEW.getCode());
+        bank.setUserId(userId);
+        bank.setValidDate(validDate);
+        bank.setSafeCode(safeCode);
+
+        return bank;
+    }
+}
---
+++ 51fbapi-web/src/test/java/com/ald/fanbei/web/test/api/dsed/DsedbindCardTest.java
@@ -0,0 +1,165 @@
+package com.ald.fanbei.web.test.api.dsed;
+
+import com.ald.fanbei.api.biz.arbitration.MD5;
+import com.ald.fanbei.api.common.enums.PayOrderSource;
+import com.ald.fanbei.api.common.enums.UserAccountLogType;
+import com.ald.fanbei.api.common.util.AesUtil;
+import com.ald.fanbei.api.common.util.DsedSignUtil;
+import com.ald.fanbei.web.test.common.BaseTest;
+import com.ald.fanbei.web.test.common.HttpUtil;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import org.apache.commons.codec.digest.DigestUtils;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.UnsupportedEncodingException;
+import java.util.*;
+
+public class DsedbindCardTest extends BaseTest {
+    /**
+     * 自测根据自己的业务修改下列属性 TODO
+     */
+//	String urlBase = "https://testapi.51fanbei.com";
+    String urlBase = "http://localhost:8080";
+    //	String userName = "13638668564";	//田建成 cardId:3111464419 支付密码123456
+    String userName = "15669066271";    //田建成 cardId:3111464125 支付密码123456
+//	String userName = "13958004662";	//胡朝永 支付密码123456
+//	String userName = "13460011555";	//张飞凯 支付密码123456
+//	String userName = "15293971826";	//秦继强 支付密码888888
+//	String userName = "13370127054";	//王卿 	支付密码123456
+//	String userName = "13656648524";	//朱玲玲 支付密码123456
+//	String userName = "13510301615";	//王绪武 支付密码123456
+//	String userName = "17756648524";	//新账号 支付密码123456
+
+    /**
+     * 自动注入登陆令牌，当needLogin为true时，不得注释此方法
+     */
+    @Before
+    public void init() {
+        super.init(userName);
+    }
+
+
+    /**
+     * 发起贷款申请
+     */
+    @Test
+    public void dsedApplyBind() {
+        String url = urlBase + "/third/xgxy/v1/applyBindBankcard";
+        Map<String, String> params = new HashMap<>();
+        params.put("bankNo", "6214835893570075");
+        params.put("bankName", "招商银行");
+        params.put("bankMobile", "18868180875");
+        params.put("userId","13989455823");
+        params.put("validDate","");
+        params.put("safeCode","");
+
+
+        String data = DsedSignUtil.paramsEncrypt(JSONObject.parseObject(JSON.toJSONString(params)),"aef5c8c6114b8d6a");
+        Map<String, String> p = new HashMap<>();
+        p.put("data", data);
+        p.put("sign", generateSign(params, "aef5c8c6114b8d6a"));
+        String respResult = com.ald.fanbei.api.common.util.HttpUtil.doHttpPostJsonParam(url, JSON.toJSONString(p));
+    }
+
+    @Test
+    public void dsedSubmitBind() {
+        String url = urlBase + "/third/xgxy/v1/submitBindBankcard";
+        Map<String, String> params = new HashMap<>();
+        params.put("bankCardId", "3111465095");
+        params.put("realname", "杨雨秋");
+        params.put("smsCode", "888888");
+        params.put("userId","13989455823");
+
+
+
+        String data = DsedSignUtil.paramsEncrypt(JSONObject.parseObject(JSON.toJSONString(params)),"aef5c8c6114b8d6a");
+        Map<String, String> p = new HashMap<>();
+        p.put("data", data);
+        p.put("sign", generateSign(params, "aef5c8c6114b8d6a"));
+        String respResult = com.ald.fanbei.api.common.util.HttpUtil.doHttpPostJsonParam(url, JSON.toJSONString(p));
+    }
+
+
+
+    /**
+     * 生成本地签名
+     *
+     * @param params
+     * @return
+     * @throws IllegalAccessException
+     * @throws IllegalArgumentException
+     */
+    public static String generateSign(Map<String, String> params, String appSecret) throws IllegalArgumentException {
+        List<String> keys = new ArrayList<String>(params.keySet());
+        keys.remove("signCode");
+        Collections.sort(keys);
+        StringBuffer result = null;
+        for (int i = 0; i < keys.size(); i++) {
+            String key = keys.get(i);
+            if (i == 0) result = new StringBuffer();
+            else result.append("&");
+            result.append(key).append("=").append(params.get(key));
+        }
+        result.append("&appSecret=" + appSecret);
+        return params == null ? null : MD5.md5(result.toString());
+    }
+
+
+    public static String paramsEncrypt(Map<String, String> params, String appSecret) {
+        List<String> keys = new ArrayList<String>(params.keySet());
+        Collections.sort(keys);
+        JSONObject obj = new JSONObject(true);
+        for (int i = 0; i < keys.size(); i++) {
+            String key = keys.get(i);
+            String value = params.get(key);
+            obj.put(key, value);
+        }
+        String result = obj.toString();
+        result = AesUtil.encryptToBase64(result, appSecret);
+        return result;
+    }
+
+
+    /**
+     * 贷款申请成功后，模拟 UPS 回调 返呗API
+     */
+    @Test
+    public void delegatePay() {
+        String url = urlBase + "/third/ups/delegatePay?";
+        String orderNo = "01dpay23425234dfssdfs";
+        String merPriv = UserAccountLogType.LOAN.getCode();
+        String tradeState = "00";
+        String reqExt = "154";
+
+        String reqStr = "orderNo=" + orderNo + "&merPriv=" + merPriv + "&tradeState=" + tradeState + "&reqExt=" + reqExt;
+        url += reqStr;
+
+        testH5(url, null, userName, true);
+    }
+
+
+
+
+
+    @Test
+    public void collect() {
+        String url = urlBase + "/third/ups/collect?";
+        String orderNo = "hq2018051510143800802";
+        String merPriv = PayOrderSource.REPAY_LOAN.getCode();
+        String tradeNo = "xianFenghq2018051510143800802";
+        String tradeState = "00";
+
+        String reqStr = "orderNo=" + orderNo + "&merPriv=" + merPriv + "&tradeNo=" + tradeNo + "&tradeState=" + tradeState;
+        url += reqStr;
+        Map<String, String> params = new HashMap<>();
+        testApi(url, params, userName, true);
+    }
+
+    @Test
+    public void offlineRepayment() throws UnsupportedEncodingException {
+
+    }
+
+}
