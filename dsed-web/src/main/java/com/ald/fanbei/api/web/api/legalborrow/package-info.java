dd0e3988487a2ae8ac96a399ec57bd73ccd945cb
朱江丰 <zhujiangfeng@edspay.com>
2018-07-02 16:15:23 +0800

    删除无用类

--- dsed-web/src/main/java/com/ald/fanbei/api/web/api/legalborrow/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * 借钱合法化专用包路径，包含所有新版借钱的业务接口
- */
-package com.ald.fanbei.api.web.api.legalborrow;
--- dsed-web/src/main/java/com/ald/fanbei/api/web/apph5/controller/ImgBackDo.java
+++
@@ -1,7 +0,0 @@
-package com.ald.fanbei.api.web.apph5.controller;
-
-import java.io.Serializable;
-
-public class ImgBackDo implements Serializable {
-
-}
