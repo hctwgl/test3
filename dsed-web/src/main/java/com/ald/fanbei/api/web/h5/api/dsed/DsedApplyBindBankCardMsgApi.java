b67d4901c70c327fa51a7ec94bc34fa33d5bc12a
guoshuaiqiang <guoshuaiqiang@edspay.com>
2018-07-05 21:22:51 +0800

    ups绑卡日志增加

--- dsed-web/src/main/java/com/ald/fanbei/api/web/h5/api/dsed/DsedApplyBindBankCardMsgApi.java
+++ dsed-web/src/main/java/com/ald/fanbei/api/web/h5/api/dsed/DsedApplyBindBankCardMsgApi.java
@@ -1,34 +1,31 @@
 package com.ald.fanbei.api.web.h5.api.dsed;
 
 
-import com.ald.fanbei.api.biz.bo.UpsAuthSignRespBo;
 import com.ald.fanbei.api.biz.service.DsedBankService;
 import com.ald.fanbei.api.biz.service.DsedUserBankcardService;
 import com.ald.fanbei.api.biz.service.DsedUserService;
-import com.ald.fanbei.api.biz.third.util.UpsUtil;
-import com.ald.fanbei.api.common.FanbeiContext;
 import com.ald.fanbei.api.common.enums.BankcardStatus;
 import com.ald.fanbei.api.common.enums.YesNoStatus;
-import com.ald.fanbei.api.common.exception.FanbeiException;
 import com.ald.fanbei.api.common.exception.FanbeiExceptionCode;
 import com.ald.fanbei.api.context.Context;
-import com.ald.fanbei.api.dal.domain.AfUserBankcardDo;
 import com.ald.fanbei.api.dal.domain.DsedBankDo;
 import com.ald.fanbei.api.dal.domain.DsedUserBankcardDo;
 import com.ald.fanbei.api.dal.domain.DsedUserDo;
-import com.ald.fanbei.api.web.common.*;
+import com.ald.fanbei.api.web.common.DsedH5Handle;
+import com.ald.fanbei.api.web.common.DsedH5HandleResponse;
 import com.ald.fanbei.api.web.validator.Validator;
+import com.alibaba.fastjson.JSON;
 import org.apache.commons.lang.ObjectUtils;
 import org.springframework.stereotype.Component;
+
 import javax.annotation.Resource;
 import java.util.HashMap;
 import java.util.Map;
 
 /**
- *@类现描述：申请绑卡银行短信
- *@author jilong
- *@version
- *@注意：本内容仅限于杭州阿拉丁信息科技股份有限公司内部传阅，禁止外泄以及用于其他的商业目的
+ * @author jilong
+ * @类现描述：申请绑卡银行短信
+ * @注意：本内容仅限于杭州阿拉丁信息科技股份有限公司内部传阅，禁止外泄以及用于其他的商业目的
  */
 @Component("dsedApplyBindBankCardMsgApi")
 @Validator("bankCardBindParam")
@@ -44,7 +41,7 @@ public class DsedApplyBindBankCardMsgApi implements DsedH5Handle {
     private DsedBankService dsedBankService;
 
     @Override
-    public DsedH5HandleResponse process(Context context)  {
+    public DsedH5HandleResponse process(Context context) {
         DsedH5HandleResponse resp = new DsedH5HandleResponse(200, "请求成功");
         String bankNo = ObjectUtils.toString(context.getData("bankNo"), null);
         String bankName = ObjectUtils.toString(context.getData("bankName"), null);
@@ -52,28 +49,29 @@ public class DsedApplyBindBankCardMsgApi implements DsedH5Handle {
         String validDate = ObjectUtils.toString(context.getData("validDate"), "");
         String safeCode = ObjectUtils.toString(context.getData("safeCode"), "");
         String openId = ObjectUtils.toString(context.getData("userId"), null);
-        DsedUserDo user=dsedUserService.getByOpenId(openId);
-       //判断是否已经被绑定
-        if(dsedUserBankcardService.getUserBankByCardNo(bankNo)>0){
+        DsedUserDo user = dsedUserService.getByOpenId(openId);
+        logger.info("dsedApplyBindBankCardMsgApi context=" + JSON.toJSONString(context));
+        //判断是否已经被绑定
+        if (dsedUserBankcardService.getUserBankByCardNo(bankNo) > 0) {
             return new DsedH5HandleResponse(1545, FanbeiExceptionCode.DSED_BANK_BINDED.getDesc());
         }
 
-      DsedBankDo bank=dsedBankService.getBankByName(bankName);
-      //是否是设主卡
-      String isMain = YesNoStatus.NO.getCode();
-      if(dsedUserBankcardService.getUserBankCardInfoByUserId(user.getRid()).size()==0){
-          isMain=YesNoStatus.YES.getCode();
-      }
-      //创建用户银行卡新加状态
-      DsedUserBankcardDo userBankcard= buildUserCard( bank.getBankCode(),bankName,bankNo,bankMobile, user.getRid(),isMain,validDate,safeCode);
-      dsedUserBankcardService.addUserBankcard(userBankcard);
-      Map<String,Object> map = new HashMap<String,Object>();
-      map.put("busiFlag",userBankcard.getRid());
-      resp.setData(map);
-      return resp;
+        DsedBankDo bank = dsedBankService.getBankByName(bankName);
+        //是否是设主卡
+        String isMain = YesNoStatus.NO.getCode();
+        if (dsedUserBankcardService.getUserBankCardInfoByUserId(user.getRid()).size() == 0) {
+            isMain = YesNoStatus.YES.getCode();
+        }
+        //创建用户银行卡新加状态
+        DsedUserBankcardDo userBankcard = buildUserCard(bank.getBankCode(), bankName, bankNo, bankMobile, user.getRid(), isMain, validDate, safeCode);
+        dsedUserBankcardService.addUserBankcard(userBankcard);
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("busiFlag", userBankcard.getRid());
+        resp.setData(map);
+        return resp;
     }
 
-    private DsedUserBankcardDo buildUserCard(String bankCode, String bankName, String cardNumber, String mobile, Long userId, String isMain,String validDate,String safeCode){
+    private DsedUserBankcardDo buildUserCard(String bankCode, String bankName, String cardNumber, String mobile, Long userId, String isMain, String validDate, String safeCode) {
         DsedUserBankcardDo bank = new DsedUserBankcardDo();
         bank.setBankCode(bankCode);
         bank.setBankName(bankName);
