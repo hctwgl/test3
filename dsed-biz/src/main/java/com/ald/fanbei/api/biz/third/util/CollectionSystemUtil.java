9db1952e7975778b3a28f1e03a05c83c2beec476
chefeipeng <chefeipeng@Alamap.com>
2018-07-12 11:16:36 +0800

    都市e贷 催收通知

--- dsed-biz/src/main/java/com/ald/fanbei/api/biz/third/util/CollectionSystemUtil.java
+++ dsed-biz/src/main/java/com/ald/fanbei/api/biz/third/util/CollectionSystemUtil.java
@@ -1,18 +1,25 @@
 package com.ald.fanbei.api.biz.third.util;
 
+import java.math.BigDecimal;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import com.ald.fanbei.api.biz.bo.CollectionDataBo;
+import com.ald.fanbei.api.biz.bo.CollectionSystemReqRespBo;
+import com.ald.fanbei.api.common.enums.AfRepayCollectionType;
+import com.ald.fanbei.api.common.enums.AfRepeatCollectionType;
+import com.ald.fanbei.api.common.util.*;
+import com.alibaba.fastjson.JSONObject;
 import org.springframework.stereotype.Component;
 
 import com.ald.fanbei.api.biz.third.AbstractThird;
 import com.ald.fanbei.api.common.Constants;
 import com.ald.fanbei.api.common.exception.FanbeiException;
-import com.ald.fanbei.api.common.util.ConfigProperties;
-import com.ald.fanbei.api.common.util.HttpUtil;
-import com.ald.fanbei.api.common.util.StringUtil;
 import com.alibaba.fastjson.JSON;
+
+import javax.annotation.Resource;
 
 /**
  * 
@@ -24,6 +31,9 @@ import com.alibaba.fastjson.JSON;
 public class CollectionSystemUtil extends AbstractThird {
 
 	private static String url = null;
+
+	@Resource
+	CommitRecordUtil commitRecordUtil;
 
 	private static String getUrl() {
 		if (url == null) {
@@ -98,6 +108,112 @@ public class CollectionSystemUtil extends AbstractThird {
 
 		return false;
 
+	}
+
+
+	/**
+	 * 都市e贷主动还款通知催收平台
+	 *
+	 * @param repayNo
+	 *            --还款编号
+	 * @param borrowNo
+	 *            --借款单号
+	 * @param cardNumber
+	 *            --卡号
+	 * @param cardName
+	 *            --银行卡名称（支付方式）
+	 * @param amount
+	 *            --还款金额
+	 * @param restAmount
+	 *            --剩余未还金额
+	 * @param repayAmount
+	 *            --理论应还款金额
+	 * @param overdueAmount
+	 *            --逾期手续费
+	 * @param repayAmountSum
+	 *            --已还总额
+	 * @param rateAmount
+	 *            --利息
+	 * @return
+	 */
+	public CollectionSystemReqRespBo consumerRepayment(String repayNo, String borrowNo, String cardNumber,
+													   String cardName, String repayTime, String tradeNo, BigDecimal amount, BigDecimal restAmount,
+													   BigDecimal repayAmount, BigDecimal overdueAmount, BigDecimal repayAmountSum, BigDecimal rateAmount, Boolean isCashOverdue) {
+		CollectionDataBo data = new CollectionDataBo();
+		Map<String, String> reqBo = new HashMap<String, String>();
+		reqBo.put("repay_no", repayNo);
+		reqBo.put("borrow_no", borrowNo);
+		reqBo.put("card_number", cardNumber);
+		reqBo.put("card_name", cardName);
+		reqBo.put("repay_time", repayTime);
+		if (StringUtil.isEmpty(tradeNo)) {
+			tradeNo = repayNo;
+		}
+		reqBo.put("trade_no", tradeNo);
+		reqBo.put("amount", amount.multiply(BigDecimalUtil.ONE_HUNDRED) + "");
+		reqBo.put("rest_amount", restAmount.multiply(BigDecimalUtil.ONE_HUNDRED) + "");
+		reqBo.put("repay_amount", repayAmount.multiply(BigDecimalUtil.ONE_HUNDRED) + "");
+		reqBo.put("overdue_amount", overdueAmount.multiply(BigDecimalUtil.ONE_HUNDRED) + "");
+		reqBo.put("repay_amount_sum", repayAmountSum.multiply(BigDecimalUtil.ONE_HUNDRED) + "");
+		reqBo.put("rate_amount", rateAmount.multiply(BigDecimalUtil.ONE_HUNDRED) + "");
+		//判断逾期是否平账
+		if(isCashOverdue){
+			data.setIsFinish("1");
+		}
+		String json = JsonUtil.toJSONString(reqBo);
+		data.setData(json);// 数据集合
+		data.setSign(DigestUtil.MD5(json));
+		String timestamp = DateUtil.getDateTimeFullAll(new Date());
+		data.setTimestamp(timestamp);
+		// APP还款类型写3 , 线下还款写4
+		data.setChannel(AfRepayCollectionType.APP.getCode());
+		try {
+			logger.info("repaymentAchieve request :" + JSON.toJSONString(data));
+			String reqResult = HttpUtil.doHttpsPostIgnoreCertUrlencoded(
+					getUrl() + "/api/getway/repayment/repaymentAchieve", getUrlParamsByMap(data));
+			logger.info(getUrl() + "/api/getway/repayment/repaymentAchieve");
+			logger.info("repaymentAchieve response :" + reqResult);
+			if (StringUtil.isBlank(reqResult)) {
+				throw new FanbeiException("consumerRepayment fail , reqResult is null");
+			} else {
+				logger.info("consumerRepayment req success,reqResult" + reqResult);
+			}
+
+			CollectionSystemReqRespBo respInfo = JSONObject.parseObject(reqResult, CollectionSystemReqRespBo.class);
+			if (respInfo != null && StringUtil.equals("200", respInfo.getCode())) {
+				return respInfo;
+			} else {
+				throw new FanbeiException(
+						"consumerRepayment fail , respInfo info is " + JSONObject.toJSONString(respInfo));
+			}
+		} catch (Exception e) {
+			logger.error("consumerRepayment error:", e);
+			commitRecordUtil.addRecord(AfRepeatCollectionType.APP_REPAYMENT.getCode(), borrowNo, json,
+					getUrl() + "/api/getway/repayment/repaymentAchieve");
+			throw new FanbeiException("consumerRepayment fail Exception is " + e + ",consumerRepayment send again");
+		}
+	}
+
+	/**
+	 * 将map转换成url
+	 *
+	 * @param map
+	 * @return
+	 */
+	public String getUrlParamsByMap(Map<String, String> map) {
+		if (map == null) {
+			return "";
+		}
+		StringBuffer sb = new StringBuffer();
+		for (Map.Entry<String, String> entry : map.entrySet()) {
+			sb.append(entry.getKey() + "=" + entry.getValue());
+			sb.append("&");
+		}
+		String s = sb.toString();
+		if (s.endsWith("&")) {
+			s = org.apache.commons.lang.StringUtils.substringBeforeLast(s, "&");
+		}
+		return s;
 	}
 
 }
