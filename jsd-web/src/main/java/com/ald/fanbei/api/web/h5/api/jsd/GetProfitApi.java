a23f4c32faf9ea62d9e71f2bd462b79412e697cc
朱江丰 <zhujiangfeng@edspay.com>
2018-09-05 18:40:28 +0800

    优化

--- jsd-web/src/main/java/com/ald/fanbei/api/web/h5/api/jsd/GetProfitApi.java
+++ jsd-web/src/main/java/com/ald/fanbei/api/web/h5/api/jsd/GetProfitApi.java
@@ -16,7 +16,7 @@ import com.ald.fanbei.api.web.common.JsdH5HandleResponse;
 import com.alibaba.fastjson.JSONObject;
 
 /**
- * @类描述：试算接口
+ * @类描述：获取利润差
  * @注意：本内容仅限于杭州阿拉丁信息科技股份有限公司内部传阅，禁止外泄以及用于其他的商业目的
  */
 @Component("getProfitApi")
