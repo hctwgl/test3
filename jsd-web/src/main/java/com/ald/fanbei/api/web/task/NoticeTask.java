e718e9d21c55e2fa804bbf8a59c3b171b96e8a27
jilong <jl>
2018-09-12 17:35:58 +0800

    修改通知补偿机制

--- jsd-web/src/main/java/com/ald/fanbei/api/web/task/NoticeTask.java
+++ jsd-web/src/main/java/com/ald/fanbei/api/web/task/NoticeTask.java
@@ -12,6 +12,7 @@ import com.ald.fanbei.api.common.Constants;
 import com.ald.fanbei.api.common.enums.JsdNoticeType;
 import com.ald.fanbei.api.common.util.ConfigProperties;
 
+import com.ald.fanbei.api.common.util.JsonUtil;
 import com.ald.fanbei.api.dal.domain.JsdNoticeRecordDo;
 import com.alibaba.fastjson.JSONObject;
 import org.apache.commons.lang.StringUtils;
@@ -85,13 +86,14 @@ public class NoticeTask {
                             if(data1.getTimestamp()!=null){
                                 data1.setTimestamp(System.currentTimeMillis());
                             }
+                            recordDo.setParams(JsonUtil.toJSONString(data1));
                         }else {
                             data=JSONObject.parseObject(recordDo.getParams(),HashMap.class);
                             if(data.get("timestamp")!=null){
                                 data.put("timestamp",System.currentTimeMillis());
                             };
+                            recordDo.setParams(JsonUtil.toJSONString(data));
                         }
-
                         if (StringUtils.equals(recordDo.getTimes(), "5") && (StringUtils.equals(recordDo.getType(), JsdNoticeType.REPAY.code))) {
                             if(StringUtils.isBlank(recordDo.getParams())){
                                 jsdNoticeRecordService.updateNoticeRecordStatus(buildRecord(recordDo));
@@ -135,6 +137,7 @@ public class NoticeTask {
                             };
                             executor.execute(thread);
                         }
+
                     }
                 }
                 logger.info("end notice tasktime="+new Date());
