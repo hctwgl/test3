418ff3ff3c0412e817b8d6e245ee7e270945ad21
朱江丰 <zhujiangfeng@edspay.com>
2018-09-17 15:51:51 +0800

    管理后台第一版

--- jsd-manager/src/main/java/com/ald/jsd/mgr/web/controller/CollectionMgrController.java
+++ jsd-manager/src/main/java/com/ald/jsd/mgr/web/controller/CollectionMgrController.java
@@ -12,9 +12,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.ald.fanbei.api.biz.service.JsdBorrowCashService;
 import com.ald.fanbei.api.biz.service.JsdResourceService;
 import com.ald.fanbei.api.biz.service.JsdUserService;
+import com.ald.fanbei.api.dal.domain.JsdUserDo;
+import com.ald.jsd.mgr.dal.dao.MgrOperatorDao;
+import com.ald.jsd.mgr.dal.domain.MgrOperatorDo;
 import com.ald.jsd.mgr.web.dto.req.CollectionListBorrowReq;
 import com.ald.jsd.mgr.web.dto.resp.CollectionListBorrowRespBody;
 import com.ald.jsd.mgr.web.dto.resp.Resp;
+import com.alibaba.fastjson.JSON;
 
 /**
  * @author ZJF
@@ -30,13 +34,16 @@ public class CollectionMgrController extends BaseController{
     @Resource
     JsdBorrowCashService jsdBorrowCashService;
     
-    
+    @Resource
+    MgrOperatorDao mgrOperatorDao;
 
     @RequestMapping(value = { "listBorrow.json" })
     public Resp<CollectionListBorrowRespBody> numProtocol(@RequestBody @Valid CollectionListBorrowReq params, HttpServletRequest request){
     	CollectionListBorrowRespBody body = new CollectionListBorrowRespBody();
     	
-    	
+    	MgrOperatorDo operator = mgrOperatorDao.getById(1L);
+    	JsdUserDo userDo = jsdUserService.getById(4L);
+    	System.out.println(JSON.toJSONString(userDo));
     	
     	return Resp.succ(body, "");
     }
