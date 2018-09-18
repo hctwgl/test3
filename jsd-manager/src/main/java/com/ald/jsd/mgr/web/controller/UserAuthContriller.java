1557c8d2681f786a8f112ecbae1ad3518f66bc69
jilong <jl>
2018-09-18 15:24:57 +0800

    用户权限

--- jsd-manager/src/main/java/com/ald/jsd/mgr/web/controller/UserAuthContriller.java
+++ jsd-manager/src/main/java/com/ald/jsd/mgr/web/controller/UserAuthContriller.java
@@ -1,9 +1,19 @@
 package com.ald.jsd.mgr.web.controller;
 
 
+import com.ald.fanbei.api.biz.service.JsdUserAuthService;
+import com.ald.fanbei.api.dal.domain.JsdUserAuthDo;
+import com.ald.fanbei.api.dal.query.JsdUserAuthQuery;
+import com.alibaba.fastjson.JSONObject;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @Controller
 @ResponseBody
@@ -11,7 +21,23 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class UserAuthContriller extends BaseController{
 
 
+    @Resource
+    JsdUserAuthService jsdUserAuthService;
 
+    @RequestMapping("/getJsdUserAuthInfo")
+    public String getJsdUserAuthInfo(@RequestBody JSONObject data, HttpServletRequest request, ModelMap model, JsdUserAuthQuery query){
+        String riskStatus = data.getString("riskStatus");
+        String searchContent = data.getString("searchContent");
+        int pageIndex = data.getInteger("pageIndex");
+        int pageSize = data.getInteger("pageSize");
+        JsdUserAuthQuery jsdUserAuthQuery=new JsdUserAuthQuery();
+        jsdUserAuthQuery.setPageIndex(pageIndex);
+        jsdUserAuthQuery.setPageSize(pageSize);
+        jsdUserAuthQuery.setRiskStatus(riskStatus);
+        jsdUserAuthQuery.setSearchContent(searchContent);
+        List<JsdUserAuthDo> list=jsdUserAuthService.getListJsdUserAuth(query);
+        return null;
+    }
 
 
 }
