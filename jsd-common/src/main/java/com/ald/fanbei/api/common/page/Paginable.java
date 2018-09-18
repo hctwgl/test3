e1676da8368d64f19d6301428e80f65a2cd65b5c
朱江丰 <zhujiangfeng@edspay.com>
2018-09-18 18:42:27 +0800

    分页优化

--- jsd-common/src/main/java/com/ald/fanbei/api/common/page/Paginable.java
+++ jsd-common/src/main/java/com/ald/fanbei/api/common/page/Paginable.java
@@ -20,25 +20,4 @@ public interface Paginable<T> {
     /** 当前页号 */
     int getPageIndex();
 
-    /** 是否第一页 */
-    boolean isFirstPage();
-
-    /** 是否最后一页 */
-    boolean isLastPage();
-
-    /** 返回下页的页号 */
-    int getNextPage();
-
-    /** 返回上页的页号 */
-    int getPrePage();
-
-    /** 取得当前页显示的项的起始序号 */
-    int getBeginIndex();
-
-    /** 取得当前页显示的末项序号 */
-    int getEndIndex();
-
-    int getBeginPage();
-
-    int getEndPage();
 }
