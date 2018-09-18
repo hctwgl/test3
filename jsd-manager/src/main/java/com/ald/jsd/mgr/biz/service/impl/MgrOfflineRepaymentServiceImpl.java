c2bb36c411fb8b2cfb74c0d256a8f138c563c7e2
jilong <jl>
2018-09-18 14:33:30 +0800

    线下还款

---
+++ jsd-manager/src/main/java/com/ald/jsd/mgr/biz/service/impl/MgrOfflineRepaymentServiceImpl.java
@@ -0,0 +1,56 @@
+package com.ald.jsd.mgr.biz.service.impl;
+
+import com.ald.fanbei.api.biz.service.JsdBorrowCashRepaymentService;
+import com.ald.fanbei.api.biz.service.JsdBorrowCashService;
+import com.ald.fanbei.api.biz.service.JsdBorrowLegalOrderCashService;
+import com.ald.fanbei.api.common.enums.JsdRepayType;
+import com.ald.fanbei.api.common.util.DateUtil;
+import com.ald.fanbei.api.common.util.StringUtil;
+import com.ald.fanbei.api.dal.domain.JsdBorrowCashDo;
+import com.ald.fanbei.api.dal.domain.JsdBorrowLegalOrderCashDo;
+import com.ald.jsd.mgr.biz.service.MgrOfflineRepaymentService;
+import com.ald.jsd.mgr.web.dto.resp.Resp;
+
+import javax.annotation.Resource;
+import java.util.Date;
+import java.util.Map;
+
+public class MgrOfflineRepaymentServiceImpl implements MgrOfflineRepaymentService {
+
+    @Resource
+    private JsdBorrowCashRepaymentService jsdBorrowCashRepaymentService;
+
+    @Resource
+    private JsdBorrowLegalOrderCashService jsdBorrowLegalOrderCashService;
+
+    @Resource
+    private JsdBorrowCashService jsdBorrowCashService;
+
+
+    @Override
+    public Resp dealOfflineRepayment(Map<String,String> data) {
+        Resp resp=new Resp();
+        String borrowNo= data.get("borrowNo");
+        String repaymentDate=  data.get("repaymentDate");
+        String channel= data.get("channel");
+        String tradeNo=  data.get("tradeNo");
+        String amount=  data.get("amount");
+        String remark=  data.get("remark");
+        if(StringUtil.isAllNotEmpty(borrowNo,channel,tradeNo,amount)){
+            JsdBorrowCashDo borrowCashDo=jsdBorrowCashService.getByBorrowNo(borrowNo);
+            if(borrowCashDo==null){
+                return resp.fail(data,9004,"借款信息为空！");
+            }
+            JsdBorrowLegalOrderCashDo legalOrderCashDo=jsdBorrowLegalOrderCashService.getBorrowLegalOrderCashByBorrowId(borrowCashDo.getRid());
+            if(repaymentDate==null){
+                repaymentDate= DateUtil.getNow();
+            }
+            String dataId= String.valueOf(borrowCashDo.getRid()+borrowCashDo.getRenewalNum());
+            jsdBorrowCashRepaymentService.offlineRepay(borrowCashDo,legalOrderCashDo,amount,tradeNo,borrowCashDo.getUserId(), JsdRepayType.ONLINE,channel,repaymentDate,null,dataId,remark);
+        }else {
+            return resp.fail(data,9001,"参数错误！");
+        }
+        return null;
+    }
+
+}
