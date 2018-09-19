0ddd25b811d028cea3d516b0abfb926553a08fc7
周锐 <zhourui@edspay.com>
2018-09-19 15:29:23 +0800

    代码优化

--- jsd-manager/src/main/java/com/ald/jsd/mgr/web/controller/RepayController.java
+++ jsd-manager/src/main/java/com/ald/jsd/mgr/web/controller/RepayController.java
@@ -2,7 +2,9 @@ package com.ald.jsd.mgr.web.controller;
 
 import com.ald.fanbei.api.biz.service.JsdBorrowCashService;
 import com.ald.fanbei.api.dal.query.LoanQuery;
+import com.ald.jsd.mgr.biz.service.MgrOfflineRepaymentService;
 import com.ald.jsd.mgr.web.dto.resp.Resp;
+import com.alibaba.fastjson.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +15,7 @@ import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import java.math.BigDecimal;
 import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @ResponseBody
@@ -21,6 +24,8 @@ public class RepayController {
 
     @Resource
     JsdBorrowCashService jsdBorrowCashService;
+    @Resource
+    MgrOfflineRepaymentService mgrOfflineRepaymentService;
 
     @RequestMapping(value = {"list.json"}, method = RequestMethod.POST)
     public Resp<LoanQuery> list(@RequestBody LoanQuery loanQuery, HttpServletRequest request) {
@@ -33,5 +38,10 @@ public class RepayController {
     public Resp<HashMap<String, BigDecimal>> statistics(HttpServletRequest request) {
         HashMap<String, BigDecimal> hashMap = jsdBorrowCashService.getRepayStatistics();
         return Resp.succ(hashMap, "");
+    }
+
+    @RequestMapping(value = {"offline.json"}, method = RequestMethod.POST)
+    public Resp<String> statistics(@RequestBody Map<String,String> data, HttpServletRequest request) {
+        return mgrOfflineRepaymentService.dealOfflineRepayment(data);
     }
 }
