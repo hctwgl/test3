7f754ca858dc94344116a54403ab6600a75e6956
yinxiangyu <yinxiangyu@edspay.com>
2018-09-21 10:23:11 +0800

    产品配置

--- jsd-manager/src/main/java/com/ald/jsd/mgr/web/controller/ResourceController.java
+++ jsd-manager/src/main/java/com/ald/jsd/mgr/web/controller/ResourceController.java
@@ -1,11 +1,13 @@
 package com.ald.jsd.mgr.web.controller;
 
 import com.ald.fanbei.api.biz.service.JsdResourceService;
+import com.ald.fanbei.api.common.enums.JsdBorrowCashReviewSwitch;
 import com.ald.fanbei.api.common.enums.ResourceSecType;
 import com.ald.fanbei.api.common.enums.ResourceType;
 import com.ald.fanbei.api.common.util.StringUtil;
 import com.ald.fanbei.api.dal.domain.JsdResourceDo;
 import com.ald.jsd.mgr.spring.NotNeedLogin;
+import com.ald.jsd.mgr.web.dto.req.ResourceReq;
 import com.ald.jsd.mgr.web.dto.resp.Resp;
 import com.yeepay.g3.utils.common.json.JSONUtils;
 import org.springframework.stereotype.Controller;
@@ -20,7 +22,7 @@ import java.util.*;
 
 @Controller
 @ResponseBody
-@RequestMapping("/api/configureModify/")
+@RequestMapping("/api/configuremodify/")
 @NotNeedLogin
 public class ResourceController {
 
@@ -49,33 +51,59 @@ public class ResourceController {
         data.put("fServiceRate",fServiceRate.multiply(base));
         data.put("fOverdueRate",fOverdueRate.multiply(base));
         data.put("defaultRate",defaultRate.multiply(base));
-        data.put("loanSection",jsdResourceDo.getValue2());
+        String[] loanSection=jsdResourceDo.getValue2().split(",");
+        data.put("littleAmount",loanSection[0]);
+        data.put("bigAmount",loanSection[1]);
         data.put("loanTerm","7天、14天");
         data.put("id",jsdResourceDo.getRid());
+        data.put("productName","极速贷");
+        data.put("loanPattern","搭售砍头模式");
+        data.put("payRoute","宝付");
+        data.put("repaymentStyle","一次性还本付息");
         return Resp.succ(data,"");
     }
 
     @RequestMapping(value = {"updateProductConfigure.json"})
-    public Resp<String> updateProductConfigure(@RequestBody String json, HttpServletRequest request){
+    public Resp<JsdResourceDo> updateProductConfigure(@RequestBody String json, HttpServletRequest request){
         Map map= JSONUtils.jsonToBean(json,Map.class);
         BigDecimal base=new BigDecimal(100);
         BigDecimal defaultRate = (new BigDecimal((String)map.get("defaultRate"))).divide(base);
-        String id= (String) map.get("id");
-        String loanSection= (String) map.get("loanSection");
+        Long id=Long.parseLong((String)map.get("id"));
+        String littleAmount= (String) map.get("littleAmount");
+        String bigAmount= (String) map.get("bigAmount");
+        String value2=littleAmount+","+bigAmount;
         JsdResourceDo jsdResourceDo=new JsdResourceDo();
         jsdResourceDo.setValue1(defaultRate.toString());
-        jsdResourceDo.setRid(Long.parseLong(id));
+        jsdResourceDo.setRid(id);
         jsdResourceDo.setValue(json);
+        jsdResourceDo.setValue2(value2);
         jsdResourceService.updateById(jsdResourceDo);
-        return Resp.succ(json,"");
+        return Resp.succ(jsdResourceDo,"");
     }
 
     @RequestMapping(value = {"getLoanInfo.json"})
     public Resp<Map<String, Object>> getLoanInfo(HttpServletRequest request){
         Map<String, Object> data=new HashMap<String, Object>();
         JsdResourceDo jsdResourceDo=jsdResourceService.getByTypeAngSecType(ResourceType.JSD_CONFIG.name(), ResourceType.JSD_CONFIG_REVIEW_MODE.name());
-        data.put("pattern",jsdResourceDo.getValue());
+        if(jsdResourceDo.getValue().equals(JsdBorrowCashReviewSwitch.AUTO.name())){
+            data.put("pattern","自动");
+        }else if(jsdResourceDo.getValue().equals(JsdBorrowCashReviewSwitch.MANUAL.name())){
+            data.put("pattern","手动");
+        }else {
+            data.put("pattern","兼容");
+        }
         data.put("loanAmount",jsdResourceDo.getValue1());
+        data.put("id",jsdResourceDo.getRid());
+        return Resp.succ(data,"");
+    }
+
+    @RequestMapping(value = {"updateLoanInfo.json"})
+    public Resp<JsdResourceDo> getLoanInfo(@RequestBody ResourceReq resourceReq, HttpServletRequest request){
+        JsdResourceDo data=new JsdResourceDo();
+        data.setValue(resourceReq.pattern);
+        data.setValue1(resourceReq.loanAmount);
+        data.setRid(resourceReq.id);
+        jsdResourceService.updateById(data);
         return Resp.succ(data,"");
     }
 }
