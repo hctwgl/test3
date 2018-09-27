f9c0fc7a834764699e0583cc1ec982039ff581ab
yinxiangyu <yinxiangyu@edspay.com>
2018-09-27 15:17:26 +0800

    设置优化

--- jsd-manager/src/main/java/com/ald/jsd/mgr/web/controller/ResourceController.java
+++ jsd-manager/src/main/java/com/ald/jsd/mgr/web/controller/ResourceController.java
@@ -136,7 +136,7 @@ public class ResourceController {
         data.setRid(resourceReq.id);
         data.setGmtModified(new Date());
         jsdResourceService.updateById(data);
-        mgrOperateLogDao.addOperateLog(Sessions.getRealname(request),"设置："+resourceReq);
+        mgrOperateLogDao.addOperateLog(Sessions.getRealname(request),"设置："+resourceReq.toString());
         return Resp.succ(data,"");
     }
 }
