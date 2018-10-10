0175209a8a93a73468cb78656e821f69c394bae9
朱江丰 <zhujiangfeng@edspay.com>
2018-10-10 17:17:55 +0800

    配置

--- jsd-web/src/test/java/com/ald/fanbei/AntxPropTest.java
+++ jsd-web/src/test/java/com/ald/fanbei/AntxPropTest.java
@@ -6,15 +6,15 @@ public class AntxPropTest {
 	public static void main(String[] args) {
 
     	// dev
-        String decryptStr = AesUtil.decryptFromBase64("OZAtwQcAyH5+BVcIbdZW6euzZCntyUxwH743Udhr9+LU+d0IhGa6JAh4bXHXkgeqEAD/gApVuKDhPmIPoVSEBQ==", "testC1b6x@6aH$2dlw");
+        String decryptStr = AesUtil.decryptFromBase64("TSrlTVn+kOtGRYD0oQiKRJqtINQbgHhne+1SK0SXiSKu39UrqNTDKD0bagvrlLJ4", "Cw5bM6x@6sH$2dlw^3JueH");
         System.out.println(decryptStr);
        
-        String dbUrl = "jdbc:mysql://192.168.112.31:3306/dsed_loan";
+        /*String dbUrl = "jdbc:mysql://192.168.112.31:3306/dsed_loan";
         String dbuser = "jsd_user";
         String dbpwd = "jsd_Password";
         System.out.println(AesUtil.encryptToBase64(dbUrl, "testC1b6x@6aH$2dlw"));
         System.out.println(AesUtil.encryptToBase64(dbuser, "testC1b6x@6aH$2dlw"));
-        System.out.println(AesUtil.encryptToBase64(dbpwd, "testC1b6x@6aH$2dlw"));
+        System.out.println(AesUtil.encryptToBase64(dbpwd, "testC1b6x@6aH$2dlw"));*/
         
         //online
         /*String online = decryptFromBase64("j1FIhYNyickGrgtmqKPeS1F7jfGVLd/SWlF10OaN4wK0R6GmKTbA6j7RqF+7x2fK", "Cw5bM6x@6sH$2dlw^3JueH");
