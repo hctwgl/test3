2ec78aaa7c2e24847bc57f9a9e1f7a8518ee8b36
chefeipeng <chefeipeng@Alamap.com>
2018-10-18 17:32:47 +0800

    协议 生成签章 修改

--- jsd-web/src/main/java/com/ald/fanbei/api/web/controller/H5ProtocolController.java
+++ jsd-web/src/main/java/com/ald/fanbei/api/web/controller/H5ProtocolController.java
@@ -306,13 +306,13 @@ public class H5ProtocolController {
 				logger.error("获取前海印章失败 => {}" + BizExceptionCode.COMPANY_SEAL_CREATE_FAILED);
 				throw new BizException(BizExceptionCode.COMPANY_SEAL_CREATE_FAILED);
 			}
-			companyUserSealDo = jsdUserSealService.getUserSealByUserName("杭州朗下网络科技有限公司");
-			if (null != companyUserSealDo && null != companyUserSealDo.getUserSeal()) {
-				map.put("lxSeal", "data:image/png;base64," + companyUserSealDo.getUserSeal());
-			} else {
-				logger.error("获取郎下印章失败 => {}" + BizExceptionCode.COMPANY_SEAL_CREATE_FAILED);
-				throw new BizException(BizExceptionCode.COMPANY_SEAL_CREATE_FAILED);
-			}
+//			companyUserSealDo = jsdUserSealService.getUserSealByUserName("杭州朗下网络科技有限公司");
+//			if (null != companyUserSealDo && null != companyUserSealDo.getUserSeal()) {
+//				map.put("lxSeal", "data:image/png;base64," + companyUserSealDo.getUserSeal());
+//			} else {
+//				logger.error("获取郎下印章失败 => {}" + BizExceptionCode.COMPANY_SEAL_CREATE_FAILED);
+//				throw new BizException(BizExceptionCode.COMPANY_SEAL_CREATE_FAILED);
+//			}
 		} catch (Exception e) {
 			logger.error("get userSeal  error", e);
 		}
