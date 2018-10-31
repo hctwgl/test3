3e228ea0c4b3e138c5802e322a53837646b4dc68
jilong <jl>
2018-10-31 16:02:45 +0800

    代扣开关

--- jsd-web/src/main/java/com/ald/fanbei/api/web/task/WithholdJob.java
+++ jsd-web/src/main/java/com/ald/fanbei/api/web/task/WithholdJob.java
@@ -8,16 +8,16 @@ import java.util.concurrent.Executors;
 
 import javax.annotation.Resource;
 
+import com.ald.fanbei.api.biz.service.*;
+import com.ald.fanbei.api.common.enums.ResourceSecType;
+import com.ald.fanbei.api.common.enums.ResourceType;
+import com.ald.fanbei.api.dal.domain.*;
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import com.ald.fanbei.api.biz.service.JsdBorrowCashRepaymentService;
-import com.ald.fanbei.api.biz.service.JsdBorrowCashService;
-import com.ald.fanbei.api.biz.service.JsdBorrowLegalOrderCashService;
-import com.ald.fanbei.api.biz.service.JsdUserBankcardService;
-import com.ald.fanbei.api.biz.service.JsdUserService;
 import com.ald.fanbei.api.biz.service.impl.JsdBorrowCashRepaymentServiceImpl.RepayRequestBo;
 import com.ald.fanbei.api.biz.util.GeneratorClusterNo;
 import com.ald.fanbei.api.biz.util.GetHostIpUtil;
@@ -27,11 +27,6 @@ import com.ald.fanbei.api.common.enums.JsdBorrowCashRepaymentStatus;
 import com.ald.fanbei.api.common.enums.RepayType;
 import com.ald.fanbei.api.common.util.BigDecimalUtil;
 import com.ald.fanbei.api.common.util.DateUtil;
-import com.ald.fanbei.api.dal.domain.JsdBorrowCashDo;
-import com.ald.fanbei.api.dal.domain.JsdBorrowCashRepaymentDo;
-import com.ald.fanbei.api.dal.domain.JsdBorrowLegalOrderCashDo;
-import com.ald.fanbei.api.dal.domain.JsdUserBankcardDo;
-import com.ald.fanbei.api.dal.domain.JsdUserDo;
 
 @Component
 public class WithholdJob {
@@ -56,36 +51,44 @@ public class WithholdJob {
     @Resource
     GeneratorClusterNo generatorClusterNo;
 
+    @Resource
+    JsdResourceService jsdResourceService;
 
     private static String NOTICE_HOST = ConfigProperties.get(Constants.CONFKEY_TASK_ACTIVE_HOST);
 
+    private static String SWITCH="open";
 
     ExecutorService executor = Executors.newFixedThreadPool(10);
 
 
-//    @Scheduled(cron = "0 50 23 * * ?")
+
+
+    @Scheduled(cron = "0 50 23 * * ?")
     public void withhold() {
-        String curHostIp = GetHostIpUtil.getIpAddress();
-        logger.info("curHostIp=" + curHostIp + ", configNoticeHost=" + NOTICE_HOST);
-        try {
-            if (StringUtils.equals(GetHostIpUtil.getIpAddress(), NOTICE_HOST)) {
-                Date bengin = DateUtil.getTodayLast();
-                int pageSize = 200;
-                int totalRecord = jsdBorrowCashService.getBorrowCashByBeforeTodayCount(bengin);
-                int totalPageNum = (totalRecord + pageSize - 1) / pageSize;
-                if (totalRecord == 0) {
-                    logger.info("withhold run finished,Loan Due size is 0.time=" + new Date());
-                } else {
-                    logger.info("withhold run start,time=" + new Date());
-                    for (int i = 0; i < totalPageNum; i++) {
-                        List<JsdBorrowCashDo> borrowCashDos = jsdBorrowCashService.getBorrowCashByBeforeToday(totalPageNum * i, pageSize,bengin);
-                        dealWithhold(borrowCashDos);
+        JsdResourceDo resourceDo=jsdResourceService.getByTypeAngSecType(ResourceType.JSD_CONFIG.getCode(), ResourceSecType.WITHHOLD_JOB_CONFIG.getCode());
+        if(SWITCH.equals(resourceDo.getValue())){
+            String curHostIp = GetHostIpUtil.getIpAddress();
+            logger.info("curHostIp=" + curHostIp + ", configNoticeHost=" + NOTICE_HOST);
+            try {
+                if (StringUtils.equals(GetHostIpUtil.getIpAddress(), NOTICE_HOST)) {
+                    Date bengin = DateUtil.getTodayLast();
+                    int pageSize = 200;
+                    int totalRecord = jsdBorrowCashService.getBorrowCashByBeforeTodayCount(bengin);
+                    int totalPageNum = (totalRecord + pageSize - 1) / pageSize;
+                    if (totalRecord == 0) {
+                        logger.info("withhold run finished,Loan Due size is 0.time=" + new Date());
+                    } else {
+                        logger.info("withhold run start,time=" + new Date());
+                        for (int i = 0; i < totalPageNum; i++) {
+                            List<JsdBorrowCashDo> borrowCashDos = jsdBorrowCashService.getBorrowCashByBeforeToday(totalPageNum * i, pageSize,bengin);
+                            dealWithhold(borrowCashDos);
+                        }
                     }
+                    logger.info("withhold run end,time=" + new Date());
                 }
-                logger.info("withhold run end,time=" + new Date());
+            } catch (Exception e) {
+                logger.error("withhold  error, case=", e);
             }
-        } catch (Exception e) {
-            logger.error("withhold  error, case=", e);
         }
 
     }
