596f5f731744d1c1d327ccb20b0397573a233d53
jilong <jl>
2018-11-19 23:27:58 +0800

    查询最新用户

--- jsd-manager/src/test/java/com/ald/fanbei/AntxPropTest.java
+++ jsd-manager/src/test/java/com/ald/fanbei/AntxPropTest.java
@@ -6,7 +6,7 @@ public class AntxPropTest {
 	public static void main(String[] args) {
 
     	// dev
-        String decryptStr = AesUtil.decryptFromBase64("OZAtwQcAyH5+BVcIbdZW6euzZCntyUxwH743Udhr9+LU+d0IhGa6JAh4bXHXkgeqEAD/gApVuKDhPmIPoVSEBQ==", "testC1b6x@6aH$2dlw");
+        String decryptStr = AesUtil.decryptFromBase64("SNDKc0tZOaoebiGNVCXMBBbDYcFJF/f2SPzQom7gePc=", "Cw5bM6x@6sH$2dlw^3JueH");
         System.out.println(decryptStr);
        
         String dbUrl = "jdbc:mysql://192.168.112.31:3306/jsd_loan";
@@ -15,7 +15,7 @@ public class AntxPropTest {
         System.out.println(AesUtil.encryptToBase64(dbUrl, "testC1b6x@6aH$2dlw"));
         System.out.println(AesUtil.encryptToBase64(dbuser, "testC1b6x@6aH$2dlw"));
         System.out.println(AesUtil.encryptToBase64(dbpwd, "testC1b6x@6aH$2dlw"));
-        
+
         //online
         /*String online = decryptFromBase64("j1FIhYNyickGrgtmqKPeS1F7jfGVLd/SWlF10OaN4wK0R6GmKTbA6j7RqF+7x2fK", "Cw5bM6x@6sH$2dlw^3JueH");
         String online1 = decryptFromBase64("cJiZKo2M0HcKZdjgGmv/vQ==", "testC1b6x@6aH$2dlw");
