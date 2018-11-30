bda6884c0b681e565fa6b1a4c4029cf11644e194
周锐 <zhourui@edspay.com>
2018-11-30 12:20:00 +0800

    手动通知催收还款接口

--- jsd-web/src/main/java/com/ald/fanbei/api/web/controller/CollectionController.java
+++ jsd-web/src/main/java/com/ald/fanbei/api/web/controller/CollectionController.java
@@ -4,27 +4,29 @@ package com.ald.fanbei.api.web.controller;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
-import com.ald.fanbei.api.biz.service.JsdBorrowCashService;
-import com.ald.fanbei.api.biz.service.JsdNoticeRecordService;
-import com.ald.fanbei.api.biz.service.JsdUserContactsService;
-import com.ald.fanbei.api.biz.service.JsdUserService;
+import com.ald.fanbei.api.biz.service.*;
 import com.ald.fanbei.api.biz.third.util.XgxyUtil;
+import com.ald.fanbei.api.common.enums.JsdRepayType;
 import com.ald.fanbei.api.common.util.StringUtil;
+import com.ald.fanbei.api.dal.dao.JsdBorrowCashRepaymentDao;
+import com.ald.fanbei.api.dal.dao.JsdBorrowLegalOrderDao;
+import com.ald.fanbei.api.dal.dao.JsdBorrowLegalOrderRepaymentDao;
 import com.ald.fanbei.api.dal.dao.JsdNoticeRecordDao;
-import com.ald.fanbei.api.dal.domain.JsdNoticeRecordDo;
-import com.ald.fanbei.api.dal.domain.JsdUserContactsDo;
-import com.ald.fanbei.api.dal.domain.JsdUserDo;
+import com.ald.fanbei.api.dal.domain.*;
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.ald.fanbei.api.biz.third.util.CuiShouUtils;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 
@@ -42,8 +44,20 @@ public class CollectionController {
     JsdUserContactsService jsdUserContactsService;
     @Resource
     JsdNoticeRecordDao jsdNoticeRecordDao;
+    @Resource
+    JsdBorrowCashRepaymentDao jsdBorrowCashRepaymentDao;
+    @Resource
+    JsdBorrowLegalOrderRepaymentDao jsdBorrowLegalOrderRepaymentDao;
+    @Resource
+    JsdBorrowCashService jsdBorrowCashService;
+    @Resource
+    JsdBorrowLegalOrderDao jsdBorrowLegalOrderDao;
+    @Resource
+    JsdCollectionService jsdCollectionService;
 
     private final Logger logger = LoggerFactory.getLogger(CollectionController.class);
+
+
     /**
      * 线下还款
      * @param request
@@ -151,6 +165,36 @@ public class CollectionController {
         jsdNoticeRecordDo.setStatus("FAIL");
         jsdNoticeRecordDo.setTimes("5");
         jsdNoticeRecordDao.updateById(jsdNoticeRecordDo);
+    }
+
+    /**
+     * 手动推送还款给催收
+     * @param data
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping(value = {"/manualPushRepayment"}, method = RequestMethod.POST)
+    public String manualPushRepayment(@RequestBody JSONObject data){
+        logger.info("start manualPushRepayment ------------ data = " + data.toJSONString());
+        Boolean isPush = data.getBoolean("isPush");
+        String reviewReuslt = data.getString("reviewReuslt");
+        JsdBorrowCashRepaymentDo jsdBorrowCashRepaymentDo = jsdBorrowCashRepaymentDao.getByTradeNo(data.getString("tradeNo"));
+        JsdBorrowLegalOrderRepaymentDo jsdBorrowLegalOrderRepaymentDo = jsdBorrowLegalOrderRepaymentDao.getBorrowLegalOrderRepaymentByTradeNo(data.getString("tradeNo"));
+        JsdBorrowCashDo jsdBorrowCashDo = jsdBorrowCashService.getBorrowByRid(jsdBorrowCashRepaymentDo.getBorrowId());
+        JsdBorrowLegalOrderDo jsdBorrowLegalOrderDo = jsdBorrowLegalOrderDao.getLastValidOrderByBorrowId(jsdBorrowCashRepaymentDo.getBorrowId());
+        BigDecimal repayAmount = jsdBorrowCashRepaymentDo.getRepaymentAmount();
+        if(jsdBorrowLegalOrderRepaymentDo != null){
+            repayAmount = jsdBorrowCashRepaymentDo.getRepaymentAmount().add(jsdBorrowLegalOrderRepaymentDo.getRepayAmount());
+        }
+        String outTradeNo = jsdBorrowCashRepaymentDo.getTradeNoUps();
+        String borrowNo = jsdBorrowCashDo.getBorrowNo();
+        Long orderId = jsdBorrowLegalOrderDo.getRid();
+        Long userId = jsdBorrowCashRepaymentDo.getUserId();
+        logger.info("manualPushRepayment ------------ repayAmount = " + repayAmount+", outTradeNo = "+outTradeNo+", borrowNo = "+borrowNo+", orderId = "+orderId+", userId = "+userId);
+        if(isPush){
+            jsdCollectionService.nofityRepayment(repayAmount, outTradeNo, borrowNo, orderId, userId, JsdRepayType.OFFLINE, reviewReuslt);
+        }
+        return "true";
     }
 
 }
