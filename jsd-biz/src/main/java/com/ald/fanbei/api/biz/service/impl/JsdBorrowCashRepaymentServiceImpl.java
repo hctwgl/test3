2145cc700b07e742babbf2a83005a6f96a7268bf
jilong <jl>
2018-12-12 16:31:45 +0800

    去除阻塞测试

--- jsd-biz/src/main/java/com/ald/fanbei/api/biz/service/impl/JsdBorrowCashRepaymentServiceImpl.java
+++ jsd-biz/src/main/java/com/ald/fanbei/api/biz/service/impl/JsdBorrowCashRepaymentServiceImpl.java
@@ -119,11 +119,6 @@ public class JsdBorrowCashRepaymentServiceImpl extends JsdUpsPayKuaijieServiceAb
 			String tradeNo = generatorClusterNo.getRepaymentBorrowCashNo(bankPayType);
 			bo.tradeNo = tradeNo;
             generateRepayRecords(bo);
-            try {
-				Thread.sleep(30000);
-			}catch (Exception e){
-
-			}
 			return doRepay(bo,bankPayType);
 		}catch (Exception e) {
 			logger.info("repay method error", e);
