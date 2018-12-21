fc303b5ee4e0ba965d8b54e37f8ae518e14ebf07
chefeipeng <chefeipeng@Alamap.com>
2018-12-21 16:52:54 +0800

    催收 通讯录

--- jsd-biz/src/main/java/com/ald/fanbei/api/biz/third/util/XgxyUtil.java
+++ jsd-biz/src/main/java/com/ald/fanbei/api/biz/third/util/XgxyUtil.java
@@ -217,7 +217,7 @@ public class XgxyUtil extends AbstractThird {
             }
             XgxyReqBo resp = JSONObject.parseObject(reqResult, XgxyReqBo.class);
             if (XGXY_REQ_CODE_SUCC.equals(resp.get("code"))) {
-                JSONObject object = JSON.parseObject(resp.get("data").toString());
+                JSONObject object = JSON.parseObject(JSON.toJSONString(resp.get("data")));
                 result.put("mxreportUrl",object.get("mxreportUrl"));
                 result.put("contacts",object.get("contacts"));
                 return result;
